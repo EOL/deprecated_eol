fb61bdca5d0a40c4db7c41d8fc09d5b191e04145
peter <peter@78829999-583a-0410-bd01-8a9b849fd409>
2009-01-16 21:01:07 +0000

    readme cleanup...get rid of some more email addresses
    git-svn-id: https://repository.eol.org/eol/branches/2.0@26 78829999-583a-0410-bd01-8a9b849fd409

--- db/migrate/006_create_contact_subjects.rb
+++ db/migrate/006_create_contact_subjects.rb
@@ -9,12 +9,12 @@ class CreateContactSubjects < ActiveRecord::Migration
   
     # Default subjects
     # Multiple recipients may be entered in a comma delimited list
-    ContactSubject.create!(:title => 'Media Contact (interviews, image requests)', :recipients => "ByrnesB@si.edu")    
+    ContactSubject.create!(:title => 'Media Contact (interviews, image requests)', :recipients => "example@example.com")    
     ContactSubject.create!(:title => 'Request to curate a page', :recipients => "affiliate@example.com")
     ContactSubject.create!(:title => 'Request to upload images', :recipients => "affiliate@example.com")
     ContactSubject.create!(:title => 'Make a correction (images, spelling & grammar, information)', :recipients => "content-comments@example.com")    
     ContactSubject.create!(:title => 'Give feedback', :recipients => "content-comments@example.com")
-    ContactSubject.create!(:title => 'Make a financial contribution', :recipients => "ByrnesB@si.edu")
+    ContactSubject.create!(:title => 'Make a financial contribution', :recipients => "example@example.com")
     ContactSubject.create!(:title => 'Technical problems (difficulties with the website, search tool or user accounts)', :recipients => "support@example.com")    
     ContactSubject.create!(:title => 'Other (ideas for the website, things you would like to see)', :recipients => "support@example.com")      
 

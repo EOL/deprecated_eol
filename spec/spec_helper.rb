8c558e764889d3cbad997f960f44cb503a774fc1
rtaylor <rtaylor@78829999-583a-0410-bd01-8a9b849fd409>
2009-01-26 20:36:05 +0000

    added a spec for testing factories ... we'll have it test that factories exist and work for *ALL* model classes ... right now, we'll specify which classes to test by hand, while we build factories
    git-svn-id: https://repository.eol.org/eol/trunk@55 78829999-583a-0410-bd01-8a9b849fd409

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,10 +2,37 @@ ENV["RAILS_ENV"] = "test"
 require File.expand_path(File.dirname(__FILE__) + "/../config/environment")
 require 'spec'
 require 'spec/rails'
+load 'composite_primary_keys/fixtures.rb' 
 require File.expand_path(File.dirname(__FILE__) + "/factories")
 
 Spec::Runner.configure do |config|
   config.use_transactional_fixtures = true
   config.use_instantiated_fixtures  = false
   config.fixture_path = RAILS_ROOT + '/spec/fixtures/'
+
+  # taken from use_db/lib/override_test_case.rb
+  #
+  # these before and after blocks make sure that spec 
+  # examples run within their own transactions for ALL 
+  # active connections (works for ALL of our databases)
+  config.before(:each) do
+    UseDbPlugin.all_use_dbs.collect do |klass|
+      klass
+    end
+
+    ActiveRecord::Base.active_connections.values.uniq.each do |conn|
+      Thread.current['open_transactions'] ||= 0
+      Thread.current['open_transactions'] += 1
+      conn.begin_db_transaction
+    end
+  end
+  config.after(:each) do
+    ActiveRecord::Base.active_connections.values.uniq.each do |conn|                  
+      conn.rollback_db_transaction
+      Thread.current['open_transactions'] = 0
+    end
+  end
 end
+
+# quite down any migrations that run during tests
+ActiveRecord::Migration.verbose = false

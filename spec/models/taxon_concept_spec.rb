af5a145c0a3f95b523c0cd1bfe31d3b73443c44e
jrice <jrice@78829999-583a-0410-bd01-8a9b849fd409>
2009-01-28 23:23:47 +0000

    Adding pending specs
    git-svn-id: https://repository.eol.org/eol/trunk@83 78829999-583a-0410-bd01-8a9b849fd409

--- spec/models/taxon_concept_spec.rb
+++ spec/models/taxon_concept_spec.rb
@@ -2,7 +2,7 @@ require File.dirname(__FILE__) + '/../spec_helper'
 
 describe TaxonConcept do
 
-  it 'should ... be a TaxonConcept ...' do
+  it 'should add a TaxonConcept to the database when created' do
     lambda {
       Factory(:taxon_concept).should be_valid
     }.should change(TaxonConcept, :count).by(1)

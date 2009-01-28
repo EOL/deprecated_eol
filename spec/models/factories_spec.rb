e4cb12aecf54028a11864fdbf2f3259cf65ce7ea
rtaylor <rtaylor@78829999-583a-0410-bd01-8a9b849fd409>
2009-01-28 00:12:58 +0000

    added a simple spec to confirm that each model can be generated *once* properly by a factory
    git-svn-id: https://repository.eol.org/eol/trunk@65 78829999-583a-0410-bd01-8a9b849fd409

--- spec/models/factories_spec.rb
+++ spec/models/factories_spec.rb
@@ -8,14 +8,25 @@ describe Factory do
   end
 
   # use all_model_classes if you want *all* ... all 97 of them  :)
-  some_model_classes.each do |klass|
-    it "should generate #{klass}" do
-      3.times do
-        lambda {
-          Factory(klass.to_s.underscore.to_sym).should be_valid
-        }.should change(klass, :count).by(3)
-      end    
+  all_model_classes.each do |klass|
+
+    it "should generate #{klass} once" do
+      lambda {
+        Factory(klass.to_s.underscore.to_sym).should be_valid
+      }.should change(klass, :count).by(1)
     end
+
+    # 
+    # DISABLED FOR NOW - re-enable when we have *basic* working factories for all models
+    #
+    #it "should generate #{klass} multiple times and still make valid, unique models" do
+      #3.times do
+      #  lambda {
+      #    Factory(klass.to_s.underscore.to_sym).should be_valid
+      #  }.should change(klass, :count).by(3)
+      #end    
+    #end
+
   end
 
 end

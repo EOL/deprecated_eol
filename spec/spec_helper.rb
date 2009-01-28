fc2e9eec5773641e168af4d3bb4a36ca4b1c6ee7
rtaylor <rtaylor@78829999-583a-0410-bd01-8a9b849fd409>
2009-01-28 18:59:43 +0000

    fixed specs - i was changing the load_paths for scenarios  (for testing) and wasn't setting them back properly
    git-svn-id: https://repository.eol.org/eol/trunk@74 78829999-583a-0410-bd01-8a9b849fd409

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -5,7 +5,10 @@ require 'spec/rails'
 load 'composite_primary_keys/fixtures.rb' 
 require File.expand_path(File.dirname(__FILE__) + "/factories")
 require File.expand_path(File.dirname(__FILE__) + "/eol_spec_helpers")
+
 require 'scenario'
+EOL::Scenario.load_paths = [ File.join(RAILS_ROOT, 'spec', 'scenarios') ]
+
 require 'rackbox'
 # require File.expand_path(File.dirname(__FILE__) + "/custom_matchers") # eval this with config context?
 

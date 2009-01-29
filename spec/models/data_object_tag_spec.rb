efb0203c33df922f893fc9be2e68bb6ba3f4620d
jrice <jrice@78829999-583a-0410-bd01-8a9b849fd409>
2009-01-29 17:54:45 +0000

    Added custom matcher for @user.should be_a_curator_of(clade).
    git-svn-id: file:///data/subversion/eol/trunk@91 78829999-583a-0410-bd01-8a9b849fd409

--- spec/models/data_object_tag_spec.rb
+++ spec/models/data_object_tag_spec.rb
@@ -12,7 +12,7 @@ describe DataObjectTag do
   it { should validate_presence_of(:value) }
   it { should validate_uniqueness_of(:value) }
 
-  it 'should require a unique key / value paid' do
+  it 'should require a unique key / value pair' do
     Factory(:data_object_tag, :key => 'foo', :value => 'w00t' ).should be_valid
     Factory.build(:data_object_tag, :key => 'foo', :value => 'w00t' ).should_not be_valid
     Factory.build(:data_object_tag, :key => 'foo', :value => 'w00t-2' ).should be_valid
--- spec/scenarios/foundation.rb
+++ spec/scenarios/foundation.rb
@@ -3,3 +3,8 @@
 Language.gen      :iso_639_1 => 'en'
 
 ContentPage.gen   :page_name => 'Home', :language_abbr => 'en'
+
+# Required Roles
+Role.gen          :title => 'Curator'
+Role.gen          :title => 'Moderator'
+Role.gen          :title => 'Administrator'

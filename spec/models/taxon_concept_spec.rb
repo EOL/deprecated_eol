6c3fe489a948f4805b046bbf88253426fe4e7967
rtaylor <rtaylor@78829999-583a-0410-bd01-8a9b849fd409>
2009-02-02 18:29:01 +0000

    d'oh - had an example uncommented in one of the specs which was blowing up
    git-svn-id: https://repository.eol.org/eol/trunk@99 78829999-583a-0410-bd01-8a9b849fd409

--- spec/models/taxon_concept_spec.rb
+++ spec/models/taxon_concept_spec.rb
@@ -6,7 +6,7 @@ describe TaxonConcept do
     concept = TaxonConcept.generate
 
     # trying to create a name ... seems to be *REALLY* hard to simply add a name to a TaxonConcept ...
-    tc.taxon_concept_names.create :name => Name.gen, :language => Language.gen, :preferred => true, :vern => 0, :source_hierarchy_entry_id => HierarchyEntry.gen.id
+    # tc.taxon_concept_names.create :name => Name.gen, :language => Language.gen, :preferred => true, :vern => 0, :source_hierarchy_entry_id => HierarchyEntry.gen.id
     # ^ adds a valid TaxonConceptName but #name returns '?-?' (whatever that means) and #names returns [] ?
   end
 

9fb2e3dec8173a0da0d510784e42ebfdb4e6f91f
rtaylor <rtaylor@78829999-583a-0410-bd01-8a9b849fd409>
2009-02-02 17:41:05 +0000

    stubbed out specs for hierarchy & hierarchy_entry
    git-svn-id: https://repository.eol.org/eol/trunk@98 78829999-583a-0410-bd01-8a9b849fd409

---
+++ spec/models/hierarchy_spec.rb
@@ -0,0 +1,18 @@
+require File.dirname(__FILE__) + '/../spec_helper'
+
+# I have no idea how Hierarchy is supposed to behave ...
+#
+# We need schema documentation  :(
+#
+# For now, I'll guess that only a label is required.
+#
+describe Hierarchy do
+
+  # ... doesn't require *anything?*
+
+  #it 'should require a label' do
+  #  Hierarchy.build( :label => nil ).should_not be_valid
+  #  Hierarchy.build( :label => 'Tree of Life 2009' ).should be_valid
+  #end
+
+end

2cbf6d5beeeb3c8635ab9813ed20dc93ff7923da
rtaylor <rtaylor@78829999-583a-0410-bd01-8a9b849fd409>
2009-02-05 23:20:11 +0000

    specs are blowing up (a factory) - added some error message that'll hopefully help debug these issues in the future
    git-svn-id: https://repository.eol.org/eol/trunk@158 78829999-583a-0410-bd01-8a9b849fd409

--- spec/eol_spec_helpers.rb
+++ spec/eol_spec_helpers.rb
@@ -46,6 +46,8 @@ class ActiveRecord::Base
   # truncate's this model's table
   def self.truncate
     connection.execute "TRUNCATE TABLE #{ table_name }"
+  rescue => ex
+    puts "#{ self.name }.truncate failed ... does the table exist?  #{ ex }"
   end
 
 end

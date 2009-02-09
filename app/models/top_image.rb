55b060e50dd9d9a82880412540306eb2c415d121
jrice <jrice@78829999-583a-0410-bd01-8a9b849fd409>
2009-02-09 19:11:46 +0000

    Refactor: extract methods (sorta) to extend Faker with Eol, so we have Faker::Eol.scientific_name, name_part, common_name, attribution, and image.
    git-svn-id: https://repository.eol.org/eol/trunk@180 78829999-583a-0410-bd01-8a9b849fd409

--- app/models/top_image.rb
+++ app/models/top_image.rb
@@ -1,5 +1,5 @@
 # This class shouldn't be used.  :)  It's really just here so that fixtures load into the right database.  Meaning, this *class*
-# doesn't perform any special function.  The top_iages table, however, is used for denormalized searches on (normal, vetted, visible,
+# doesn't perform any special function.  The top_images table, however, is used for denormalized searches on (normal, vetted, visible,
 # published) images. That table is referenced by DataObject#cached_images_for_taxon().  That table is *built* using PHP, so you will
 # not see any other ref to it.
 class TopImage < SpeciesSchemaModel

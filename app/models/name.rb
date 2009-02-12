21d094b74cffc9ff55198c41d290e13fedd1d580
rtaylor <rtaylor@78829999-583a-0410-bd01-8a9b849fd409>
2009-02-12 20:07:50 +0000

    added soome specs for Name / NormalizedName / NormalizedLink & added some doco to these models
    git-svn-id: https://repository.eol.org/eol/trunk@221 78829999-583a-0410-bd01-8a9b849fd409

--- app/models/name.rb
+++ app/models/name.rb
@@ -1,7 +1,17 @@
+#
 # Name is used for storing different variations of names of species (TaxonConcept)
 #
 # These names are not "official."  If they have a CanonicalForm, the 
 # CanonicalForm is the "accepted" scientific name for the species.
+#
+# You might also want to see NormalizedName.  Name is broken down 
+# into unique parts (parsed by NormalizedName) which are linked 
+# back to a Name via NormalizedLinks
+#
+# NormalizedName is currently what we search on when we do string 
+# searches, then we use the NormalizedLink to find Name objects that 
+# use the NormalizedName ... and we can then find TaxonConcepts / HierarchyEntries 
+# that use the Name.
 #
 class Name < SpeciesSchemaModel
 
@@ -13,9 +23,10 @@ class Name < SpeciesSchemaModel
   has_many :normalized_links
   has_many :mappings
 
-  validates_presence_of :string
-  validates_presence_of :italicized
-  validates_presence_of :canonical_form
+  validates_presence_of   :string
+  validates_uniqueness_of :string
+  validates_presence_of   :italicized
+  validates_presence_of   :canonical_form
 
   def taxon_concepts
     return taxon_concept_names.collect {|tc_name| tc_name.taxon_concept}.flatten
@@ -30,6 +41,11 @@ class Name < SpeciesSchemaModel
     # return "(no canonical form, tc: #{ taxon_concepts.map(&:id).join(',') })" unless canonical_form
     return 'not assigned' unless canonical_form and canonical_form.string and not canonical_form.string.empty?
     return "<i>#{canonical_form.string}</i>"
+  end
+
+  # String representation of a Name is its Name#string
+  def to_s
+    string
   end
 
 end

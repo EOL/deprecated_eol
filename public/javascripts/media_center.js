5ac5cdba550f87cc97dd6c48c1f2f1fe2fb1d04d
bsmith <bsmith@78829999-583a-0410-bd01-8a9b849fd409>
2009-02-17 00:05:34 +0000

    merged: added code to handle curation and new inappropriate and invisible icons. refs EOLINFRASTRUCTURE-533
    git-svn-id: file:///data/subversion/eol/trunk@239 78829999-583a-0410-bd01-8a9b849fd409

--- public/javascripts/media_center.js
+++ public/javascripts/media_center.js
@@ -2,3 +2,31 @@ if(!EOL) var EOL = {};
 if(!EOL.MediaCenter) EOL.MediaCenter = {};
 
 EOL.MediaCenter.image_hash = {};
+
+EOL.MediaCenter.update_thumbnail_icons = function(element) {
+  data_object_id = element.readAttribute('data-data_object_id');
+  image = EOL.MediaCenter.image_hash[data_object_id];
+
+  $$('a#thumbnail_'+data_object_id+' .inappropriate_icon')[0].hide();
+  $$('a#thumbnail_'+data_object_id+' .invisible_icon')[0].hide();
+  $$('a#thumbnail_'+data_object_id+' .unpublished_icon')[0].hide();
+  $$('a#thumbnail_'+data_object_id+' .published_icon')[0].hide();
+
+  if (image.published_by_agent){
+    $$('a#thumbnail_'+data_object_id+' .published_icon')[0].show();
+  } else if(!image.published) {
+    $$('a#thumbnail_'+data_object_id+' .unpublished_icon')[0].show();
+  }
+
+  if (image.visibility_id == EOL.Curation.INVISIBLE_ID) {
+    $$('a#thumbnail_'+data_object_id+' .invisible_icon')[0].show();
+  } else if (image.visibility_id == EOL.Curation.INAPPROPRIATE_ID) {
+    $$('a#thumbnail_'+data_object_id+' .inappropriate_icon')[0].show();
+  }
+};
+
+EOL.MediaCenter.Behaviors = {
+  'div#thumbnails a ul': function(e) {
+    EOL.MediaCenter.update_thumbnail_icons(this)
+  }
+};

2ae73e7c5f2a6a760639256556289ce9bef8750e
bsmith <bsmith@78829999-583a-0410-bd01-8a9b849fd409>
2009-02-18 22:29:49 +0000

    merged: major refactoring and cleanup of javascript for curation. refs EOLINFRASTRUCTURE-533
    git-svn-id: https://repository.eol.org/eol/trunk@261 78829999-583a-0410-bd01-8a9b849fd409

--- public/javascripts/curation.js
+++ public/javascripts/curation.js
@@ -1,27 +1,44 @@
 if(!EOL) var EOL = {};
 if(!EOL.Curation) EOL.Curation = {};
 
-EOL.Curation.quick_curate = function(element,vetted_id) {
-  element.ancestors()[1].down('div.spinner img').appear();
-  new Ajax.Request(element.href, {asynchronous:true, evalScripts:true, onComplete:function () {EOL.Curation.after_quick_curate(element,vetted_id);}.bind(element, vetted_id)});
+EOL.Curation.post_curate_text = function(data_object_id, visibility_id, vetted_id) {
+  try {
+    $$('div#text_buttons_'+data_object_id+' div.trust_button')[0].disappear();
+    $$('div#text_buttons_'+data_object_id+' div.untrust_button')[0].disappear();
+  } catch(err) {
+    //no quick curate buttons
+  }
+  EOL.Curation.update_text_background(data_object_id, vetted_id);
 };
 
-EOL.Curation.after_quick_curate = function(element, vetted_id) {
-  data_object_id = element.readAttribute('data-data_object_id');
-  data_object_type = element.readAttribute('data-data_object_type');
-
-  element.ancestors()[1].down('div.spinner img').disappear();
-  element.ancestors()[1].down('div.trust_button').disappear();
-  element.ancestors()[1].down('div.untrust_button').disappear();
-
-  if(data_object_type == EOL.Curation.IMAGE_ID) {
-    EOL.MediaCenter.image_hash[data_object_id].vetted_id = vetted_id;
-    EOL.MediaCenter.image_hash[data_object_id].curated = true;
-    eol_update_credit(EOL.MediaCenter.image_hash[data_object_id]);
-    EOL.Curation.update_thumbnail_background(vetted_id, data_object_id);
-  } else if(data_object_type == EOL.Curation.TEXT_ID) {
-    EOL.Curation.update_text_background(data_object_id, vetted_id);
+EOL.Curation.post_curate_image = function(data_object_id, visibility_id, vetted_id) {
+  try {
+    $('large-image-trust-button').disappear();
+    $('large-image-untrust-button').disappear();
+  } catch (err) {
+    //no quick curate buttons
   }
+
+  EOL.MediaCenter.image_hash[data_object_id].vetted_id = vetted_id;
+  EOL.MediaCenter.image_hash[data_object_id].visibility_id = visibility_id;
+  EOL.MediaCenter.image_hash[data_object_id].curated = true;
+  
+  eol_update_credit(EOL.MediaCenter.image_hash[data_object_id]);
+  EOL.Curation.update_thumbnail_background(vetted_id, data_object_id);
+  EOL.MediaCenter.update_thumbnail_icons($$('div#thumbnails a#thumbnail_'+data_object_id+' ul')[0]);
+};
+
+EOL.Curation.quick_curate = function(element) {
+  element.ancestors()[1].down('div.spinner img').appear();
+  new Ajax.Request(element.href,
+                    {
+                      asynchronous:true,
+                      evalScripts:true,
+                      onComplete:function(){
+                        element.ancestors()[1].down('div.spinner img').disappear();
+                      }.bind(element)
+                    }
+                  );
 };
 
 EOL.Curation.update_thumbnail_background = function(vetted_id, data_object_id) {
@@ -50,65 +67,26 @@ EOL.Curation.update_text_icons = function(data_object_id) {
   
 };
 
-EOL.Curation.after_curate = function(form,vetted_id) {
-  id = form.readAttribute('data-data_object_id');
-  type = form.readAttribute('data-data_object_type');
-  form.enable();
-  form.down('div.label img').fade();
-  if(type == EOL.Curation.IMAGE_ID) {
-    $('large-image-trust-button').disappear();
-    $('large-image-untrust-button').disappear();
-    EOL.MediaCenter.image_hash[id].curated = true;
-
-    if ($('curate_trust_'+id).checked) {
-      EOL.MediaCenter.image_hash[id].vetted_id = EOL.Curation.TRUSTED_ID;
-      EOL.Curation.update_thumbnail_background(EOL.Curation.TRUSTED_ID, id);
-    } else if ($('curate_untrust_'+id).checked) {
-      EOL.MediaCenter.image_hash[id].vetted_id = EOL.Curation.UNTRUSTED_ID;
-      EOL.Curation.update_thumbnail_background(EOL.Curation.UNTRUSTED_ID, id);
-    }
-
-    if($('curate_invisible_'+id).checked) {
-      EOL.MediaCenter.image_hash[id].visibility_id = EOL.Curation.INVISIBLE_ID;
-    } else if($('curate_visible_'+id).checked) {
-      EOL.MediaCenter.image_hash[id].visibility_id = EOL.Curation.VISIBLE_ID;
-    } else if($('curate_inappropriate_'+id).checked) {
-      EOL.MediaCenter.image_hash[id].visibility_id = EOL.Curation.INAPPROPRIATE_ID;
-    }
-
-    eol_update_credit(EOL.MediaCenter.image_hash[id]);
-
-    EOL.MediaCenter.update_thumbnail_icons($$('div#thumbnails a#thumbnail_'+id+' ul')[0]);
-  } else if(type == EOL.Curation.TEXT_ID) {
-    $$('div#text_buttons_'+id+' div.trust_button')[0].disappear();
-    $$('div#text_buttons_'+id+' div.untrust_button')[0].disappear();
-    EOL.Curation.update_text_background(id, vetted_id);
-  }
-};
-
 EOL.Curation.Behaviors = {
   'div.visibility form div.option input:click, div.vetted form div.option input:click': function(e) {
     this.form.down('div.label img').appear();
     form = this.form
-    vetted_id = this.readAttribute('data-vetted_id');
     new Ajax.Request(this.form.action,
                      {
                        asynchronous:true,
                        evalScripts:true,
                        method:'put',
-                       onComplete:function(){EOL.Curation.after_curate(form,vetted_id);}.bind(form,vetted_id),
+                       onComplete:function() {
+                         form.enable();
+                         form.down('div.label img').fade();
+                       }.bind(form),
                        parameters:Form.serialize(this.form)
                      });
     this.form.disable();
   },
   
-  'div.trust_button a:click': function(e) {
-    EOL.Curation.quick_curate(this,EOL.Curation.TRUSTED_ID);
-    e.stop();
-  },
-  
-  'div.untrust_button a:click': function(e) {
-    EOL.Curation.quick_curate(this,EOL.Curation.UNTRUSTED_ID);
+  'div.trust_button a:click, div.untrust_button a:click': function(e) {
+    EOL.Curation.quick_curate(this);
     e.stop();
   }
 };

b5264fee5a64a48d2dc9f3d06e69d0ed7f127eda
bsmith <bsmith@78829999-583a-0410-bd01-8a9b849fd409>
2009-03-11 21:27:59 +0000

    moved add text link/buttons. fixes EOLINFRASTRUCTURE-677
    git-svn-id: https://repository.eol.org/eol/trunk@336 78829999-583a-0410-bd01-8a9b849fd409

--- public/javascripts/text_objects.js
+++ public/javascripts/text_objects.js
@@ -2,7 +2,7 @@ if(!EOL) var EOL = {};
 if(!EOL.TextObjects) EOL.TextObjects = {};
 
 EOL.TextObjects.Behaviors = {
-  'div.add_text_button a': function(e) {
+  'li.add_text a, div.add_text_button a': function(e) {
     new EOL.PopupLink(this,{insert_after:'insert_text', additional_classes:'insert_text'});
   }
 };

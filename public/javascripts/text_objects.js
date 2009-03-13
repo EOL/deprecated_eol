ddb34282461b97fb3dc9e354b99858fe61b84345
bsmith <bsmith@78829999-583a-0410-bd01-8a9b849fd409>
2009-03-13 16:56:40 +0000

    refactored chapter selection ui to discourage the creation of new chapters. fixes EOLINFRASTRUCTURE-678
    git-svn-id: https://repository.eol.org/eol/trunk@337 78829999-583a-0410-bd01-8a9b849fd409

--- public/javascripts/text_objects.js
+++ public/javascripts/text_objects.js
@@ -4,5 +4,19 @@ if(!EOL.TextObjects) EOL.TextObjects = {};
 EOL.TextObjects.Behaviors = {
   'li.add_text a, div.add_text_button a': function(e) {
     new EOL.PopupLink(this,{insert_after:'insert_text', additional_classes:'insert_text'});
+  },
+
+  '#data_objects_toc_category_toc_id:change': function(e) {
+    if(this.value==='new') {
+      $('existing_toc').hide();
+      $('new_toc').show();
+    }
+  },
+
+  '#new_toc a:click': function(e) {
+    $$('#existing_toc select')[0].selectedIndex = 0;
+    $('existing_toc').show();
+    $('new_toc').hide();
+    return false;
   }
 };

82a1cbf86d9f7abb5dfe9c55ba0fbf29a38301fe
ashipunova <ashipunova@78829999-583a-0410-bd01-8a9b849fd409>
2009-04-08 21:46:50 +0000

    staff for to mark curator's comments 1) migration adds column to comments table 2) in comments_controller value for new comment goes to db
    git-svn-id: file:///data/subversion/eol/trunk@449 78829999-583a-0410-bd01-8a9b849fd409

---
+++ db/migrate/20090406173750_add_curatorial_to_comments.rb
@@ -0,0 +1,10 @@
+class AddCuratorialToComments < ActiveRecord::Migration
+
+  def self.up
+     add_column :comments, :from_curator, :boolean, :default => nil, :null => false    
+  end
+
+  def self.down
+        remove_column :comments, :from_curator
+  end
+end

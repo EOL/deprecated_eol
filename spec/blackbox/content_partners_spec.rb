f75de6f3ecb875f226967e52ca3c632d20bfd609
peter <peter@78829999-583a-0410-bd01-8a9b849fd409>
2009-04-08 13:33:23 +0000

    fix breaking spec tests for EOLINFRASTRUCTURE-713
    git-svn-id: file:///data/subversion/eol/trunk@443 78829999-583a-0410-bd01-8a9b849fd409

--- spec/blackbox/content_partners_spec.rb
+++ spec/blackbox/content_partners_spec.rb
@@ -6,7 +6,7 @@ describe 'Content Partners' do
     Scenario.load :foundation
   end
 
-  it 'should have desc-personal tag with "Hello [full_name]" and a logout link when logged in' do
+  it 'home page of EOL should have desc-personal tag with "Hello [full_name]" and a logout link when logged in' do
     pass  = 'timey-wimey'
     agent = Agent.gen(:hashed_password => Digest::MD5.hexdigest(pass))
     cp    = ContentPartner.gen(:agent => agent)
@@ -19,5 +19,17 @@ describe 'Content Partners' do
       end
     end
   end
+
+  it 'content partner home page should have cp-desc-personal tag with "Welcome [full_name]" and a logout link when logged in' do
+    pass  = 'timey-wimey'
+    agent = Agent.gen(:hashed_password => Digest::MD5.hexdigest(pass))
+    cp    = ContentPartner.gen(:agent => agent)
+    login_content_partner(:username => agent.username, :password => pass)
+    body  = request('/content_partner').body
+    body.should have_tag('div#cp-desc-personal', :text => /Welcome ?\s+#{agent.full_name}/) do
+      with_tag('a[href*=?]', /logout/)
+    end
+  end
+
 
 end

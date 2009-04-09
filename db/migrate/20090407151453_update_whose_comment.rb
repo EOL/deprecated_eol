bdd33b3d9d314ea929497c36c0c85a1e02097c6a
ashipunova <ashipunova@78829999-583a-0410-bd01-8a9b849fd409>
2009-04-09 21:38:56 +0000

    git-svn-id: file:///data/subversion/eol/trunk@459 78829999-583a-0410-bd01-8a9b849fd409

--- db/migrate/20090407151453_update_whose_comment.rb
+++ db/migrate/20090407151453_update_whose_comment.rb
@@ -3,12 +3,12 @@ class UpdateWhoseComment < ActiveRecord::Migration
   def self.up
     #for old comments marks from_curator as true if author of a comment can curate a object of commenting
     Comment.find(:all).each do |comment|
-			cu = User.find_by_id(comment.user_id)
-			comment.parent_type == 'DataObject' ? ob = DataObject.find_by_id(comment.parent_id) : ob = TaxonConcept.find_by_id(comment.parent_id) 
+			comment_user = User.find(comment.user_id)
+			comment.parent_type == 'DataObject' ? comment_parent = DataObject.find(comment.parent_id) : comment_parent = TaxonConcept.find(comment.parent_id) 
 
-			is_cur = ob.is_curatable_by?(cu)
+			this_is_curatable = comment_parent.is_curatable_by?(comment_user)
       
-			comment.update_attributes(:from_curator => is_cur) # true, if author of a comment is a curator
+			comment.update_attributes(:from_curator => this_is_curatable) # true, if author of a comment is a curator
     end
   end
 
@@ -20,3 +20,4 @@ class UpdateWhoseComment < ActiveRecord::Migration
     end
   end
 end
+

e053e91111f4de1a29f523d02361aefd28afe00b
ashipunova <ashipunova@78829999-583a-0410-bd01-8a9b849fd409>
2009-04-09 20:22:22 +0000

    migration to mark old comments: from_curator is true if author of a comment can curate a object of commenting
    git-svn-id: file:///data/subversion/eol/trunk@458 78829999-583a-0410-bd01-8a9b849fd409

---
+++ db/migrate/20090407151453_update_whose_comment.rb
@@ -0,0 +1,22 @@
+class UpdateWhoseComment < ActiveRecord::Migration
+  
+  def self.up
+    #for old comments marks from_curator as true if author of a comment can curate a object of commenting
+    Comment.find(:all).each do |comment|
+			cu = User.find_by_id(comment.user_id)
+			comment.parent_type == 'DataObject' ? ob = DataObject.find_by_id(comment.parent_id) : ob = TaxonConcept.find_by_id(comment.parent_id) 
+
+			is_cur = ob.is_curatable_by?(cu)
+      
+			comment.update_attributes(:from_curator => is_cur) # true, if author of a comment is a curator
+    end
+  end
+
+
+  def self.down
+    #restore false for all comments!
+    Comment.find(:all).each do |comment| 
+      comment.update_attributes(:from_curator => false)
+    end
+  end
+end

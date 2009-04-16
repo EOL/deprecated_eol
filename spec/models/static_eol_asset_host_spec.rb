2c9e3ac0999b0ca1b65c7c90d7d414dc04d6421f
jrice <jrice@78829999-583a-0410-bd01-8a9b849fd409>
2009-04-16 19:43:46 +0000

    Just noticed a simple regex problem (causing a test to fail periodically and fixed it.
    git-svn-id: https://repository.eol.org/eol/trunk@483 78829999-583a-0410-bd01-8a9b849fd409

--- spec/models/static_eol_asset_host_spec.rb
+++ spec/models/static_eol_asset_host_spec.rb
@@ -9,18 +9,18 @@ describe StaticEolAssetHost do
   
   it "should use static1 for CSS assets" do
     css = "/stylesheets/test.css?1230601161"
-    @asset_host.call(css).should =~ /static1/
+    @asset_host.call(css).should =~ /static1\./
   end
   
   it "should use static2 for JS assets" do
     js = "/js/test.js?1230601161"
-    @asset_host.call(js).should =~ /static2/
+    @asset_host.call(js).should =~ /static2\./
   end
   
   it "should use use neither static1 nor static2 for image assets" do
     png = "/images/test.png?1230601161"
-    @asset_host.call(png).should_not =~ /static1/
-    @asset_host.call(png).should_not =~ /static2/
+    @asset_host.call(png).should_not =~ /static1\./
+    @asset_host.call(png).should_not =~ /static2\./
   end
 
 end

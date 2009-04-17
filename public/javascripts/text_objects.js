d9788b274d5b2781764e55f6e7def8e7c7050bb9
bsmith <bsmith@78829999-583a-0410-bd01-8a9b849fd409>
2009-04-17 21:30:17 +0000

    implemented user submitted text objects. refs EOLINFRASTRUCTURE-787 EOLINFRASTRUCTURE-788
    git-svn-id: file:///data/subversion/eol/trunk@485 78829999-583a-0410-bd01-8a9b849fd409

--- public/javascripts/text_objects.js
+++ public/javascripts/text_objects.js
@@ -6,17 +6,53 @@ EOL.TextObjects.Behaviors = {
     new EOL.PopupLink(this,{insert_after:'insert_text', additional_classes:'insert_text'});
   },
 
-  '#data_objects_toc_category_toc_id:change': function(e) {
-    if(this.value==='new') {
-      $('existing_toc').hide();
-      $('new_toc').show();
+  'div#insert_text_popup form:submit': function(e) {
+    EOL.TextObjects.remove_preview();
+    if($$('form#new_data_object textarea')[0].value.strip() == '') {
+      $('missing_text_error').show();
+      Effect.Pulsate('missing_text_error');
+      return false;
     }
+
+    new Ajax.Request(this.action,
+                     {
+                       asynchronous:true,
+                       evalScripts:true,
+                       method:'post',
+                       parameters:Form.serialize(this)
+                     });
+                     
+    return false;
   },
 
-  '#new_toc a:click': function(e) {
-    $$('#existing_toc select')[0].selectedIndex = 0;
-    $('existing_toc').show();
-    $('new_toc').hide();
+  'input#preview_text:click': function(e) {
+    EOL.TextObjects.remove_preview();
+    form = $('new_data_object')
+    new Ajax.Request(form.action+'/preview',
+                     {
+                       asynchronous:true,
+                       evalScripts:true,
+                       method:'post',
+                       parameters:Form.serialize(form)
+                     });
+
     return false;
   }
 };
+
+EOL.TextObjects.insert_new_text = function(text) {
+  EOL.popups["insert_text_popup"].hide();
+  Element.insert("insert_text", { after: text });
+  EOL.reload_behaviors();
+  setTimeout(EOL.popups["insert_text_popup"].destroy(), 1000);
+}
+
+EOL.TextObjects.preview_text = function(text) {
+  Element.insert("insert_text", { after: text });
+}
+
+EOL.TextObjects.remove_preview = function() {
+  if($('text_')) {
+    $('text_').up().remove();
+  }
+}

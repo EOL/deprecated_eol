2aa9e30da0f34bdb0f52fa947f42072ce774f820
jrice <jrice@78829999-583a-0410-bd01-8a9b849fd409>
2009-05-04 21:53:09 +0000

    RandomTaxa were screwing up one of the tests.  This fixes it.  We should be green again.
    git-svn-id: file:///data/subversion/eol/trunk@530 78829999-583a-0410-bd01-8a9b849fd409

--- app/models/ref_identifier.rb
+++ app/models/ref_identifier.rb
@@ -1,11 +1,15 @@
 class RefIdentifier < SpeciesSchemaModel
 
+  set_primary_key :ref_id, :ref_identifier_type_id
+
   belongs_to :ref
   belongs_to :ref_identifier_type
 
   has_and_belongs_to_many :taxa
 
-end# == Schema Info
+end
+
+# == Schema Info
 # Schema version: 20080922224121
 #
 # Table name: ref_identifiers

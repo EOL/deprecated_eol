26d09b8802eee072a9aaa834509f6ce0d5a9badb
jrice <jrice@78829999-583a-0410-bd01-8a9b849fd409>
2009-05-06 23:00:39 +0000

    You would think I would remember to add new files ONCE...
    git-svn-id: file:///data/subversion/eol/trunk@537 78829999-583a-0410-bd01-8a9b849fd409

---
+++ vendor/gems/jscruggs-metric_fu-1.0.1/lib/generators/flay.rb
@@ -0,0 +1,29 @@
+require 'generator'
+module MetricFu
+  
+  class Flay < Generator
+
+    def emit
+      files_to_flay = MetricFu.flay[:dirs_to_flay].map{|dir| Dir[File.join(dir, "**/*.rb")] }
+      @output = `flay #{files_to_flay.join(" ")}`
+    end
+
+    def analyze
+      @matches = @output.chomp.split("\n\n").map{|m| m.split("\n  ") }
+    end
+
+    def to_h
+      target = []
+      total_score = @matches.shift.first.split('=').last.strip
+      @matches.each do |problem|
+        reason = problem.shift.strip
+        lines_info = problem.map do |full_line|
+          name, line = full_line.split(":")
+          {:name => name.strip, :line => line.strip}
+        end
+        target << [:reason => reason, :matches => lines_info]
+      end
+      {:flay => {:total_score => total_score, :matches => target.flatten}}
+    end
+  end
+end

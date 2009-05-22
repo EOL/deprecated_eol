eefd3dc9d8cbda66fbcc4bb62cae43c31e2ed3a0
ashipunova <ashipunova@78829999-583a-0410-bd01-8a9b849fd409>
2009-05-22 18:06:14 +0000

    change default rating to 2.5 for new DataObject
    git-svn-id: https://repository.eol.org/eol/trunk@598 78829999-583a-0410-bd01-8a9b849fd409

---
+++ db/migrate/20090522175548_add_default_rating_two_point_five.rb
@@ -0,0 +1,13 @@
+class AddDefaultRatingTwoPointFive < ActiveRecord::Migration
+  def self.database_model
+    return "SpeciesSchemaModel"
+  end
+
+  def self.up
+    change_column :data_objects, :data_rating, :float, :default => 2.5
+  end
+
+  def self.down
+    change_column :data_objects, :data_rating, :float
+  end
+end

44b3a3dd59f29bd68d0a13634b45459bc195bd73
cappy <cappy@78829999-583a-0410-bd01-8a9b849fd409>
2009-06-13 10:00:06 +0000

    updated demo.rb file
    git-svn-id: file:///data/subversion/eol/trunk@710 78829999-583a-0410-bd01-8a9b849fd409

--- config/environments/demo.rb
+++ config/environments/demo.rb
@@ -1,35 +1,69 @@
-# Settings specified here will take precedence over those in config/environment.rb
+#----------------------------------------------------------------
+#                     demo.rb
+# Environment specific settings for the Preview (beta) environment
+#
+# Settings specified here will override those in config/environment.rb
+#
+# # Configuration files are loaded in the following order with the settings
+# in each file overriding the settings in prior files
+#
+# 1) config/environment.rb
+# 2) config/environments/[RAILS_ENV].rb
+# 3) config/environments/[RAILS_ENV]_eol_org.rb
+# 4) config/environment_eol_org.rb
+#---------------------------------------------------------------
 
-# The production environment is meant for finished, "live" apps.
-# Code is not reloaded between requests
-config.cache_classes = true
+# In the development environment your application's code is reloaded on
+# every request.  This slows down response time but is perfect for 
+# development since you don't have to restart the webserver when you make 
+# code changes.
+config.cache_classes = false
 
-# Use a different logger for distributed setups
-# config.logger = SyslogLogger.new
+# Log error messages when you accidentally call methods on nil.
+config.whiny_nils = false
 
-# Full error reports are disabled and caching is turned on
+# Show full error reports and disable caching
 config.action_controller.consider_all_requests_local = false
-config.action_controller.perform_caching             = true
+config.action_controller.perform_caching             = false
 config.action_view.debug_rjs                         = false
 
-# Enable serving of images, stylesheets, and javascripts from an asset server
-# config.action_controller.asset_host                  = "http://assets.example.com"
-
-# Disable delivery errors, bad email addresses will be ignored
+# email error reporting 
 config.action_mailer.raise_delivery_errors = false
 
-# ENVIRONMENT SPECIFIC CONFIGURATION
+# logging level
+# config.log_level = :debug
 
-ActionMailer::Base.delivery_method = :smtp
-ActionMailer::Base.smtp_settings = {
-:address => "rubus.eol.org",
-:port => 25,
-:domain => "eol.org",
-} 
+# set to false turn off notification of exceptions via email 
+$EXCEPTION_NOTIFY=false
+ 
+# set to true to record uncaught application errors in sql database file
+$ERROR_LOGGING=true 
 
-$ENABLE_ANALYTICS=false
-$SHOW_SURVEYS=false
+# set to true to enable recaptcha on registration and contact us form
+$ENABLE_RECAPTCHA=false
 
-$EXCEPTION_NOTIFY=false # set to false to not be notified of exceptions via email in production mode (set email addresses below)
-$ERROR_LOGGING=false # set to true to record uncaught application errors in sql database file 
+# EVALUATIONS/SURVEYS CONFIGURATION
+# Set SHOW_SURVEYS to true to show surveys; logic on when to show 
+# surveys is set in the "show_survey?" method in the application 
+# controller
+$SHOW_SURVEYS=false 
+$SURVEY_URL="http://vovici.com/wsb.dll/s/6ea8g3124f"
 
+
+# ANALYTICS CONFIGURATION
+# Set ENABLE_ANALYTICS to true to enable google analytics.  The
+# GOOGLE_ANALYTICS_ID must be set to the google analytics ID
+# to use if analytics is enabled.
+$ENABLE_ANALYTICS=false 
+$GOOGLE_ANALYTICS_ID="UA-3298646-1" 
+
+# The following tells Rails to check the database connection every 2 minutes
+# and if it isn't connected reconnect instead of throwing an exception
+ActiveRecord::Base.verification_timeout = 120
+
+#This part of the code should stay at the bottom to ensure that www.eol.org - related settings override everything
+begin
+  require File.join(File.dirname(__FILE__), 'demo_eol_org')
+rescue LoadError
+  puts '*************WARNING: COULD NOT LOAD demo_eol_org FILE***********************'
+end

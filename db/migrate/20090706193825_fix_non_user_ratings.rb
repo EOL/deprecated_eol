5ab631211adad5afebc1fddd2ebf8e52cdf0babd
bsmith <bsmith@78829999-583a-0410-bd01-8a9b849fd409>
2009-07-07 18:27:00 +0000

    updated rating fix migration to be more efficient. ref EOLINFRASTRUCTURE-1118
    git-svn-id: https://repository.eol.org/eol/trunk@825 78829999-583a-0410-bd01-8a9b849fd409

--- db/migrate/20090706193825_fix_non_user_ratings.rb
+++ db/migrate/20090706193825_fix_non_user_ratings.rb
@@ -1,9 +1,9 @@
 class FixNonUserRatings < ActiveRecord::Migration
   def self.up
-    execute("update #{DataObject.full_table_name} dato left join #{UsersDataObjectsRating.full_table_name} udor on (udor.data_object_id=dato.id and udor.id=null) set dato.data_rating=3.9 where dato.data_rating>4.0;")
+    execute("update #{DataObject.full_table_name} dato left join #{UsersDataObjectsRating.full_table_name} udor on (udor.data_object_id=dato.id) set dato.data_rating=3.9 where dato.data_rating>3.9 and udor.id IS NULL;")
   end
 
   def self.down
-    execute("update #{DataObject.full_table_name} dato left join #{UsersDataObjectsRating.full_table_name} udor on (udor.data_object_id=dato.id and udor.id=null) set dato.data_rating=5.0 where dato.data_rating=3.9;")
+    execute("update #{DataObject.full_table_name} dato left join #{UsersDataObjectsRating.full_table_name} udor on (udor.data_object_id=dato.id) set dato.data_rating=5.0 where dato.data_rating=3.9 and udor.id IS NULL;")
   end
 end

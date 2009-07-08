ba2c10587a3812c2150f1d2930e94535d0f59478
jrice <jrice@78829999-583a-0410-bd01-8a9b849fd409>
2009-07-08 21:38:11 +0000

    Ooops, you need this if tagging fails
    git-svn-id: file:///data/subversion/eol/trunk@833 78829999-583a-0410-bd01-8a9b849fd409

---
+++ lib/exceptions.rb
@@ -0,0 +1,7 @@
+# Bring out 'yer dead!  Bring out 'yer dead!
+# 
+# This is a module for all of EOL's custom exceptions.  Remember that it is superior to raise exceptions and catch
+# them than it is to use return values that signify success and failure and parse them.
+module Exceptions
+  class FailedToCreateTag < StandardError; end
+end

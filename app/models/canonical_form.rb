05e797d1b5ca1a588bd1abb4aca844a60b747779
mchung <mchung@78829999-583a-0410-bd01-8a9b849fd409>
2009-07-08 18:36:03 +0000

    Updated documentation for CanonicalForm
    git-svn-id: https://repository.eol.org/eol/trunk@831 78829999-583a-0410-bd01-8a9b849fd409

--- app/models/canonical_form.rb
+++ app/models/canonical_form.rb
@@ -1,5 +1,8 @@
-# A canonical form of a scientific name is the name parts without authorship, rank information, or anthing except the latinized
-# name parts. These are for the most part algorithmically generated. Every Name should have a CanonicalForm.
+# A canonical form of a scientific name is the name parts without authorship,
+# rank information, or anything except the latinized name parts. These are for
+# the most part algorithmically generated. 
+#
+# Every Name should have a CanonicalForm.
 class CanonicalForm < SpeciesSchemaModel
   has_many :names
 end

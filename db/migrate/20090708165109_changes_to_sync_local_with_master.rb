52fe5f6cea099dee704cf149dacb59dbffb6f12b
pleary <pleary@78829999-583a-0410-bd01-8a9b849fd409>
2009-07-13 16:58:48 +0000

    Added migrations to add new field to data_objects ad add indices; Update taxon_concept.rb and factories.rb for #TAX-278
    git-svn-id: file:///data/subversion/eol/trunk@843 78829999-583a-0410-bd01-8a9b849fd409

---
+++ db/migrate/20090708165109_changes_to_sync_local_with_master.rb
@@ -0,0 +1,31 @@
+class ChangesToSyncLocalWithMaster < ActiveRecord::Migration
+  def self.database_model
+    return "SpeciesSchemaModel"
+  end
+  
+  def self.up
+    add_index :data_objects_taxa, :identifier, :name => 'identifier'
+    add_index :harvest_events_taxa, :taxon_id, :name => 'taxon_id'
+    add_index :refs, :full_reference, :name => 'full_reference'
+    add_index :resources_taxa, :identifier, :name => 'identifier'
+    add_index :synonyms, :name_id, :name => 'name_id'
+    add_index :taxon_concepts, :supercedure_id, :name => 'supercedure_id'
+    add_index :taxon_concepts, :vetted_id, :name => 'vetted_id'
+    
+    remove_column :hierarchy_entries, :remote_id
+    execute('alter table harvest_events modify `id` int(10) unsigned NOT NULL auto_increment')
+  end
+  
+  def self.down
+    remove_index :data_objects_taxa, :name => :identifier
+    remove_index :harvest_events_taxa, :name => :taxon_id
+    remove_index :refs, :name => :full_reference
+    remove_index :resources_taxa, :name => :identifier
+    remove_index :synonyms, :name => :name_id
+    remove_index :taxon_concepts, :name => :supercedure_id
+    remove_index :taxon_concepts, :name => :vetted_id
+    
+    execute('alter table hierarchy_entries add `remote_id` varchar(255) character set ascii NOT NULL after `identifier`')
+    execute('alter table harvest_events modify `id` int(10) unsigned NOT NULL auto_increment')
+  end
+end

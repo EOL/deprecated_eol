61bb5793048f451700a8f10a965ecc15ce814969
peter <peter@78829999-583a-0410-bd01-8a9b849fd409>
2009-07-23 17:57:49 +0000

    Add rake task for ad-hoc external link reports; add in image guessing game from EOL labs; small message for administrators for uploaded content
    git-svn-id: https://repository.eol.org/eol/trunk@892 78829999-583a-0410-bd01-8a9b849fd409

---
+++ app/controllers/games_controller.rb
@@ -0,0 +1,27 @@
+class GamesController < ApplicationController
+  
+  layout 'main'
+  
+  def identify_the_image
+     
+     @num_images=5
+     
+     # original ordering of random taxa
+     @taxa  = RandomTaxon.random_set(@num_images)
+     
+     # now scramble the order into an array
+     @random_order = Array.new
+     
+     while @random_order.length < @num_images do
+       
+       # get a random number from 0..num_images
+       random_number=rand(@num_images)
+       
+       # check to see if we've already included that number in our random array, otherwise add it
+       @random_order << random_number unless @random_order.include? random_number
+       
+     end
+     
+  end
+  
+end

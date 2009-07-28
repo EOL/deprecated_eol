972661b1af5beab1d3d47d546faa7b03b5024320
ashipunova <ashipunova@78829999-583a-0410-bd01-8a9b849fd409>
2009-07-28 15:31:52 +0000

    EOLINFRASTRUCTURE-1174: taxon -> taxon_concept
    git-svn-id: file:///data/subversion/eol/trunk@915 78829999-583a-0410-bd01-8a9b849fd409

--- app/models/actions_history.rb
+++ app/models/actions_history.rb
@@ -7,20 +7,20 @@ class ActionsHistory < ActiveRecord::Base
   validates_presence_of :user_id, :changeable_object_type_id, :action_with_object_id, :created_at 
     
   #TODO: These "magic numbers" in the case statement correspond to rows in the 'changeable_object_types' model, can they be refactored in such a way that they aren't harcoded in multiple methods?  
-  def taxon_name
+  def taxon_concept_name
     case changeable_object_type_id  
       when 1: 
       #data_object
-        taxon_name = data_object.taxa_names_taxon_concept_ids[0][:taxon_name]
+        taxon_concept_name = data_object.taxa_names_taxon_concept_ids[0][:taxon_name]
       when 2: 
       #comment
         if comment_object.parent_type == 'TaxonConcept'
-          taxon_name = comment_parent.scientific_name
+          taxon_concept_name = comment_parent.scientific_name
         elsif comment_object.parent_type == 'DataObject'
           if comment_parent.user.nil?
-            taxon_name = comment_parent.taxa_names_taxon_concept_ids[0][:taxon_name]
+            taxon_concept_name = comment_parent.taxa_names_taxon_concept_ids[0][:taxon_name]
           else
-            taxon_name = comment_parent.taxon_concept_for_users_text.name
+            taxon_concept_name = comment_parent.taxon_concept_for_users_text.name
           end
         end
       when 3:    
@@ -28,36 +28,36 @@ class ActionsHistory < ActiveRecord::Base
           #not counts at present
       when 4:
       #"users_submitted_text"
-        taxon_name = udo_taxon_concept.name
+        taxon_concept_name = udo_taxon_concept.name
       else 
         raise "Don't know how to get taxon name from a changeable object type of id #{changeable_object_type_id}"
       end
-    return taxon_name   
+    return taxon_concept_name   
   end
       
-  def taxon_id
+  def taxon_concept_id
     case changeable_object_type_id  
       when 1: 
       #data_object
-        taxon_id = data_object.taxa_names_taxon_concept_ids[0][:taxon_concept_id]
+        taxon_concept_id = data_object.taxa_names_taxon_concept_ids[0][:taxon_concept_id]
       when 2: 
       #comment
         if comment_object.parent_type == 'TaxonConcept'
-          taxon_id = comment_parent.id
+          taxon_concept_id = comment_parent.id
         else
           if comment_parent.user.nil?
-            taxon_id = comment_object.taxon_concept_id
+            taxon_concept_id = comment_object.taxon_concept_id
           else
-            taxon_id = comment_parent.taxon_concept_for_users_text.id
+            taxon_concept_id = comment_parent.taxon_concept_for_users_text.id
           end 
         end
       when 4:
       #users_data_object
-        taxon_id = udo_taxon_concept.id
+        taxon_concept_id = udo_taxon_concept.id
       else
         raise "Don't know how to get the taxon id from a changeable object type of id #{changeable_object_type_id}"
     end
-    return taxon_id
+    return taxon_concept_id
   end
            
   #-------- data_object ---------

99695f9b4a4dd7d40a7cfc01d40322cba495fe5d
mchung <mchung@78829999-583a-0410-bd01-8a9b849fd409>
2009-07-29 23:39:15 +0000

    EOLINFRASTRUCTURE-609: Added taxon_concept_id
    git-svn-id: https://repository.eol.org/eol/trunk@933 78829999-583a-0410-bd01-8a9b849fd409

--- app/models/taxon_concept.rb
+++ app/models/taxon_concept.rb
@@ -565,6 +565,7 @@ EOIUCNSQL
     # handled by TaxonConcept#get_default_content
 
     ccb = CategoryContentBuilder.new
+    options[:taxon_concept_id] = id
     content = ccb.content_for(toc_item, options)
     content = get_default_content(toc_item) if content.nil?
     content

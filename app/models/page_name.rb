3746501a768a184c9d724321d30cd70775cada5b
mchung <mchung@78829999-583a-0410-bd01-8a9b849fd409>
2009-08-05 15:55:30 +0000

    #EOL-609: Finished up refactoring the Tablf Contents
    git-svn-id: https://repository.eol.org/eol/trunk@959 78829999-583a-0410-bd01-8a9b849fd409

--- app/models/page_name.rb
+++ app/models/page_name.rb
@@ -2,6 +2,17 @@ class PageName < SpeciesSchemaModel
   belongs_to :item_page
   belongs_to :name
   set_primary_keys :name_id, :item_page_id
+  
+  def self.page_names_for?(taxon_concept_id)
+    return PageName.count_by_sql(['SELECT 1 FROM taxon_concept_names tcn 
+                                     JOIN page_names pn 
+                                    USING (name_id) 
+                                    WHERE tcn.taxon_concept_id = ? 
+                                    LIMIT 1', taxon_concept_id]) > 0
+  end
+      
+
+  
 end
 # == Schema Info
 # Schema version: 20081020144900

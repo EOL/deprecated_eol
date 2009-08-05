2b065b18917c044fd676e2570df8347dabd0c179
bsmith <bsmith@78829999-583a-0410-bd01-8a9b849fd409>
2009-08-05 17:15:13 +0000

    fixed curator for selenium tests
    git-svn-id: https://repository.eol.org/eol/trunk@960 78829999-583a-0410-bd01-8a9b849fd409

--- scenarios/bootstrap.rb
+++ scenarios/bootstrap.rb
@@ -121,7 +121,7 @@ test_user2 = User.gen(:username => 'test_user2', :password => 'password', :given
 test_user2.save
 
 #curator for selenium tests (NB: page #20)
-curator = User.gen(:username => 'test_curator', :password => 'password', 'given_name' => 'test', :family_name => 'curator', :curator_hierarchy_entry_id => 5, :curator_approved => true)
+curator = User.gen(:username => 'test_curator', :password => 'password', 'given_name' => 'test', :family_name => 'curator', :curator_hierarchy_entry_id => 11, :curator_approved => true)
 curator.save
 
 

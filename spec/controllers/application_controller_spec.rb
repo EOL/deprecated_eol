ed3b2a01b220321afc1eac028eed6efd79d3c657
jrice <jrice@78829999-583a-0410-bd01-8a9b849fd409>
2009-08-06 19:20:36 +0000

    EOLINFRASTRUCTURE-1173 completed.  ...Of course, black-box tests were most appropraite, and have only been described as pending for now, until we resolve RackBox/Webrat/Whatever solution. - MISSING FILES, sorry
    git-svn-id: https://repository.eol.org/eol/trunk@968 78829999-583a-0410-bd01-8a9b849fd409

---
+++ spec/controllers/application_controller_spec.rb
@@ -0,0 +1,28 @@
+require File.dirname(__FILE__) + '/../spec_helper'
+
+describe ApplicationController do
+
+  before(:all) do
+    Factory(:language, :label => 'English')
+    @taxon_name          = "<i>italic</i> foo & bar"
+    @taxon_name_with_amp = "<i>italic</i> foo &amp; bar"
+    @taxon_name_no_tags  = "italic foo & bar"
+    @taxon_name_no_html  = "&lt;i&gt;italic&lt;/i&gt; foo &amp; bar"
+  end
+  
+  it 'should use Gibberish' do
+    Gibberish.should_receive(:use_language)
+    get set_flash_enabled_path
+  end
+
+  it 'should have hh' do
+    @controller.send(:hh, @taxon_name).should == @taxon_name_with_amp
+  end
+
+  # This is a built-in rails function.  I'm only testing it here to make it clear how it differs from hh and
+  # remove_html:
+  it 'should have h' do
+    @controller.send(:h, @taxon_name).should == @taxon_name_no_html
+  end
+
+end

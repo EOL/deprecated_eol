8f9859c46ad14742a78f7ffb19568547f70e5689
jrice <jrice@78829999-583a-0410-bd01-8a9b849fd409>
2009-08-06 19:07:05 +0000

    EOLINFRASTRUCTURE-1173 completed.  ...Of course, black-box tests were most appropraite, and have only been described as pending for now, until we resolve RackBox/Webrat/Whatever solution.
    git-svn-id: https://repository.eol.org/eol/trunk@967 78829999-583a-0410-bd01-8a9b849fd409

--- app/models/category_content_builder.rb
+++ app/models/category_content_builder.rb
@@ -13,142 +13,117 @@ class CategoryContentBuilder
 
     # A list of current sub_names:
     #
-    # search_the_web
-    # common_names
-    # specialist_projects
     # biodiversity_heritage_library
+    # common_names
+    # medical_concepts
+    # search_the_web
+    # specialist_projects
 
-    if sub_name == "search_the_web"
-      content = search_the_web(options)
-      content[:content_type] = sub_name
+    content = {
+      :category_name => toc_item.label,
+      :content_type  => sub_name
+    }
+
+    if sub_name == "biodiversity_heritage_library"
+      content.merge! biodiversity_heritage_library(options)
     elsif sub_name == "common_names"
-      content = common_names(options)
-      content[:content_type] = sub_name
+      content.merge! common_names(options)
+    elsif sub_name == "medical_concepts"
+      content.merge! medical_concepts(options)
+    elsif sub_name == "search_the_web"
+      content.merge! search_the_web(options)
     elsif sub_name == "specialist_projects"
-      content = specialist_projects(options)
-      content[:content_type] = sub_name
-    elsif sub_name == "biodiversity_heritage_library"
-      content = biodiversity_heritage_library(options)
-      content[:content_type] = sub_name
+      content.merge! specialist_projects(options)
     else
-      nil
+      return nil # We don't handle this toc_item.
     end
 
     content
   end
 
-  private
-  
-  # =============== The following are methods specific to content_by_category
+# =============== The following are methods specific to content_by_category
+private
 
-    # These should never be called; they're containers, not a leaf nodes:
-    # references_and_more_information
-    # evolution_and_systematics
+  # TODO - change this (and the view) so that it's not reliant on hashes.  Paginate it.
+  def biodiversity_heritage_library(options)
 
-    def search_the_web(options)
-      {
-        :category_name => 'Search the Web',
-        :items => []
+    tc_id = options[:taxon_concept_id]
+
+    items = SpeciesSchemaModel.connection.execute(%Q{
+      SELECT DISTINCT ti.id item_id, pt.title publication_title, pt.url publication_url,
+                      pt.details publication_details, ip.year item_year, ip.volume item_volume,
+                      ip.issue item_issue, ip.prefix item_prefix, ip.number item_number, ip.url item_url
+      FROM taxon_concept_names tcn
+        JOIN page_names pn ON (tcn.name_id = pn.name_id)
+        JOIN item_pages ip ON (pn.item_page_id = ip.id)
+        JOIN title_items ti ON (ip.title_item_id = ti.id)
+        JOIN publication_titles pt ON (ti.publication_title_id = pt.id)
+      WHERE tcn.taxon_concept_id = #{tc_id}
+      LIMIT 0,1000
+    }).all_hashes
+
+    sorted_items = items.sort_by do|item|
+      [item["publication_title"], item["item_year"], item["item_volume"], item["item_issue"], item["item_number"].to_i]
+    end
+
+    return {:items => sorted_items}
+
+  end
+
+  def common_names(options)
+    return {
+        :items => Name.find_by_sql([%q{
+                    SELECT names.string, l.iso_639_1 language_label, l.label, l.name
+                    FROM taxon_concept_names tcn JOIN names ON (tcn.name_id = names.id)
+                      LEFT JOIN languages l ON (tcn.language_id = l.id)
+                    WHERE tcn.taxon_concept_id = ? AND vern = 1
+                    ORDER BY language_label, string
+                  }, options[:taxon_concept_id]])
       }
+  end
+
+  def medical_concepts(options)
+    return {:items => Mapping.for_taxon_concept_id(options[:taxon_concept_id],
+                                                   :collection_id => Collection.ligercat.id)}
+  end
+
+  # This is all hard-coded in the view.
+  def search_the_web(options)
+    return {}
+  end
+
+  def specialist_projects(options)
+    # I did not include these outlinks as data object in the traditional sense. For now, you'll need to go through the
+    # collections and mappings tables to figure out which links pertain to the taxon (mappings has the name_id field). I
+    # had some thoughts about including these in the taxa/data_object route, but I don't have plans to make this change
+    # any time soon.
+    # 
+    # I had the table hierarchies_content which was supposed to let us know roughly what we had for each
+    # hierarchies_entry (text, images, maps...). But, maybe it makes sense to cache the table of contents / taxon
+    # relationships as well as media. Another de-normalized table. It may seem sloppy, but I'm sure we'll have to use
+    # de-normalized tables a lot in this project.
+
+    tc_id = options[:taxon_concept_id]
+    vetted = options[:vetted]
+
+    return_mapping_objects = []
+    mappings = SpeciesSchemaModel.connection.execute(%Q{
+      SELECT DISTINCT m.id mapping_id, m.foreign_key foreign_key, a.full_name agent_name,
+                      c.title collection_title, c.link collection_link, c.uri collection_uri 
+        FROM taxon_concept_names tcn 
+        JOIN mappings m ON (tcn.name_id = m.name_id) 
+        JOIN collections c ON (m.collection_id = c.id) 
+        JOIN agents a ON (c.agent_id = a.id) 
+        WHERE tcn.taxon_concept_id = #{options[:taxon_concept_id]} AND (c.vetted=1 OR c.vetted=#{vetted}) 
+        GROUP BY c.id
+    }).all_hashes
+    mappings.sort_by { |mapping| mapping["agent_name"] }.each do |m|
+      mapping_object = Mapping.find(m["mapping_id"].to_i)
+      return_mapping_objects << mapping_object
     end
 
-    def common_names(options)
-      tc_id = options[:taxon_concept_id]
-
-      # NOTES: we had a notion of "unspecified" language.  Results were sorted.
-      result = {
-          :category_name => 'Common Names',
-          :items         => Name.find_by_sql([
-                              'SELECT names.string, l.iso_639_1 language_label, l.label, l.name
-                                 FROM taxon_concept_names tcn JOIN names ON (tcn.name_id = names.id)
-                                   LEFT JOIN languages l ON (tcn.language_id = l.id)
-                                 WHERE tcn.taxon_concept_id = ? AND vern = 1
-                                 ORDER BY language_label, string', tc_id])
-        }
-      return result
-    end
-
-    def specialist_projects(options)
-      # I did not include these outlinks as data object in the traditional sense. For now, you'll need to go through the
-      # collections and mappings tables to figure out which links pertain to the taxon (mappings has the name_id field). I
-      # had some thoughts about including these in the taxa / data_object route, but I don't have plans to make this change
-      # any time soon.
-      # 
-      # I had the table hierarchies_content which was supposed to let us know roughly what we had for each hierarchies_entry
-      # (text, images, maps...). But, maybe it makes sense to cache the table of contents / taxon relationships as well as
-      # media. Another de-normalized table. It may seem sloppy, but I'm sure we'll have to use de-normalized tables a lot in
-      # this project.
-
-  #     mappings = Mapping.find_by_sql([<<EO_MAPPING_SQL, id, @current_user.vetted])
-  #       
-  #       SELECT DISTINCT m.*, a.full_name agent_full_name, c.*
-  #         FROM taxon_concept_names tcn
-  #           LEFT JOIN mappings m USING (name_id)
-  #           LEFT JOIN collections c ON (m.collection_id = c.id)
-  #           LEFT JOIN agents a ON (c.agent_id = a.id)
-  #         WHERE tcn.taxon_concept_id = ?
-  #           AND (c.vetted = 1 OR c.vetted = ?) # Specialist Projects / Mappings
-  # 
-  # EO_MAPPING_SQL
-  # 
-  #     results = []
-  #     mappings.each do |mapping|
-  #       collection_url = mapping.collection.uri.gsub!(/FOREIGNKEY/, mapping.foreign_key)
-  #       results << {
-  #         :agent_name       => mapping.agent_full_name || '[unspecified]',
-  #         :collection_title => mapping.collection.title,
-  #         :collection_link  => mapping.collection.link,
-  #         :url              => collection_url,
-  #         :icon             => mapping.collection.logo_url # FIX THIS LATER TODO
-  #       }
-  #     end
-
-      tc_id = options[:taxon_concept_id]
-      vetted = options[:vetted]
-
-      return_mapping_objects = []
-      mappings = SpeciesSchemaModel.connection.execute(
-        "SELECT DISTINCT m.id mapping_id, m.foreign_key foreign_key, a.full_name agent_name, c.title collection_title, c.link collection_link, c.uri collection_uri 
-           FROM taxon_concept_names tcn 
-           JOIN mappings m ON (tcn.name_id = m.name_id) 
-           JOIN collections c ON (m.collection_id = c.id) 
-           JOIN agents a ON (c.agent_id = a.id) 
-           WHERE tcn.taxon_concept_id = #{options[:taxon_concept_id]} AND (c.vetted=1 OR c.vetted=#{vetted}) 
-           GROUP BY c.id").all_hashes
-      mappings.sort_by { |mapping| mapping["agent_name"] }.each do |m|
-        mapping_object = Mapping.find(m["mapping_id"].to_i)
-        return_mapping_objects << mapping_object
-      end
-
-      return {
-            :category_name => 'Specialist Projects',
-            :projects => return_mapping_objects
-          }
-      
-    end
-
-    def biodiversity_heritage_library(options)
-      tc_id = options[:taxon_concept_id]
-
-      items = SpeciesSchemaModel.connection.execute(
-        "SELECT DISTINCT ti.id item_id, pt.title publication_title, pt.url publication_url,
-                         pt.details publication_details, ip.year item_year, ip.volume item_volume,
-                         ip.issue item_issue, ip.prefix item_prefix, ip.number item_number, ip.url item_url
-         FROM taxon_concept_names tcn
-           JOIN page_names pn ON (tcn.name_id = pn.name_id)
-           JOIN item_pages ip ON (pn.item_page_id = ip.id)
-           JOIN title_items ti ON (ip.title_item_id = ti.id)
-           JOIN publication_titles pt ON (ti.publication_title_id = pt.id)
-         WHERE tcn.taxon_concept_id = #{tc_id}
-         LIMIT 0,1000").all_hashes
-
-      sorted_items = items.sort_by { |item| [item["publication_title"], item["item_year"], item["item_volume"], item["item_issue"], item["item_number"].to_i] }
-
-      return {
-            :category_name => 'Biodiversity Heritage Library',
-            :items         => sorted_items
-          }
-
-    end
+    return {:projects => return_mapping_objects}
+    
+  end
 
 end
--- app/models/toc_item.rb
+++ app/models/toc_item.rb
@@ -4,46 +4,47 @@ class TocItem < SpeciesSchemaModel
   acts_as_tree :order => 'view_order'
   
   attr_writer :has_content
-  attr_accessor :has_unpublished_content, :has_invisible_content, :has_inappropriate_content, :has_published_content
   
   has_many :info_items, :foreign_key => :toc_id
   
   has_and_belongs_to_many :data_objects, :join_table => 'data_objects_table_of_contents', :foreign_key => 'toc_id'
 
   def self.bhl
-    YAML.load(Rails.cache.fetch('toc_items/bhl') do
-      TocItem.find_by_label('Biodiversity Heritage Library').to_yaml
-    end)
+    Rails.cache.fetch('toc_items/bhl') do
+      TocItem.find_by_label('Biodiversity Heritage Library')
+    end
   end
   
   def self.specialist_projects
-    YAML.load(Rails.cache.fetch('toc_items/specialist_projects') do
-      TocItem.find_by_label('Specialist Projects').to_yaml
-    end)
+    Rails.cache.fetch('toc_items/specialist_projects') do
+      TocItem.find_by_label('Specialist Projects')
+    end
   end
   
   def self.common_names
-    YAML.load(Rails.cache.fetch('toc_items/common_names') do
-      TocItem.find_by_label('Common Names').to_yaml
-    end)
+    Rails.cache.fetch('toc_items/common_names') do
+      TocItem.find_by_label('Common Names')
+    end
   end
   
   def self.overview
-    YAML.load(Rails.cache.fetch('toc_items/overview') do
-      TocItem.find_by_label('Overview').to_yaml
-    end)
+    Rails.cache.fetch('toc_items/overview') do
+      TocItem.find_by_label('Overview')
+    end
   end
   
   def self.search_the_web
-    YAML.load(Rails.cache.fetch('toc_items/search_the_web') do
-      TocItem.find_by_label('Search the Web').to_yaml
-    end)
+    Rails.cache.fetch('toc_items/search_the_web') do
+      TocItem.find_by_label('Search the Web')
+    end
   end
   
-  def has_content?
-    @has_content
+  def self.medical_concepts
+    Rails.cache.fetch('toc_items/medical_concepts') do
+      TocItem.find_by_label('Medical Concepts')
+    end
   end
-
+  
   def is_child?
     !(self.parent_id.nil? or self.parent_id == 0) 
   end

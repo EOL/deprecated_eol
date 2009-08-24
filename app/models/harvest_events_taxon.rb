8027399c364e37646a7abf08754969121d6eb1b0
jrice <jrice@78829999-583a-0410-bd01-8a9b849fd409>
2009-08-24 19:00:37 +0000

    Closes EOLINFRASTRUCTURE-1239 and EOLINFRASTRUCTURE-1203 by allowing Agents to check whether a TaxonConcept was included in their latest Harvest Event.
    git-svn-id: https://repository.eol.org/eol/trunk@1060 78829999-583a-0410-bd01-8a9b849fd409

--- app/models/harvest_events_taxon.rb
+++ app/models/harvest_events_taxon.rb
@@ -2,15 +2,7 @@ class HarvestEventsTaxon < SpeciesSchemaModel
   belongs_to :harvest_event
   belongs_to :taxon
   belongs_to :status
-end# == Schema Info
-# Schema version: 20081002192244
-#
-# Table name: harvest_events_taxa
-#
-#  harvest_event_id :integer(4)      not null
-#  status_id        :integer(1)      not null
-#  taxon_id         :integer(4)      not null
-#  guid             :string(32)      not null
+end
 
 # == Schema Info
 # Schema version: 20081020144900

c3aaffb9e115570cf482a69f84a6eb4080493513
pleary <pleary@78829999-583a-0410-bd01-8a9b849fd409>
2009-08-25 21:17:08 +0000

    Jeremy updated the toc_builder spec and an associated file to remove duplicate TOC entries
    git-svn-id: https://repository.eol.org/eol/trunk@1075 78829999-583a-0410-bd01-8a9b849fd409

--- spec/models/toc_builder_spec.rb
+++ spec/models/toc_builder_spec.rb
@@ -15,25 +15,24 @@ describe TocBuilder do
       # tc = build_taxon_concept(:toc=> [{:toc_item => TocItem.overview}], :bhl => [{}])
       tc = build_taxon_concept(
         :toc=> [
-          {:toc_item => TocItem.overview}, 
-          # TODO - these three are specified incorrectly.
-          # Search the Web is only created when the user (specified in options) has vetted = false.
-          # Specialist Projects is only when there are collections and mappings for the TC.
-          # BHL is only when there are really BHL entries.  There is an argument to build_taxon_concept that allows
-          # you to specify these directly.
-          {:toc_item => TocItem.search_the_web}, 
-          {:toc_item => TocItem.specialist_projects}, 
-          {:toc_item => TocItem.bhl}
+          {:toc_item => TocItem.overview}
         ],
+        :bhl => [{}, {}],
         :biomedical_terms => true, # The LigerCat entry.
         :images => [],  # this just speeds things up
         :comments => [] # this also speeds things up a bit.
       )
+      
+      user = User.create_new
+      user.vetted = false
+      
+      # In order to get specialist projects, we need a mapping realted to one of the tc's names.
+      Mapping.gen(:name => Name.last) # Cheating.  I know that the last name built was created for this TC
 
       # Just asserting an assumption about label ordering.
       tb = TocBuilder.new
       
-      toc = tb.toc_for(tc)
+      toc = tb.toc_for(tc, :user => user)
       toc[0].label.should == "Overview"
       toc[1].label.should == "References and More Information"
       toc[2].label.should == "Biodiversity Heritage Library"

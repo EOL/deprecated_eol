a07aaa85062b788164623aeb9e697d732644283c
peter <peter@78829999-583a-0410-bd01-8a9b849fd409>
2009-08-27 18:23:03 +0000

    update for migration that will fail if the AgentStatus.active state isn't in the database when the migration is run
    git-svn-id: https://repository.eol.org/eol/trunk@1087 78829999-583a-0410-bd01-8a9b849fd409

--- db/migrate/20090821195247_update_agent_status_for_content_partner.rb
+++ db/migrate/20090821195247_update_agent_status_for_content_partner.rb
@@ -4,7 +4,8 @@ class UpdateAgentStatusForContentPartner < ActiveRecord::Migration
   end
 
   def self.up
-    execute("UPDATE agents SET agent_status_id=#{AgentStatus.active.id} WHERE username<>'' AND (agent_status_id is null OR agent_status_id='')")
+    active_id=(AgentStatus.active.blank? ? '1' : AgentStatus.active.id) # ok, maybe the active state isn't in the database yet, so let's assume it will be 1
+    execute("UPDATE agents SET agent_status_id=#{active_id} WHERE username<>'' AND (agent_status_id is null OR agent_status_id='')")
     AgentStatus.create(:label=>'Inactive')
     add_column :content_partners, :admin_notes, :text, :default=>''
   end

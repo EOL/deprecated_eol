e73a1e01ded483b84ee668d6b92f06549da8240c
bsmith <bsmith@78829999-583a-0410-bd01-8a9b849fd409>
2009-08-27 22:29:06 +0000

    refactored more admin javascript functionality.
    git-svn-id: file:///data/subversion/eol/trunk@1094 78829999-583a-0410-bd01-8a9b849fd409

--- public/javascripts/admin.js
+++ public/javascripts/admin.js
@@ -11,5 +11,36 @@ EOL.Admin.Behaviors = {
       onLoading:function(request){showAjaxIndicator();}
       });
     return false;
+  },
+
+  'form#page_form input#preview:click': function(e) {
+		$('page_form').target="_blank";
+		$('page_form').action='/administrator/content_page/preview/'+this.form.readAttribute('data-page_id');
+		$('page_form').submit();
+  },
+
+  'form#page_form input#publish:click': function(e) {
+		$('page_form').target="_self";
+		$('page_form').action='/administrator/content_page/update/'+this.form.readAttribute('data-page_id');
+		$('page_form').submit();
+  },
+
+  'select#content_page_archive_id:change': function(e) {
+		content_page_archive_id=this.options[this.selectedIndex].value;
+		if (content_page_archive_id != '') {
+			new Ajax.Request('/administrator/content_page/get_archived_page', {
+				asynchronous: true,
+				evalScripts: true,
+				method: 'post',
+				onComplete: function(request){
+					hideAjaxIndicator();
+          EOL.reload_behaviors();
+				},
+				onLoading: function(request){
+					showAjaxIndicator();
+				},
+				parameters: 'content_page_archive_id=' + content_page_archive_id + '&content_page_id=' + this.form.readAttribute('data-page_id')
+			});
+		}
   }
 };

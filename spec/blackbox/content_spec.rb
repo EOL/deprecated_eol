3824b58f0d5d494a3e29f38b09004be5bc9e2f99
ashipunova <ashipunova@78829999-583a-0410-bd01-8a9b849fd409>
2009-09-11 06:45:01 +0000

    test for best images xml, ref EOLINFRASTRUCTURE-1271
    git-svn-id: https://repository.eol.org/eol/trunk@1163 78829999-583a-0410-bd01-8a9b849fd409

--- spec/blackbox/content_spec.rb
+++ spec/blackbox/content_spec.rb
@@ -3,28 +3,30 @@ require File.dirname(__FILE__) + '/../spec_helper'
 describe 'APIs' do
   Scenario.load :foundation
 
-  describe 'Taxon Concepts API' do
+  # describe 'Taxon Concepts API' 
+  # do
     # it 'a call to the URL should create .gz file' do
     #   file_dir = File.dirname(__FILE__) + "/../../public/content/tc_api.gz"
     # 
     #   RackBox.request("/content/tc_api/")
     #   File.exist?(file_dir).should eql(true)
     # end
-  end
+  # end
   
   describe 'Highest-Rated Images API' do
-    # # begin
-    # tc = build_taxon_concept(:id => 1) 
-    # # rescue
-    # #   #there's already a tc with that id
-    # # end      
-    #    
-    # it 'a call to the URL should create .gz file' do
-    #   file_dir = File.dirname(__FILE__) + "/../../public/content/hr_images_api.gz"
-    #   RackBox.request("/content/highest_rated_images_api?id=1/")
-    #   File.exist?(file_dir).should eql(true)
-    # end
-    # 
+    
+    before(:all) do
+      t1 = TaxonConcept.find(1)
+      t1.destroy if t1                            
+      @taxon_concept = build_taxon_concept(:id => 1)
+      @taxon_concept.images.each {|i| i.data_rating = 5.0}
+      @taxon_concept.images.each {|i| i.save}
+      @taxon_concept_xml = Nokogiri::XML(RackBox.request("/pages/#{@taxon_concept.id}/best_images.xml").body)
+    end
+    
+    it 'should serve XML on call to /pages/1/best_images.xml' do
+      @taxon_concept_xml.at(:dataObject).inner_html.should_not be_empty
+    end
   end
   
 end

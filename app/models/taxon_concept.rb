00a3c70c9b362153101c6b0d9df4ba3ccc4e07fc
jrice <jrice@78829999-583a-0410-bd01-8a9b849fd409>
2009-10-05 15:27:00 +0000

    Using update_attribute rather than changing the attribute and saving.  The latter appears to make Rails update multiple fields, which causes a duplicate entry error (perhaps lame on MySQL's part, but c'est la vie).
    git-svn-id: https://repository.eol.org/eol/branches/2.17@1225 78829999-583a-0410-bd01-8a9b849fd409

--- app/models/taxon_concept.rb
+++ app/models/taxon_concept.rb
@@ -77,13 +77,10 @@ class TaxonConcept < SpeciesSchemaModel
     if new_preferred_name
       unless old_preferred_names.empty?
         old_preferred_names.each do |old_preferred_name|
-          old_preferred_name.preferred = 0
-          old_preferred_name.save!
+          old_preferred_name.update_attribute(:preferred, 0)
         end
       end
-      # Important to do this AFTER the above code, lest the DB complain about duplicate keys:
-      new_preferred_name.preferred = 1
-      new_preferred_name.save!
+      old_preferred_name.update_attribute(:preferred, 1)
     else
       raise "Couldn't find a TaxonConceptName with a name_id of #{name_id}"
     end

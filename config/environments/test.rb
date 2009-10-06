85c9b3274787423a4bf7ed6df1bf9e32471480a1
cappy <cappy@78829999-583a-0410-bd01-8a9b849fd409>
2009-10-06 19:43:17 +0000

    small change to comment to test ci server
    git-svn-id: file:///data/subversion/eol/trunk@1240 78829999-583a-0410-bd01-8a9b849fd409

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -6,7 +6,7 @@
 
 
 # The test environment is used exclusively to run your application's
-# test suite.  You never need to work with it otherwise.  Remember that
+# test suite.  Otherwise, you never need to work with it.  Remember that
 # your test database is "scratch space" for the test suite and is wiped
 # and recreated between test runs.  Don't rely on the data there!
 config.cache_classes = true

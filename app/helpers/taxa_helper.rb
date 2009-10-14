b9072bc527622b18affea1d070f1a01e32793270
pleary <pleary@78829999-583a-0410-bd01-8a9b849fd409>
2009-10-14 21:28:45 +0000

    Edited wikipedia stylesheet; including wiki js and css on ALL pages now; fixed bug in agent_partial helper
    git-svn-id: file:///data/subversion/eol/trunk@1261 78829999-583a-0410-bd01-8a9b849fd409

--- app/helpers/taxa_helper.rb
+++ app/helpers/taxa_helper.rb
@@ -28,7 +28,7 @@ module TaxaHelper
     agents = [agents] unless agents.class == Array # Allows us to pass in a single agent, if needed.
     agents = [agents[0]] if params[:only_first]
     agent_list = agents.collect do |agent|
-      params[:linked] ? external_link_to(hh(agent.full_name), agent.homepage, {:show_link_icon => params[:show_link_icon]}) : hh(agent.full_name)
+      params[:linked] ? external_link_to(allow_some_html(agent.full_name), agent.homepage, {:show_link_icon => params[:show_link_icon]}) : allow_some_html(agent.full_name)
     end.join(', ') # I know this looks awkward, but I'm making it more readable.  : )
     agent_list += ', et al.' if params[:only_first] and original_agents.length > 1
     return agent_list

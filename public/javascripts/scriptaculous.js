79f6a7ce048c589e9c2db3a101e870f9d0f53da1
pleary <pleary@78829999-583a-0410-bd01-8a9b849fd409>
2009-10-16 20:45:57 +0000

    Commented out secondary hierarchy option; removed slider reference from scriptaculous.js
    git-svn-id: https://repository.eol.org/eol/trunk@1269 78829999-583a-0410-bd01-8a9b849fd409

--- public/javascripts/scriptaculous.js
+++ public/javascripts/scriptaculous.js
@@ -42,7 +42,7 @@ var Scriptaculous = {
     }).each( function(s) {
       var path = s.src.replace(/scriptaculous\.js(\?.*)?$/,'');
       var includes = s.src.match(/\?.*load=([a-z,]*)/);
-      (includes ? includes[1] : 'builder,effects,dragdrop,controls,slider').split(',').each(
+      (includes ? includes[1] : 'builder,effects,dragdrop,controls').split(',').each(
        function(include) { Scriptaculous.require(path+include+'.js') });
     });
   }

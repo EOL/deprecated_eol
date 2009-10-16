e8046f7b3e0e90bab884c1fd84dd98977e7a96f9
dima <dima@78829999-583a-0410-bd01-8a9b849fd409>
2009-10-16 21:12:30 +0000

    fixed small problems with migrations
    git-svn-id: https://repository.eol.org/eol/trunk@1270 78829999-583a-0410-bd01-8a9b849fd409

--- db/migrate/20091013171219_add_guid_to_users_data_objects_ratings.rb
+++ db/migrate/20091013171219_add_guid_to_users_data_objects_ratings.rb
@@ -1,9 +1,11 @@
 class AddGuidToUsersDataObjectsRatings < ActiveRecord::Migration
   def self.up
-    add_column :users_data_objects_ratings, :data_object_guid, :string, :limit => 32
+    execute "alter table users_data_objects_ratings add column data_object_guid varchar(32) character set ascii not null"
+    #remove_column :users_data_objects_ratings, :data_object_id
   end
 
   def self.down
+    #add_column :users_data_objects_ratings, :data_object_id, :integer
     remove_column :users_data_objects_ratings, :data_object_guid
   end
 end

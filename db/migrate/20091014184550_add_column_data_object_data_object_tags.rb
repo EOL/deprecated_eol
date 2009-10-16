bf7207f09b86fb2c255b064f014530f500690dbe
dima <dima@78829999-583a-0410-bd01-8a9b849fd409>
2009-10-16 21:12:30 +0000

    fixed small problems with migrations
    git-svn-id: https://repository.eol.org/eol/trunk@1270 78829999-583a-0410-bd01-8a9b849fd409

--- db/migrate/20091014184550_add_column_data_object_data_object_tags.rb
+++ db/migrate/20091014184550_add_column_data_object_data_object_tags.rb
@@ -2,6 +2,7 @@ class AddColumnDataObjectDataObjectTags < ActiveRecord::Migration
   def self.up
     #add_column :data_object_data_object_tags, :data_object_guid, :string, :limit => 32, :null => false, :charset => 'ascii'
     execute "alter table data_object_data_object_tags add column data_object_guid varchar(32) character set ascii not null"
+    
   end
 
   def self.down

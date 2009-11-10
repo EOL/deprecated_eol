ae90d5f4508b52e5d77edb13ea321d78ce3a45e8
dima <dima@78829999-583a-0410-bd01-8a9b849fd409>
2009-11-10 20:44:46 +0000

    intermediate
    git-svn-id: https://repository.eol.org/eol/trunk@1340 78829999-583a-0410-bd01-8a9b849fd409

--- app/models/taxon_concept.rb
+++ app/models/taxon_concept.rb
@@ -13,7 +13,10 @@
 #
 # See the comments at the top of the Taxon for more information on this.
 # I include there a basic biological definition of what a Taxon is.
+require 'eol/solr_search'
+
 class TaxonConcept < SpeciesSchemaModel
+  extend EOL::Solr::Search
 
   #TODO belongs_to :taxon_concept_content
   belongs_to :vetted
@@ -842,62 +845,6 @@ EOIUCNSQL
                                         LIMIT 1',taxon_concept_id]) > 0
   end
   
-  # You don't want to call this directly.  Nor do you want to call paginate_all_by_solr.  Neither will work.
-  #
-  # When will_paginate calls this, it MUST have a total_entries argument, otherwise it won't work. It will
-  # throw some error about solr not being a valid field, because it attempts to #count using solr as its argument.
-  #
-  # To avoid this, call #search_with_pagination ... which will do all the right things.
-  def self.find_all_by_solr(*args)
-    query   = args.first
-    options = args.last
-    per_page = options[:limit] ? options[:limit].to_i : 10
-    offset = options[:offset] ? options[:offset].to_i : 0
-    page = offset_to_page(offset, per_page)
-    return @@search_results_for["#{query}_#{page}_#{per_page}"]
-  end
-
-  def self.offset_to_page(offset, limit)
-    raise "offset and limit should be integers" unless offset.class == Fixnum and limit.class == Fixnum
-    offset / limit + 1
-  end
-
-  def self.page_to_offset(page, limit)
-    raise "page and limit should be integers" unless  page.class == Fixnum and limit.class == Fixnum
-    (page - 1) * limit
-  end
-
-  # Returns an array of result hashes, using will_paginate.  Don't use paginate_all_by_solr directly, as that will either fail
-  # or cause duplicate queries.
-  # TODO - use a class rather than a class variable for search_results_for
-  def self.search_with_pagination(query, options = {})
-    options[:page]        ||= 1
-    options[:per_page]    ||= 10
-    options[:search_type] ||= :common_name
-    clean_query = query.gsub('_', ' ') # Handles some of the "clean" URL "ids" that may get passed in.
-    obj = TaxonConcept.solr_search(clean_query, options)
-    # Store the results of the search to "fake" them coming from find_all_by_solr:
-    @@search_results_for ||= {}
-    @@search_results_for["#{clean_query}_#{options[:page]}_#{options[:per_page]}"] = obj['response']['docs']
-    TaxonConcept.paginate_all_by_solr(clean_query, options.merge(:total_entries => obj['response']['numFound']))
-  end
-  
-  # Returns the actual search results object.  Generally, you will want to use search_with_pagination instead.
-  # Restult looks like this:
-  # [{"top_image_id"=>1, "preferred_scientific_name"=>["Procyon lotor"], "published"=>[true], "scientific_name"=>["Procyon
-  # lotor"], "supercedure_id"=>[0], "vetted_id"=>[1], "taxon_concept_id"=>[14]}]
-  def self.solr_search(query, options = {})
-    url =  'http://localhost:8983/solr/select/?version=2.2&indent=on&wt=json&q='
-    url << URI.encode(%Q[{!lucene} #{options[:search_type]}:"#{query}" AND published:1 AND supercedure_id:0])
-    limit  = options[:per_page] ? options[:per_page].to_i : 10
-    page = options[:page] ? options[:page].to_i : 1
-    offset = page_to_offset(page, limit)
-    url << '&start=' << URI.encode(offset.to_s)
-    url << '&rows='  << URI.encode(limit.to_s)
-    res = open(url).read
-    return JSON.load res
-  end
-
 #####################
 private
 
---
+++ lib/eol/solr_search.rb
@@ -0,0 +1,49 @@
+module EOL
+  module Solr
+    module Search
+      # Returns an array of result hashes, using will_paginate.  Don't use paginate_all_by_solr directly, as that will either fail
+      # or cause duplicate queries.
+      # TODO - use a class rather than a class variable for search_results_for
+      def search_with_pagination(query, options = {})
+        options[:page]        ||= 1
+        options[:per_page]    ||= 10
+        options[:search_type] ||= :common_name
+        clean_query = query.gsub('_', ' ') # Handles some of the "clean" URL "ids" that may get passed in.
+        res = solr_search(clean_query, options)['results'].paginate :page => options[:page], :per_page => options[:per_page]
+        puts 'URA'
+        pp res
+        res
+      end
+      
+      private
+      
+      # Returns the actual search results object.  Generally, you will want to use search_with_pagination instead.
+      # Restult looks like this:
+      # [{"top_image_id"=>1, "preferred_scientific_name"=>["Procyon lotor"], "published"=>[true], "scientific_name"=>["Procyon
+      # lotor"], "supercedure_id"=>[0], "vetted_id"=>[1], "taxon_concept_id"=>[14]}]
+      def solr_search(query, options = {})
+        url =  $SOLR_SERVER + '/?version=2.2&indent=on&wt=json&q='
+        url << URI.encode(%Q[{!lucene} #{options[:search_type]}:"#{query}" AND published:1 AND supercedure_id:0])
+        limit  = options[:per_page] ? options[:per_page].to_i : 10
+        page = options[:page] ? options[:page].to_i : 1
+        offset = page_to_offset(page, limit)
+        url << '&start=' << URI.encode(offset.to_s)
+        url << '&rows='  << URI.encode(limit.to_s)
+        res = open(url).read
+        JSON.load res
+      end
+      
+      def offset_to_page(offset, limit)
+        raise "offset and limit should be integers" unless offset.class == Fixnum and limit.class == Fixnum
+        offset / limit + 1
+      end
+
+      def page_to_offset(page, limit)
+        raise "page and limit should be integers" unless  page.class == Fixnum and limit.class == Fixnum
+        (page - 1) * limit
+      end
+
+
+    end
+  end
+end

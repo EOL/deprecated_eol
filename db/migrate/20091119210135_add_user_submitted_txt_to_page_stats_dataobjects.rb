20a0e5ce93e81b18a999b767fa5cee1955cd1f4e
eagbayani <eagbayani@78829999-583a-0410-bd01-8a9b849fd409>
2009-11-19 21:18:15 +0000

    git-svn-id: https://repository.eol.org/eol/trunk@1378 78829999-583a-0410-bd01-8a9b849fd409

---
+++ db/migrate/20091119210135_add_user_submitted_txt_to_page_stats_dataobjects.rb
@@ -0,0 +1,9 @@
+class AddUserSubmittedTxtToPageStatsDataobjects < ActiveRecord::Migration
+  def self.up
+    add_column  :page_stats_dataobjects, :user_submitted_text, :integer
+  end
+
+  def self.down
+    remove_column   :page_stats_dataobjects, :user_submitted_text
+  end
+end

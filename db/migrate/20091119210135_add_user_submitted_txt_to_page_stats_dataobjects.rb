45cbc525e12cf60a8ca244fb43e9ee30a74f2e48
eagbayani <eagbayani@78829999-583a-0410-bd01-8a9b849fd409>
2009-11-19 21:19:58 +0000

    migration to add user_submitted_text field (integer) into page_stats_dataobjects table
    git-svn-id: https://repository.eol.org/eol/trunk@1379 78829999-583a-0410-bd01-8a9b849fd409

--- db/migrate/20091119210135_add_user_submitted_txt_to_page_stats_dataobjects.rb
+++ db/migrate/20091119210135_add_user_submitted_txt_to_page_stats_dataobjects.rb
@@ -2,7 +2,7 @@ class AddUserSubmittedTxtToPageStatsDataobjects < ActiveRecord::Migration
   def self.up
     add_column  :page_stats_dataobjects, :user_submitted_text, :integer
   end
-
+  
   def self.down
     remove_column   :page_stats_dataobjects, :user_submitted_text
   end

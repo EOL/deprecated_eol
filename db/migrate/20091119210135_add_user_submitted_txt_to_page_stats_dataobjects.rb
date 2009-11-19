9eef1e8454f29e55288b05084dfb0aff0c13fa1b
eagbayani <eagbayani@78829999-583a-0410-bd01-8a9b849fd409>
2009-11-19 21:46:20 +0000

    added the database_model
    git-svn-id: file:///data/subversion/eol/trunk@1380 78829999-583a-0410-bd01-8a9b849fd409

--- db/migrate/20091119210135_add_user_submitted_txt_to_page_stats_dataobjects.rb
+++ db/migrate/20091119210135_add_user_submitted_txt_to_page_stats_dataobjects.rb
@@ -1,4 +1,9 @@
 class AddUserSubmittedTxtToPageStatsDataobjects < ActiveRecord::Migration
+
+  def self.database_model
+    return "SpeciesSchemaModel"    
+  end
+  
   def self.up
     add_column  :page_stats_dataobjects, :user_submitted_text, :integer
   end

0cda26647b1f12fbeb00df6fb614c180a54d76e0
dima <dima@78829999-583a-0410-bd01-8a9b849fd409>
2009-11-19 15:27:15 +0000

    made one spec test for feeds
    git-svn-id: https://repository.eol.org/eol/trunk@1376 78829999-583a-0410-bd01-8a9b849fd409

--- spec/blackbox/feeds_spec.rb
+++ spec/blackbox/feeds_spec.rb
@@ -1,14 +1,28 @@
 require File.dirname(__FILE__) + '/../spec_helper'
 
 describe 'Curator Feeds' do
-  it 'should verify that comments feed for a species with no childen in tree only has comment for that species'
-  it 'should verify that images feed for a species with no childen in tree only has images for that species'
-  it 'should verify that text feed for a species with no childen in tree only has text for that species'
+  before(:all) do
+    Scenario.load('foundation')
+  end
 
-  it 'should verify that comments feed for a species with childen in the tree has comments for itself and all its children'
-  it 'should verify that images feed for a species with childen in the tree has images for itself and all its children'
-  it 'should verify that text feed for a species with childen in the tree has text for itself and all its children'
+#  it 'should verify that comments feed for a species with no childen in tree only has comment for that species'
+#  it 'should verify that images feed for a species with no childen in tree only has images for that species'
+#  it 'should verify that text feed for a species with no childen in tree only has text for that species'
+#
+#  it 'should verify that comments feed for a species with childen in the tree has comments for itself and all its children'
+#  it 'should verify that images feed for a species with childen in the tree has images for itself and all its children'
+#  it 'should verify that text feed for a species with childen in the tree has text for itself and all its children'
+#
+#  it 'should verify that comments feed has comments for old and new version of re-harvested data'
+  it 'should verify that all feed contains text, images, and comments' do
+    tc = build_taxon_concept
+    result = RackBox.request("/feeds/all/#{tc.id}")
+    result.body.downcase.should include('new text')
+    result.body.downcase.should include('new image')
 
-  it 'should verify that all feed contains text, images, and comments'
-  it 'should verify that comments feed has comments for old and new version of re-harvested data'
+    #result = RackBox.request("/feeds/all/")
+    #puts result.body
+    #result.body.downcase.should include('new comment')
+  end
+
 end

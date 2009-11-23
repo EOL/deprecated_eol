5a58073964f94b59461d16b096fd442e406ea74f
dima <dima@78829999-583a-0410-bd01-8a9b849fd409>
2009-11-23 16:53:04 +0000

    solr_api tests work
    git-svn-id: file:///data/subversion/eol/trunk@1389 78829999-583a-0410-bd01-8a9b849fd409

--- spec/lib/solr_api_spec.rb
+++ spec/lib/solr_api_spec.rb
@@ -16,10 +16,11 @@ describe SolrAPI do
     @solr.get_results("*:*")['numFound'].should == 0
   end
 
-  it 'should create solr doc out of ruby hash' do
+  it 'should convert ruby array or hash to solr-compatible xml' do
     data = {:common_name => ['Atlantic cod', 'Pacific cod'], :preferred_scientific_name => ['Gadus mohua'], :taxon_concept_id => ['1'], :top_image => 'http:/example.com/img1.jpg'}
     res = @solr.build_solr_xml('add', data)
-    res.should == ''
+    res.should == "<?xml version=\"1.0\"?>\n<add>\n  <doc>\n    <field name=\"top_image\">http:/example.com/img1.jpg</field>\n    <field name=\"common_name\">Atlantic cod</field>\n    <field name=\"common_name\">Pacific cod</field>\n    <field name=\"preferred_scientific_name\">Gadus mohua</field>\n    <field name=\"taxon_concept_id\">1</field>\n  </doc>\n</add>\n"
+
   end
 
 end

7cf34a37f33d7934208d2d42dd901cac44ae0808
jrice <jrice@78829999-583a-0410-bd01-8a9b849fd409>
2009-11-24 16:14:43 +0000

    Rake task (solr:build) to read the existing TaxonConcepts and re-build the Solr indexes based on the actual information from those TCs.  Satisfies EOLINFRASTRUCTURE-1401
    git-svn-id: file:///data/subversion/eol/trunk@1394 78829999-583a-0410-bd01-8a9b849fd409

--- spec/lib/solr_api_spec.rb
+++ spec/lib/solr_api_spec.rb
@@ -1,8 +1,22 @@
 require File.dirname(__FILE__) + '/../spec_helper'
 
+def test_xml(xml, node, data)
+  result = xml.xpath("/add/doc/field[@name='#{node}']").map {|n| n.content }
+  result.sort.should == data.sort
+end
+
 describe SolrAPI do
   before(:all) do
+    Vetted.gen(:label => 'Trusted') unless Vetted.trusted
+    trusted = Vetted.trusted
     @solr = SolrAPI.new
+    @data = {:common_name => ['Atlantic cod', 'Pacific cod'],
+             :preferred_scientific_name => ['Gadus mohua'],
+             :taxon_concept_id => ['1'],
+             :top_image_id => '123',
+             :supercedure_id => 0, # This is not *required*, but the search specifies = 0, soooooo....
+             :vetted_id => trusted.id,
+             :published => 1}
   end
 
   it 'should connect to solr server from environment' do
@@ -17,10 +31,17 @@ describe SolrAPI do
   end
 
   it 'should convert ruby array or hash to solr-compatible xml' do
-    data = {:common_name => ['Atlantic cod', 'Pacific cod'], :preferred_scientific_name => ['Gadus mohua'], :taxon_concept_id => ['1'], :top_image => 'http:/example.com/img1.jpg'}
-    res = @solr.build_solr_xml('add', data)
-    res.should == "<?xml version=\"1.0\"?>\n<add>\n  <doc>\n    <field name=\"top_image\">http:/example.com/img1.jpg</field>\n    <field name=\"common_name\">Atlantic cod</field>\n    <field name=\"common_name\">Pacific cod</field>\n    <field name=\"preferred_scientific_name\">Gadus mohua</field>\n    <field name=\"taxon_concept_id\">1</field>\n  </doc>\n</add>\n"
+    res = @solr.build_solr_xml('add', @data)
+    xml = Nokogiri::XML(res)  
+    xml.xpath('/add/doc').should_not be_empty
+    [:top_image_id, :preferred_scientific_name, :taxon_concept_id, :common_name].each do |node|
+      test_xml(xml, node, @data[node])
+    end
+  end
 
+  it 'should add an index for a document on a call to #create' do
+    res = @solr.create(@data)
+    @solr.get_results("common_name:cod")['numFound'].should > 0
   end
 
 end

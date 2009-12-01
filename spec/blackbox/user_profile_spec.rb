518b5a56bd8d0fa7b7a9e919d422850a292aa70b
jrice <jrice@78829999-583a-0410-bd01-8a9b849fd409>
2009-12-01 17:13:15 +0000

    Fixed a tiny bug with comments and uncommented (and fixed) a user spec.  Also removed a superflous login call in another spec.
    git-svn-id: https://repository.eol.org/eol/trunk@1409 78829999-583a-0410-bd01-8a9b849fd409

--- spec/blackbox/user_profile_spec.rb
+++ spec/blackbox/user_profile_spec.rb
@@ -4,32 +4,25 @@ def create_user username, password
   User.gen :username => username, :password => password
 end
 
-# NOTE !!!!!!!
-#
-# I crippled this file, temporarily, in order to commit fixes elsewhere in the suite.
-
 describe 'User Profile' do
 
-  #Scenario.load :foundation
+  Scenario.load :foundation
 
   before(:all) do
-    #@username = 'bobbafett'
-    #@password = 'ihatesolo'
-    #@user     = create_user(@username, @password)
-    #@settings_body = request('/settings').body
-    #login_as @user
-    #pp request('/account/authenticate',
-            #:params => {'user[username]' => @username,
-                        #'user[password]' => @password })
-    #@profile_body = request('/profile').body # first try is a redirect (why?)
-    #@profile_body = request('/profile').body
+    @username = 'userprofilespec'
+    @password = 'beforeall'
+    @user     = create_user(@username, @password)
+    
+    @settings_body = RackBox.request('/settings').body
+    login_as @user
+    @profile_body = RackBox.request('/settings').body # Aaaaactually, this will probably fail 'cause it needs https
   end
 
-  #it 'should allow change of filter content hierarchy' do
-    #@settings_body.should include('Filter images')
-    #@settings_body.should have_tag('input#user_filter_content_by_hierarchy')
-    #@profile_body.should include('Filter images')
-    #@profile_body.should have_tag('input#user_filter_content_by_hierarchy')
-  #end
+  it 'should allow change of filter content hierarchy' do
+    @settings_body.should include('Filter EOL')
+    @settings_body.should have_tag('input#user_filter_content_by_hierarchy')
+    @profile_body.should include('Filter EOL')
+    @profile_body.should have_tag('input#user_filter_content_by_hierarchy')
+  end
 
 end

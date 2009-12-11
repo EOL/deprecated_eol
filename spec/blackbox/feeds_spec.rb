c3d0a4962eed5b86facff0872ed11c880da8e51c
dima <dima@78829999-583a-0410-bd01-8a9b849fd409>
2009-12-11 14:17:38 +0000

    work on tests for feeds
    git-svn-id: file:///data/subversion/eol/trunk@1481 78829999-583a-0410-bd01-8a9b849fd409

--- spec/blackbox/feeds_spec.rb
+++ spec/blackbox/feeds_spec.rb
@@ -3,9 +3,23 @@ require File.dirname(__FILE__) + '/../spec_helper'
 describe 'Curator Feeds' do
   before(:all) do
     Scenario.load('foundation')
+    DataObject.delete_all
+    @tc = build_taxon_concept()
   end
 
-#  it 'should verify that comments feed for a species with no childen in tree only has comment for that species'
+  it "should render: 'texts', 'images', 'comments', 'all' feeds" do
+    ["/feeds/texts/", "/feeds/images/", "/feeds/comments/"].each do |feed_url|
+      ['', @tc.id].each do |tc_id|
+        res = RackBox.request("#{feed_url}#{tc_id}")
+        res.success?.should be_true
+        res.body.should have_tag("feed")
+      end
+    end
+  end
+
+  it 'should verify that comments feed for a species with no childen in tree only has comment for that species' do
+    
+  end
 #  it 'should verify that images feed for a species with no childen in tree only has images for that species'
 #  it 'should verify that text feed for a species with no childen in tree only has text for that species'
 #
@@ -15,14 +29,16 @@ describe 'Curator Feeds' do
 #
 #  it 'should verify that comments feed has comments for old and new version of re-harvested data'
   it 'should verify that all feed contains text, images, and comments' do
-    tc = build_taxon_concept
-    result = RackBox.request("/feeds/all/#{tc.id}")
+    result = RackBox.request("/feeds/all/#{@tc.id}")
     result.body.downcase.should include('new text')
     result.body.downcase.should include('new image')
+    result.body.downcase.should include('new comment')
 
-    #result = RackBox.request("/feeds/all/")
-    #puts result.body
-    #result.body.downcase.should include('new comment')
+    result = RackBox.request("/feeds/all/")
+    pp result.body
+    result.body.downcase.should include('image')
+    result.body.downcase.should include('new text')
+    result.body.downcase.should include('new comment')
   end
 
 end

d91cab4ef3cf4d280d97642ec7217b4ee52b025c
jrice <jrice@78829999-583a-0410-bd01-8a9b849fd409>
2009-12-17 13:55:10 +0000

    Merge with branch 2009.11.30 ... adds lots of Solr search changes and some reset password stuff, among other changes
    git-svn-id: https://repository.eol.org/eol/trunk@1520 78829999-583a-0410-bd01-8a9b849fd409

--- lib/core_extensions.rb
+++ lib/core_extensions.rb
@@ -2,6 +2,17 @@
 # (ActiveRecord and what-not).
 class String
 
+  # Normalize a string for better matching, e.g. for searches
+  def normalize
+    @@normalization_regex ||= /[;:,\.\(\)\[\]\!\?\*_\\\/\"\']/
+    @@spaces_regex        ||= /\s+/
+    @@tag_regex           ||= /<[^>]*>/
+    name = self.clone
+    return name.downcase.gsub(@@normalization_regex, '').gsub(@@tag_regex, '').gsub(@@spaces_regex, ' ')
+    return name.downcase.gsub(@@normalization_regex, '').gsub(@@spaces_regex, ' ')
+  end
+
+
   # Note that I change strong to b, 'cause strong appears to be overridden in our CSS.  Hrmph.
   def allow_some_html
     # inline RegEx passed to gsub calls leak memory! We declare them first:
@@ -84,9 +95,28 @@ class String
 
 end
 
-# I need this to sanitize SQL into strings:
-class << ActiveRecord::Base
-  public :sanitize_sql
+class Array
+
+  # some methods on Hash return an Array like [ [key,value], [key,value] ] instead 
+  # of returning a Hash.  this turns an Array of that style back into a Hash.
+  def hashify
+    inject({}) do |all,this|
+      all[this.first] = this.last
+      all 
+    end
+  end
+
+end
+
+class Hash
+
+  # does the same as Array#hashify.
+  #
+  # assumes an Array like [ [key,value], [key,value] ]
+  def self.from_array array
+    array.hashify
+  end
+
 end
 
 class ActiveRecord::Migration
@@ -97,3 +127,39 @@ class ActiveRecord::Migration
   end
 end
 
+
+# I need this to sanitize SQL into strings:
+class << ActiveRecord::Base
+  public :sanitize_sql
+end
+
+module ActiveRecord
+  class Base
+    class << self
+
+      # returns the full table name of this ActiveRecord::Base, 
+      # including the database name.
+      #
+      #   >> User.full_table_name
+      #   => "eol_development.users"
+      #
+      def full_table_name
+        database_name + '.' + table_name
+      end
+
+      # returns a hash of configuration variables for this ActiveRecord::Base's connection adapter
+      def database_config
+        # in production, we have a ConnectionProxy with many adapters 
+        # otherwise #connection directly returns the adapter
+        adapter = self.connection.instance_eval { @current } || self.connection
+        adapter.instance_eval { @config }
+      end
+      
+      # returns the name of the database for this ActiveRecord::Base
+      def database_name
+        database_config[:database]
+      end
+
+    end
+  end
+end
--- spec/blackbox/feeds_spec.rb
+++ spec/blackbox/feeds_spec.rb
@@ -4,23 +4,44 @@ describe 'Curator Feeds' do
   before(:all) do
     Scenario.load('foundation')
     DataObject.delete_all
+    Comment.delete_all
     @tc = build_taxon_concept()
+    @feeds = ["/feeds/texts/", "/feeds/images/", "/feeds/comments/", "/feeds/all/"]
   end
   after(:all) do
     truncate_all_tables
   end
 
   it "should render: 'texts', 'images', 'comments', 'all' feeds" do
-    ["/feeds/texts/", "/feeds/images/", "/feeds/comments/"].each do |feed_url|
+    @feeds.each do |feed_url|
       ['', @tc.id].each do |tc_id|
-        res = RackBox.request("#{feed_url}#{tc_id}")
+        res = request("#{feed_url}#{tc_id}")
         res.success?.should be_true
         res.body.should have_tag("feed")
       end
     end
   end
 
-  it 'should verify that comments feed for a species with no childen in tree only has comment for that species'
+  it "should show information in a time descentind order" do
+    @feeds.each do |feed_url|
+      res = request(feed_url)
+      dates = Nokogiri.XML(res.body).xpath("//xmlns:updated").map {|i| Time.parse(i.text)}
+      dates.should == dates.sort.reverse
+    end
+  end
+
+  it "should should only information about a specific species if species are selected" do
+    #Brittle test, may be is should be removed...
+    # Wish we had a better way to see if all info is about the same species. 
+    # Picking species name from the title and mangle it to get a canonical name
+    
+    res = request("/feeds/all/#{@tc.id}")
+    titles =  Nokogiri.XML(res.body).xpath("//xmlns:title").map {|i| i.text.match /new\s+(image|text|comment|)\s+for\s+(.*)\s*$/i}
+    titles = titles.select {|i| i}.map {|i| i[2].strip.split(/\s+/)[0..1].join(" ")}
+    titles.uniq.size.should == 1
+  end
+
+#  it 'should verify that comments feed for a species with no childen in tree only has comment for that species'
 
 #  it 'should verify that images feed for a species with no childen in tree only has images for that species'
 #  it 'should verify that text feed for a species with no childen in tree only has text for that species'
@@ -30,14 +51,15 @@ describe 'Curator Feeds' do
 #  it 'should verify that text feed for a species with childen in the tree has text for itself and all its children'
 #
 #  it 'should verify that comments feed has comments for old and new version of re-harvested data'
+
   it 'should verify that all feed contains text, images, and comments' do
-    result = RackBox.request("/feeds/all/#{@tc.id}")
+    result = request("/feeds/all/#{@tc.id}")
     result.body.downcase.should include('new text')
     result.body.downcase.should include('new image')
     result.body.downcase.should include('new comment')
 
-    result = RackBox.request("/feeds/all/")
-    result.body.downcase.should include('image')
+    result = request("/feeds/all/")
+    #result.body.downcase.should include('new image')  don't get any images in first 100 results 
     result.body.downcase.should include('new text')
     result.body.downcase.should include('new comment')
   end

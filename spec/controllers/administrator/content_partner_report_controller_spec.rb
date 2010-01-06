7c33dba45bbdf83f92006c1a42de6ac77f2e299a
dima <dima@78829999-583a-0410-bd01-8a9b849fd409>
2010-01-06 23:23:48 +0000

    mail google analytics reports to content partners, work in progress, tests should pass
    git-svn-id: https://repository.eol.org/eol/trunk@1561 78829999-583a-0410-bd01-8a9b849fd409

---
+++ spec/controllers/administrator/content_partner_report_controller_spec.rb
@@ -0,0 +1,22 @@
+require File.dirname(__FILE__) + '/../../spec_helper'
+
+describe Administrator::ContentPartnerReportController do
+  before(:all) do
+    Scenario.load :foundation
+  end
+
+  it "should send monthly report email" do
+    Agent.should_receive(:content_partners_contact_info).and_return([{:username => "johndoe", :email => "johndoe@example.com", :full_name => "John Doe"}])
+    # expect
+    Notifier.should_receive(:deliver_monthly_stats).with({:username => "johndoe", :email => "johndoe@example.com", :full_name => "John Doe"})
+    # when
+
+    admin = User.gen(:username => "admin", :password => "admin")
+    admin.roles = Role.find(:all, :conditions => 'title LIKE "Administrator%"')
+    admin.save!
+    session[:user] = admin
+    session[:user_id] = admin.id
+
+    get :monthly_stats_email
+  end
+end

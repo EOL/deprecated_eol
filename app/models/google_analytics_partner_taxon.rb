d1ff2663c0556b9bd9bfc38663b754a2950dc75b
jrice <jrice@78829999-583a-0410-bd01-8a9b849fd409>
2010-01-25 21:18:35 +0000

    Attempting to UNDO the stuff that I'd done before to merge from branch, will try again
    git-svn-id: file:///data/subversion/eol/trunk@1625 78829999-583a-0410-bd01-8a9b849fd409

--- app/models/google_analytics_partner_taxon.rb
+++ app/models/google_analytics_partner_taxon.rb
@@ -2,3 +2,7 @@ class GoogleAnalyticsPartnerTaxon < SpeciesSchemaModel
     belongs_to :agent
     belongs_to :taxon_concept
 end
+class GoogleAnalyticsPartnerTaxon < SpeciesSchemaModel
+    belongs_to :agent
+    belongs_to :taxon_concept
+end

d9b912e7e53f2226b6c446fe2d107bc076b3ee39
dima <dima@78829999-583a-0410-bd01-8a9b849fd409>
2010-01-26 18:32:47 +0000

    Hopfeully a fix for: Validation Error when trying to change user preferences REF:   EOLINFRASTRUCTURE-1539 I could not reproduce this problem, but sounds like it is something depending on cache in a browser. So I added a javascript that empties these lines automatically
    git-svn-id: file:///data/subversion/eol/trunk@1649 78829999-583a-0410-bd01-8a9b849fd409

--- public/javascripts/users.js
+++ public/javascripts/users.js
@@ -1,31 +1,37 @@
+//I use jquery according to our new policy to use it for every new js
+$j(function() {
+  $j("#user_entered_password").attr("value","");
+  $j("#user_entered_password_confirmation").attr("value","");
+});
+
 // ajax call to determine if username is unique
 function check_username() {
-	new Ajax.Request('/account/check_username/', {
+	new ajax.request('/account/check_username/', {
 	 		parameters: { username: $('user_username').value }, 		
 			asynchronous:true,
-			evalScripts:true});		
+			evalscripts:true});		
 }
 
 // ajax call to determine if email is unique
 function check_email() {
-	new Ajax.Request('/account/check_email/', {
+	new ajax.request('/account/check_email/', {
 	 		parameters: { email: $('user_email').value }, 		
 			asynchronous:true,
-			evalScripts:true});		
+			evalscripts:true});		
 }
 
 // instant feedback to user about password matching
 function check_passwords() {
 	if ($('user_entered_password').value != $('user_entered_password_confirmation').value)
 	{
-		$('password_warn').innerHTML='Passwords must match';
+		$('password_warn').innerhtml='passwords must match';
 	}
 	else
 	{
-		$('password_warn').innerHTML='';
+		$('password_warn').innerhtml='';
 	}
 }
 
 function reset_curator_panel() {
-	EOL.Effect.appear("curator_request_options");
+	eol.effect.appear("curator_request_options");
 }

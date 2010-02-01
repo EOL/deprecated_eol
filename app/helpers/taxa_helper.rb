84a36208b4ce8844f619b109943023bdf059ccc3
pleary <pleary@78829999-583a-0410-bd01-8a9b849fd409>
2010-02-01 18:50:02 +0000

    Now only showing one version of common name to curator; fixed Google Analytics spec
    git-svn-id: https://repository.eol.org/eol/branches/2010.01.15@1684 78829999-583a-0410-bd01-8a9b849fd409

--- app/helpers/taxa_helper.rb
+++ app/helpers/taxa_helper.rb
@@ -239,14 +239,37 @@ private
     eng = Language.english
     pref = Language.find(preferred_language_id)
     unknown = Language.unknown
+    curator_hierarchy = Hierarchy.eol_contributors
+    names_in_curator_hierarchy = {}
     names.each do |name|
+      k = name[:language_label]
+      next if k.nil?
       language = {:id => name[:language_id], :label => name[:language_label], :name => name[:language_name]}
       names = {:agent_id => name[:agent_id].to_i, :id => name[:name_id].to_i, 
                :string => name[:name_string], :synonym_id => name[:synonym_id].to_i, 
-               :preferred => name[:preferred].to_i > 0} 
-      k = name[:language_label]
+               :hierarchy_id => name[:hierarchy_id], :preferred => name[:preferred].to_i > 0} 
+      
+      # make note if the name was submitted by a curator
+      if !name[:hierarchy_id].nil? && name[:hierarchy_id].to_i == curator_hierarchy.id
+        langauge_id = name[:language_id].to_i
+        names_in_curator_hierarchy[langauge_id] ||= {}
+        names_in_curator_hierarchy[langauge_id][name[:name_id].to_i] = true
+      end
       res.key?(k) ? res[k][:names] << names : res[k] = {:language => language, :names => [names]}
     end
+    
+    # loop through languages and delete the name if its also in the Curator hierarchy
+    res.each do |key, value|
+      language_id = value[:language][:id].to_i
+      value[:names].delete_if do |n|
+        # was it submitted by a curator?
+        is_curator_name = !n[:hierarchy_id].nil? && n[:hierarchy_id].to_i == curator_hierarchy.id
+        # does the curator hierarchy have an entry for this?
+        curator_has_name = !names_in_curator_hierarchy[language_id].nil? && names_in_curator_hierarchy[language_id][n[:id].to_i] == true
+        !is_curator_name && curator_has_name
+      end
+    end
+    
     names_by_lang = []
     names_by_lang << [pref.label, res.delete(pref.label)] if res.key?(pref.label)
     names_by_lang << [eng.label, res.delete(eng.label)] if res.key?(eng.label)

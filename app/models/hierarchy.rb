21b73623d51e1c7bf4de5bf8321022b412c8f18d
jrice <jrice@78829999-583a-0410-bd01-8a9b849fd409>
2010-02-04 14:59:16 +0000

    Moved the default hierarchy name to the config file.
    git-svn-id: file:///data/subversion/eol/trunk@1694 78829999-583a-0410-bd01-8a9b849fd409

--- app/models/hierarchy.rb
+++ app/models/hierarchy.rb
@@ -25,7 +25,7 @@ class Hierarchy < SpeciesSchemaModel
 
   def self.default
     YAML.load(Rails.cache.fetch('hierarchies/default') do
-      Hierarchy.find_by_label("Species 2000 & ITIS Catalogue of Life: Annual Checklist 2009").to_yaml
+      Hierarchy.find_by_label($DEFAULT_HIERARCHY_NAME).to_yaml
     end)
   end
 

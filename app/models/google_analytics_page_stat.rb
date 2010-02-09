43b5f3c0c154b7d0689632e720b92ad0dc495f3f
eagbayani <eagbayani@78829999-583a-0410-bd01-8a9b849fd409>
2010-02-09 05:04:22 +0000

    fixed model, report format
    git-svn-id: https://repository.eol.org/eol/trunk@1702 78829999-583a-0410-bd01-8a9b849fd409

--- app/models/google_analytics_page_stat.rb
+++ app/models/google_analytics_page_stat.rb
@@ -4,22 +4,29 @@ class GoogleAnalyticsPageStat < SpeciesSchemaModel
   
   
   def self.page_summary(agent_id, year, month, page)
-    query="Select
-      taxa.scientific_name,
-      google_analytics_page_stats.taxon_concept_id,
-      Sum(google_analytics_page_stats.page_views) page_views,
-      Sum(google_analytics_page_stats.unique_page_views) unique_page_views,
-      Sum(time_to_sec(google_analytics_page_stats.time_on_page)) `timeonpage`
-      From google_analytics_page_stats
-      Inner Join google_analytics_partner_taxa ON google_analytics_partner_taxa.taxon_concept_id = google_analytics_page_stats.taxon_concept_id AND google_analytics_partner_taxa.`year` = google_analytics_page_stats.`year` AND google_analytics_partner_taxa.`month` = google_analytics_page_stats.`month`
-      Inner Join taxon_concept_names ON google_analytics_page_stats.taxon_concept_id = taxon_concept_names.taxon_concept_id
-      Inner Join taxa ON taxon_concept_names.name_id = taxa.name_id
+    query="
+Select names.`string` scientific_name,
+google_analytics_page_stats.taxon_concept_id,
+google_analytics_page_stats.page_views,
+google_analytics_page_stats.unique_page_views,
+time_to_sec(google_analytics_page_stats.time_on_page) `timeonpage`
+From
+google_analytics_page_stats
+Join google_analytics_partner_taxa ON google_analytics_page_stats.taxon_concept_id = google_analytics_partner_taxa.taxon_concept_id
+Join taxon_concepts ON taxon_concepts.id = google_analytics_page_stats.taxon_concept_id
+Join taxon_concept_names ON taxon_concepts.id = taxon_concept_names.taxon_concept_id
+Join names ON taxon_concept_names.name_id = names.id
+
       Where google_analytics_partner_taxa.agent_id = ? AND
       google_analytics_partner_taxa.`year` = ? AND
       google_analytics_partner_taxa.`month` = ?
-      Group By google_analytics_page_stats.taxon_concept_id
-      Order By google_analytics_page_stats.page_views Desc"
-    self.paginate_by_sql [query, agent_id, year, month], :page => page, :per_page => 100, :order => 'page_views'
+
+
+group by google_analytics_page_stats.taxon_concept_id
+order by google_analytics_page_stats.page_views desc"
+      
+      
+    self.paginate_by_sql [query, agent_id, year, month], :page => page, :per_page => 50 , :order => 'page_views'
     
     
     

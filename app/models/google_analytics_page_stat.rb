28877d3365f759546d8438f5c89b4167d4037f55
eagbayani <eagbayani@78829999-583a-0410-bd01-8a9b849fd409>
2010-02-11 20:08:49 +0000

    fixed summaries in report
    git-svn-id: https://repository.eol.org/eol/trunk@1705 78829999-583a-0410-bd01-8a9b849fd409

--- app/models/google_analytics_page_stat.rb
+++ app/models/google_analytics_page_stat.rb
@@ -3,33 +3,26 @@ class GoogleAnalyticsPageStat < SpeciesSchemaModel
   set_primary_keys :taxon_concept_id, :year, :month, :page_views
   
   
-  def self.page_summary(agent_id, year, month, page)
-    query="
-Select names.`string` scientific_name,
-google_analytics_page_stats.taxon_concept_id,
-google_analytics_page_stats.page_views,
-google_analytics_page_stats.unique_page_views,
-time_to_sec(google_analytics_page_stats.time_on_page) `timeonpage`
-From
-google_analytics_page_stats
-Join google_analytics_partner_taxa ON google_analytics_page_stats.taxon_concept_id = google_analytics_partner_taxa.taxon_concept_id
-Join taxon_concepts ON taxon_concepts.id = google_analytics_page_stats.taxon_concept_id
-Join taxon_concept_names ON taxon_concepts.id = taxon_concept_names.taxon_concept_id
-Join names ON taxon_concept_names.name_id = names.id
-
-      Where google_analytics_partner_taxa.agent_id = ? AND
-      google_analytics_partner_taxa.`year` = ? AND
-      google_analytics_partner_taxa.`month` = ?
-
-
-group by google_analytics_page_stats.taxon_concept_id
-order by google_analytics_page_stats.page_views desc"
-      
-      
+  def self.page_summary(agent_id, year, month, page)      
+    query=" 
+    Select '' as scientific_name,
+    google_analytics_partner_taxa.taxon_concept_id,
+    Sum(google_analytics_page_stats.page_views) page_views,
+    Sum(google_analytics_page_stats.unique_page_views) unique_page_views,
+    sum(time_to_sec(google_analytics_page_stats.time_on_page)) `timeonpage`
+    From
+    google_analytics_page_stats
+    Inner Join google_analytics_partner_taxa ON google_analytics_partner_taxa.taxon_concept_id = google_analytics_page_stats.taxon_concept_id AND google_analytics_partner_taxa.`year` = google_analytics_page_stats.`year` AND google_analytics_partner_taxa.`month` = google_analytics_page_stats.`month`
+    Where google_analytics_partner_taxa.agent_id = ? AND
+    google_analytics_partner_taxa.`year` = ? AND
+    google_analytics_partner_taxa.`month` = ?
+    Group By
+    google_analytics_partner_taxa.taxon_concept_id,
+    google_analytics_partner_taxa.agent_id,
+    google_analytics_partner_taxa.`year`,
+    google_analytics_partner_taxa.`month`
+    order by page_views desc "      
     self.paginate_by_sql [query, agent_id, year, month], :page => page, :per_page => 50 , :order => 'page_views'
-    
-    
-    
     
    end
 end

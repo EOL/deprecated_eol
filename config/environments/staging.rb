1afa8e40bcd3e9e615fb8aba7d5f35dfe65050bd
pleary <pleary@78829999-583a-0410-bd01-8a9b849fd409>
2010-03-03 17:21:36 +0000

    reverted a staging.rb environment file I accidentally committed
    git-svn-id: https://repository.eol.org/eol/trunk@1743 78829999-583a-0410-bd01-8a9b849fd409

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -16,12 +16,9 @@
 # config.logger = SyslogLogger.new
 
 # Full error reports are disabled and caching is turned on
-config.action_controller.consider_all_requests_local = true
-config.action_controller.perform_caching             = false
+config.action_controller.consider_all_requests_local = false
+config.action_controller.perform_caching             = true
 config.action_view.debug_rjs                         = false
-
-config.log_level = :debug # :error
-
 
 # Disable delivery errors, bad email addresses will be ignored
 config.action_mailer.raise_delivery_errors = false
@@ -32,11 +29,11 @@ config.cache_classes = true
 # Set up the master database connection for writes using masochism plugin
 # NOTE: for this to work, you *must* also use config.cache_classes = true
 # (default for production)
-# config.after_initialize do 
-#   ActiveReload::ConnectionProxy.setup_for ActiveReload::MasterDatabase, ActiveRecord::Base
-#   ActiveReload::ConnectionProxy.setup_for SpeciesSchemaWriter, SpeciesSchemaModel
-#   ActiveReload::ConnectionProxy.setup_for LoggingWriter, LoggingModel
-# end
+config.after_initialize do 
+  ActiveReload::ConnectionProxy.setup_for ActiveReload::MasterDatabase, ActiveRecord::Base
+  ActiveReload::ConnectionProxy.setup_for SpeciesSchemaWriter, SpeciesSchemaModel
+  ActiveReload::ConnectionProxy.setup_for LoggingWriter, LoggingModel
+end
 
 # set to true to force users to use SSL for the login and signup pages 
 $USE_SSL_FOR_LOGIN = true  

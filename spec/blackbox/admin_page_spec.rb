6e7d2726855c544811427606d85649eec37f7489
jrice <jrice@78829999-583a-0410-bd01-8a9b849fd409>
2010-03-09 19:22:36 +0000

    Resolving that same ticket, merged with existing code... had a LOT of trouble with this, due to not following the git workflow.  Ick.
    git-svn-id: https://repository.eol.org/eol/trunk@1747 78829999-583a-0410-bd01-8a9b849fd409

--- spec/blackbox/admin_page_spec.rb
+++ spec/blackbox/admin_page_spec.rb
@@ -1,63 +1,65 @@
 require File.dirname(__FILE__) + '/../spec_helper'
 
 describe 'Content Partner Pages' do
-  
-  Scenario.load :foundation
-  
-  describe '(Reports)' do
-    
-    #doesn't work for now, see EOLINFRASTRUCTURE-1061
-    
-     # it 'should check if we come to /administrator/reports' do
-     #        pass  = 'timey-wimey'
-     #        user = User.gen(:hashed_password => Digest::MD5.hexdigest(pass))
-     #        user.roles = Role.find(:all, :conditions => 'title LIKE "Admin%"')
-     #        login_as(:username => user.username, :password => pass)
-     #        body  = request('/administrator/reports').body
-     #        debugger
-     #      
-     #        body.should have_tag('div')
-     #  end
-    
-    # non logged in
-    it 'should redirect from administrator/reports/admin_whole_report to login if not logged in'
-    it 'should be text "Nothing To Report" below the title'
-    
-    #logged in as a admin
-    it 'should have date in "human" format (e.g. "1 day ago")'
-    it 'latest line should be above all'
-    it 'name of Taxon_concept should be a link to Taxon_concept'
-    it 'should have username of change\'s author'
-    it 'username should be a link to account, if user is a curator'
-    
-    #Comments lines
-    it 'lines should started with "Comment"'
-    it 'should show text of comment'
-    it 'should show entire short comment by user'
-    it 'should show concatenated version of long comment (~30 ch.)'
-    it 'should show name of action ("created" or "changed to hide/show")'
-    it 'should show name of Taxon_concept (= page name)'
 
-    #text or image changes lines
-    it 'lines should started with "Text" or "Image"'
-    it 'should be small picture in the next line after line "Image"'
-    it '"Text" should have name of toc_label (e.g. Text for "Overview"...)'  
-    it 'should show name of action ("changed to hide/show/trusted/untrusted/unupropiated")'
- 
-    # for All changes  
-    it 'should have title "Changing of objects status and comments" on the center of a page'
-    it 'should have all items from actions_histories table'
-        
-  end
+  it 'ALL of these tests have been disabled in the interest of time.  Implement them later.'
   
-end
-
-describe 'Administrator Web Users Pages' do
-  
-  Scenario.load :foundation
-  
-  describe 'user/edit' do
-   it 'should have "Cc: affiliate@eol.org" in a head of an email from /administrator/user/edit/# page'
-  end
+#TEMP  Scenario.load :foundation
+#TEMP  
+#TEMP  describe '(Reports)' do
+#TEMP    
+#TEMP    #doesn't work for now, see EOLINFRASTRUCTURE-1061
+#TEMP    
+#TEMP     # it 'should check if we come to /administrator/reports' do
+#TEMP     #        pass  = 'timey-wimey'
+#TEMP     #        user = User.gen(:hashed_password => Digest::MD5.hexdigest(pass))
+#TEMP     #        user.roles = Role.find(:all, :conditions => 'title LIKE "Admin%"')
+#TEMP     #        login_as(:username => user.username, :password => pass)
+#TEMP     #        body  = request('/administrator/reports').body
+#TEMP     #        debugger
+#TEMP     #      
+#TEMP     #        body.should have_tag('div')
+#TEMP     #  end
+#TEMP    
+#TEMP    # non logged in
+#TEMP    it 'should redirect from administrator/reports/admin_whole_report to login if not logged in'
+#TEMP    it 'should be text "Nothing To Report" below the title'
+#TEMP    
+#TEMP    #logged in as a admin
+#TEMP    it 'should have date in "human" format (e.g. "1 day ago")'
+#TEMP    it 'latest line should be above all'
+#TEMP    it 'name of Taxon_concept should be a link to Taxon_concept'
+#TEMP    it 'should have username of change\'s author'
+#TEMP    it 'username should be a link to account, if user is a curator'
+#TEMP    
+#TEMP    #Comments lines
+#TEMP    it 'lines should started with "Comment"'
+#TEMP    it 'should show text of comment'
+#TEMP    it 'should show entire short comment by user'
+#TEMP    it 'should show concatenated version of long comment (~30 ch.)'
+#TEMP    it 'should show name of action ("created" or "changed to hide/show")'
+#TEMP    it 'should show name of Taxon_concept (= page name)'
+#TEMP
+#TEMP    #text or image changes lines
+#TEMP    it 'lines should started with "Text" or "Image"'
+#TEMP    it 'should be small picture in the next line after line "Image"'
+#TEMP    it '"Text" should have name of toc_label (e.g. Text for "Overview"...)'  
+#TEMP    it 'should show name of action ("changed to hide/show/trusted/untrusted/unupropiated")'
+#TEMP 
+#TEMP    # for All changes  
+#TEMP    it 'should have title "Changing of objects status and comments" on the center of a page'
+#TEMP    it 'should have all items from actions_histories table'
+#TEMP        
+#TEMP  end
+#TEMP  
+#TEMPend
+#TEMP
+#TEMPdescribe 'Administrator Web Users Pages' do
+#TEMP  
+#TEMP  Scenario.load :foundation
+#TEMP  
+#TEMP  describe 'user/edit' do
+#TEMP   it 'should have "Cc: affiliate@eol.org" in a head of an email from /administrator/user/edit/# page'
+#TEMP  end
   
 end
--- spec/blackbox/content_partner_pages_spec.rb
+++ spec/blackbox/content_partner_pages_spec.rb
@@ -2,106 +2,108 @@ require File.dirname(__FILE__) + '/../spec_helper'
 
 describe 'Content Partner Pages' do
   
-  Scenario.load :foundation
-  
-  describe '(Reports)' do
-    
-    #TODO: do all this tests after fixing EOLINFRASTRUCTURE-1061   
-      
-    # before(:all) do
-      # Scenario.load :foundation      
-      
-      # password = 'fumbletricket'
-      # @content_partner  = Agent.gen(:hashed_password => 
-      #                                Digest::MD5.hexdigest(password))
-      # @cp               = ContentPartner.gen(:agent => @content_partner)
-      # @taxon_concept    = build_taxon_concept(:agent => @content_partner)
-      # @another_taxon_concept = build_taxon_concept
-      # @user             = User.gen
-      # @short_comment_from_user = 'Comment from user'
-      # @curator          = build_curator(@taxon_concept)
-      # @taxon_concept.images.first.comment(@user, @short_comment_from_user)
-      # login_content_partner(:username => @content_partner.username,
-      #                       :password => password)
-      #                       debugger
-      # @request          = request('/content_partner/reports')
-    # end
-    
-    # Examples:
-    #   Comment "This is a witty comment on the..." was changed to "hide" on "Animalia Linn." by e_howe25 1 day ago
-    # OR
-    #   Text for "Overview" on "Animalia Linn." was changed to "untrusted" by e_howe25 about 5 hours ago
-    # OR
-    #   Image on "Animalia Linn." was changed to "show" by e_howe25 about 5 hours ago
-    
-    # non logged in
-    it 'should redirect from content_partner/reports to content_partner/reports/login if not logged in'
-    
-    #logged in as a content_partner
-    
-    #for every report
-    it 'should only show activity for the content partner\'s clade'
-    it 'should be "All changes", "Comments", "Statuses" items below "USAGE REPORTS" in TOC on content_partner/reports'
-    it 'corresponding bullet should be blue if choose "All changes" / "Comments" / Statuses'
-    it 'should have date in "human" format (e.g. "1 day ago")'
-    it 'latest line should be above all'
-    it 'name of Taxon_concept should be a link to Taxon_concept'
-    it 'should have username of change\'s author'
-    it 'username should be a link to account, if user is a curator'
-    
-    #for Comments report
-    it 'should have title "Changing of comments"'
-    it 'should have only lines started with "Comment..."'
-    it 'should show text of comment'
-    it 'should show entire short comment by user'
-    it 'should show concatenated version of long comment (30 ch.)'
-    it 'should show name of action ("created" or "changed to hide/show")'
-    it 'should show name of Taxon_concept (= page name)'
+  it 'ALL of these tests have been disabled in the interest of time.  Implement later.'
 
-    #for Statuses report
-    it 'should have title "Changing of objects status"'
-    it 'should have only lines started with "Text" or "Image"'
-    it 'should be small picture in the next line after line "Image"'
-    it '"Text" should have name of toc_label (e.g. Text for "Overview"...)'  
-    it 'should show name of action ("changed to hide/show/trusted/untrusted/unupropiated")'
- 
-    # for All changes  
-    it 'should have title "Changing of objects status and comments" on the center of a page'
-    it 'should have all items from "Comments" and "Statuses" reports'
-    it 'should have only items from "Comments" and "Statuses" reports'  
-        
-    #doesn't work for now, see EOLINFRASTRUCTURE-1061
-
-    # it 'should show entire short comment by user' do
-    #   @request.should include @short_comment_from_user
-    # end
-    # 
-    
-    # it 'should check if we come to /content_partner/reports and it has left-pane tag' do
-    #   pass  = 'timey-wimey'
-    #   agent = Agent.gen(:hashed_password => Digest::MD5.hexdigest(pass))
-    #   cp    = ContentPartner.gen(:agent => agent)
-    #   login_content_partner(:username => agent.username, :password => pass)
-    #   body  = request('/content_partner/reports').body
-    #   # debugger
-    # 
-    #   body.should have_tag('div#left-pane')
-    # end
-
-    # it 'should check if we come to /administrator/reports' do
-    #   pass  = 'timey-wimey'
-    #   user = User.gen(:hashed_password => Digest::MD5.hexdigest(pass))
-    #   user.roles = Role.find(:all, :conditions => 'title LIKE "Admin%"')
-    #   login_as(:username => user.username, :password => pass)
-    #   body  = request('/administrator/reports').body
-    #   debugger
-    # 
-    #   body.should have_tag('div')
-    # end
-    
-    
-    
-  end
+#TEMP  Scenario.load :foundation
+#TEMP  
+#TEMP  describe '(Reports)' do
+#TEMP    
+#TEMP    #TODO: do all this tests after fixing EOLINFRASTRUCTURE-1061   
+#TEMP      
+#TEMP    # before(:all) do
+#TEMP      # Scenario.load :foundation      
+#TEMP      
+#TEMP      # password = 'fumbletricket'
+#TEMP      # @content_partner  = Agent.gen(:hashed_password => 
+#TEMP      #                                Digest::MD5.hexdigest(password))
+#TEMP      # @cp               = ContentPartner.gen(:agent => @content_partner)
+#TEMP      # @taxon_concept    = build_taxon_concept(:agent => @content_partner)
+#TEMP      # @another_taxon_concept = build_taxon_concept
+#TEMP      # @user             = User.gen
+#TEMP      # @short_comment_from_user = 'Comment from user'
+#TEMP      # @curator          = build_curator(@taxon_concept)
+#TEMP      # @taxon_concept.images.first.comment(@user, @short_comment_from_user)
+#TEMP      # login_content_partner(:username => @content_partner.username,
+#TEMP      #                       :password => password)
+#TEMP      #                       debugger
+#TEMP      # @request          = request('/content_partner/reports')
+#TEMP    # end
+#TEMP    
+#TEMP    # Examples:
+#TEMP    #   Comment "This is a witty comment on the..." was changed to "hide" on "Animalia Linn." by e_howe25 1 day ago
+#TEMP    # OR
+#TEMP    #   Text for "Overview" on "Animalia Linn." was changed to "untrusted" by e_howe25 about 5 hours ago
+#TEMP    # OR
+#TEMP    #   Image on "Animalia Linn." was changed to "show" by e_howe25 about 5 hours ago
+#TEMP    
+#TEMP    # non logged in
+#TEMP    it 'should redirect from content_partner/reports to content_partner/reports/login if not logged in'
+#TEMP    
+#TEMP    #logged in as a content_partner
+#TEMP    
+#TEMP    #for every report
+#TEMP    it 'should only show activity for the content partner\'s clade'
+#TEMP    it 'should be "All changes", "Comments", "Statuses" items below "USAGE REPORTS" in TOC on content_partner/reports'
+#TEMP    it 'corresponding bullet should be blue if choose "All changes" / "Comments" / Statuses'
+#TEMP    it 'should have date in "human" format (e.g. "1 day ago")'
+#TEMP    it 'latest line should be above all'
+#TEMP    it 'name of Taxon_concept should be a link to Taxon_concept'
+#TEMP    it 'should have username of change\'s author'
+#TEMP    it 'username should be a link to account, if user is a curator'
+#TEMP    
+#TEMP    #for Comments report
+#TEMP    it 'should have title "Changing of comments"'
+#TEMP    it 'should have only lines started with "Comment..."'
+#TEMP    it 'should show text of comment'
+#TEMP    it 'should show entire short comment by user'
+#TEMP    it 'should show concatenated version of long comment (30 ch.)'
+#TEMP    it 'should show name of action ("created" or "changed to hide/show")'
+#TEMP    it 'should show name of Taxon_concept (= page name)'
+#TEMP
+#TEMP    #for Statuses report
+#TEMP    it 'should have title "Changing of objects status"'
+#TEMP    it 'should have only lines started with "Text" or "Image"'
+#TEMP    it 'should be small picture in the next line after line "Image"'
+#TEMP    it '"Text" should have name of toc_label (e.g. Text for "Overview"...)'  
+#TEMP    it 'should show name of action ("changed to hide/show/trusted/untrusted/unupropiated")'
+#TEMP 
+#TEMP    # for All changes  
+#TEMP    it 'should have title "Changing of objects status and comments" on the center of a page'
+#TEMP    it 'should have all items from "Comments" and "Statuses" reports'
+#TEMP    it 'should have only items from "Comments" and "Statuses" reports'  
+#TEMP        
+#TEMP    #doesn't work for now, see EOLINFRASTRUCTURE-1061
+#TEMP
+#TEMP    # it 'should show entire short comment by user' do
+#TEMP    #   @request.should include @short_comment_from_user
+#TEMP    # end
+#TEMP    # 
+#TEMP    
+#TEMP    # it 'should check if we come to /content_partner/reports and it has left-pane tag' do
+#TEMP    #   pass  = 'timey-wimey'
+#TEMP    #   agent = Agent.gen(:hashed_password => Digest::MD5.hexdigest(pass))
+#TEMP    #   cp    = ContentPartner.gen(:agent => agent)
+#TEMP    #   login_content_partner(:username => agent.username, :password => pass)
+#TEMP    #   body  = request('/content_partner/reports').body
+#TEMP    #   # debugger
+#TEMP    # 
+#TEMP    #   body.should have_tag('div#left-pane')
+#TEMP    # end
+#TEMP
+#TEMP    # it 'should check if we come to /administrator/reports' do
+#TEMP    #   pass  = 'timey-wimey'
+#TEMP    #   user = User.gen(:hashed_password => Digest::MD5.hexdigest(pass))
+#TEMP    #   user.roles = Role.find(:all, :conditions => 'title LIKE "Admin%"')
+#TEMP    #   login_as(:username => user.username, :password => pass)
+#TEMP    #   body  = request('/administrator/reports').body
+#TEMP    #   debugger
+#TEMP    # 
+#TEMP    #   body.should have_tag('div')
+#TEMP    # end
+#TEMP    
+#TEMP    
+#TEMP    
+#TEMP  end
   
 end
 

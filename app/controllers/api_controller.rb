33a7e577ab90ac5a8d045a5c2351ace0d824ff7e
pleary <pleary@78829999-583a-0410-bd01-8a9b849fd409>
2010-03-09 21:26:46 +0000

    Changed the name and attribute names of the APIs
    git-svn-id: file:///data/subversion/eol/trunk@1750 78829999-583a-0410-bd01-8a9b849fd409

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -1,12 +1,12 @@
 class ApiController < ApplicationController
-  def taxon_concept_details
+  def pages
     taxon_concept_id = params[:id] || 0
-    params[:media_count] ||= 1
-    params[:text_count] ||= 1
+    params[:images] ||= 1
+    params[:text] ||= 1
     
     taxon_concept = TaxonConcept.find(taxon_concept_id)
     unless taxon_concept.nil? || !taxon_concept.published?
-      @details_hash = taxon_concept.details_hash(:return_media_limit => params[:media_count].to_i, :subject => params[:subject], :return_text_limit => params[:text_count].to_i)
+      @details_hash = taxon_concept.details_hash(:return_media_limit => params[:images].to_i, :subject => params[:subject], :return_text_limit => params[:text].to_i)
     end
     
     respond_to do |format|
@@ -14,7 +14,7 @@ class ApiController < ApplicationController
     end
   end
   
-  def data_object_details
+  def data_objects
     data_object_guid = params[:id] || 0
     
     @details_hash = DataObject.details_for_object(data_object_guid)

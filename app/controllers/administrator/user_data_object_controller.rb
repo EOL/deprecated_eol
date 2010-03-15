d3639def30e5e3e524edbe60be058c0e8c46df14
eagbayani <eagbayani@78829999-583a-0410-bd01-8a9b849fd409>
2010-03-15 22:03:06 +0000

    EOLINFRASTRUCTURE-1637
    git-svn-id: https://repository.eol.org/eol/trunk@1763 78829999-583a-0410-bd01-8a9b849fd409

--- app/controllers/administrator/user_data_object_controller.rb
+++ app/controllers/administrator/user_data_object_controller.rb
@@ -5,6 +5,10 @@ class Administrator::UserDataObjectController < AdminController
  
     @user_id=params[:user_id] || 'all'
     @user_list=User.users_with_submitted_text
+    
+    @object_ids = UsersDataObject.get_user_submitted_data_object_ids(@user_id)
+    @obj_toc_info = DataObject.get_toc_info(@object_ids)
+    
     if(@user_id == 'all') then    
       @comments=UsersDataObject.paginate(:order=>'id desc',:include=>:user,:page => params[:page])
       @comment_count=UsersDataObject.count()

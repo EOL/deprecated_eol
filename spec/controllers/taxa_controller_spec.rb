b9578341602a2cca447ae81e9ac099922ee05508
jrice <jrice@78829999-583a-0410-bd01-8a9b849fd409>
2010-03-16 20:37:44 +0000

    tests passing (finally) after adding information to disambiguate matching search results
    git-svn-id: https://repository.eol.org/eol/trunk@1765 78829999-583a-0410-bd01-8a9b849fd409

--- spec/controllers/taxa_controller_spec.rb
+++ spec/controllers/taxa_controller_spec.rb
@@ -1,12 +1,60 @@
 require File.dirname(__FILE__) + '/../spec_helper'
 
+def hijack_search_for_tiger
+  TaxonConcept.should_receive(:search_with_pagination).at_least(1).times.and_return(@common_collection.paginate({:page => 1, :per_page => 10}))
+  get 'search', :q => 'tiger'
+end
+
 describe TaxaController do
   
-  before(:each) do
-    Factory(:language, :label => 'English')
+  describe 'search' do
+
+    before(:all) do
+      Scenario.load :search_with_duplicates
+      @tc_id                   = SearchScenarioResults.tc_id
+      @new_common_name         = SearchScenarioResults.new_common_name
+      @ancestor_concept        = SearchScenarioResults.ancestor_concept
+      @parent_concept          = SearchScenarioResults.parent_concept
+      @taxon_concept           = SearchScenarioResults.taxon_concept
+      @duplicate_taxon_concept = SearchScenarioResults.duplicate_taxon_concept
+      @query_results           = SearchScenarioResults.query_results
+
+      # We call it with our bogus results:
+      @common_collection = EOL::SearchResultsCollection.new(@query_results, :querystring => 'tiger', :type => :common)
+    end
+
+    describe 'with duplicates' do
+
+      integrate_views # Note I am NOT using RackBox for these examples; faster.
+
+      it 'should show the source hierarchy on the duplicates' do
+        hijack_search_for_tiger
+        response.should have_tag('span.recognized_by', :text => "Taxon recognized by #{@taxon_concept.entry.hierarchy.label}")
+      end
+
+      it 'should show the parent taxon_concept of the duplicates' do
+        hijack_search_for_tiger
+        response.should have_tag('div.parent', :text => @parent_concept.common_name)
+        response.should have_tag('div.parent', :text => @parent_concept.scientific_name)
+      end
+
+      it 'should show the ancestor (grandparent) taxon_concept of the duplicates' do
+        hijack_search_for_tiger
+        response.should have_tag('div.ancestor', :text => @ancestor_concept.common_name)
+        response.should have_tag('div.ancestor', :text => @ancestor_concept.scientific_name)
+      end
+
+      it 'should add the "duplicate" class to the result div when there are duplicates' do
+        hijack_search_for_tiger
+        response.should have_tag('div.duplicate')
+      end
+
+    end
+
   end
-  
+
   it "should find no results on an empty search" do
+    Factory(:language, :label => 'English') # Required to make the get run.
     get :search
     assigns[:all_results].should == []
   end

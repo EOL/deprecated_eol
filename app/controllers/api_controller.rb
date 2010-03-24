f6d27ee6a6d29e23b34184143a4515deab93e147
pleary <pleary@78829999-583a-0410-bd01-8a9b849fd409>
2010-03-24 15:41:01 +0000

    Created an html view for APIs
    git-svn-id: https://repository.eol.org/eol/trunk@1794 78829999-583a-0410-bd01-8a9b849fd409

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -13,8 +13,12 @@ class ApiController < ApplicationController
       @details_hash = taxon_concept.details_hash(:return_media_limit => params[:images].to_i, :subject => params[:subject], :return_text_limit => params[:text].to_i, :details => params[:details])
     end
     
-    respond_to do |format|
-       format.xml { render :layout=>false }
+    if params[:format] == 'html'
+      render :layout => false
+    else
+      respond_to do |format|
+         format.xml { render :layout=>false }
+      end
     end
   end
   
@@ -22,9 +26,12 @@ class ApiController < ApplicationController
     data_object_guid = params[:id] || 0
     
     @details_hash = DataObject.details_for_object(data_object_guid)
-    
-    respond_to do |format|
-       format.xml { render :layout=>false }
+    if params[:format] == 'html'
+      render :layout => false
+    else
+      respond_to do |format|
+        format.xml { render :layout=>false }
+      end
     end
   end
   

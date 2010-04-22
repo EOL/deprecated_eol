8b03613ef66dd0dfa38a52247ffac2655dd3c206
jrice <jrice@78829999-583a-0410-bd01-8a9b849fd409>
2010-04-22 17:35:37 +0000

    Removing some pending tests
    git-svn-id: https://repository.eol.org/eol/trunk@1900 78829999-583a-0410-bd01-8a9b849fd409

--- spec/models/hierarchy_spec.rb
+++ spec/models/hierarchy_spec.rb
@@ -1,29 +1,6 @@
 require File.dirname(__FILE__) + '/../spec_helper'
 
-# I have no idea how Hierarchy is supposed to behave ...
-#
-# We need schema documentation  :(
-#
-# For now, I'll guess that only a label is required.
-#
-# Hierarchy requires a lot of columns, some of which I simple don't understand:
-#   >> Hierarchy.create :label => 'hi', :url => 'foo', :hierarchy_group_id => 1, :hierarchy_group_version => 0, :description => 'hi', :agent_id => 1
-#
 describe Hierarchy do
-
-  it 'should have documentation!'
-
-  # ... doesn't require *anything?*
-
-  #it 'should require a label' do
-  #  Hierarchy.build( :label => nil ).should_not be_valid
-  #  Hierarchy.build( :label => 'Tree of Life 2009' ).should be_valid
-  #end
-
-  it 'should be easy to create HierarchyEntries'
-
-  # i have no idea how to create HierarchyEntries without having to create 
-  # a lot of additional dependencies
 
   it 'should be able to find the Encyclopedia of Life Curators hierarchy with #eol_curators' do
     contributors_hierarchy = Hierarchy.find_by_label('Encyclopedia of Life Contributors') || Hierarchy.gen(:label => 'Encyclopedia of Life Contributors')

962c68ebebb2d8cd3083855f4b92f1672afc9af1
jrice <jrice@78829999-583a-0410-bd01-8a9b849fd409>
2010-04-30 18:49:12 +0000

    I made this change in production, live, so it's not being tagged, but I screwed up a word in the migration.  (Forgot 'table'.)  So, no big, but needs to be included for the merge.
    git-svn-id: https://repository.eol.org/eol/branches/2010.04.09@1942 78829999-583a-0410-bd01-8a9b849fd409

--- db/migrate/20100429210443_force_unique_username.rb
+++ db/migrate/20100429210443_force_unique_username.rb
@@ -1,9 +1,9 @@
 class ForceUniqueUsername < ActiveRecord::Migration
   def self.up
-    execute('alter users add unique index unique_username (username);') 
+    execute('alter table users add unique index unique_username (username);') 
   end
 
   def self.down
-    execute('alter users drop index unique_username;') 
+    execute('alter table users drop index unique_username;') 
   end
 end

f545cdbe1aedee5a9bd2a4f0c382852ee597b932
jrice <jrice@78829999-583a-0410-bd01-8a9b849fd409>
2010-05-05 13:27:21 +0000

    Password reset stuff, resolves EOLINFRASTRUCTURE-1654
    git-svn-id: https://repository.eol.org/eol/trunk@1946 78829999-583a-0410-bd01-8a9b849fd409

--- config/routes.rb
+++ config/routes.rb
@@ -21,11 +21,13 @@ ActionController::Routing::Routes.draw do |map|
 
   map.settings     'settings',     :controller => 'taxa',:action=>'settings'
 
-  map.connect 'boom',
-              :controller => 'content', :action => 'error' 
+  map.connect 'boom', :controller => 'content', :action => 'error' 
 
   map.open_id_complete 'authenticate', :controller => "account", :action => "authenticate", :requirements => { :method => :get }
   map.save_reset_password 'account/save_reset_password', :controller => "account", :action => "save_reset_password"
+  map.reset_specific_users_password 'account/reset_specific_users_password',
+                                    :controller => 'account',
+                                    :action => 'reset_specific_users_password'
 
   # The priority is based upon order of creation: first created -> highest priority.
   map.with_options(:controller => 'account') do |account|

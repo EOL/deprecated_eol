f68fe2a9207921f7f2d487a320c2b45512c9fd8c
dima <dima@78829999-583a-0410-bd01-8a9b849fd409>
2010-05-17 21:25:52 +0000

    Another take on (EOLINFRASTRUCTURE-1539) Validation Error when trying to change user preferences
    added method which removes passwords remembered by firefox during update of user information,
    refactored out duplication code
    
    git-svn-id: file:///data/subversion/eol/trunk@1981 78829999-583a-0410-bd01-8a9b849fd409

--- spec/controllers/account_controller_spec.rb
+++ spec/controllers/account_controller_spec.rb
@@ -23,14 +23,39 @@ describe AccountController do
       
       end
 
-      describe "POST /account/save_reset_password" do
-        it "should change password for a user" do
+      describe "POST /account/profile" do
+        it "should not change password if user is not logged in" do
           user = User.gen(:username => 'johndoe', :email => 'johndoe@example.com')
           user.save!
           new_password = "newpass"
           old_hashed_password = User.find(user).hashed_password
-          post :save_reset_password, "user" => {"id" => user.id, "entered_password" => new_password, "entered_password_confirmation" => new_password}
+          post :profile, "user" => {"id" => user.id, "entered_password" => new_password, "entered_password_confirmation" => new_password}
+          User.find(user).hashed_password.should == old_hashed_password
+        end
+
+        it "should change password for a user" do
+          user = User.gen(:username => 'johndoe', :email => 'johndoe@example.com')
+          user.save!
+          new_password = "newpass"
+          session[:user] = :user
+          session[:user_id] = user.id
+          old_hashed_password = User.find(user).hashed_password
+          post :profile, "user" => {"id" => user.id, "entered_password" => new_password, "entered_password_confirmation" => new_password}
           User.find(user).hashed_password.should_not == old_hashed_password
+          User.find(user).hashed_password.should == User.hash_password(new_password)
+        end
+      
+        it "should unset password wrongly remembered by a broswer" do
+          user = User.gen(:username => 'johndoe', :email => 'johndoe@example.com')
+          user.save!
+          new_password = "newpass"
+          old_hashed_password = User.find(user).hashed_password
+          session[:user] = :user
+          session[:user_id] = user.id
+          post :profile, "user" => {"id" => user.id, "entered_password" => new_password, "entered_password_confirmation" => new_password}
+          User.find(user).hashed_password.should_not == old_hashed_password
+          User.find(user).hashed_password.should == User.hash_password(new_password)
+          post :profile, "user" => {"id" => user.id, "entered_password" => new_password, "entered_password_confirmation" => ''}
           User.find(user).hashed_password.should == User.hash_password(new_password)
         end
       end

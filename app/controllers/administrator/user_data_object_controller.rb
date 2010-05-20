a3f6791fe734d0be8cb7a39dfb6617dcfe00e1ba
eagbayani <eagbayani@78829999-583a-0410-bd01-8a9b849fd409>
2010-05-20 18:09:57 +0000

    New report in admin page: Content Partner Data Objects Stats. Also did some SQL table abbreviation of old reports.
    git-svn-id: https://repository.eol.org/eol/trunk@1985 78829999-583a-0410-bd01-8a9b849fd409

--- app/controllers/administrator/user_data_object_controller.rb
+++ app/controllers/administrator/user_data_object_controller.rb
@@ -6,13 +6,13 @@ class Administrator::UserDataObjectController < AdminController
 
     @page_title = 'User Submitted Text'
  
-    @user_id=params[:user_id] || 'all'
+    @user_id=params[:user_id] || 'All'
     @user_list=User.users_with_submitted_text
     
     @object_ids = UsersDataObject.get_user_submitted_data_object_ids(@user_id)
     @obj_toc_info = DataObject.get_toc_info(@object_ids)
     
-    if(@user_id == 'all') then    
+    if(@user_id == 'All') then    
       @comments=UsersDataObject.paginate(:order=>'id desc',:include=>:user,:page => params[:page])
       @comment_count=UsersDataObject.count()
     else

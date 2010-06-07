6d5d0d55ee734ad355eb9429f1b2804175f59528
eagbayani <eagbayani@78829999-583a-0410-bd01-8a9b849fd409>
2010-06-07 19:32:03 +0000

    Included user-submitted-text in count of dataObjects per SPM subject report. Re-write old queries to follow SQL writing convention.
    git-svn-id: https://repository.eol.org/eol/trunk@2024 78829999-583a-0410-bd01-8a9b849fd409

--- app/models/data_object.rb
+++ app/models/data_object.rb
@@ -1124,19 +1124,18 @@ AND data_type_id IN (#{data_type_ids.join(',')})
     obj_toc_info = {} #same Hash.new
     if(obj_ids.length > 0) then
       sql = "
-      Select data_objects.id, vetted.label AS vetted_label, visibilities.label AS visible,        
-      concat(toc2.label, ' - ', table_of_contents.label) as toc
-      From data_objects
-      left Join vetted ON data_objects.vetted_id = vetted.id
-      left Join visibilities ON data_objects.visibility_id = visibilities.id
-      Left Join data_objects_table_of_contents ON data_objects.id = data_objects_table_of_contents.data_object_id
-      left Join table_of_contents ON data_objects_table_of_contents.toc_id = table_of_contents.id
-      left Join table_of_contents toc2 ON table_of_contents.parent_id = toc2.id
-      Where data_objects.id in (#{obj_ids.join(',')})"
+      SELECT do.id, vetted.label AS vetted_label, visibilities.label AS visible, concat(toc2.label, ' - ', tc.label) as toc
+      From data_objects do
+      LEFT JOIN vetted                              ON do.vetted_id = vetted.id
+      LEFT JOIN visibilities                        ON do.visibility_id = visibilities.id
+      LEFT JOIN data_objects_table_of_contents dotc ON do.id = dotc.data_object_id
+      LEFT JOIN table_of_contents tc                ON dotc.toc_id = tc.id
+      LEFT JOIN table_of_contents toc2              ON tc.parent_id = toc2.id
+      WHERE do.id IN (#{obj_ids.join(',')})"
       rset = DataObject.find_by_sql([sql])            
       rset.each do |post|
         obj_toc_info["#{post.id}"] = "#{post.toc}"
-        obj_toc_info["e#{post.id}"] = "#{post.vetted_label} <br>  #{post.visible}"
+        obj_toc_info["e#{post.id}"] = "#{post.vetted_label} <br> #{post.visible}"
       end    
     end    
     return obj_toc_info
@@ -1145,11 +1144,11 @@ AND data_type_id IN (#{data_type_ids.join(',')})
   def self.tc_ids_from_do_ids(obj_ids)
     obj_tc_id = {} #same Hash.new
     if(obj_ids.length > 0) then      
-      sql = "Select dotc.taxon_concept_id tc_id , do.data_type_id, do.id do_id
-      From data_objects_taxon_concepts dotc
-      Join data_objects do ON dotc.data_object_id = do.id
-      Join taxon_concepts tc ON dotc.taxon_concept_id = tc.id
-      Where tc.published and dotc.data_object_id in (#{obj_ids.join(',')})"      
+      sql = "SELECT dotc.taxon_concept_id tc_id , do.data_type_id, do.id do_id
+      FROM data_objects_taxon_concepts dotc
+      JOIN data_objects do ON dotc.data_object_id = do.id
+      JOIN taxon_concepts tc ON dotc.taxon_concept_id = tc.id
+      WHERE tc.published AND dotc.data_object_id IN (#{obj_ids.join(',')})"      
       rset = DataObject.find_by_sql([sql])            
       rset.each do |post|
         obj_tc_id["#{post.do_id}"] = post.tc_id
@@ -1163,24 +1162,45 @@ AND data_type_id IN (#{data_type_ids.join(',')})
 
 
   def self.get_dataobjects(obj_ids,page) 
-    query="Select do.* From data_objects do
-    Join vetted v ON do.vetted_id = v.id
+    query="SELECT do.* FROM data_objects do
+    JOIN vetted v ON do.vetted_id = v.id
     WHERE do.id IN (#{ obj_ids.join(', ') })"
     self.paginate_by_sql [query, obj_ids], :page => page, :per_page => 20 , :order => 'id'  
   end
   
   def self.get_SPM_count_on_dataobjects(arr_SPM)        
     arr_count = {} #same Hash.new
-    arr_SPM.each do |rec|
-      id = rec["id"]
-      rset = DataObject.find_by_sql(["Select Count(do.id) as total 
-      From data_objects do
-      Join data_objects_info_items doii ON do.id = doii.data_object_id
-      where doii.info_item_id = #{id} and do.published and do.vetted_id != #{Vetted.untrusted.id}"])            
+    arr_SPM.each do |profile|
+      id = profile["id"]
+      rset = DataObject.find_by_sql(["SELECT Count(do.id) total 
+      FROM data_objects do
+      JOIN data_objects_info_items doii ON do.id = doii.data_object_id
+      WHERE doii.info_item_id = #{id} AND do.published AND do.vetted_id != #{Vetted.untrusted.id}"])            
       rset.each do |rec|
         arr_count["#{id}"] = rec.total
       end      
     end        
+    return arr_count
+  end
+
+  def self.add_user_dataobjects_on_SPM_count(arr_count, arr_user_object_ids)
+    if(arr_user_object_ids.length > 0) then      
+      rset = DataObject.find_by_sql(["SELECT ii.id, Count(ii.id) total
+      FROM data_objects_table_of_contents dotc      
+      JOIN info_items ii ON dotc.toc_id = ii.toc_id
+      JOIN data_objects do ON dotc.data_object_id = do.id
+      WHERE dotc.data_object_id IN (#{arr_user_object_ids.join(',')}) 
+      AND do.vetted_id != #{Vetted.untrusted.id}
+      AND do.published
+      Group By ii.id "])            
+      rset.each do |rec|
+        if(arr_count["#{rec.id}"] != nil) then
+          arr_count["#{rec.id}"] = arr_count["#{rec.id}"].to_i + rec.total.to_i
+        else
+          arr_count["#{rec.id}"] = rec.total.to_i
+        end
+      end      
+    end
     return arr_count
   end
 
@@ -1188,22 +1208,20 @@ AND data_type_id IN (#{data_type_ids.join(',')})
     arr_count = {} #same Hash.new
     arr_SPM.each do |rec|
       id = rec["id"]
-      rset = DataObject.find_by_sql(["Select count(distinct ar.agent_id) total
-      From data_objects_info_items      doii
-      Join data_objects_harvest_events  dohe  ON doii.data_object_id = dohe.data_object_id
-      Join data_objects                 do    ON do.id = doii.data_object_id
-      Join harvest_events               he    ON dohe.harvest_event_id = he.id
-      Join resources                    r     ON he.resource_id = r.id
-      Join agents_resources             ar    ON r.id = ar.resource_id
-      where doii.info_item_id = #{id} and do.published and do.vetted_id != #{Vetted.untrusted.id}"])            
+      rset = DataObject.find_by_sql(["SELECT COUNT(distinct ar.agent_id) total
+      FROM data_objects_info_items      doii
+      JOIN data_objects_harvest_events  dohe  ON doii.data_object_id = dohe.data_object_id
+      JOIN data_objects                 do    ON do.id = doii.data_object_id
+      JOIN harvest_events               he    ON dohe.harvest_event_id = he.id
+      JOIN resources                    r     ON he.resource_id = r.id
+      JOIN agents_resources             ar    ON r.id = ar.resource_id
+      WHERE doii.info_item_id = #{id} AND do.published AND do.vetted_id != #{Vetted.untrusted.id}"])            
       rset.each do |rec|
         arr_count["#{id}"] = rec.total
       end      
     end        
     return arr_count
   end
-
-
   
   def self.details_for_object(data_object_guid, options = {})
     data_objects = DataObject.find_all_by_guid(data_object_guid, :conditions => "published=1 AND visibility_id=#{Visibility.visible.id}")

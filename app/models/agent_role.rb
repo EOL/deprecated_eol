ba1d9e31d628bb200896726406d6c53a8f17b6e3
jrice <jrice@78829999-583a-0410-bd01-8a9b849fd409>
2010-06-08 17:19:12 +0000

    Resolving conflict
    git-svn-id: https://repository.eol.org/eol/branches/rails23@2030 78829999-583a-0410-bd01-8a9b849fd409

--- app/models/agent_role.rb
+++ app/models/agent_role.rb
@@ -1,10 +1,5 @@
 # An enumerated list of the different kinds of roles an Agent fills.
 class AgentRole < SpeciesSchemaModel
-<<<<<<< HEAD
-  acts_as_enum
-=======
->>>>>>> master
-
   has_many :agents_data_objects
   has_many :agents_synonyms
 

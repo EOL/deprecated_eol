35c71c39f6a630d6cddc9ab0009e9a66e0ba9790
jrice <jrice@78829999-583a-0410-bd01-8a9b849fd409>
2010-06-08 17:22:32 +0000

    Removing reference to ActsAsEnum
    git-svn-id: https://repository.eol.org/eol/branches/rails23@2031 78829999-583a-0410-bd01-8a9b849fd409

--- vendor/plugins/attribute_fu/init.rb
+++ vendor/plugins/attribute_fu/init.rb
@@ -1,4 +1,3 @@
 ActiveRecord::Base.class_eval { include AttributeFu::Associations }
 ActionView::Helpers::FormBuilder.class_eval { include AttributeFu::AssociatedFormHelper }
-ActiveRecord::Base.send(:include, ActsAsEnum)
 

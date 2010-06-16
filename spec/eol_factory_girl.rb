feae49259a7c6c08006807a8034e948fb32ac4aa
jrice <jrice@78829999-583a-0410-bd01-8a9b849fd409>
2010-06-16 16:55:47 +0000

    Removing some warnings by fixing some deprecations and getting rid of our tinyint table id checking
    git-svn-id: https://repository.eol.org/eol/branches/rails23@2079 78829999-583a-0410-bd01-8a9b849fd409

--- spec/eol_factory_girl.rb
+++ spec/eol_factory_girl.rb
@@ -35,37 +35,8 @@ module EOL::FactoryGirlActiveRecordBaseExtensions
 
 end
 
-module EOL::FactoryGirlExtensions
-
-  def self.included base
-    begin
-      base.class_eval {
-        alias_method_chain :create, :duplicate_entry_checking
-      }
-    rescue NameError 
-      puts "** WARNING: It seems you are trying to load 'spec/spec_helper' in development."
-      puts "   Try it in test; not all required gems are loaded in this environment."
-    end
-  end
-
-  def create_with_duplicate_entry_checking *args
-    begin
-      create_without_duplicate_entry_checking *args
-    rescue Exception => ex
-      if ex.message.include? "Mysql::Error: Duplicate entry '255'"
-        puts "\nTrying to generate a Factory resulted in a Mysql::Error" + 
-             "\nIt's likely that a table ran out of primary keys (255)"  + 
-             "\nYou should try truncating these tables first.\n\n"
-      end
-      raise ex
-    end
-  end
-
-end
-
 # Extends all ActiveRecord::Base models with extensions 
 # that integrate with factory-girl
 ActiveRecord::Base.class_eval do
   extend EOL::FactoryGirlActiveRecordBaseExtensions
 end
-Factory.send :include, EOL::FactoryGirlExtensions

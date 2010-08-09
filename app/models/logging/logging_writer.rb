6b7ca21b0ea24de66f91512ead62caef69ba08f3
jrice <jrice@78829999-583a-0410-bd01-8a9b849fd409>
2010-08-09 19:23:29 +0000

    Actually, I think this is a better way to stop logging (blacklist instead of whitelist).
    git-svn-id: file:///data/subversion/eol/trunk@2240 78829999-583a-0410-bd01-8a9b849fd409

--- app/models/logging/logging_writer.rb
+++ app/models/logging/logging_writer.rb
@@ -3,6 +3,6 @@ class LoggingWriter < ActiveRecord::Base
  
   self.abstract_class = true
   
-  establish_connection :master_logging_database if RAILS_ENV =~ /prod/
+  establish_connection :master_logging_database unless RAILS_ENV =~ /(test|development)/
 
 end

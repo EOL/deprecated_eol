a57cbbaeaabfdd994d5a15424df9ef9e201c1c29
dima <dima@78829999-583a-0410-bd01-8a9b849fd409>
2010-08-18 18:57:06 +0000

    Fix for: WEB-1621 EOL system messages sent from "from@example.com"
    git-svn-id: file:///data/subversion/eol/trunk@2276 78829999-583a-0410-bd01-8a9b849fd409

--- app/models/notifier.rb
+++ app/models/notifier.rb
@@ -1,33 +1,32 @@
 class Notifier < ActionMailer::Base
  
-  @@from = $WEBSITE_EMAIL_FROM_ADDRESS
   @@test_recipient = "junk@example.com" # testing only if needed
 
   def forgot_password_email(user, port)
     subject     "EOL Forgot Password"
     recipients  user.email
-    from        @@from
+    from        $WEBSITE_EMAIL_FROM_ADDRESS
     body        :user => user, :password_reset_url=>user.password_reset_url(port)
   end
   
   def agent_forgot_password_email(agent, new_password)
     subject     "EOL Forgot Password"
     recipients  agent.email
-    from        @@from
+    from        $WEBSITE_EMAIL_FROM_ADDRESS
     body        :agent => agent, :password => new_password
   end
   
   def agent_is_ready_for_agreement(agent,recipient)
     subject    "EOL Content Partner Ready For Agreement"
     recipients recipient
-    from       @@from
+    from       $WEBSITE_EMAIL_FROM_ADDRESS
     body       :agent =>agent
   end  
   
   def agent_contact_form_email(agent, contact, recipient)
     subject     "EOL Content Partner Contact"
     recipients  recipient
-    from        @@from
+    from        $WEBSITE_EMAIL_FROM_ADDRESS
     body        :agent => agent, :contact => contact
   end
   
@@ -35,7 +34,7 @@ class Notifier < ActionMailer::Base
   def donation_eol_email(donation)
     subject     "New Donation"
     recipients  @@test_recipient
-    from        @@from
+    from        $WEBSITE_EMAIL_FROM_ADDRESS
     body        :donation => donation
   end
 
@@ -43,49 +42,49 @@ class Notifier < ActionMailer::Base
   def donation_donator_email(donation)
     subject     "Thank you for your donation"
     recipients  donation.email
-    from        @@from
+    from        $WEBSITE_EMAIL_FROM_ADDRESS
     body        :donation => donation
   end
   
   def welcome_registration(user)
     subject     "Thanks for registering with the Encyclopedia of Life"
     recipients  user.email
-    from        @@from
+    from        $WEBSITE_EMAIL_FROM_ADDRESS
     body        :user => user 
   end
 
   def registration_confirmation(user)
     subject     "Please confirm your registration with the Encyclopedia of Life"
     recipients  user.email
-    from        @@from
+    from        $WEBSITE_EMAIL_FROM_ADDRESS
     body        :user => user 
   end
   
   def contact_us_auto_response(contact)
     subject     "Thanks for contacting the Encyclopedia of Life"
     recipients  contact.email
-    from        @@from
+    from        $WEBSITE_EMAIL_FROM_ADDRESS
     body        :contact => contact 
   end
   
   def media_contact_auto_response(contact)
     subject     "Thanks for contacting the Encyclopedia of Life"
     recipients  contact.email
-    from        @@from
+    from        $WEBSITE_EMAIL_FROM_ADDRESS
     body        :contact => contact     
   end
   
   def curator_approved(user)
     subject     "Your request to be a curator for the Encyclopedia of Life has been approved"
     recipients  user.email
-    from        @@from
+    from        $WEBSITE_EMAIL_FROM_ADDRESS
     body        :user => user       
   end
 
   def curator_unapproved(user)
     subject     "Your curator privileges for the Encyclopedia of Life have been removed"
     recipients  user.email
-    from        @@from
+    from        $WEBSITE_EMAIL_FROM_ADDRESS
     body        :user => user       
   end
   
@@ -93,14 +92,14 @@ class Notifier < ActionMailer::Base
     subject     "A message from the Encyclopedia of Life"
     recipients  email
     cc          "affiliate@eol.org"
-    from        @@from
+    from        $WEBSITE_EMAIL_FROM_ADDRESS
     body        :name => name, :message => message           
   end
   
   def user_changed_mailer_setting(old_user,new_user,recipient)
     subject     "EOL user changed their mailing list or email address setting"
     recipients  recipient
-    from        @@from
+    from        $WEBSITE_EMAIL_FROM_ADDRESS
     body        :old_user => old_user, :new_user=>new_user
   end
   
@@ -112,7 +111,7 @@ class Notifier < ActionMailer::Base
     
     subject     "EOL Contact Us: #{contact_subject.title}"
     recipients  contact_recipients
-    from        @@from
+    from        $WEBSITE_EMAIL_FROM_ADDRESS
     body        :contact => contact    
     
   end 

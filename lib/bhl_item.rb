705cc99237e2a611b61ae084d5bd7e46448a1404
jrice <jrice@78829999-583a-0410-bd01-8a9b849fd409>
2010-08-27 16:44:47 +0000

    Fixed the BhlItem (again).  Sorry about that.
    git-svn-id: file:///data/subversion/eol/trunk@2305 78829999-583a-0410-bd01-8a9b849fd409

--- lib/bhl_item.rb
+++ lib/bhl_item.rb
@@ -1,17 +1,19 @@
 class BhlItem
+
   attr_reader :item_id, :publication_title, :publication_url, :publication_details, :item_year, :item_volume,
               :item_issue, :item_prefix, :item_number, :item_url
+
   def initialize(hash)
-    @item_id             = hash[:item_id] || ''
-    @publication_title   = hash[:publication_title] || ''
-    @publication_url     = hash[:publication_url] || ''
-    @publication_details = hash[:publication_details] || ''
-    @item_year           = hash[:item_year] || ''
-    @item_volume         = hash[:item_volume] || ''
-    @item_issue          = hash[:item_issue] || ''
-    @item_prefix         = hash[:item_prefix] || ''
-    @item_number         = hash[:item_number] || ''
-    @item_url            = hash[:item_url] || ''
+    @item_id             = hash['item_id'] || ''
+    @publication_title   = hash['publication_title'] || ''
+    @publication_url     = hash['publication_url'] || ''
+    @publication_details = hash['publication_details'] || ''
+    @item_year           = hash['item_year'] || ''
+    @item_volume         = hash['item_volume'] || ''
+    @item_issue          = hash['item_issue'] || ''
+    @item_prefix         = hash['item_prefix'] || ''
+    @item_number         = hash['item_number'] || ''
+    @item_url            = hash['item_url'] || ''
   end
 
   def name

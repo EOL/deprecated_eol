7404d60b1dd686f39c9ae704796d3dc9236e0fa4
jrice <jrice@78829999-583a-0410-bd01-8a9b849fd409>
2010-10-04 14:01:58 +0000

    No, I'm not done.  Right now, the TODO list includes but is not limited to: text_objects.js, taxa/maps, fixing all of the ()s still in the code, curation, add new text, checking the rest of administration (but I think it's done), clean-up taxa-page.js, check all :javascript tags, check all <script tags, check all onclick code, mediacenter tabs don't highlight, changing ratings reloads the page (but works), showing the first image next to the Please Wait, hidden/invisible icons on images don't update, clade-selector doesn't show the spinner.  That's it...  actually not THAT much, considering...  Eeesh.
    git-svn-id: file:///data/subversion/eol/branches/jquery@2404 78829999-583a-0410-bd01-8a9b849fd409

--- public/javascripts/taxa-page.js
+++ public/javascripts/taxa-page.js
@@ -99,10 +99,24 @@ $('#center-page-content-loading').ajaxComplete(function() {
 });
 
 // Displays the Photosynth interface in the image pane.
-function load_photosynth_interface(source_url)
-{
-  // TODO - put this in the view, and just show/hide it.  We don't want HTML here.
+function load_photosynth_interface(source_url) {
+  // TODO - put this in the view, and just show/hide it.  We don't want HTML here.  ...A bit tricky, because we're using
+  // jQuery Tools tabs, here, so if we put in an "extra" div for this, will it behave correctly?  Not sure.  Also, note I
+  // changed the id on the table to a class (of the same name).  Same with main-image-bg!
   synth = "<table id='main-image-table'><tr><td><iframe frameborder='0' src='" + source_url.replace("view.aspx", "embed.aspx") + "&delayLoad=true&slideShowPlaying=false' width='425' height='355'></iframe><img id='main-image'></td></tr></table>";
   $('#main-image-bg').html = synth;  
 }
 
+// Show some spinners when we're doing stuff:
+$('#ajax-indicator').ajaxStart(function() {
+  $(this).show();
+});
+$('#center-page-content-loading').ajaxStart(function() {
+  $(this).show();
+});
+$('#ajax-indicator').ajaxComplete(function() {
+  $(this).hide();
+});
+$('#center-page-content-loading').ajaxComplete(function() {
+  $(this).hide();
+});

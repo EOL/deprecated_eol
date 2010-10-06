e274d1cd5a27d8c89e68da00984f1fcfccfc476d
Patrick Leary <pleary@mbl.edu>
2010-10-06 10:21:12 -0400

    Fixed more images button on homepage #WEB-1716

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -6,15 +6,16 @@ function explore_more_taxa() {
   if (updating) {
     updating = false;
   }
-  // TODO - rewrite this, simpler.
-  new Ajax.Updater('photos_area','/content/explore_taxa',
-    {
-      onComplete:function(request){taxa_number_to_replace = 1;setTimeout("start_explore_updates()",interval_between_loads);new EOL.Effect.appear('photos_area',{duration: 0.2,from:0.05,to:1.0});},
-      onLoading:function(request){new EOL.Effect.disappear('photos_area',{duration: 0.2,from:1.0,to:0.05});},
-      asynchronous:true, 
-      evalScripts:true	
+  $.ajax({
+    url: '/content/explore_taxa',
+    beforeSend: function() {$('#photos_area').fadeTo(200, 0.03);},
+    success: function(response){$('#photos_area').html(response);},
+    complete: function(){
+        taxa_number_to_replace = 1;
+        setTimeout("start_explore_updates()",interval_between_loads);
+        $('#photos_area').delay(300).fadeTo(750, 1);
     }
-  )	
+  });
 }
 
 function fade_out_taxa(num) {
@@ -40,7 +41,7 @@ function replace_single_taxa(taxa_number) {
       failure: function(request){fade_in_taxa(taxa_number);},
       success: function(request){fade_in_taxa(taxa_number);},
       beforeSend: function(request){fade_out_taxa(taxa_number);}
-    });			
+    });
     taxa_number_to_replace += 1;
   }
 }
@@ -51,13 +52,13 @@ function get_new_explore_taxa() {
 }
 
 function pause_explore_updates() {
-  updating = false
+  updating = false;
   $('#play-button').attr('src', png_host + '/images/homepage/play.gif');
   $('#play-button').attr('alt', 'play');
 }
 
 function start_explore_updates() {
-  updating = true
+  updating = true;
   $('#play-button').attr('src', png_host + '/images/homepage/pause.gif');
   $('#play-button').attr('alt', 'pause');
 }

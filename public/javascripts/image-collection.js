c9213d90eddde1c667d030949720da38873b650b
Jeremy Rice <jrice.blue@gmail.com>
2010-10-07 07:32:51 -0700

    WIP ... need to jump to master, wanted to commit.  Working on curating ratings.  Almost done.  MUCH cleaner...

--- public/javascripts/image-collection.js
+++ public/javascripts/image-collection.js
@@ -1,22 +1,29 @@
-
 $(document).ready(function() {
+  // Image pagination:
   $('.mc-navigation a').click(function() {
     $.ajax({
       url: $(this).attr('href'),
       beforeSend: function() {$('#media-images').fadeTo(300, 0.3);},
       success: function(response) {$('#media-images').html(response);},
       error: function() {$('#media-images').html('<p>Sorry, there was an error.</p>');},
-      complete: function() {$('#media-images').delay(100).fadeTo(100, 1);}
+      complete: function() {$('#media-images').delay(25).fadeTo(100, 1);}
     });
     return false;
   });
+  // Show warnings for unvetted thumbnails:
   $("#image-collection img[title]").tooltip();
+  // clicking the large image shows its attribution
+  $('img.main-image').click(function() {
+    $('#large-image-attribution-button-popup-link').click(); // "click" the PopupLink
+  });
+  // Clicking on a thumbnail does... a lot:
   $("#thumbnails a").click(function() {
     var id = $(this).attr('href');
     $("#notes-container div.mc-notes").hide();
     $("#notes-container #mc-notes-"+id).show();
     $("#large-image .main-image-bg").hide();
     $("#large-image #image-"+id).show();
+    $("#image-ratings").show();
     $("#image-ratings .image-rating").hide();
     $("#image-ratings #rating-"+id).show();
     // Change the HREFs of all the image's links, IF they have an href to replace it in.  :)
@@ -29,15 +36,10 @@ $(document).ready(function() {
     $('.overlay').fadeOut(200);
     return false;
   });
-  // click() the selected image, or just the first.
+  // Now that everything is set up, click() the selected image (or just the first) to show all of it's information.
   if(!selected_image_id) {
     $("#thumbnails a:nth-child(1)").click();
   } else {
     $("#thumbnails a[href="+selected_image_id+"]").click();
   }
-  $("#image-ratings").show(); // Now that we've only got one showing, we can unhide the ratings (without this, it's ugly).
-  // clicking the large image shows its attribution
-  $('img.main-image').click(function() {
-    $('#large-image-attribution-button-popup-link').click(); // "click" the PopupLink
-  });
 });
--- public/javascripts/rating.js
+++ public/javascripts/rating.js
@@ -2,25 +2,19 @@ if(!EOL) var EOL = {};
 if(!EOL.Rating) EOL.Rating = {};
 
 EOL.Rating.update_average_image_rating = function(data_object_id, rating) {
-  var items = $('div.image-rating ul.average-rating li');
-  if (items.length > 0)
-    items[0].css('width', rating * 20+'%');
+  $('#average-rating-'+data_object_id).css('width', rating * 20+'%');
 };
 
 EOL.Rating.update_average_text_rating = function(data_object_id, rating) {
-  $('div#text_buttons_'+data_object_id+' div.text-rating ul.average-rating li')[0].css('width', rating * 20+'%');
+  $('#average-rating-'+data_object_id).css('width', rating * 20+'%');
 };
 
 EOL.Rating.update_user_image_rating = function(data_object_id, rating) {
-  var items = $('div.image-rating ul.user-rating li');
-  if (items.length > 0)
-    items[0].css('width', rating * 20+'%');
-  if(EOL.MediaCenter.image_hash[data_object_id])
-    EOL.MediaCenter.image_hash[data_object_id].user_rating = rating;
+  $('#user-rating-'+data_object_id).css('width', rating * 20+'%');
 };
 
 EOL.Rating.update_user_text_rating = function(data_object_id, rating) {
-  $('div#text_buttons_'+data_object_id+' div.text-rating ul.user-rating li')[0].css('width', rating * 20+'%');
+  $('#user-rating-'+data_object_id).css('width', rating * 20+'%');
 };
 
 $(document).ready(function() {

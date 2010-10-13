89df43eed4de40bfe284c22622713e1ff923c454
Jeremy Rice <jrice.blue@gmail.com>
2010-10-13 13:25:39 -0700

    WIP - Okay, all inline JS specified *as haml* should be converted.  Now I'll be looking for 'raw' html versions of the same... [sigh]

--- public/javascripts/search.js
+++ public/javascripts/search.js
@@ -5,9 +5,9 @@ EOL.Search.show_top_spinner = function() {
   $('#top_search_spinner').show();
 }
 
-EOL.validate_search_parameters = function(form,advanced_search) {
-  if ($('#q').attr('value').trim() == "") {
-    error_message = 'Please enter a search term!';
+EOL.validate_search_parameters = function(form, advanced_search) {
+  if ($.trim($('#q').attr('value')) == "") {
+    error_message = 'Please enter a search term!'; // TODO - i18n.  ...Why isn't this in the view?!  This is dumb.
     $('#search_message').html(error_message);
     if (advanced_search == true) {
       $('#advanced_search_message').html(error_message);

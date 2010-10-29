c82a8681bbb7acf4e8f7549a102d5ab7c1295272
Patrick Leary <pleary@mbl.edu>
2010-10-29 12:17:28 -0400

    Fixed PhotoSynth interface

--- public/javascripts/image-collection.js
+++ public/javascripts/image-collection.js
@@ -66,9 +66,26 @@ if (!EOL.replace_dato_id || !EOL.click_selected_image || !EOL.toggle_main_img_ic
       EOL.replace_dato_id(id);
       EOL.toggle_main_img_icons(id);
       EOL.init_popup_overlays();
+      EOL.toggle_photosynth_icon(id);
       return false;
     });
     EOL.click_selected_image();
+  };
+  
+  EOL.toggle_photosynth_icon = function(id) {
+    var source_url = $("#mc-notes-"+id+" a.source_url").attr('href');
+    if(source_url.search(/photosynth.net/) == -1)
+    { //regular static image    
+      $("#photosynth-message").html("");
+      $("#photosynth-message").unbind('click');
+    }
+    else
+    { //photosynth image
+      $("#photosynth-message").html("<img src='http://mslabs-999.vo.llnwd.net/e1/inc/images/master/logo.png' height='27' alt='Photosynth' title='Image is part of a Photosynth'/>");
+      $("#photosynth-message").click(function() {
+        $("#large-image #image-"+id+" td").html("<iframe frameborder='0' src='" + source_url.replace("view.aspx", "embed.aspx") + "&delayLoad=true&slideShowPlaying=false' width='425' height='355'></iframe>");
+      });
+    }
   };
 }
 

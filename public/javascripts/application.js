b9cc0fb96aacbd00ee89407bb966f080cb2ed288
Jeremy Rice <jrice.blue@gmail.com>
2010-10-28 17:38:59 -0700

    Changing the way external links work... simplified and less JS.

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,31 +1,34 @@
-// Behaviours to attach globally:
+if (!EOL) { var EOL = {}; }
+if (!EOL.addReturnTo || !EOL.use_external_link_popups) {
+  // Notice that this does NOT interrupt the event.  If you add this to a click(), it should follow the link.
+  EOL.addReturnTo = function(link) {
+    if ($(link).attr('href') == null) { // Not a link, silly!
+      return;
+    } else if ($(link).attr('href').match(/\?/) == null) { // There are no params in it yet
+      $(link).attr('href', $(link).attr('href') + '?');
+    } else { // There are already params in this link
+      $(link).attr('href', $(link).attr('href') + '&');
+    }
+    $(link).attr('href', $(link).attr('href') + "return_to=" + $(link).attr('href').replace(/^http:\/\/[^\/]*/, '')); // Removes protocol + host
+  };
+
+  EOL.use_external_link_popups = function() {
+    $('a.external_link').click(function() {
+      // TODO - i18n
+      var agree = confirm("The link you have clicked will take you to an external website.  Are you sure you wish to proceed?");
+      if (agree) {
+        $(this).attr('target', '_blank');
+      } else {
+        return false;
+      }
+    });
+  }
+}
+
 $(document).ready(function() {
-  $('a.external_link').click(function() {
-    var agree = true;
-    if (EOL.USE_EXTERNAL_LINK_POPUPS) {
-      agree = confirm("The link you have clicked will take you to an external website.  Are you sure you wish to proceed?");
-    }
-    if (agree) {
-      $(this).attr('target', '_blank');
-    } else {
-      return false;
-    }
-  });
+  EOL.use_external_link_popups();
   $('a.return_to').click(function() {
     EOL.addReturnTo(this);
   });
 });
-
-if (!EOL) { var EOL = {}; }
-// Notice that this does NOT interrupt the event.  If you add this to a click(), it should follow the link.
-EOL.addReturnTo = function(link) {
-  if ($(link).attr('href') == null) { // Not a link, silly!
-    return;
-  } else if ($(link).attr('href').match(/\?/) == null) { // There are no params in it yet
-    $(link).attr('href', $(link).attr('href') + '?');
-  } else { // There are already params in this link
-    $(link).attr('href', $(link).attr('href') + '&');
-  }
-  $(link).attr('href', $(link).attr('href') + "return_to=" + $(link).attr('href').replace(/^http:\/\/[^\/]*/, '')); // Removes protocol + host
-}
 

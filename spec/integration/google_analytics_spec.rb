9f250162538fef1fdf47400e6a0fe27332c367f0
Eli Agbayani <eli@eol.org>
2010-11-18 09:58:13 -0500

    Almost forgot to remove a temporary hard-coded filter in a view, which fixed the test as well.

--- spec/integration/google_analytics_spec.rb
+++ spec/integration/google_analytics_spec.rb
@@ -53,7 +53,7 @@ describe "Google Analytics Stats Page" do
     partner_taxa = GoogleAnalyticsPartnerTaxon.gen(:year => year, :month => month, :taxon_concept => @tc, :agent => @agent )
     
     visit('/content_partner/reports/monthly_page_stats', :method => :post, :params => {:year_month => "#{year}_#{month}", :agent_id => @agent.id})
-    visit('/content_partner/reports/monthly_page_stats')
+    #visit('/content_partner/reports/monthly_page_stats')
     #select "", :agent_id => @agent.id, :year_month => "#{year}_#{month}"
 
     body.should have_tag("form[action=/content_partner/reports/monthly_page_stats]")

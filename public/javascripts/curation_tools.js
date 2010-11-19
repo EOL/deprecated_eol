8756753c6d91792b74f804d99798fe106cf39c42
Dmitry Mozzherin <dmozzherin@gmail.com>
2010-11-19 18:20:21 -0500

    fixed a background bug for trusted images, refactored javascript

--- public/javascripts/curation_tools.js
+++ public/javascripts/curation_tools.js
@@ -1,10 +1,22 @@
 function undo_move(opts) {
-  alert(opts.the_node);
-  var data_object_id = opts['the_node'].parent().parent().attr('id').substring(10);
+  opts.node.unbind();
+  var data_object_id = opts.node.parent().parent().attr('id').substring(10);
   var data = "data_object_id=" + data_object_id
-  $.ajax({type: opts['method'], url: '/user_ignored_data_objects/' + opts['action'], data: data, success: function(){
+  $.ajax({type: opts.method, url: '/user_ignored_data_objects/' + opts.action, data: data, success: function(){
     $('#undo-move-' + data_object_id).addClass('hide');
     $('#curation-item-' + data_object_id).removeClass('hide');
+    opts.node.click(function() { undo_move(opts)});
+  } });
+}
+
+function toggle_ignore(opts) {
+  opts.node.unbind();
+  var data_object_id = opts.node.parent().attr('id').substring(7);
+  var data = "data_object_id=" + data_object_id
+  $.ajax({type: opts.method, url: '/user_ignored_data_objects/' + opts.action, data: data, success: function(){
+    $('#undo-move-' + data_object_id).removeClass('hide');
+    $('#curation-item-' + data_object_id).addClass('hide');
+    opts.node.click(function() { toggle_ignore(opts)});
   } });
 }
 
@@ -17,46 +29,31 @@ $(function() {
       wrap.load(this.getTrigger().attr("href"));
     }
   });
-
-
-	$('form.comment').submit(function() {
-	  form_element = this;
-	  $.post($(this).attr('action'), $(this).serialize(), function() {
-	    // TODO - move this to rjs... once we can use jQuery.  :\
-	    $(form_element).children().not(':submit, :hidden').val(''); // reset the form
-	    $(form_element).after('<p id="remove-me" class="submitted">Submitted.</p>');
-	    $(form_element).parent().children('#remove-me').fadeOut(5000);
-	  });
-	  return false;
-	});
-
-	$('.untrust_reasons input').click(function() {
-	    alert('start');
-	  form = $(this).parent().parent().parent();
-	  $.post(form.attr('action'), form.serialize());
-	    alert('stop');
-	});
-
-	$('a.undo-restore-image').click(undo_move({the_node: $(this), method: "POST", action: "create"}));
-
-	$('a.undo-ignore-image').click(undo_move({the_node: $(this), method: "DELETE", action: "destroy"}));
-
-
-	$('.is_ignored_false').find('.action-value').find('a').click(function() {
-	  var data_object_id = $(this).parent().attr('id').substring(7);
-	  var data = "data_object_id=" + data_object_id
-	  $.ajax({type: "POST", url: '/user_ignored_data_objects/create', data: data, success: function(){
-	    $('#undo-move-' + data_object_id).removeClass('hide');
-	    $('#curation-item-' + data_object_id).addClass('hide');
-	  } });
-	});
-
-	$('.is_ignored_true').find('.action-value').find('a').click(function() {
-	  var data_object_id = $(this).parent().attr('id').substring(7);
-	  var data = "data_object_id=" + data_object_id
-	  $.ajax({type: "DELETE", url: '/user_ignored_data_objects/destroy', data: data, success: function(){
-	    $('#undo-move-' + data_object_id).removeClass('hide');
-	    $('#curation-item-' + data_object_id).addClass('hide');
-	  } });
-	});
 });
+
+$('form.comment').submit(function() {
+  form_element = this;
+  $.post($(this).attr('action'), $(this).serialize(), function() {
+    // TODO - move this to rjs... once we can use jQuery.  :\
+    $(form_element).children().not(':submit, :hidden').val(''); // reset the form
+    $(form_element).after('<p id="remove-me" class="submitted">Submitted.</p>');
+    $(form_element).parent().children('#remove-me').fadeOut(5000);
+  });
+  return false;
+});
+
+$('.untrust_reasons input').click(function() {
+    alert('start');
+  form = $(this).parent().parent().parent();
+  $.post(form.attr('action'), form.serialize());
+    alert('stop');
+});
+
+$('a.undo-restore-image').click(function() { undo_move({ node: $(this), method: "POST", action: "create" })});
+
+$('a.undo-ignore-image').click(function() { undo_move({ node: $(this), method: "DELETE", action: "destroy" })});
+
+$('.is_ignored_false').find('.action-value').find('a').click(function() { toggle_ignore({ node: $(this), method: 'POST', action: 'create' })});
+
+$('.is_ignored_true').find('.action-value').find('a').click(function() { toggle_ignore({ node: $(this), method: 'DELETE', action: 'destroy' })});
+

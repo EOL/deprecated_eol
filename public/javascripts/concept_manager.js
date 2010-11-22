71e9861a8f32df2c8126e2a5f678fdbd60760f1d
Patrick Leary <pleary@mbl.edu>
2010-11-22 13:20:30 -0500

    Some small changes to the concept manager; removed some empty directories

--- public/javascripts/concept_manager.js
+++ public/javascripts/concept_manager.js
@@ -5,19 +5,19 @@ function lookup_entry(input_id) {
   lookup_id(input_id, '/concept_manager/lookup_entry/');
 }
 
-
-
-
-
-
 function lookup_id(input_id, url_prefix) {
   var input = $(input_id);
   $.ajax({
     url: url_prefix + input.val(),
-    beforeSend: function(xtr) { input.siblings(".entries").html('looking up...'); },
+    beforeSend: function(xtr) { input.siblings(".entries").html('<p style="text-align: center;"><img src="/images/green_loader.gif"/></p>'); },
     success: function(response) {
       input.siblings(".entries").html(response);
+      input.siblings(".entries").attr('taxon_concept_id', $(this).attr('id'));
     },
     error: function(xtr) { input.siblings(".entries").html('lookup failed'); }
   });
 }
+
+
+$(document).ready(function() {
+});

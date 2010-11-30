d597d439f44fcc697917e58b2e140e2cb27dcbad
Lisa Walley <lwalley@eol.org>
2010-11-30 14:26:36 -0500

    Added cucumber test for adding text, WEB-1848. Corrected values of 'for' attributes of labels in the add text form to match the 'id' of their form field element, and added col elements and CSS to replace the use of width attribute on the add text form's table td.

--- features/step_definitions/user_steps.rb
+++ features/step_definitions/user_steps.rb
@@ -13,7 +13,6 @@ end
 When /^I am logged in as (?:a |an |the )?(.+)$/ do |user_type_or_name|
   # Get actual username, create user if they don't exist, log user in
   user = get_user(user_type_or_name)
-  require 'ruby-debug'; debugger
   login_as(user)
   Then "I am logged in"
 end

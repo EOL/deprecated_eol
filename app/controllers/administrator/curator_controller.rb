a5a15743d113c40be9caea990f2919ea9ebb2398
Jeremy Rice <jrice.blue@gmail.com>
2010-12-07 13:48:37 -0800

    I added some markup to show immediately which users are not approved curators.  It's ugly, but it works.

--- app/controllers/administrator/curator_controller.rb
+++ app/controllers/administrator/curator_controller.rb
@@ -8,53 +8,39 @@ class Administrator::CuratorController < AdminController
     
   def index
     @page_title = 'Curators'
-    @user_search_string=params[:user_search_string] || ''
-    search_string_parameter='%' + @user_search_string + '%' 
-    @only_unapproved=EOLConvert.to_boolean(params[:only_unapproved])
-    @only_without_clade=EOLConvert.to_boolean(params[:only_without_clade])
+    @user_search_string = params[:user_search_string] || ''
+    @only_unapproved = EOLConvert.to_boolean(params[:only_unapproved])
+    @only_without_clade = EOLConvert.to_boolean(params[:only_without_clade])
 
-    only_unapproved_condition=' curator_approved = 0 AND ' if @only_unapproved
+    only_unapproved_condition = ' curator_approved = 0 AND ' if @only_unapproved
     if @only_without_clade
-       clade_condition="curator_hierarchy_entry_id IS NULL AND (credentials <> '' OR curator_scope<> '')"
+      clade_condition = "curator_hierarchy_entry_id IS NULL AND (credentials <> '' OR curator_scope<> '')"
     else
-      clade_condition="curator_hierarchy_entry_id IS NOT NULL OR credentials <> '' OR curator_scope<> ''"     
+      clade_condition = "curator_hierarchy_entry_id IS NOT NULL OR credentials <> '' OR curator_scope<> ''"     
     end
     
-    condition="(#{clade_condition}) AND #{only_unapproved_condition} (email like ? OR username like ? OR given_name like ? OR identity_url like ? OR family_name like ? OR username like ?)"
+    condition = "(#{clade_condition}) AND #{only_unapproved_condition} (email like ? OR username like ? OR given_name like ? OR identity_url like ? OR family_name like ? OR username like ?)"
+    search_string_parameters = ['%' + @user_search_string + '%'] * 6 # ...because there are six '?'s.
 
-    @users=User.paginate(
-                         :conditions=>[condition,
-    search_string_parameter,
-    search_string_parameter,
-    search_string_parameter,
-    search_string_parameter,
-    search_string_parameter,
-    search_string_parameter],
-    :order=>'curator_approved ASC, created_at DESC',:page => params[:page])
-    @user_count=User.count(
-                           :conditions=>[condition,
-    search_string_parameter,
-    search_string_parameter,
-    search_string_parameter,
-    search_string_parameter,
-    search_string_parameter,
-    search_string_parameter])
+    @users = User.paginate(:conditions => [condition, *search_string_parameters],
+      :order => 'curator_approved ASC, created_at DESC',:page => params[:page])
+    @user_count = User.count(:conditions => [condition, *search_string_parameters])
     
   end
 
   def export
 
-    @users=User.find(:all,:conditions=>['curator_hierarchy_entry_id IS NOT NULL OR credentials <> "" OR curator_scope<> ""'])
+    @users = User.find(:all,:conditions => ['curator_hierarchy_entry_id IS NOT NULL OR credentials <> "" OR curator_scope<> ""'])
       report = StringIO.new
       CSV::Writer.generate(report, ',') do |title|
           title << ['Id', 'Username', 'Name', 'Email', 'Credentials','Scope','Clade','Approved','Registered Date','Objects Curated','Comments Moderated','Species Curated','Objects Shown','Objects Hidden','Objects Marked as Inappropriate','Text Data Objects Submitted']
           @users.each do |u|
             clade_name = u.curator_hierarchy_entry.nil? ? '' : u.curator_hierarchy_entry.name
-            title << [u.id,u.username,u.full_name,u.email,u.credentials.gsub(/\r\n/,'; '),u.curator_scope.gsub(/\r\n/,'; '),clade_name,u.curator_approved,u.created_at,u.total_objects_curated,u.total_comments_curated,u.total_species_curated,u.total_objects_curated_by_action('show'),u.total_objects_curated_by_action('hide'),u.total_objects_curated_by_action('inappropriate'),UsersDataObject.count(:conditions=>['user_id = ?',u.id])]       
+            title << [u.id,u.username,u.full_name,u.email,u.credentials.gsub(/\r\n/,'; '),u.curator_scope.gsub(/\r\n/,'; '),clade_name,u.curator_approved,u.created_at,u.total_objects_curated,u.total_comments_curated,u.total_species_curated,u.total_objects_curated_by_action('show'),u.total_objects_curated_by_action('hide'),u.total_objects_curated_by_action('inappropriate'),UsersDataObject.count(:conditions => ['user_id = ?',u.id])]       
           end
        end
        report.rewind
-       send_data(report.read,:type=>'text/csv; charset=iso-8859-1; header=present',:filename => 'EOL_curators_report_' + Time.now.strftime("%m_%d_%Y-%I%M%p") + '.csv', :disposition =>'attachment', :encoding => 'utf8')
+       send_data(report.read,:type => 'text/csv; charset=iso-8859-1; header=present',:filename => 'EOL_curators_report_' + Time.now.strftime("%m_%d_%Y-%I%M%p") + '.csv', :disposition => 'attachment', :encoding => 'utf8')
 
   end
   

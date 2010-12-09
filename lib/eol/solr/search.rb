f9f5d546529e36a7138eae7197a20ff50c2c5ad5
Vitthal <vitthalkudal@gmail.com>
2010-12-09 15:26:36 -0500

    Refactored search.rb and created a new file lib/eol/solr.rb to use in both search.rb and solr_search_data_objects.rb

--- lib/eol/solr/search.rb
+++ lib/eol/solr/search.rb
@@ -1,18 +1,6 @@
 module EOL
   module Solr
 
-    def self.query_lucene(solr_endpoint, query, options = {})
-      query_url = solr_endpoint + '/select/?wt=json&q='
-      query_url << URI.encode(%Q[{!lucene}#{query}])
-      query_url << "&sort=" << URI.encode(%Q[#{options[:sort]}]) unless options[:sort].blank?
-      query_url << "&start=#{options[:start]}" unless options[:start].blank?
-      query_url << "&rows=#{options[:rows]}" unless options[:rows].blank?
-      query_url << "&fl=" << URI.encode(%Q[#{options[:fields]}]) unless options[:fields].blank?
-
-      res = open(query_url).read
-      JSON.load res
-    end
-
     module Search
       # Returns an array of result hashes, using will_paginate.  Don't use paginate_all_by_solr directly, as that will either fail
       # or cause duplicate queries.

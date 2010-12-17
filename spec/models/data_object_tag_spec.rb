cf66bfb85edf4815344961cb25bc2c3fa34ffc84
Jeremy Rice <jrice.blue@gmail.com>
2010-12-17 07:40:29 -0800

    Fixed a tagging problem.  Hmmmn.

--- spec/models/data_object_tag_spec.rb
+++ spec/models/data_object_tag_spec.rb
@@ -2,12 +2,6 @@ require File.dirname(__FILE__) + '/../spec_helper'
 
 describe DataObjectTag do
 
-  # be warned: as these are 1 liners, there's no down-side 
-  #            in having them ... but they do NOT test
-  #            comprehensively ... the validations might not 
-  #            even work and these will pass if the 
-  #            validations are merely defined
-
   it { should validate_presence_of(:key) }
   it { should validate_presence_of(:value) }
   it { should validate_uniqueness_of(:value) }

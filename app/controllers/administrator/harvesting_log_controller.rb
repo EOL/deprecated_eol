bb7ff258674ac20c8befcb1c99d3631fd11e1094
Patrick Leary <pleary@mbl.edu>
2011-01-03 14:21:14 -0500

    Created migration, controller, view for Harvest Process logs #WEB-1939 (cherry picked from commit 21d906c7a5e909d034f94551621b9681e0734de2)

---
+++ app/controllers/administrator/harvesting_log_controller.rb
@@ -0,0 +1,37 @@
+class Administrator::HarvestingLogController < AdminController
+  
+  layout 'left_menu'
+  before_filter :set_layout_variables
+  access_control :content_partners
+  
+  def index
+    @page_title = 'Harvesting Processes Log'
+    unless params[:date].blank?
+      if params[:date] == 'all'
+        @date = 'all'
+      else
+        @date = Time.parse(params[:date])
+      end
+    else
+      @date = 'all'
+    end
+    conditions = "began_at BETWEEN '#{@date.strftime("%Y-%m-%d")}' AND '#{(@date+1.day).strftime("%Y-%m-%d")}'" if @date != 'all'
+    @logs = HarvestProcessLog.paginate(:order => 'id desc', :page => params[:page], :conditions => conditions)
+    
+    @distinct_dates = []
+    last_date = Time.now
+    for i in 1..30
+      date = last_date.strftime("%d-%b-%Y")
+      @distinct_dates << [date, date]
+      last_date = last_date - 1.day
+    end
+  end
+  
+private
+
+  def set_layout_variables
+    @page_title = $ADMIN_CONSOLE_TITLE
+    @navigation_partial = '/admin/navigation'
+  end
+
+end

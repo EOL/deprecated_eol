e6196f1ad1e8ba23009c293396481028fb08de40
Patrick Leary <pleary@mbl.edu>
2011-01-07 17:26:09 -0500

    Added tests to make sure DataLogging can be turned on and off for #WEB-1934 (cherry picked from commit 340891983723c07c9d6c6af8143c62f4df0ef602)

---
+++ spec/integration/configuration_spec.rb
@@ -0,0 +1,69 @@
+require File.dirname(__FILE__) + '/../spec_helper'
+
+def recreate_indexes
+  solr = SolrAPI.new
+  solr.delete_all_documents
+  solr.build_indexes
+end
+
+
+describe 'Configuration' do
+  before :all do
+    truncate_all_tables
+    load_foundation_cache
+    
+    @tiger_name = 'Tiger'
+    @taxon_concept = build_taxon_concept(:common_names => [@tiger_name])
+    
+    recreate_indexes
+  end
+  
+  after :each do
+    truncate_all_logging_tables
+  end
+  
+  describe ' : DataLogging' do
+    describe ' : disabled' do
+      before :all do
+        $OLD_LOGGING_VALUE = $ENABLE_DATA_LOGGING
+        $ENABLE_DATA_LOGGING = false
+      end
+      after :all do
+        $ENABLE_DATA_LOGGING = $OLD_LOGGING_VALUE
+      end
+      
+      it 'should not generate any taxa page logs when logging is disabled' do
+        visit("/pages/#{@taxon_concept.id}")
+        PageViewLog.all.size.should == 0
+      end
+      
+      it 'should not generate any search logs when logging is disabled' do
+        visit("/search/?q=#{@tiger_name}")
+        SearchLog.all.size.should == 0
+      end
+    end
+    
+    describe ' : enabled' do
+      before :all do
+        $OLD_LOGGING_VALUE = $ENABLE_DATA_LOGGING
+        $ENABLE_DATA_LOGGING = true
+      end
+      after :all do
+        $ENABLE_DATA_LOGGING = $OLD_LOGGING_VALUE
+      end
+      
+      it 'should generate taxa page logs when logging is enabled' do
+        visit("/pages/#{@taxon_concept.id}")
+        PageViewLog.all.size.should > 0
+      end
+      
+      it 'should generate search logs when logging is enabled' do
+        visit("/search/?q=#{@tiger_name}")
+        SearchLog.all.size.should > 0
+      end
+    end
+    
+  end
+  
+end
+

91498cb4688b89eed5bb86d810dcc9cab43ef460
Jeremy Rice <jrice.blue@gmail.com>
2011-01-19 10:57:47 -0800

    removing caching on content sections... they are ONLY read by the layout, which is itself cached.

--- app/models/language.rb
+++ app/models/language.rb
@@ -25,19 +25,8 @@ class Language < SpeciesSchemaModel
     iso_639_1.upcase
   end
 
-end
-# == Schema Info
-# Schema version: 20081020144900
-#
-# Table name: languages
-#
-#  id           :integer(2)      not null, primary key
-#  iso_639_1    :string(6)       not null
-#  iso_639_2    :string(6)       not null
-#  iso_639_3    :string(6)       not null
-#  label        :string(100)     not null
-#  name         :string(100)     not null
-#  sort_order   :integer(1)      not null, default(1)
-#  source_form  :string(100)     not null
-#  activated_on :timestamp
+  def abbr
+    iso_639_1.downcase
+  end
 
+end

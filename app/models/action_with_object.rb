22fa7adfd5df8bf5cdcf7c2780091b50205ed636
Lisa Walley <lwalley@eol.org>
2011-02-25 13:20:04 -0500

    Added missing methods show and hide to action_with_object with regards to curator actions.

--- app/models/action_with_object.rb
+++ app/models/action_with_object.rb
@@ -1,10 +1,10 @@
 class ActionWithObject < ActiveRecord::Base
   CACHE_ALL_ROWS = true
   has_many :actions_histories
-  
+
   validates_presence_of   :action_code
   validates_uniqueness_of :action_code
-  
+
   # Helper to provide consistent calculation of curator actions when using actions_histories_on_data_objects
   # association
   def self.raw_curator_action_ids
@@ -19,27 +19,35 @@ class ActionWithObject < ActiveRecord::Base
   def self.rate
     cached_find(:action_code, 'rate')
   end
-  
+
   def self.created
     cached_find(:action_code, 'create')
   end
-  
+
   def self.trusted
     cached_find(:action_code, 'trusted')
   end
-  
+
   def self.untrusted
     cached_find(:action_code, 'untrusted')
   end
-  
+
+  def self.show
+    cached_find(:action_code, 'show')
+  end
+
+  def self.hide
+    cached_find(:action_code, 'hide')
+  end
+
   def self.unreviewed
     cached_find(:action_code, 'unreviewed')
   end
-  
+
   def self.inappropriate
     cached_find(:action_code, 'inappropriate')
   end
-  
+
   def self.delete
     cached_find(:action_code, 'delete')
   end

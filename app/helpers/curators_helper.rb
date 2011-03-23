a5b894aae50b2a47bcac147a8a18adf421edeb27
Vitthal <vitthalkudal@gmail.com>
2011-03-23 09:51:37 -0400

    committed the fixes related to curator worklist specified in tickets #WEB-2080 and #WEB-2121

--- app/helpers/curators_helper.rb
+++ app/helpers/curators_helper.rb
@@ -1,7 +1,11 @@
 module CuratorsHelper
 
-  def active_if_action_is(action)
-    controller.action_name == action ? 'active' : ''
+  def active_if_action_is(action, id = nil)
+    if id.nil?
+      controller.action_name == action ? 'active' : ''
+    else
+      (controller.action_name == action) && (id == controller.params[:id]) ? 'active' : ''
+    end
   end
 
 end

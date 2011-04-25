0fd8a6b8b3ddb9e49b97193900e408e2605c21d0
Eli Agbayani <eli@eol.org>
2011-04-25 02:06:49 -0400

    Undo accidental change in boot.rb

--- config/boot.rb
+++ config/boot.rb
@@ -2,7 +2,7 @@
 # Configure your app in config/environment.rb and config/environments/*.rb
 
 RAILS_ROOT = "#{File.dirname(__FILE__)}/.." unless defined?(RAILS_ROOT)
-require "thread"
+
 module Rails
   class << self
     def boot!

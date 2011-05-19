0c2a32639c0bfc6373388fcaf270a761c535b35c
Lisa Walley <lwalley@eol.org>
2011-05-19 15:57:25 -0400

    Updated HashRocket CSS and JS design files

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,4 +1,20 @@
 $(function() {
+  (function($ss) {
+    $ss.find(".images").cycle({
+      speed: 500,
+      timeout: 0,
+      pagerAnchorBuilder: function(idx) {
+        return $ss.selector + " .thumbnails a:eq(" + idx + ")";
+      }
+    });
+    $ss.find(".thumbnails a").click(function() {
+      var $e = $(this).closest("li");
+      $e.closest("ul").find(".active").removeClass("active");
+      $e.addClass("active");
+      return false;
+    });
+  })($(".gallery"));
+
   $("input[placeholder]").each(function() {
     var $e = $(this),
         placeholder = $e.attr("placeholder");

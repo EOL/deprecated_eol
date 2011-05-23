db6562f397c38c2f94da040fcd3b1d93ed60b168
Lisa Walley <lwalley@eol.org>
2011-05-23 17:08:11 -0400

    Implemented designs for show a community and show a collection pages - missing some functionality and HAML needs some work e.g. tab sections. Some issues re CSS classes in shared partials remain e.g. feed items index. WEB-2247. WEB-2249.

--- app/helpers/collection_items_helper.rb
+++ app/helpers/collection_items_helper.rb
@@ -1,6 +1,22 @@
 module CollectionItemsHelper
 
-  def collection_item_link_to(item)
+  def collection_item_name_link_to(item)
+    case item.object_type
+    when 'TaxonConcept'
+      link_to item.name, taxon_concept_path(item.object)
+    when 'DataObject'
+      link_to item.name, data_object_path(item.object)
+    when 'Community'
+      link_to item.name, community_path(item.object)
+    when 'User'
+      link_to item.name, user_infor_path(item.object)
+    when 'Collection'
+      link_to item.name, collection_path(item.object)
+    else
+    end
+  end
+
+  def collection_item_view_type_link_to(item)
     case item.object_type
     when 'TaxonConcept'
       link_to I18n.t(:view_taxon_link), taxon_concept_path(item.object), :title => strip_tags(item.name)

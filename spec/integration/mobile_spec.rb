7f692caff2d106a7e8de1dd2cc940de9671341c9
Silvio Relli <silvio@relli.org>
2011-05-28 16:21:11 +0200

    Added /mobile route and cleaned a test

--- spec/integration/mobile_spec.rb
+++ spec/integration/mobile_spec.rb
@@ -11,8 +11,6 @@ describe 'Mobile' do
     open_session # Opens a new session instance
     headers = {"User-Agent" => "iPhone"}
     request_via_redirect(:get, '/', {}, headers) # Allows you to make an HTTP request and follow any subsequent redirects.
-    puts "----------------------------------------------- current_path: #{current_path} ------------------"
-    puts "----------------------------------------------- path: #{path} ------------------"
     assert_equal '/mobile/contents', path
   end
   

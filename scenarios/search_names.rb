6170537f3674a91a0e8e23d3cea4166ed19eb7e0
Jeremy Rice <jrice@eol.org>
2011-05-31 16:22:48 -0600

    Fixing some errors after applhying WEB-2227 cherry-pick.

--- scenarios/search_names.rb
+++ scenarios/search_names.rb
@@ -31,8 +31,7 @@ results[:plantain].add_scientific_name_synonym(results[:plantain_synonym])
 another = build_taxon_concept(:scientific_name => "#{results[:plantain_name]} L.",
                               :common_names => ["big #{results[:plantain_common]}"])
 another.add_scientific_name_synonym(results[:plantain_synonym]) # I'm only doing this so we get two results and not redirected.
-SearchSuggestion.gen(:taxon_id => results[:plantain].id, :scientific_name => results[:plantain_name],
-                     :term => results[:plantain_name], :common_name => results[:plantain_common])
+SearchSuggestion.gen(:taxon_id => results[:plantain].id, :term => results[:plantain_name])
 
 results[:dog_name]      = 'Dog'
 results[:domestic_name] = "Domestic #{results[:dog_name]}"
@@ -43,19 +42,14 @@ results[:wolf] = build_taxon_concept(:scientific_name => results[:wolf_sci_name]
 results[:dog]  = build_taxon_concept(:scientific_name => results[:dog_sci_name], :common_names => [results[:domestic_name]],
                                      :parent_hierarchy_entry_id => results[:wolf].hierarchy_entries.first.id)
 
-SearchSuggestion.gen(:taxon_id => results[:dog].id, :term => results[:dog_name],
-                     :scientific_name => results[:dog].scientific_name,
-                     :common_name => results[:dog].common_name)
-SearchSuggestion.gen(:taxon_id => results[:wolf].id, :term => results[:dog_name],
-                     :scientific_name => results[:wolf].scientific_name,
-                     :common_name => results[:wolf].common_name)
+SearchSuggestion.gen(:taxon_id => results[:dog].id, :term => results[:dog_name])
+SearchSuggestion.gen(:taxon_id => results[:wolf].id, :term => results[:dog_name])
 
 results[:tricky_search_suggestion] = 'Bacteria'
 results[:bacteria_common] = results[:tricky_search_suggestion]
 results[:bacteria] = build_taxon_concept(:scientific_name => results[:tricky_search_suggestion],
                                          :common_names => [results[:bacteria_common]])
-SearchSuggestion.gen(:taxon_id => results[:bacteria].id, :scientific_name => results[:tricky_search_suggestion],
-                     :term => results[:tricky_search_suggestion], :common_name => results[:bacteria_common])
+SearchSuggestion.gen(:taxon_id => results[:bacteria].id, :term => results[:tricky_search_suggestion])
 
 # I'm only doing this so we get two results and not redirected.
 build_taxon_concept(:scientific_name => results[:tricky_search_suggestion])

29e72e42abe3d0edeb597c6d6736e5fd6e145976
Silvio Relli <silvio@relli.org>
2011-06-07 17:59:08 +0200

    Tests for mobile-to-full-switch story

--- spec/integration/mobile_spec.rb
+++ spec/integration/mobile_spec.rb
@@ -14,6 +14,51 @@ describe 'Mobile' do
     assert_equal '/mobile/contents', path
   end
   
+  it 'should have a link for going from mobile to full site' do
+    open_session
+    headers = {"User-Agent" => "iPhone"}
+    visit "/mobile/contents"
+    #page.should have_content("<a onclick=\"jQuery.ajax({data:\'\', dataType:\'script\', type:\'post\', url:\'/mobile/contents/disable\'}); return false;\" href=\"#\" class=\"button flip\">Full site</a>\"")
+    page.should have_link("Full site")
+    click_link("Full site")
+  end
   
+  it 'should remember user decision to browse the full app' do
+    open_session
+    headers = {"User-Agent" => "iPhone"}
+    page.driver.post('/mobile/contents/disable') #AJAX request fired when user clicks on "Full site"
+    page.driver.status_code.should eql 200
+    body.should include "window.location.href = \"/\";" #AJAX response redirect to full app homepage
+    # TO-DO Test session cookie, something like:   session[:mobile_disabled].should == true
+    
+    ###############################################
+    #OLD attempts - keeping them for reference   
+    #{:post => "/mobile/contents/disable"}.should route_to(:controller => "mobile/contents", :action => "disable")
+    #--------------
+    #visit("/mobile/contents/disable", :method => :post)
+    #response.should be_redirect
+    #--------------
+    #visit("/mobile/contents/disable", :method => :post)
+    #should route_to(:controller => "mobile/contents", :action => "disable")
+    #--------------
+    #visit "/mobile/contents"
+    #visit("/mobile/contents/disable", :method => :post)
+    #assert_equal '/', path
+    #response.should redirect_to('/')  
+    #body.should include "Global access to knowledge about life on Earth"
+    #--------------
+    #cookies = Capybara.current_session.driver.current_session.instance_variable_get(:@rack_mock_session).cookie_jar    
+    #cookies[:mobile_disabled].should be_nil
+    ###############################################
+  end
+  
+  it 'should remember user decision to browse the mobile app' do
+    open_session
+    headers = {"User-Agent" => "iPhone"}
+    page.driver.post('/mobile/contents/enable') #AJAX request fired when user clicks on "Full site"
+    page.driver.status_code.should eql 200
+    body.should include "window.location.href = \"/mobile/contents\";" #AJAX response redirect to full app homepage
+    # TO-DO Test session cookie, something like:   session[:mobile_disabled].should == false
+  end
   
 end

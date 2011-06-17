c18dea5e63db249131e3130aa8d7f48c8e37c259
Lisa Walley <lwalley@eol.org>
2011-06-17 10:03:36 -0400

    Added exemplar image to taxon details view.

--- app/controllers/taxa/details_controller.rb
+++ app/controllers/taxa/details_controller.rb
@@ -9,7 +9,8 @@ class Taxa::DetailsController < TaxaController
       { :published_hierarchy_entries => [ :name , :hierarchy, :hierarchies_content, :vetted ] },
       { :data_objects => { :toc_items => :info_items } },
       { :curator_activity_logs => :user },
-      { :users_data_objects => { :data_object => :toc_items } }]
+      { :users_data_objects => { :data_object => :toc_items } },
+      { :taxon_concept_exemplar_image => :data_object }]
     selects = {
       :taxon_concepts => '*',
       :hierarchy_entries => [ :id, :rank_id, :identifier, :hierarchy_id, :parent_id, :published, :visibility_id, :lft, :rgt, :taxon_concept_id, :source_url ],
@@ -17,17 +18,18 @@ class Taxa::DetailsController < TaxaController
       :hierarchies => [ :agent_id, :browsable, :outlink_uri, :label ],
       :hierarchies_content => [ :content_level, :image, :text, :child_image, :map, :youtube, :flash ],
       :vetted => :view_order,
-      :data_objects => [ :id, :data_type_id, :vetted_id, :visibility_id, :published, :guid, :data_rating ],
+      :data_objects => [ :id, :data_type_id, :vetted_id, :visibility_id, :published, :guid, :data_rating, :object_cache_url ],
       :table_of_contents => '*',
       :curator_activity_logs => '*',
-      :users => [ :given_name, :family_name, :logo_cache_url ] }
+      :users => [ :given_name, :family_name, :logo_cache_url ] ,
+      :taxon_concept_exemplar_image => '*' }
     @taxon_concept = TaxonConcept.core_relationships(:include => includes, :select => selects).find_by_id(@taxon_concept.id)
 
     @details = @taxon_concept.details_for_toc_items(ContentTable.details.toc_items)
 
     @toc = TocBuilder.new.toc_for_toc_items(@details.collect{|d| d[:toc_item]})
 
-    @exemplar_image = 'FIXME'
+    @exemplar_image = @taxon_concept.taxon_concept_exemplar_image.data_object unless @taxon_concept.taxon_concept_exemplar_image.blank?
 
     @assistive_section_header = I18n.t(:assistive_details_header)
 

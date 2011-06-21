9c19cd7d362eac438dea471cbe4c41ad697543d9
Silvio Relli <silvio@relli.org>
2011-06-21 16:52:43 +0200

    Updates and fixes for working with last upstream commits

--- lib/taxa.rb
+++ lib/taxa.rb
@@ -23,6 +23,19 @@ module Taxa
     @taxon_concept = find_taxon_concept
   end
   
+  # from taxa_controller
+  def promote_exemplar(data_objects)
+    return data_objects if @taxon_concept.blank? || data_objects.blank? || @taxon_concept.taxon_concept_exemplar_image.blank?
+    data_objects.each_with_index do |m, index|
+      if m.id == @taxon_concept.taxon_concept_exemplar_image.data_object_id
+        data_objects.delete_at(index)
+        data_objects.unshift(m)
+        break
+      end
+    end
+    data_objects
+  end
+  
   # from taxa_controlle
   def redirect_if_invalid
     redirect_to_missing_page_on_error do

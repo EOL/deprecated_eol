a4aee40e8f0478df160f401b9a93d969223e8340
Jeremy Rice <jrice@eol.org>
2011-06-24 10:26:12 -0600

    Re-enabling metrics... if you want to disable them, could you please use a rescue block instead of commenting them out?  Thanks.  Also, some updates to the README to reflect changes and update the introduction.

--- tmp/metric_fu/output/flay.js
+++ tmp/metric_fu/output/flay.js
@@ -6,6 +6,6 @@
       g.marker_font_size = "10px"
 
         g.title = 'Flay: duplication';
-        g.data('flay', [244,1754]);
-        g.labels = {"0":"12/23","1":"1/28"};
+        g.data('flay', [244,1754,2288]);
+        g.labels = {"0":"12/23","1":"1/28","2":"6/24"};
         g.draw();

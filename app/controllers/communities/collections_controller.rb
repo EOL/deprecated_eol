7ba53b8c5d620b034d264b53cd550fbc603ba432
Lisa Walley <lwalley@eol.org>
2011-06-29 13:32:31 -0400

    Integrated HAML changes from HashRocket for collections show and collections tab on profile and community.

--- app/controllers/communities/collections_controller.rb
+++ app/controllers/communities/collections_controller.rb
@@ -3,7 +3,7 @@ class Communities::CollectionsController < CommunitiesController
   before_filter :load_community_and_dependent_vars, :only => [:index]
 
   def index
-    # TODO: Sort endorsed collections param[:sort_by] :oldest or :newest
+    # TODO: Sort collections param[:sort_by] :oldest or :newest
   end
 
 end

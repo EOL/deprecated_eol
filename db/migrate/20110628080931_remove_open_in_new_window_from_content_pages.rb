ddee3b70640c1f854feca95d7fd21ea84b6cea25
Eli Agbayani <eli@eol.org>
2011-07-05 11:01:48 -0400

    Remove identical migration

--- db/migrate/20110628080931_remove_open_in_new_window_from_content_pages.rb
+++
@@ -1,9 +0,0 @@
-class RemoveOpenInNewWindowFromContentPages < ActiveRecord::Migration
-  def self.up
-    remove_column :content_pages, :open_in_new_window
-  end
-
-  def self.down
-    add_column :content_pages, :open_in_new_window, :tinyint
-  end
-end

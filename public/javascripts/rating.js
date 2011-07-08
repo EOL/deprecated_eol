e7c99514a32d9afc34a466f53ea134005c2dcfde
Lisa Walley <lwalley@eol.org>
2011-07-08 14:23:54 -0400

    Added HashRocket JS and SASS files up to and including HR commit 401218af931 2011-07-08.

--- public/javascripts/rating.js
+++ public/javascripts/rating.js
@@ -1,24 +1,11 @@
-if(!EOL) { var EOL = {}; }
-if(!EOL.Rating) { EOL.Rating = {}; }
-
-// These do get called from rjs and curation.js, so need to be methods, despite being simple:
-EOL.Rating.update_average_rating = function(data_object_id, rating) {
-  $('#average-rating-'+data_object_id).css('width', rating * 20+'%');
-};
-EOL.Rating.update_user_rating = function(data_object_id, rating) {
-  $('#user-rating-'+data_object_id).css('width', rating * 20+'%');
-};
-
-// Set up the links to be clickable. A 'scope' of an empty string ('') will do everything on the page...
-EOL.Rating.init_links = function(scope) {
-  if (scope.length > 0) { scope = scope + ' '; }
-  $(scope+'ul.small-star li a').click(function(e) {
-    EOL.Rating.update_user_rating($(this).attr('data-data_object_id'), $(this).html());
-    $.ajax({url: $(this).attr('href'), type: 'PUT'});
+$(function() {
+  $(".rating ul a").click(function() {
+    var $e = $(this),
+        idx = $e.closest("ul").find("a").index($e);
+    idx++;
+    $e.closest("ul").find("li[class^=current]").removeClass()
+      .addClass("current_rating_" + idx).text("Current rating: " + idx + " of 5");
+    $e.closest(".ratings").addClass("rated");
     return false;
   });
-};
-
-$(document).ready(function() {
-  EOL.Rating.init_links('');
 });

8ce1beb4a6e5fb853be3c641797c81e8d928a72d
Jeremy Rice <jrice@eol.org>
2011-07-10 11:01:42 -0600

    ...And now select all works via JS, if enabled.

--- public/javascripts/collections.js
+++ public/javascripts/collections.js
@@ -1,5 +1,8 @@
 $(document).ready(function() {
   // Submit the sort when it's changed:
   $('#sort_by').change(function() {$(this).closest('form').submit();});
+  $('input[name=commit_select_all]').click(function() {
+    $('.object_list input[name="collection_items[]"]').prop('checked', true); return(false);
+  });
   $('input[name=commit_sort]').hide();
 });

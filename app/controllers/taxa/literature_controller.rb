fb9c98ffb75d9c299b1f16ff1e65fb1963a6795a
Patrick Leary <pleary@mbl.edu>
2011-07-23 21:29:41 -0400

    Added Literature controller, views and functionality

---
+++ app/controllers/taxa/literature_controller.rb
@@ -0,0 +1,35 @@
+class Taxa::LiteratureController < TaxaController
+  before_filter :instantiate_taxon_concept
+  
+  def show
+    references
+  end
+  
+  def references
+    @assistive_section_header = I18n.t(:assistive_literature_header)
+    @references = Ref.find_refs_for(@taxon_concept.id)
+    pp @references
+    @references = Ref.sort_by_full_reference(@references)
+    current_user.log_activity(:viewed_taxon_concept_literature, :taxon_concept_id => @taxon_concept.id)
+  end
+  
+  def bhl
+    @assistive_section_header = I18n.t(:assistive_literature_header)
+    @sort = params[:sort]
+    @page = params[:page]
+    @sort = 'year' unless ['title', 'title_desc', 'year', 'year_desc'].include?(@sort)
+    @bhl_results = EOL::Solr::BHL.search(@taxon_concept, :sort => @sort, :page => @page)
+    current_user.log_activity(:viewed_taxon_concept_literature, :taxon_concept_id => @taxon_concept.id)
+  end
+  
+  def bhl_title
+    @title_item_id = params[:title_item_id]
+    unless @title_item_id && @title_item_id.is_numeric?
+      redirect_to bhl_taxon_literature_path(@taxon_concept)
+    end
+    @assistive_section_header = I18n.t(:assistive_literature_header)
+    @bhl_title_results = EOL::Solr::BHL.search_publication(@taxon_concept, @title_item_id)
+    
+    current_user.log_activity(:viewed_taxon_concept_bhl_title, :taxon_concept_id => @taxon_concept.id)
+  end
+end
--- lib/eol/solr/bhl.rb
+++ lib/eol/solr/bhl.rb
@@ -3,62 +3,71 @@ module EOL
     class BHL
       def self.search(taxon_concept, options = {})
         options[:page]        ||= 1
-        options[:per_page]    ||= 300
+        options[:per_page]    ||= 100
         options[:per_page]      = 10 if options[:per_page] == 0
         
         response = solr_search(taxon_concept, options)
-        return response
+        
+        total_results = response['grouped']['title_item_id']['ngroups']
+        query_time = response['responseHeader']['QTime']
+        return_hash = { :time => query_time,
+                        :total => total_results }
+        
+        return_hash[:results] = []
+        response['grouped']['title_item_id']['groups'].each do |g|
+          doc = g['doclist']['docs'][0].merge({ 'numFound' => g['doclist']['numFound'] })
+          return_hash[:results] << doc
+        end
+        
+        return_hash[:results] = WillPaginate::Collection.create(options[:page], options[:per_page], total_results) do |pager|
+           pager.replace(return_hash[:results])
+        end
+        
+        return return_hash
       end
-
+      
+      def self.search_publication(taxon_concept, title_item_id, options = {})
+        options[:page]        ||= 1
+        options[:per_page]    ||= 500
+        options[:per_page]      = 10 if options[:per_page] == 0
+        options[:sort] = 'number'
+        response = solr_search(taxon_concept, options.merge({ :title_item_id => title_item_id }))
+        query_time = response['responseHeader']['QTime']
+        total_results = response['response']['numFound']
+        return_hash = { :time => query_time,
+                        :total => total_results,
+                        :results => response['response']['docs'] }
+        return return_hash
+      end
+      
       private
       
       def self.solr_search(taxon_concept, options = {})
-        url =  $SOLR_SERVER + $SOLR_BHL_CORE + '/select/?wt=json&fl=publication_title,details&q=' + CGI.escape(%Q[{!lucene}])
         
-        
-        server_url = $SOLR_SERVER
-        core = $SOLR_BHL_CORE
-        server_url += '/' unless server_url[-1,1] == '/'
-        action_url = server_url + core.to_s
-        # this one should NOT end in a slash
-        action_url = action_url[0...-1] if action_url[-1,1] == '/'
-        action_uri = URI.parse(action_url)
-        
-        name_ids = TaxonConceptName.find_all_by_taxon_concept_id_and_vern(taxon_concept, 0, :select => 'name_id').collect{|tcn| tcn.name_id}.uniq.sort
-        name_ids = name_ids[0..400]
-        
-        # post_url = action_url + "/select"
-        # request = Net::HTTP::Post.new(post_url)
-        # request.set_form_data({"wt" => "json", "q" => "name_id:(#{name_ids.join(' ')})"})
-        # response = Net::HTTP.start(action_uri.host, action_uri.port) do |http|
-        #   http.open_timeout = 30
-        #   http.read_timeout = 240
-        #   http.request(request)
-        # end
-        # rescue Timeout::Error => e
-        #   puts "Timeout accessing #{post_url}"
-        #   pp e.message
-        #   pp e.backtrace
-        #   nil
-        # rescue => e
-        #   puts "Error accessing #{post_url}"
-        #   pp e.message
-        #   pp e.backtrace
-        #   nil
-        # 
-        # return
-        
-        url << CGI.escape("name_id:(#{name_ids.join(' ')})")
-        
-        # # add sorting
-        # if options[:sort_by] == SortStyle.newest
-          url << '&sort=' + CGI.escape("year asc, start_year asc, publication_title asc, number asc")
-        # elsif options[:sort_by] == SortStyle.oldest
-        #   url << '&sort=date_modified+asc'
-        # end
-        
-        url << '&fl=' + CGI.escape("publication_title, details, year, start_year, end_year, volume, issue, number, prefix");
-        
+        url =  $SOLR_SERVER + $SOLR_BHL_CORE + '/select/?wt=json&q=' + CGI.escape(%Q[{!lucene}])
+        name_ids = TaxonConceptName.connection.select_values("SELECT DISTINCT(name_id) FROM taxon_concept_names WHERE taxon_concept_id=#{taxon_concept.id} AND vern=0 LIMIT 100")
+        url << CGI.escape("(name_id:(#{name_ids.join(' ')}) NOT year:0)")
+        if options[:title_item_id]
+          url << CGI.escape(" AND title_item_id:#{options[:title_item_id]}")
+        end
+        # add sort
+        if options[:sort] == 'number'
+          url << '&sort=' + CGI.escape("number asc")
+        elsif options[:sort] == 'title'
+          url << '&sort=' + CGI.escape("publication_title asc, volume asc, year asc")
+        elsif options[:sort] == 'title_desc'
+          url << '&sort=' + CGI.escape("publication_title desc, volume asc, year asc")
+        elsif options[:sort] == 'year_desc'
+          url << '&sort=' + CGI.escape("year desc, publication_title asc, volume desc")
+        else # year
+          url << '&sort=' + CGI.escape("year asc, publication_title asc, volume asc")
+        end
+        # add group by publication
+        unless options[:title_item_id]
+          url << "&group=true&group.ngroups=true&group.field=title_item_id"
+        end
+        # return fields
+        url << '&fl=id,publication_title,publication_id,title_item_id,details,year,start_year,end_year,volume,issue,number,prefix,volume_info'
         # add paging
         limit  = options[:per_page] ? options[:per_page].to_i : 10
         page = options[:page] ? options[:page].to_i : 1

ee00162d6723d08d04629ba6270f29eb79dfd210
Patrick Leary <pleary@mbl.edu>
2011-07-29 11:43:03 -0400

    Added create_defaults to recent migrations

--- app/models/user_identity.rb
+++ app/models/user_identity.rb
@@ -17,7 +17,7 @@ class UserIdentity < ActiveRecord::Base
     default_identites.each do |label|
       if !self.cached_find_translated(:label, label)
         ui = UserIdentity.create(:sort_order => sort_order)
-        TranslatedUserIdentity.create(:label => label, :language => Language.english, :user_identity => ui)
+        TranslatedUserIdentity.create(:label => label, :language => Language.english_for_migrations, :user_identity => ui)
       end
       sort_order += 1
     end

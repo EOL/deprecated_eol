e0243c2ca3590d9d432cfef7b4012262b41ef5e6
Lisa Walley <lwalley@eol.org>
2011-07-30 02:34:43 -0400

    Tidied up user collections tab, and fixed styles for page actions.

--- app/controllers/users/collections_controller.rb
+++ app/controllers/users/collections_controller.rb
@@ -2,9 +2,9 @@ class Users::CollectionsController < UsersController
 
   def index
     @user = User.find(params[:user_id])
-    @collection_items = @user.collection_items
-    @collections = (params[:sort_by] && params[:sort_by].to_sym == :oldest) ?
+    @featured_collections = (params[:sort_by] && params[:sort_by].to_sym == :oldest) ?
       @user.collections.sort_by(&:created_at) : @user.collections.sort_by{|c| - c.created_at.to_i}
+    @featured_collections.delete_if{|c| c.id == @user.watch_collection.id}
   end
 
 end

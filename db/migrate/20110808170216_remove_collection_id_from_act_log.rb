7e645c1dfdff73201be65beb549473179dc70abe
Jeremy Rice <jrice@eol.org>
2011-08-08 20:02:46 -0600

    Sorry, JRice is a dork.

--- db/migrate/20110808170216_remove_collection_id_from_act_log.rb
+++ db/migrate/20110808170216_remove_collection_id_from_act_log.rb
@@ -1,10 +1,9 @@
 class RemoveCollectionIdFromActLog < EOL::LoggingMigration
   def self.up
-    # TODO: this column never existed. Do we need this migration?
-    # remove_column :community_activity_logs, :collection_id
+    remove_column :community_activity_logs, :collection_id
   end
 
   def self.down
-    # add_column :community_activity_logs, :collection_id, :integer
+    add_column :community_activity_logs, :collection_id, :integer
   end
 end

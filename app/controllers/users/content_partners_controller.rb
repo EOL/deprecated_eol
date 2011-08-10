08833b003417635019fc2e4bd173730151b03ce9
Lisa Walley <lwalley@eol.org>
2011-08-10 11:51:56 -0400

    WIP: Restricing access to add content partner link to only users viewing themselves.

--- app/controllers/users/content_partners_controller.rb
+++ app/controllers/users/content_partners_controller.rb
@@ -5,7 +5,6 @@ class Users::ContentPartnersController < UsersController
     @user = User.find(params[:user_id])
     # TODO: Will need to be modified when we move to many to many relationship between users and partners
     @partners = [@user.content_partner].compact
-    @new_partner = ContentPartner.new
   end
 
 end

cf7855cfaa17900947d908d2b62ac850e022d626
Vitthal Kudal <vitthalkudal@gmail.com>
2011-08-22 11:10:45 -0400

    removed a method defined twice in the worklist_controller

--- app/controllers/taxa/worklist_controller.rb
+++ app/controllers/taxa/worklist_controller.rb
@@ -57,17 +57,7 @@ class Taxa::WorklistController < TaxaController
       return
     end
   end
-  
-  def data_objects
-    @current_data_object = DataObject.find(params[:data_object_id])
-    params.delete(:worklist_return_to)
-    params.delete(:data_object_id)
-    params.delete(:action)
-    params[:worklist_return_to] = taxon_worklist_data_object_path(@taxon_concept, @current_data_object)
-    params[:force_return_to] = taxon_worklist_data_object_path(@taxon_concept, @current_data_object)
-    render(:partial => 'curation_content')
-  end
-  
+
   def data_objects
     @current_data_object = DataObject.find(params[:data_object_id])
     params.delete(:worklist_return_to)

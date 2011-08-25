1542c997cf0105daa5d233e541efed38f581d74f
lwalley <lwalley@eol.org>
2011-08-25 05:28:32 -0400

    Minor style and assistive tweaks to taxon header.

--- app/controllers/taxa/communities_controller.rb
+++ app/controllers/taxa/communities_controller.rb
@@ -4,16 +4,19 @@ class Taxa::CommunitiesController < TaxaController
 
   def show
     @curators = @taxon_concept.acting_curators
+    @assistive_section_header = I18n.t(:assistive_taxon_community_header)
     current_user.log_activity(:viewed_taxon_concept_community_curators, :taxon_concept_id => @taxon_concept.id)
   end
 
   def collections
     @collections = @taxon_concept.collections.select{ |c| !c.watch_collection? }
+    @assistive_section_header = I18n.t(:assistive_taxon_community_header)
     current_user.log_activity(:viewed_taxon_concept_community_collections, :taxon_concept_id => @taxon_concept.id)
   end
 
   def communities
     @communities = @taxon_concept.communities
+    @assistive_section_header = I18n.t(:assistive_taxon_community_header)
     current_user.log_activity(:viewed_taxon_concept_community_communities, :taxon_concept_id => @taxon_concept.id)
   end
 

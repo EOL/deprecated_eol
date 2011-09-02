9c540c2103e2ac50be94db9e9fdb16ac9f1c4fed
lwalley <lwalley@eol.org>
2011-09-02 02:50:50 -0400

    Refactored data object sound and jplayer partials, removing inline javascript, adjusting styles and made some improvements for accessibility. Tested in Firefox, Safari and Chrome on Mac, testing still needed on Windows browsers.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -128,14 +128,17 @@ module ApplicationHelper
   # We can include the item description but its not always going to make sense or be short enough
   # ideally we need a short description provided by the content partner, describing what is in the item.
   def alternative_text(data_object, en_type, taxon_concept = nil)
-    taxon_name = taxon_concept.title_canonical() unless taxon_concept.nil?
-    taxon_name = taxon_name.blank? ? I18n.t(:a_taxon) : Sanitize.clean(taxon_name)
-    data_object_vetted = data_object.vetted_by_taxon_concept(taxon_concept, :find_best => true) unless taxon_concept.nil?
-    data_object_vetted_label = (data_object_vetted.blank? || data_object_vetted.label.blank?) ? "" : data_object_vetted.label
     alt = data_object.object_title || nil
-    alt = data_object.description_teaser if alt.blank?
-    alt = I18n.t("#{en_type}_alt_text", :vetted_status => data_object_vetted_label.downcase,
+    if alt.blank? && data_object.description_teaser.blank?
+      taxon_name = taxon_concept.title_canonical() unless taxon_concept.nil?
+      taxon_name = taxon_name.blank? ? I18n.t(:a_taxon) : Sanitize.clean(taxon_name)
+      data_object_vetted = data_object.vetted_by_taxon_concept(taxon_concept, :find_best => true) unless taxon_concept.nil?
+      data_object_vetted_label = (data_object_vetted.blank? || data_object_vetted.label.blank?) ? "" : data_object_vetted.label
+      alt = I18n.t("#{en_type}_alt_text", :vetted_status => data_object_vetted_label.downcase,
                  :taxon_name => taxon_name) if alt.blank?
+    else
+      alt = data_object.description_teaser
+    end
     alt = Sanitize.clean(alt)
   end
 

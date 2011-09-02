0b9e9ccbaca190c6ec263423f670d3c5d96f7439
Jeremy Rice <jrice@eol.org>
2011-09-02 09:20:32 -0600

    I really want the top collections sorted correctly, even if this isn't the fastest algorithm.

--- app/models/taxon_concept.rb
+++ app/models/taxon_concept.rb
@@ -1215,7 +1215,7 @@ class TaxonConcept < SpeciesSchemaModel
   def top_collections
     return @top_collections if @top_collections
     all_containing_collections = Collection.which_contain(self).select{ |c| !c.watch_collection? }
-    @top_collections = all_containing_collections.sort_by(&:relevance)[0..2]
+    @top_collections = all_containing_collections.sort_by(&:relevance).reverse.[0..2]
   end
 
   def flattened_ancestor_ids

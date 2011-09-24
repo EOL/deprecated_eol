126d1cb48f5199fac42118dcc53bfac131f45bdb
Johan Harjono <johanharjono@gmail.com>
2011-09-24 17:13:19 -0400

    Trimmed down the layout for mobile even more

--- app/controllers/mobile/mobile_controller.rb
+++ app/controllers/mobile/mobile_controller.rb
@@ -1,6 +1,6 @@
 ## Head mobile controller.
 ## All mobile app controllers must hereditate from this.
 class Mobile::MobileController < ApplicationController
-  layout "v2/mobile/application.html.haml"
+  layout "v2/mobile/application"
 
 end

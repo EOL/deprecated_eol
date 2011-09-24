b97b07c04d3aab59eca1f096c0868e54ebd7e84e
Johan Harjono <johanharjono@gmail.com>
2011-09-24 17:07:29 -0400

    change the layout call to use Rails default

--- app/controllers/mobile/mobile_controller.rb
+++ app/controllers/mobile/mobile_controller.rb
@@ -1,6 +1,6 @@
 ## Head mobile controller.
 ## All mobile app controllers must hereditate from this.
 class Mobile::MobileController < ApplicationController
-  layout "#{RAILS_ROOT}/app/views/layouts/v2/mobile/application.html.haml"
+  layout "v2/mobile/application.html.haml"
 
 end

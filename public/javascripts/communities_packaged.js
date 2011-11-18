c2eb2a2da884e61b7e36724ece701cd28590a74c
Jeremy Rice <jrice@eol.org>
2011-11-18 14:01:08 -0700

    Fixed some assets...

---
+++ public/javascripts/communities_packaged.js
@@ -0,0 +1,2 @@
+if($.fn.spotlite!=undefined){$(function(){$.ajax({url:"/users/usernames",dataType:'json',success:function(data){$("dd.invite").spotlite({pool:data}).find("textarea").hide().end().find(":text").show();$("#community_new form :submit").click(function(){var i=0
+$(this).closest('form').find('.spotlite-results input').remove();$(this).closest('form').find('.spotlite-results li').each(function(){$(this).append('<input type="hidden" value="'+$(this).html()+'" name="invite_list['+i+++']"/>')});});}});});}

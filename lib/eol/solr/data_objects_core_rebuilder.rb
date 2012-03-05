069760624af68538ca306cf9a89a6b342ccba613
Patrick Leary <pleary@mbl.edu>
2012-03-05 00:30:02 -0500

    Fixed checking of parameters for data_objects_from_solr; Faster checking for empty values when querying Solr (cherry picked from commit e2dd91eda5d42dae5ceeb4522828ac32214986c8)

--- lib/eol/solr/data_objects_core_rebuilder.rb
+++ lib/eol/solr/data_objects_core_rebuilder.rb
@@ -79,7 +79,7 @@ module EOL
           'data_subtype_id' => data_object.data_subtype_id || 0,
           'published' => data_object.published? ? 1 : 0,
           'data_rating' => data_object.data_rating,
-          'language_id' => data_object.language_id,
+          'language_id' => data_object.language_id || 0,
           'license_id' => data_object.license_id,
           'created_at' => data_object.created_at ? data_object.created_at.solr_timestamp : '1960-01-01T00:00:01Z'
         }

6786bca29e1dce4b8cb94ea9fcfd02cf15a0406a
Jeremy Rice <jrice@eol.org>
2012-03-26 11:13:47 -0600

    Collection item edits don't superfluously attempt to re-load the item it just saved.  [shakes head in self-disappointment]

--- app/controllers/collections_controller.rb
+++ app/controllers/collections_controller.rb
@@ -388,27 +388,22 @@ private
   def annotate
     if @collection.update_attributes(params[:collection])
       if @collection.show_references
-        collection_item = CollectionItem.find(params[:collection][:collection_items_attributes].keys.map {|i|
+        @collection_item = CollectionItem.find(params[:collection][:collection_items_attributes].keys.map {|i|
               params[:collection][:collection_items_attributes][i][:id] }.first)
-        collection_item.refs.clear
+        @collection_item.refs.clear
         unless params[:references].blank?
           params[:references].split("\n").each do |original_ref|
             reference = original_ref.strip
             unless reference.blank?
               ref = Ref.find_or_create_by_full_reference_and_user_submitted_and_published_and_visibility_id(reference, 1, 1, Visibility.visible.id)
-              collection_item.refs << ref
-              collection_item.save!
+              @collection_item.refs << ref
+              @collection_item.save!
             end
           end
         end
       end
       respond_to do |format|
         format.js do
-          # Sorry this is confusing, but we don't know which attribute number will have the id:
-          CollectionItem.with_master do
-            @collection_item = CollectionItem.find(params[:collection][:collection_items_attributes].keys.map {|i|
-              params[:collection][:collection_items_attributes][i][:id] }.first)
-          end
           render :partial => 'collection_items/show_editable_attributes',
             :locals => { :collection_item => @collection_item, :item_editable => true }
         end

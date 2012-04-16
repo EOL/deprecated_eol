f75f93ef25f226c75d848442aa4cd102b6476edc
Vitthal Kudal <vitthalkudal@gmail.com>
2012-04-16 17:10:44 -0400

    Fixed migration

--- db/migrate/20120416134434_rename_serbian_cyrillic_and_latin_languages.rb
+++ db/migrate/20120416134434_rename_serbian_cyrillic_and_latin_languages.rb
@@ -1,11 +1,11 @@
 class RenameSerbianCyrillicAndLatinLanguages < ActiveRecord::Migration
   def self.up
-    execute "UPDATE languages SET iso_639_1 = 'sr-EC' WHERE iso_639_1 = 'sr'"
-    execute "UPDATE languages SET iso_639_1 = 'sr-el' WHERE iso_639_1 = 'sr-CS'"
+    execute "UPDATE languages SET iso_639_1 = 'sr' WHERE iso_639_1 = 'sr-EC'"
+    execute "UPDATE languages SET iso_639_1 = 'sr-CS' WHERE iso_639_1 = 'sr-el'"
   end
 
   def self.down
-    execute "UPDATE languages SET iso_639_1 = 'sr' WHERE iso_639_1 = 'sr-EC'"
-    execute "UPDATE languages SET iso_639_1 = 'sr-CS' WHERE iso_639_1 = 'sr-el'"
+    execute "UPDATE languages SET iso_639_1 = 'sr-EC' WHERE iso_639_1 = 'sr'"
+    execute "UPDATE languages SET iso_639_1 = 'sr-el' WHERE iso_639_1 = 'sr-CS'"
   end
 end

a38ac5bab920eba17b7aa66f368d5f6890c8d88b
Jeremy Rice <jrice@eol.org>
2012-04-17 16:18:00 -0600

    Fixing ... kinda... a migration that fails on test_master

--- db/migrate/20120416205738_add_missing_taxon_concept_ids_for_udos_in_curator_activity_logs.rb
+++ db/migrate/20120416205738_add_missing_taxon_concept_ids_for_udos_in_curator_activity_logs.rb
@@ -1,13 +1,33 @@
 class AddMissingTaxonConceptIdsForUdosInCuratorActivityLogs < ActiveRecord::Migration
   def self.up
-    curator_action_ids = Activity.raw_curator_action_ids
-    ch_object_type_id = ChangeableObjectType.users_data_object.id
-    CuratorActivityLog.connection.execute "UPDATE `#{LoggingModel.database_name}`.`curator_activity_logs` cal join `#{ActiveRecord::Base.database_name}`.`users_data_objects` udo SET cal.taxon_concept_id = udo.taxon_concept_id WHERE cal.object_id = udo.data_object_id AND cal.changeable_object_type_id = #{ch_object_type_id} AND cal.activity_id IN (#{curator_action_ids.join(',')})"
+    db_populated = begin
+                     Activity.trusted
+                   rescue
+                     false
+                   end
+    if db_populated
+      curator_action_ids = Activity.raw_curator_action_ids
+      ch_object_type_id = ChangeableObjectType.users_data_object.id
+      CuratorActivityLog.connection.execute "UPDATE `#{LoggingModel.database_name}`.`curator_activity_logs` cal join `#{ActiveRecord::Base.database_name}`.`users_data_objects` udo SET cal.taxon_concept_id = udo.taxon_concept_id WHERE cal.object_id = udo.data_object_id AND cal.changeable_object_type_id = #{ch_object_type_id} AND cal.activity_id IN (#{curator_action_ids.join(',')})"
+    else
+      puts "!! WARNING: This is not a failure, but the database didn't have a 'trusted' activity,"
+      puts "   so I couldn't run this migration for real. This may be normal, but you should make sure."
+    end
   end
 
   def self.down
-    curator_action_ids = Activity.raw_curator_action_ids
-    ch_object_type_id = ChangeableObjectType.users_data_object.id
-    CuratorActivityLog.connection.execute "UPDATE `#{LoggingModel.database_name}`.`curator_activity_logs` cal SET cal.taxon_concept_id = NULL WHERE cal.changeable_object_type_id = #{ch_object_type_id} AND cal.activity_id IN (#{curator_action_ids.join(',')})"
+    db_populated = begin
+                     Activity.trusted
+                   rescue
+                     false
+                   end
+    if db_populated
+      curator_action_ids = Activity.raw_curator_action_ids
+      ch_object_type_id = ChangeableObjectType.users_data_object.id
+      CuratorActivityLog.connection.execute "UPDATE `#{LoggingModel.database_name}`.`curator_activity_logs` cal SET cal.taxon_concept_id = NULL WHERE cal.changeable_object_type_id = #{ch_object_type_id} AND cal.activity_id IN (#{curator_action_ids.join(',')})"
+    else
+      puts "!! WARNING: This is not a failure, but the database didn't have a 'trusted' activity,"
+      puts "   so I couldn't run this migration for real."
+    end
   end
 end

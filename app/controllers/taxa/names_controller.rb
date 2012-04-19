43a390ea4da62a89b34e126c3a2818bed02e1ae7
Jeremy Rice <jrice@eol.org>
2012-04-19 15:20:32 -0600

    WIP - The classifications view is in place, though doesn't have any additional functionality and seems a bit redundant at the moment.  More later.

--- app/controllers/taxa/names_controller.rb
+++ app/controllers/taxa/names_controller.rb
@@ -4,12 +4,10 @@ class Taxa::NamesController < TaxaController
   before_filter :add_page_view_log_entry
   before_filter :set_vet_options, :only => [:common_names, :vet_common_name]
   before_filter :authentication_for_names, :only => [ :create, :update ]
-  before_filter :preload_core_relationships_for_names, :only => [ :related_names, :common_names, :synonyms ]
+  before_filter :preload_core_relationships_for_names, :only => [ :index, :related_names, :common_names, :synonyms ]
   before_filter :count_browsable_hierarchies, :only => [:index, :related_names, :common_names, :synonyms]
 
   def index
-    @hierarchy_entries = @taxon_concept.browsable_published_browsable_hierarchy_entries
-    HierarchyEntry.preload_associations(@hierarchy_entries, [ { :agents_hierarchy_entries => :agent }, :rank, { :hierarchy => :agent } ], :select => {:hierarchy_entries => [:id, :parent_id, :taxon_concept_id]} )
     @assistive_section_header = I18n.t(:assistive_names_classifications_header)
     common_names_count
     render :action => 'classifications'

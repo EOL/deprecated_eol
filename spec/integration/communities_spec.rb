971213e8e7bfe79ad0963ce5b8a0e5f3472739df
Lisa Walley <lwalley@eol.org>
2012-05-01 12:52:20 -0400

    Removing debugger from communities spec.

--- spec/integration/communities_spec.rb
+++ spec/integration/communities_spec.rb
@@ -87,7 +87,6 @@ describe "Communities" do
         fill_in('community_name', :with => new_name)
         fill_in('community_description', :with => 'This is a long description.')
         click_button('Create community')
-        debugger
         new_comm = Community.last
         new_comm.name.should == new_name
         new_comm.description.should == 'This is a long description.'

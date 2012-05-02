790690ccadfcfa569aa07187d7c8e70f03e2592e
Jeremy Rice <jrice@eol.org>
2012-05-02 15:17:19 -0600

    WIP - Logging activity for preferred classifications... something/s wrong with the Activity added by the migration.... Oh... .it's because we're in a migration... we'll have to do this manually and/or with a rake task, sorry.

--- app/models/logging/activity.rb
+++ app/models/logging/activity.rb
@@ -35,6 +35,8 @@ class Activity < LazyLoggingModel
     Activity.find_or_create('add_common_name')
     Activity.find_or_create('remove_common_name')
 
+    Activity.find_or_create('preferred_classification')
+
     Activity.find_or_create('trust_common_name')
     Activity.find_or_create('untrust_common_name')
     Activity.find_or_create('inappropriate_common_name')
--- app/models/taxon_concept_exemplar_image.rb
+++ app/models/taxon_concept_exemplar_image.rb
@@ -7,9 +7,7 @@ class TaxonConceptExemplarImage < ActiveRecord::Base
     return if taxon_concept.nil?
     exemplar = self.find_or_create_by_taxon_concept_id(taxon_concept.id)
     exemplar.update_attributes(:data_object_id => data_object_id)
-
-    tci_exists = TopConceptImage.find_by_taxon_concept_id_and_data_object_id(taxon_concept.id, data_object_id)
-    tci_exists.destroy unless tci_exists.nil?
+    TopConceptImage.delete_all("taxon_concept_id = #{taxon_concept.id} AND data_object_id = #{data_object_id}")
     connection.execute("UPDATE top_concept_images SET view_order=view_order+1 WHERE taxon_concept_id=#{taxon_concept.id}");
     $CACHE.delete(TaxonConcept.cached_name_for("best_image_#{taxon_concept.id}"))
     TopConceptImage.create(:taxon_concept_id => taxon_concept.id, :data_object_id => data_object_id, :view_order => 1)

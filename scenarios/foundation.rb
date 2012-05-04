54faef15b9ac1dcd542cf991ea45b9703eeecb41
JRice <jrice@eol.org>
2012-05-04 15:42:07 -0600

    Added specs for preferred hierarchy ... at least for one controller.  This is really hard.  :\

--- scenarios/foundation.rb
+++ scenarios/foundation.rb
@@ -189,6 +189,7 @@ ChangeableObjectType.gen_if_not_exists(:ch_object_type => 'hierarchy_entry')
 ChangeableObjectType.gen_if_not_exists(:ch_object_type => 'curated_data_objects_hierarchy_entry')
 ChangeableObjectType.gen_if_not_exists(:ch_object_type => 'data_objects_hierarchy_entry')
 ChangeableObjectType.gen_if_not_exists(:ch_object_type => 'users_submitted_text')
+ChangeableObjectType.gen_if_not_exists(:ch_object_type => 'curated_taxon_concept_preferred_entry')
 
 RefIdentifierType.gen_if_not_exists(:label => 'url')
 

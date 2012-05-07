3411c52bd1b6cc6b8eadc96874c305aaa0a8097a
Vitthal Kudal <vitthalkudal@gmail.com>
2012-05-07 16:01:49 -0400

    Removed the before filters on the controllers inherited from collection controller. Also there are some css changes.

--- app/controllers/collections/editors_controller.rb
+++ app/controllers/collections/editors_controller.rb
@@ -2,20 +2,13 @@ class Collections::EditorsController < CollectionsController
 
   skip_before_filter :user_able_to_edit_collection
   skip_before_filter :build_collection_items
-  before_filter :set_filter_to_editors
 
   layout 'v2/collections'
 
   def show
     @rel_canonical_href = collection_editors_url(@collection)
-  end
-
-private
-
-  # This aids in the views and in the methods from the parent controller:
-  def set_filter_to_editors
-    params[:filter] = 'editors'
-    @filter = 'editors'
+    # This aids in the views and in the methods from the parent controller:
+    @filter = params[:filter] = 'editors'
   end
 
 end

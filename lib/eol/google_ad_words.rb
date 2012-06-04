0b32b25e819ac2a111d136bdde83470c4d984354
Lisa Walley <lwalley@eol.org>
2012-06-04 16:46:05 -0400

    WEB-3697 Added format variable to Google AdWords settings. Format determines whether or not to display a notification (and if so in what format) to the user that we are tracking their sign up. We are currently opting to not display a notification.

--- lib/eol/google_ad_words.rb
+++ lib/eol/google_ad_words.rb
@@ -2,13 +2,18 @@ module EOL
   module GoogleAdWords
 
     def self.create_signup_conversion
-      Conversion.new(:id => 1013677372, :label => "O9CDCPyliwIQvPqt4wM", :value => 20)
+      Conversion.new(:id => 1013677372, :label => "O9CDCPyliwIQvPqt4wM", :value => 20, :format => EOL::GoogleAdWords.no_notification)
+    end
+
+    def self.no_notification
+      3
     end
 
     class Conversion
-      attr_accessor :id, :label, :value
+      attr_accessor :format, :id, :label, :value
       def initialize(settings = {})
         @id = settings[:id]
+        @format = settings[:format]
         @label = settings[:label]
         @value = settings[:value]
       end

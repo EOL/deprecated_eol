8c7e72f27cabee31a9c94bded4031cad513f1ec2
Patrick Leary <pleary@mbl.edu>
2012-06-27 19:31:14 -0400

    Added common names to the march of life for #WEB-3779

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -297,8 +297,18 @@ $(function() {
             // make sure this image isn't already featured on this page
             if($("img[src='" + image_data['image_url'] + "']").size() == 0) {
               // add the HTML for the new image
+              scientific_name = image_data['taxon_scientific_name'];
+              common_name = image_data['taxon_common_name'];
+              if(common_name == null) {
+                common_name = ''
+              }
+              alt_text = scientific_name
               opts.addSlide('<a href="'+ image_data['taxon_page_path'] + '"><img src="' +
-                image_data['image_url'] + '" alt="' + image_data['taxon_name'] +'" width="130" height="130"/></a>');
+                image_data['image_url'] +
+                '" alt="' + alt_text + 
+                '" data-scientific_name="' + scientific_name + 
+                '" data-common_name="' + common_name + 
+                '" width="130" height="130"/></a>');
               // since we had to add a slide we need to change the index of the next slide
               opts.nextSlide = opts.currSlide + 1;
               enable_mouseover_alt_text_for_march_of_life();
@@ -322,9 +332,17 @@ $(function() {
       if($e.is($(".thumbnails li:last"))) {
         right_pos = right_pos - 15;
       }
+      var line_height = '';
+      if($(this).attr("data-common_name") == null || $(this).attr("data-common_name") == '') {
+        line_height = '25px';
+      }
+      var name_html = '<span class="scientific">' + $(this).attr("data-scientific_name") + '</span>';
+      if($(this).attr("data-common_name") != null && $(this).attr("data-common_name") != '') {
+        name_html += '<span class="common">' + $(this).attr("data-common_name") + '</span>'
+      }
       term_p.css({
         textAlign: 'center'
-      }).css("margin-left", left_pos+"px").css("margin-right", right_pos+"px").text($(this).attr("alt"));
+      }).css("margin-left", left_pos+"px").css("margin-right", right_pos+"px").css("line-height", line_height).html(name_html);
     });
   }
   enable_mouseover_alt_text_for_march_of_life();

13de06a9ff3b76ac8802601a9f6b8e84f6a91643
Patrick Leary <pleary@mbl.edu>
2012-07-02 13:04:44 -0400

    Making it so RandomHierarchyImage does not need a default hierarchy

--- app/models/random_hierarchy_image.rb
+++ app/models/random_hierarchy_image.rb
@@ -8,9 +8,6 @@ class RandomHierarchyImage < ActiveRecord::Base
   belongs_to :hierarchy
   belongs_to :taxon_concept
 
-  delegate :quick_scientific_name, :to => :taxon_concept
-  delegate :quick_common_name, :to => :taxon_concept
-
   def self.random_set_cached
     begin
       RandomHierarchyImage.random_set_precache_class_loads
@@ -34,27 +31,35 @@ class RandomHierarchyImage < ActiveRecord::Base
   end
 
   def self.random_set(limit = 10, hierarchy = nil, options = {})
-    hierarchy ||= Hierarchy.default
     options[:size] ||= '130_130'
     options[:language] ||= Language.english
-
-    starting_id = rand(self.hierarchy_count(hierarchy) - limit).floor
-    # This next line only applies when there are very few RandomTaxa.
-    starting_id = 0 if starting_id > (self.hierarchy_count(hierarchy) - limit)
-    starting_id = starting_id + self.min_id()
+    
+    if hierarchy
+      starting_id = rand(self.hierarchy_count(hierarchy) - limit).floor
+      # This next line only applies when there are very few RandomTaxa.
+      starting_id = 0 if starting_id > (self.hierarchy_count(hierarchy) - limit)
+      starting_id = starting_id + self.min_id()
+    else
+      starting_id = rand(self.max_id() - limit).floor
+      # This next line only applies when there are very few RandomTaxa.
+      starting_id = 0 if starting_id > (self.max_id() - limit)
+      starting_id = starting_id + self.min_id()
+    end
 
     # this query now grabs all the metadata we'll need including:
     # sci_name, common_name, taxon_concept_id, object_cache_url
     # it also looks for twice the limit as we still have some concepts with more than one preferred common name
 
     if $HOMEPAGE_MARCH_RICHNESS_THRESHOLD
+      hierarchy_condition = hierarchy ? "AND hierarchy_id=#{hierarchy.id}" : ""
       random_image_result = RandomHierarchyImage.find(:all,
-        :conditions => "hierarchy_id=#{hierarchy.id} AND random_hierarchy_images.id>#{starting_id} AND tcm.richness_score>#{$HOMEPAGE_MARCH_RICHNESS_THRESHOLD.to_s} AND tc.published=1",
+        :conditions => "random_hierarchy_images.id>#{starting_id} AND tcm.richness_score>#{$HOMEPAGE_MARCH_RICHNESS_THRESHOLD.to_s} AND tc.published=1 #{hierarchy_condition}",
         :joins => "JOIN taxon_concept_metrics tcm USING (taxon_concept_id) JOIN taxon_concepts tc ON tc.id=random_hierarchy_images.taxon_concept_id",
         :limit => limit*2)
     else
+      hierarchy_condition = hierarchy ? "AND hierarchy_id=#{hierarchy.id}" : ""
       random_image_result = RandomHierarchyImage.find(:all,
-        :conditions => "hierarchy_id=#{hierarchy.id} AND random_hierarchy_images.id>#{starting_id} AND tc.published=1",
+        :conditions => "random_hierarchy_images.id>#{starting_id} AND tc.published=1 #{hierarchy_condition}",
         :joins => "JOIN taxon_concepts tc ON tc.id=random_hierarchy_images.taxon_concept_id",
         :limit => limit*2)
     end
@@ -69,8 +74,7 @@ class RandomHierarchyImage < ActiveRecord::Base
     end
 
     RandomHierarchyImage.preload_associations(random_image_result,
-      [ :data_object,
-        { :taxon_concept => [
+      [ { :taxon_concept => [
           { :preferred_entry => { :hierarchy_entry => [ :hierarchy, { :name => [ :canonical_form, :ranked_canonical_form ] } ] } },
           { :taxon_concept_exemplar_image => :data_object },
           { :preferred_common_names => :name } ] } ],
@@ -82,7 +86,7 @@ class RandomHierarchyImage < ActiveRecord::Base
         :hierarchies => '*',
         :taxon_concept_names => '*' })
 
-    random_images = self.random_set(limit, Hierarchy.default, :size => options[:size]) if random_images.blank? && hierarchy.id != Hierarchy.default.id
+    random_images = self.random_set(limit, nil, :size => options[:size]) if random_images.blank? && hierarchy
     #raise "Found no Random Taxa in the database (#{starting_id}, #{limit})" if random_images.blank?
 
     # by calling this here, the cached values will contain the pre-cached name. This saves a bunch of load time on the homepage
@@ -90,16 +94,18 @@ class RandomHierarchyImage < ActiveRecord::Base
     return random_images
   end
 
-  # The first one takes a little longer, since it needs to populate the class variables.  But after that, it's quite fast:
-  def self.random(hierarchy = nil, options = {})
-    return self.random_set(1, hierarchy, options)[0]
-  end
-
   def self.min_id()
     $CACHE.fetch('random_hierarchy_image/min_id', :expires_in => 60.minutes) do
       self.connection.select_value("select min(id) min from random_hierarchy_images").to_i
     end
   end
+  
+  def self.max_id()
+    $CACHE.fetch('random_hierarchy_image/max_id', :expires_in => 60.minutes) do
+      self.connection.select_value("select max(id) min from random_hierarchy_images").to_i
+    end
+  end
+  
 
   def self.hierarchy_count(hierarchy)
     $CACHE.fetch("random_hierarchy_image/hierarchy_count_#{hierarchy.id}", :expires_in => 60.minutes) do

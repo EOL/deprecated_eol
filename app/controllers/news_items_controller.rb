755482b926634b54b636160d09c31be6add68923
Vitthal Kudal <vitthalkudal@gmail.com>
2012-07-17 23:43:06 -0400

    Added a check box on user edit form to toggle display of the news in preferred language and all languages on home page and news page. WEB-3831

--- app/controllers/news_items_controller.rb
+++ app/controllers/news_items_controller.rb
@@ -4,10 +4,25 @@ class NewsItemsController < ApplicationController
   
   def index
     @page_title = I18n.t(:page_title, :scope => [:news_items, :index])
-    @translated_news_items = TranslatedNewsItem.paginate(
-      :conditions=>['translated_news_items.language_id = ? and translated_news_items.active_translation=1 and news_items.active=1 and news_items.activated_on<=?', Language.from_iso(current_language.iso_639_1), DateTime.now.utc],
-      :joins => "inner join news_items on news_items.id = translated_news_items.news_item_id",
-      :order=>'news_items.display_date desc', :page => params[:page], :per_page => 25)
+    if current_user.news_in_preferred_language
+      @translated_news_items = TranslatedNewsItem.paginate(
+        :conditions=>['translated_news_items.language_id = ? and translated_news_items.active_translation=1 and news_items.active=1 and news_items.activated_on<=?', Language.from_iso(current_language.iso_639_1), DateTime.now.utc],
+        :joins => "inner join news_items on news_items.id = translated_news_items.news_item_id",
+        :order=>'news_items.display_date desc', :page => params[:page], :per_page => 25)
+    else
+      news_items = NewsItem.find(:all, :conditions=>['news_items.active=1 and news_items.activated_on<=?', DateTime.now.utc],
+        :order=>'news_items.display_date desc', :include => :translations)
+      translated_news_items = []
+      news_items.each do |news_item|
+        translations = news_item.translations
+        if translated_news_item = translations.detect{|tr| tr.language_id == current_language.id}
+          translated_news_items << translated_news_item
+        else
+          translated_news_items << translations.sort_by(&:created_at).first
+        end
+      end
+      @translated_news_items = translated_news_items.paginate(:page => params[:page], :per_page => 25)
+    end
 
     # @rel_canonical_href = recent_activities_url(:page => rel_canonical_href_page_number(@log))
     # @rel_prev_href = rel_prev_href_params(@log) ? recent_activities_url(@rel_prev_href_params) : nil

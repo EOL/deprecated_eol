401af145b9281aff3a882adec22ebfff5dd3edef
Lisa Walley <lwalley@eol.org>
2012-07-31 10:15:00 -0400

    WEB-3822 Fixed statistics pages in the V2 adminsitration section.

--- spec/models/eol_statistic_spec.rb
+++ spec/models/eol_statistic_spec.rb
@@ -3,36 +3,47 @@ describe EolStatistic do
   before(:all) do
     EolStatistic.gen
   end
-  it 'should select overall stats' do
-    stats = EolStatistic.overall
-    EolStatistic.report_attributes['overall'].map{|attribute| stats[0].has_attribute?(attribute).should be_true}
+
+  describe "#named_scope" do
+    it 'should select overall stats' do
+      stats = EolStatistic.overall
+      EolStatistic.sorted_report_attributes(:overall).map{|attribute| stats[0].has_attribute?(attribute).should be_true}
+    end
+    it 'should select content_partner stats' do
+      stats = EolStatistic.content_partners
+      EolStatistic.sorted_report_attributes(:content_partners).map{|attribute| stats[0].has_attribute?(attribute).should be_true}
+    end
+    it 'should select page_richness stats' do
+      stats = EolStatistic.page_richness
+      EolStatistic.sorted_report_attributes(:page_richness).map{|attribute| stats[0].has_attribute?(attribute).should be_true}
+    end
+    it 'should select curator stats' do
+      stats = EolStatistic.curators
+      EolStatistic.sorted_report_attributes(:curators).map{|attribute| stats[0].has_attribute?(attribute).should be_true}
+    end
+    it 'should select lifedesk stats' do
+      stats = EolStatistic.lifedesks
+      EolStatistic.sorted_report_attributes(:lifedesks).map{|attribute| stats[0].has_attribute?(attribute).should be_true}
+    end
+    it 'should select marine stats' do
+      stats = EolStatistic.marine
+      EolStatistic.sorted_report_attributes(:marine).map{|attribute| stats[0].has_attribute?(attribute).should be_true}
+    end
+    it 'should select users data objects stats' do
+      stats = EolStatistic.users_data_objects
+      EolStatistic.sorted_report_attributes(:users_data_objects).map{|attribute| stats[0].has_attribute?(attribute).should be_true}
+    end
+    it 'should select data objects stats' do
+      stats = EolStatistic.data_objects
+      EolStatistic.sorted_report_attributes(:data_objects).map{|attribute| stats[0].has_attribute?(attribute).should be_true}
+    end
+    it 'should select earliest' do
+      stats = EolStatistic.overall.earliest
+      EolStatistic.sorted_report_attributes(:overall).map{|attribute| stats[0].has_attribute?(attribute).should be_true}
+    end
+    it 'should select latest' do
+      stats = EolStatistic.overall.latest
+      EolStatistic.sorted_report_attributes(:overall).map{|attribute| stats[0].has_attribute?(attribute).should be_true}
+    end
   end
-   it 'should select content_partner stats' do
-     stats = EolStatistic.content_partners
-     EolStatistic.report_attributes['content_partners'].map{|attribute| stats[0].has_attribute?(attribute).should be_true}
-   end
-   it 'should select page_richness stats' do
-     stats = EolStatistic.page_richness
-     EolStatistic.report_attributes['page_richness'].map{|attribute| stats[0].has_attribute?(attribute).should be_true}
-   end
-   it 'should select curator stats' do
-     stats = EolStatistic.curators
-     EolStatistic.report_attributes['curators'].map{|attribute| stats[0].has_attribute?(attribute).should be_true}
-   end
-   it 'should select lifedesk stats' do
-     stats = EolStatistic.lifedesks
-     EolStatistic.report_attributes['lifedesks'].map{|attribute| stats[0].has_attribute?(attribute).should be_true}
-   end
-   it 'should select marine stats' do
-     stats = EolStatistic.marine
-     EolStatistic.report_attributes['marine'].map{|attribute| stats[0].has_attribute?(attribute).should be_true}
-   end
-   it 'should select users data objects stats' do
-     stats = EolStatistic.users_data_objects
-     EolStatistic.report_attributes['users_data_objects'].map{|attribute| stats[0].has_attribute?(attribute).should be_true}
-   end
-   it 'should select data objects stats' do
-     stats = EolStatistic.data_objects
-     EolStatistic.report_attributes['data_objects'].map{|attribute| stats[0].has_attribute?(attribute).should be_true}
-   end
 end

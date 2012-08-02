6a449585e39fddbf2071720e380262a1935c75cc
JRice <jrice@eol.org>
2012-08-02 15:43:31 -0600

    Removed cucumber.  Sorry. (now really)

--- features/step_definitions/data_object_curation_steps.rb
+++
@@ -1,5 +0,0 @@
-Then /^I it should have a new comment with "([^"]*)" and "([^"]*)" for the main picture$/ do |phrase1, phrase2|
-  comments = Comment.find_all_by_user_id(get_user('curator').id)
-  [phrase1, phrase2].each { |phrase| comments.last.body.should match(phrase) }
-end
-

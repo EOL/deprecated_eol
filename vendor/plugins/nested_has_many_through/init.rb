d6476c4f67a5185081a846965b6c5eab7830341e
JRice <jrice@eol.org>
2012-08-03 14:02:04 -0600

    Nested has_many_through handled by a gem, now.

--- vendor/plugins/nested_has_many_through/init.rb
+++
@@ -1,4 +0,0 @@
-require 'nested_has_many_through'
-
-ActiveRecord::Associations::HasManyThroughAssociation.send :include, NestedHasManyThrough::Association
-ActiveRecord::Reflection::AssociationReflection.send :include, NestedHasManyThrough::Reflection

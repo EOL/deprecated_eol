f5de7a1a8aac9132934b88e38bb00f4acf2aaf9b
JRice <jrice@eol.org>
2012-08-08 13:09:07 -0600

    Disabled a handful of tests using rspec-on-rails-matchers (not worth keeping them), removed us_states plugin which didn't seem to be used (grepped for every method it defines).

--- spec/models/users_data_object_spec.rb
+++
@@ -1,8 +0,0 @@
-require File.dirname(__FILE__) + '/../spec_helper'
-
-describe UsersDataObject do
-  it { should validate_presence_of(:user_id)}
-  it { should validate_presence_of(:data_object_id)}
-  it { should validate_uniqueness_of(:data_object_id)}
-
-end

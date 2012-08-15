e682a5186abde3a2f6f1f6f3974beed39f866cc9
JRice <jrice@eol.org>
2012-08-15 09:15:07 -0600

    Yes, I just broke all of the styles.  Bear with me, this is worth it.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,5 +1,22 @@
-if(!EOL) { var EOL = {}; }
+// This is a manifest file that'll be compiled into application.js, which will include all the files
+// listed below.
+//
+// Any JavaScript/Coffee file within this directory, lib/assets/javascripts, vendor/assets/javascripts,
+// or vendor/assets/javascripts of plugins, if any, can be referenced here using a relative path.
+//
+// It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
+// the compiled file.
+//
+// WARNING: THE FIRST BLANK LINE MARKS THE END OF WHAT'S TO BE PROCESSED, ANY BLANK LINE SHOULD
+// GO AFTER THE REQUIRES BELOW.
+//
+//= require jquery
+//= require jquery_ujs
+//= require_tree .
 
+// The rest is the old application.js file:
+
+if(!EOL) { var EOL = {}; }
 // Fix for Chrome, adding taxa to collections on map tab (and possibly other problems). See http://stackoverflow.com/questions/7825448/webkit-issues-with-event-layerx-and-event-layery
 (function(){
     // remove layerX and layerY
--- config/application.rb
+++ config/application.rb
@@ -9,10 +9,7 @@ require "sprockets/railtie"
 # require "rails/test_unit/railtie"
 
 if defined?(Bundler)
-  # If you precompile assets before deploying to production, use this line
   Bundler.require(*Rails.groups(:assets => %w(development test)))
-  # If you want your assets lazily compiled in production, use this line
-  # Bundler.require(:default, :assets, Rails.env)
 end
 
 module EolUpgrade

af0871d42689e58d8c297cf34ac68ba630c3466f
JRice <jrice@eol.org>
2012-08-15 14:04:34 -0600

    TEMPORARY fix for TranslationLog... fixing another error blocking specs)

--- app/models/logging/translation_log.rb
+++ app/models/logging/translation_log.rb
@@ -1,9 +1,13 @@
 class Logging::TranslationLog < LoggingModel
   def self.inc(which)
     if $ENABLE_TRANSLATION_LOGS
-      Logging::TranslationLog.connection.execute(
+      self.connection.execute(
         "INSERT INTO translation_logs (`key`, count) VALUES ('#{which}', 1) ON DUPLICATE KEY UPDATE count = count+1"
       )
     end
   end
+end
+
+# TEMP!  I'm testing things. Delete this.
+class TranslationLog
 end

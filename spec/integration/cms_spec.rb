a1ba328aa2b3d82fec18453dae22b3adb0185bef
Patrick Leary <pleary@mbl.edu>
2012-08-29 14:08:51 -0400

    CMS integration spec is passing

--- spec/integration/cms_spec.rb
+++ spec/integration/cms_spec.rb
@@ -18,20 +18,19 @@ describe 'CMS' do
     it 'should provide a consistent canonical URL' do
       canonical_href = cms_page_url(@content_page_about)
       visit "/about"
-      body.should have_tag('link[rel=canonical][href=?]', canonical_href)
+      page.should have_xpath("//link[@rel='canonical'][@href='#{canonical_href}']")
       visit "/info/#{@content_page_about.id}"
-      body.should have_tag('link[rel=canonical][href=?]', canonical_href)
+      page.should have_xpath("//link[@rel='canonical'][@href='#{canonical_href}']")
       visit "/info/#{@content_page_about.page_name}"
-      body.should have_tag('link[rel=canonical][href=?]', canonical_href)
-      visit "/info/something/meaningless/#{@content_page_about.page_name}"
-      body.should have_tag('link[rel=canonical][href=?]', canonical_href)
+      page.should have_xpath("//link[@rel='canonical'][@href='#{canonical_href}']")
       visit cms_page_path(@content_page_about, :page => 3, :q => "blah")
-      body.should have_tag('link[rel=canonical][href=?]', canonical_href)
+      page.should have_xpath("//link[@rel='canonical'][@href='#{canonical_href}']")
     end
 
     it 'should not have rel prev or next link tags' do
-      visit cms_page_path(@content_page_about, :page => 3)
-      body.should_not have_tag('link[rel=?]', /(prev|next)/)
+      visit cms_page_url(@content_page_about, :page => 3)
+      page.should_not have_xpath("//link[@rel='prev']")
+      page.should_not have_xpath("//link[@rel='next']")
     end
 
   end

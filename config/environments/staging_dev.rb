bb4da32632cb7263db3cece5572133128bc8e7b2
Patrick Leary <pleary@mbl.edu>
2012-09-11 10:28:08 -0400

    A few small changes for specs

--- config/environments/staging_dev.rb
+++ config/environments/staging_dev.rb
@@ -1,38 +1,48 @@
-# Settings specified here will take precedence over those in config/environment.rb
-# 1) config/environment.rb
-# 2) config/environments/[Rails.env].rb
-# 3) config/environments/[Rails.env]_eol_org.rb
-# 4) config/environment_eol_org.rb
+EolUpgrade::Application.configure do
+  # Settings specified here will take precedence over those in config/application.rb
 
-# Allow breakpoints in mongrel:
-require "ruby-debug"
+  # In the development environment your application's code is reloaded on
+  # every request. This slows down response time but is perfect for development
+  # since you don't have to restart the web server when you make code changes.
+  config.cache_classes = false
 
-config.whiny_nils = true
-config.action_controller.consider_all_requests_local = true
-config.action_controller.perform_caching = false
-config.action_view.debug_rjs = false
-config.cache_classes = false
-config.cache_store = :dalli_store
-config.action_mailer.raise_delivery_errors = false
+  # Log error messages when you accidentally call methods on nil.
+  config.whiny_nils = true
 
-config.log_level = :debug # :error
-ActiveRecord::Base.logger = Logger.new(STDOUT)
-ActionController::Base.logger = Logger.new(STDOUT)
-ActiveSupport::Cache::MemCacheStore.logger = Logger.new(STDOUT)
+  # Show full error reports and disable caching
+  config.consider_all_requests_local       = true
+  config.action_controller.perform_caching = false
 
-$PARENT_CLASS_MUST_USE_MASTER = ActiveRecord::Base
-$LOG_USER_ACTIVITY = true
-$EXCEPTION_NOTIFY = true
-$ERROR_LOGGING = true
-$ENABLE_ANALYTICS = false
-$ENABLE_RECAPTCHA = false
-$LOG_WEB_SERVICE_EXECUTION_TIME = true
-$USE_SSL_FOR_LOGIN=false
-$SKIP_URL_VALIDATIONS = true
+  # Don't care if the mailer can't send
+  config.action_mailer.raise_delivery_errors = false
 
-# Override some values and add new ones with private information included
-begin
-  require File.join(File.dirname(__FILE__), 'staging_dev_private')
-rescue LoadError
-  puts '*************WARNING: COULD NOT LOAD staging_dev_private FILE***********************'
+  # Print deprecation notices to the Rails logger
+  config.active_support.deprecation = :log
+
+  # Only use best-standards-support built into browsers
+  config.action_dispatch.best_standards_support = :builtin
+
+  # Raise exception on mass assignment protection for Active Record models
+  config.active_record.mass_assignment_sanitizer = :strict
+
+  # Log the query plan for queries taking more than this (works
+  # with SQLite, MySQL, and PostgreSQL)
+  # config.active_record.auto_explain_threshold_in_seconds = 1
+
+  # Do not compress assets
+  config.assets.compress = false
+
+  # Expands the lines which load the assets
+  config.assets.debug = true
+  
+  ActiveRecord::Base.logger = Logger.new(STDOUT)
+  ActionController::Base.logger = Logger.new(STDOUT)
+  Dalli.logger = Logger.new(STDOUT)
+  
+  config.after_initialize do
+    $INDEX_RECORDS_IN_SOLR_ON_SAVE = false
+    $SOLR_SERVER = 'http://128.128.164.109:8080/solr/'
+    $CONTENT_SERVERS = ['http://content70.eol.org/','http://content71.eol.org/','http://content72.eol.org/','http://content73.eol.org/','http://content74.eol.org/','http://content75.eol.org/']
+  end
+  
 end

939b3c75d72155cb0baafec37a309aaae706b360
Patrick Leary <pleary@mbl.edu>
2012-09-14 12:17:18 -0400

    Properly caching homepage news; making sure news can show in different languages; added see more link for homepage stats (cherry picked from commit 2992fd904a447fe6f993ccf784a645051337b451)
    Conflicts:
    
    	app/models/news_item.rb
    	config/locales/en.yml
    	public/stylesheets/home.css
    	public/stylesheets/rtl/home.css

--- app/models/news_item.rb
+++ app/models/news_item.rb
@@ -32,5 +32,49 @@ class NewsItem < ActiveRecord::Base
       return Language.find_active
     end
   end
+
+  def page_url
+    all_pages_with_this_name = NewsItem.find_all_by_page_name(page_name)
+    if all_pages_with_this_name.count > 1 && all_pages_with_this_name.first != self
+      return self.id
+    else
+      return self.page_name.gsub(' ', '_').downcase
+    end
+  end
+
+  def alternate_page_url
+    all_pages_with_this_name = NewsItem.find_all_by_page_name(page_name)
+    if all_pages_with_this_name.count == 1
+      return self.id
+    end
+  end
   
+  def self.homepage_news_for_user(current_user, current_language)
+    if current_user.news_in_preferred_language
+      translated_news_items = TranslatedNewsItem.find(:all, :conditions=>['translated_news_items.language_id = ? and translated_news_items.active_translation=1 and news_items.active=1 and news_items.activated_on<=?', Language.from_iso(current_language.iso_639_1), DateTime.now.utc], :joins => "inner join news_items on news_items.id = translated_news_items.news_item_id", :order=>'news_items.display_date desc', :limit => $NEWS_ON_HOME_PAGE)
+    else
+      news_items = NewsItem.find(:all, :conditions=>['news_items.active=1 and news_items.activated_on<=?', DateTime.now.utc],
+        :order=>'news_items.display_date desc', :include => :translations, :limit => $NEWS_ON_HOME_PAGE)
+      translated_news_items = []
+      news_items.each do |news_item|
+        translations = news_item.translations
+        if translated_news_item = translations.detect{|tr| tr.language_id == current_language.id && tr.active_translation == 1}
+          translated_news_items << translated_news_item
+        else
+          active_translations = translations.collect{|tr| tr if tr.active_translation == 1}.compact
+          translated_news_items << active_translations.sort_by{|t| t.created_at || 0}.first unless active_translations.blank?
+        end
+      end
+    end
+    translated_news_items
+  end
+
+private
+
+  def destroy_translations
+    translations.each do |translated_news_item|
+      translated_news_item.destroy
+    end
+  end
+
 end

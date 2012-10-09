8479300e36cd079ffa16ed220e509bc131065715
Patrick Leary <pleary@mbl.edu>
2012-10-09 13:40:17 -0400

    Added more columns to curator stats spreadsheet (cherry picked from commit a1182868a3a92525d86bc64e3453fde9ecdb15b5)
    Conflicts:
    
    	app/controllers/administrator/curator_controller.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -379,6 +379,22 @@ class User < Curator
                                 GROUP BY user_id}, user_id)
   end
 
+  def self.count_objects_rated(user_id = nil)
+    self.count_complex_query(UsersDataObject,
+                             %Q{SELECT user_id, COUNT(DISTINCT data_object_guid) AS count
+                                FROM users_data_objects_ratings
+                                #{user_id ? "WHERE user_id = #{user_id}" : ""}
+                                GROUP BY user_id}, user_id)
+  end
+
+  def self.count_comments_added(user_id = nil)
+    self.count_complex_query(Comment,
+                             %Q{SELECT user_id, COUNT(*) AS count
+                                FROM comments
+                                #{user_id ? "WHERE user_id = #{user_id}" : ""}
+                                GROUP BY user_id}, user_id)
+  end
+
   def self.count_user_rows(klass, user_id = nil)
     query = "SELECT user_id, COUNT(*) as count FROM #{klass.full_table_name} "
     if user_id.class == Fixnum

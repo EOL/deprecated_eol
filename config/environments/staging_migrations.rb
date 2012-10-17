620d0969aa78fa301092faefceb3ffc5b1017b86
JRice <jrice@eol.org>
2012-10-17 09:01:36 -0600

    abstract magic wand #wave method applied to staging_migration environment file. Proceed with deploy.

--- config/environments/staging_migrations.rb
+++ config/environments/staging_migrations.rb
@@ -1,49 +1,81 @@
-#======================================================================
-#                       v2staging.rb
-# Environment specific settings for the Staging environment
-#
-# Settings specified here will override those in config/environment.rb.
-# Configuration files are loaded in the following order with the settings
-# in each file overriding the settings in prior files
-#
-# 1) config/environment.rb
-# 2) config/environments/[Rails.env].rb
-# 3) config/environments/[Rails.env]_eol_org.rb
-# 4) config/environment_eol_org.rb
-#======================================================================
+EolUpgrade::Application.configure do
+  # Settings specified here will take precedence over those in config/application.rb
 
-# Use a different logger for distributed setups
-# config.logger = SyslogLogger.new
+  # Code is not reloaded between requests
+  config.cache_classes = true
 
-# Full error reports are disabled and caching is turned on
-config.action_controller.consider_all_requests_local = false
-config.action_controller.perform_caching             = true
-config.action_view.debug_rjs                         = false
+  # Full error reports are disabled and caching is turned on
+  config.consider_all_requests_local       = false
+  config.action_controller.perform_caching = true
 
-# Disable delivery errors, bad email addresses will be ignored
-config.action_mailer.raise_delivery_errors = false
+  # Disable Rails's static asset server (Apache or nginx will already do this)
+  config.serve_static_assets = false
 
-# Code is not reloaded between requests
-config.cache_classes = true
+  # Compress JavaScripts and CSS
+  config.assets.compress = true
 
-# Set up the master database connection for writes using masochism plugin
-# NOTE: for this to work, you *must* also use config.cache_classes = true
-# (default for production)
-config.after_initialize do 
-  ActiveReload::ConnectionProxy.setup_for ActiveReload::MasterDatabase, ActiveRecord::Base
-  ActiveReload::ConnectionProxy.setup_for LoggingWriter, LoggingModel
-  $PARENT_CLASS_MUST_USE_MASTER = ActiveReload::MasterDatabase
+  # Don't fallback to assets pipeline if a precompiled asset is missed
+  config.assets.compile = false
+
+  # Generate digests for assets URLs
+  config.assets.digest = true
+
+  # Defaults to nil and saved in location specified by config.assets.prefix
+  # config.assets.manifest = YOUR_PATH
+
+  # Specifies the header that your server uses for sending files
+  # config.action_dispatch.x_sendfile_header = "X-Sendfile" # for apache
+  # config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect' # for nginx
+
+  # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
+  # config.force_ssl = true
+
+  # See everything in the log (default is :info)
+  config.log_level = :debug
+
+  # Prepend all log lines with the following tags
+  # config.log_tags = [ :subdomain, :uuid ]
+
+  # Use a different logger for distributed setups
+  # config.logger = ActiveSupport::TaggedLogging.new(SyslogLogger.new)
+
+  # Use a different cache store in staging
+  # config.cache_store = :mem_cache_store
+
+  # Enable serving of images, stylesheets, and JavaScripts from an asset server
+  # config.action_controller.asset_host = "http://assets.example.com"
+
+  # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
+  # config.assets.precompile += %w( search.js )
+
+  # Disable delivery errors, bad email addresses will be ignored
+  # config.action_mailer.raise_delivery_errors = false
+
+  # Enable threaded mode
+  # config.threadsafe!
+
+  # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
+  # the I18n.default_locale when a translation can not be found)
+  config.i18n.fallbacks = true
+
+  # Send deprecation notices to registered listeners
+  config.active_support.deprecation = :notify
+
+  # Log the query plan for queries taking more than this (works
+  # with SQLite, MySQL, and PostgreSQL)
+  # config.active_record.auto_explain_threshold_in_seconds = 0.5
 end
-$LOGGING_READ_FROM_MASTER = true
 
+# TODO - move this!  Grrr.  I'm not sure where things like this go, now. (Though I would argue we s/b putting these
+# in the DB, dammit.)
+$UNSUBSCRIBE_NOTIFICATIONS_KEY = 'f0de2a0651aa88a090e5679e5e3a7d28'
+$LOGGING_READ_FROM_MASTER = true
 # set to true to force users to use SSL for the login and signup pages 
 $USE_SSL_FOR_LOGIN = false
 
 #This part of the code should stay at the bottom to ensure that www.eol.org - related settings override everything
 begin
-  require File.join(File.dirname(__FILE__), 'v2staging_eol_org')
+  require File.join(File.dirname(__FILE__), 'staging_eol_org')
 rescue LoadError
-  puts '*************WARNING: COULD NOT LOAD V2STAGING_EOL_ORG FILE***********************'
+  puts '*************WARNING: COULD NOT LOAD STAGING_EOL_ORG FILE***********************'
 end
-
-

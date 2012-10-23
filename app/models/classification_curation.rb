3b46bc5e4d4dba3666f35692b140889446e597bb
JRice <jrice@eol.org>
2012-10-22 20:29:59 -0600

    Bugs with notification about the split to another page ... fixed.

--- app/models/classification_curation.rb
+++ app/models/classification_curation.rb
@@ -123,7 +123,7 @@ class ClassificationCuration < ActiveRecord::Base
       # is that we don't have linking ability in models (which, IMO, is lame)... so, in the meantime, I'm leaving a
       # LAME comment (I really don't want to do URL generation like this):
       new_id = hierarchy_entry_moves.first.hierarchy_entry.taxon_concept_id
-      Comment.create!(:user_id => user_id, :body => "Classifications that were on this page have been moved to page <a href='http://#{$SITE_DOMAIN_OR_IP}/pages/#{new_id}'>#{new_id}</a>", :parent => parent)
+      Comment.create!(:user_id => user_id, :body => "Classifications that were on this page have been moved to page <a href='http://#{$SITE_DOMAIN_OR_IP}/pages/#{new_id}'>#{new_id}</a>", :parent => moved_from)
     end
     if activity_log
       force_immediate_notification_of(activity_log)

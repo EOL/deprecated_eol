3c664f46af490f172f95c862b2c63099d0b427b3
Patrick Leary <pleary@mbl.edu>
2012-11-07 12:54:53 -0500

    Removed the require oauth2 line

--- config/initializers/oauth2.rb
+++ config/initializers/oauth2.rb
@@ -1,5 +1,4 @@
 # This is required to parse facebook user's profile information
-require 'oauth2'
 OAuth2::Response.register_parser(:text, 'text/plain') do |body|
   key, value = body.split('=')
   {key => value}

d9ea5ae06fa1ad094a2e26ccc14a2e083ff6d4f1
JRice <jrice@eol.org>
2012-12-18 10:26:11 -0700

    Getting desperate to get this migration to work. ...I just don't understand.

--- db/migrate/20121214213210_rename_curator_activity_logs_object_id.rb
+++ db/migrate/20121214213210_rename_curator_activity_logs_object_id.rb
@@ -1,9 +1,14 @@
 class RenameCuratorActivityLogsObjectId < EOL::LoggingMigration
   def up
-    CuratorActivityLog.connection.rename_column :curator_activity_logs, :object_id, :target_id
+    # TODO - why isn't this working? Note, I tried it without the CuratorActivityLog, also without the #connection.
+    # CuratorActivityLog.connection.rename_column :curator_activity_logs, :object_id, :target_id
+    CuratorActivityLog.connection.execute \
+      "ALTER TABLE curator_activity_logs CHANGE object_id target_id int(11) DEFAULT NULL"
   end
 
   def down
-    CuratorActivityLog.connection.rename_column :curator_activity_logs, :target_id, :object_id
+    # CuratorActivityLog.connection.rename_column :curator_activity_logs, :target_id, :object_id
+    CuratorActivityLog.connection.execute \
+      "ALTER TABLE curator_activity_logs CHANGE target_id object_id int(11) DEFAULT NULL"
   end
 end

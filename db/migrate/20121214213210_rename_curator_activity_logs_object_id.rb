783c761e28c83739246bdf2a51528b07b63e971f
JRice <jrice@eol.org>
2012-12-18 10:15:23 -0700

    Forcing the issue by using the specific model connection. We shouldn't have to do this, but I need this to work...

--- db/migrate/20121214213210_rename_curator_activity_logs_object_id.rb
+++ db/migrate/20121214213210_rename_curator_activity_logs_object_id.rb
@@ -1,9 +1,9 @@
 class RenameCuratorActivityLogsObjectId < EOL::LoggingMigration
   def up
-    rename_column :curator_activity_logs, :object_id, :target_id
+    CuratorActivityLog.connection.rename_column :curator_activity_logs, :object_id, :target_id
   end
 
   def down
-    rename_column :curator_activity_logs, :target_id, :object_id
+    CuratorActivityLog.connection.rename_column :curator_activity_logs, :target_id, :object_id
   end
 end

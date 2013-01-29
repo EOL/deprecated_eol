def3ec9fbdae9632f07ca1d49148193eadb7a89e
JRice <jrice@eol.org>
2013-01-29 13:27:51 -0700

    Fixed the API spec by making it check the actual data rather than just a guess at what the data should have been.  ...Still not sure why the data changed for this test, but this is technically a more accurate test anyway.

--- spec/integration/api/pages_spec.rb
+++ spec/integration/api/pages_spec.rb
@@ -225,9 +225,14 @@ describe 'API:pages' do
 
   it 'pages should show which common names are preferred' do
     response = get_as_xml("/api/pages/0.4/#{@taxon_concept.id}?common_names=1")
-    # NOTE - this is failing because we end up with two preferred names. I'm not sure why.  :\
-    response.xpath('//xmlns:taxon/xmlns:commonName[1]/@eol_preferred').inner_text.should == 'true'
-    response.xpath('//xmlns:taxon/xmlns:commonName[2]/@eol_preferred').inner_text.should == ''
+    @taxon_concept.common_names.each do |name|
+      (1..@taxon_concept.common_names.length).each do |index|
+        if response.xpath("//xmlns:taxon/xmlns:commonName[#{index}]").inner_text == name.name.string
+          value = name.preferred? ? 'true' : ''
+          response.xpath("//xmlns:taxon/xmlns:commonName[#{index}]/@eol_preferred").inner_text.should == value
+        end
+      end
+    end
   end
 
   it 'pages should show data object vetted status and rating by default' do

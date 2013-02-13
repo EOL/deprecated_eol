2ea635639b349e37af6b44e7fdc5b75d107ee4ad
JRice <jrice@eol.org>
2013-02-13 10:34:37 -0700

    Permission spec done. (Pretty minimal)

--- app/models/permission.rb
+++ app/models/permission.rb
@@ -8,7 +8,7 @@ class Permission < ActiveRecord::Base
 
   def self.create_defaults
     KNOWN_PERMISSIONS.each do |sym|
-      name = sym.gsub('_', ' ')
+      name = Permission.stringify_sym(sym)
       perm = cached_find_translated(:name, name)
       unless perm
         perm = Permission.create
@@ -20,16 +20,22 @@ class Permission < ActiveRecord::Base
 
   def self.method_missing(sym, *args, &block)
     super unless KNOWN_PERMISSIONS.include?(sym)
-    cached_find_translated(:name, sym.gsub('_', ' ')) || super
+    cached_find_translated(:name, Permission.stringify_sym(sym)) || super
   end
 
+  def self.stringify_sym(sym)
+    sym.to_s.gsub('_', ' ')
+  end
+
+  # NOTE - I don't know why 'self' is required here, but it's nil otherwise. :|
   def inc_user_count
-    users_count += 1
+    self.users_count += 1
     save
   end
 
   def dec_user_count
-    users_count -= 1
+    self.users_count -= 1
+    self.users_count = 0 if self.users_count < 0
     save
   end
 
--- spec/models/permission_spec.rb
+++ spec/models/permission_spec.rb
@@ -1,5 +1,30 @@
 require 'spec_helper'
 
 describe Permission do
-  pending "add some examples to (or delete) #{__FILE__}"
+  
+  # NOTE - this is a tricky test. It actually also tests that symbols are stringified as expected, that translations
+  # work, and of course, that all of the specific defaults we expect to see are really there:
+  it 'shoud create defaults' do
+    TranslatedPermission.delete_all
+    Permission.delete_all
+    Permission.create_defaults
+    Permission.edit_permissions.should_not be_nil
+    Permission.edit_permissions.name.should == "edit permissions"
+  end
+
+  it 'should count users properly' do
+    perm = TranslatedPermission.gen.permission # NOTE - I hate doing that, but that's the expectation with uses_translations.
+    perm.users_count = 0
+    perm.inc_user_count
+    perm.users_count.should == 1
+    perm.inc_user_count
+    perm.users_count.should == 2
+    perm.dec_user_count
+    perm.users_count.should == 1
+    perm.dec_user_count
+    perm.users_count.should == 0
+    perm.dec_user_count
+    perm.users_count.should == 0
+  end
+
 end

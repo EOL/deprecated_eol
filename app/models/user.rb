4e0e2b2b5f87d41c7fd57d860d4aa0de6d59e98f
JRice <jrice@eol.org>
2013-04-01 11:16:23 -0600

    Bug in collected name for user.

--- app/models/user.rb
+++ app/models/user.rb
@@ -224,8 +224,6 @@ class User < ActiveRecord::Base
   alias :summary_name :full_name # This is for collection item duck-typing, you need not use this elsewhere.
   alias :collected_name :full_name # This is for collection item duck-typing, you need not use this elsewhere.
 
-  alias_attribute :collected_name, :username # This is for collection item duck-typing, you need not use this elsewhere.
-
   # Note that this can end up being expensive, but avoids errors.  Watch your qeries!
   def reload_all_values_if_missing(which)
     which = [which] unless which.is_a?(Array)

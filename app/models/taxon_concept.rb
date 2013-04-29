fbc7efb1110d807d8509b253c46724926aac626b
JRice <jrice@eol.org>
2013-04-29 13:17:18 -0600

    Fix for create_preferred_entry being private is to make it public. CSB.

--- app/models/taxon_concept.rb
+++ app/models/taxon_concept.rb
@@ -1007,6 +1007,12 @@ class TaxonConcept < ActiveRecord::Base
     taxon_concept_metric && !taxon_concept_metric.richness_score.blank?
   end
 
+  def create_preferred_entry(entry)
+    return if entry.nil?
+    TaxonConceptPreferredEntry.destroy_all(:taxon_concept_id => self.id)
+    TaxonConceptPreferredEntry.create(:taxon_concept_id => self.id, :hierarchy_entry_id => entry.id)
+  end
+
 private
 
   # Assume this method is expensive.
@@ -1049,12 +1055,6 @@ private
     else 
       false
     end
-  end
-
-  def create_preferred_entry(entry)
-    return if entry.nil?
-    TaxonConceptPreferredEntry.destroy_all(:taxon_concept_id => self.id)
-    TaxonConceptPreferredEntry.create(:taxon_concept_id => self.id, :hierarchy_entry_id => entry.id)
   end
 
   def vet_taxon_concept_names(options = {})

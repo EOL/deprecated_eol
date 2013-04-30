206de409b743646319ff3e1d113af58be9b36caf
JRice <jrice@eol.org>
2013-04-30 13:12:46 -0600

    Whoa, that was harder than expected, but I think the caching of associations will now work... and really be quite a bit faster...

--- app/models/data_object_caching.rb
+++ app/models/data_object_caching.rb
@@ -3,6 +3,14 @@ class DataObjectCaching
   attr_accessor :data_object
 
   DEFAULT_EXPIRATION = 12.hours
+  PRELOAD_ARRAYS = {
+    CuratedDataObjectsHierarchyEntry =>
+      [:vetted, :visibility, :data_object, :user, {:hierarchy_entry => [:name, :taxon_concept]}],
+    DataObjectsHierarchyEntry =>
+      [:vetted, :visibility, :data_object, {:hierarchy_entry => [:name, :taxon_concept]}],
+    UsersDataObject =>
+      [:vetted, :visibility, :data_object, :user, :taxon_concept]
+  }
 
   class << self
 
@@ -39,24 +47,48 @@ class DataObjectCaching
     end
   end
 
-  def associations(filter)
-    cache_name = DataObject.cached_name_for("title_#{data_object.id}_#{language.id}")
-  end
-
   # The only filter allowed right now is :published.
+  # Some association lists are just too big, so we have to store a hash of the classes and ids used to build them.
   def associations(filter = nil)
-    store_value("associations_#{data_object.id}_#{filter}") do
-      data_object.uncached_data_object_taxa(filter)
+    name = "associations_#{data_object.id}_#{filter}"
+    assocs = []
+    if exists?(name)
+      assoc_hash = read(name) # Read the hash,
+      assoc_hash.each do |key, values|
+        objects = key.send(:find, values)
+        key.send(:preload_associations, objects, PRELOAD_ARRAYS[key])
+        objects.each do |this|
+          assocs << DataObjectTaxon.new(this) # Rebuild the associations
+        end
+      end
+    else
+      store_value("associations_#{data_object.id}_#{filter}") do
+        hash = {}
+        assocs = data_object.uncached_data_object_taxa(filter)
+        assocs.group_by { |el| el.source.class }.each do |k,v|
+          hash[k] = v.map(&:source).map(&:id)
+        end
+        hash # Store the hash,
+      end
     end
+    assocs # Return the associations.
   end
 
   private
 
+  def exists?(name)
+    Rails.cache.exist?(DataObject.cached_name_for(name))
+  end
+
+  def read(name)
+    Rails.cache.read(DataObject.cached_name_for(name))
+  end
+
   def store_value(name, &block)
     cache_name = DataObject.cached_name_for(name)
-    value = Rails.cache.fetch(cache_name, expires_in: DEFAULT_EXPIRATION) do
-      yield
-    end
+    value = yield
+    value.dup if value.frozen?
+    Rails.cache.fetch(cache_name, expires_in: DEFAULT_EXPIRATION) { value }
     register(cache_name)
     value
   end

5db00588c8cdcc62d36bcdf053b1850a1dd93d9f
Patrick Leary <pleary@mbl.edu>
2013-08-12 10:44:04 -0400

    Fixed bug with overzealous uri labelling

--- lib/eol/sparql.rb
+++ lib/eol/sparql.rb
@@ -35,7 +35,7 @@ module EOL
     end
 
     def self.uri_to_readable_label(uri)
-      if matches = uri.to_s.match(/(\/|#)([a-z0-9,_-]{1,})$/i)
+      if is_uri?(uri) && matches = uri.to_s.match(/(\/|#)([a-z0-9,_-]{1,})$/i)
         return matches[2].underscore.tr('_', ' ').capitalize_all_words
       end
     end

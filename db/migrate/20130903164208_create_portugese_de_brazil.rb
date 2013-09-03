82d4fcce6646aadbe778a03846a9b146926633f7
JRice <jrice@eol.org>
2013-09-03 13:04:20 -0400

    WEB-4632 - OCCITAN and Brazilian Portugese added. Slight change to the language DB-import code to handle a border case. Other duties as required.

---
+++ db/migrate/20130903164208_create_portugese_de_brazil.rb
@@ -0,0 +1,12 @@
+class CreatePortugeseDeBrazil < ActiveRecord::Migration
+  def up
+    unless Language.exists?(iso_639_1: 'pt-BR')
+      Language.create(iso_639_1: "pt-BR", iso_639_2: "", iso_639_3: "", source_form: "português do Brasil", sort_order: 1,
+                      activated_on: Time.now)
+    end
+  end
+
+  def down
+    # Nothing to do.
+  end
+end

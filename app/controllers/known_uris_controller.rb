9b9554a12d26b57d20aa90dc4f776f4012122e8e
JRice <jrice@eol.org>
2013-09-03 08:44:42 -0400

    WIP - still not getting the relationship-saving correct in the controller (or perhaps in the form... or maybe the model's definition. Unclear.)

--- app/controllers/known_uris_controller.rb
+++ app/controllers/known_uris_controller.rb
@@ -137,6 +137,7 @@ class KnownUrisController < ApplicationController
 
   def update
     @known_uri = KnownUri.find(params[:id])
+    debugger
     if @known_uri.update_attributes(params[:known_uri])
       flash[:notice] = I18n.t(:known_uri_updated)
       redirect_back_or_default(known_uris_path)

2811d939a40ac8352135cb71db83300672073a63
JRice <jrice@eol.org>
2013-09-03 15:21:35 -0400

    So it turns out you need to translate languages that you add...

--- db/migrate/20130903164208_create_portugese_de_brazil.rb
+++ db/migrate/20130903164208_create_portugese_de_brazil.rb
@@ -2,8 +2,10 @@
 class CreatePortugeseDeBrazil < ActiveRecord::Migration
   def up
     unless Language.exists?(iso_639_1: 'pt-BR')
-      Language.create(iso_639_1: "pt-BR", iso_639_2: "", iso_639_3: "", source_form: "português do Brasil", sort_order: 1,
-                      activated_on: Time.now)
+      l = Language.create(iso_639_1: "pt-BR", iso_639_2: "", iso_639_3: "", source_form: "português do Brasil", sort_order: 1,
+                          activated_on: Time.now)
+      TranslatedLangauge.create(language_id: Language.default.id, original_language_id: l.id, label: 'Brazilian Portuguese')
+      TranslatedLangauge.create(language_id: l.id, original_language_id: l.id, label: 'português do Brasil')
     end
   end
 

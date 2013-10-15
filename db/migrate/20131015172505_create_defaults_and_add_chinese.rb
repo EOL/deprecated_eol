9f036b4a96d3f1fcdccfff7b412a83263b386b60
JRice <jrice@eol.org>
2013-10-15 13:26:10 -0400

    Prep for adding traditional chinese and bocce to production.

---
+++ db/migrate/20131015172505_create_defaults_and_add_chinese.rb
@@ -0,0 +1,12 @@
+class CreateDefaultsAndAddChinese < ActiveRecord::Migration
+  def up
+    ChangeableObjectType.create_defaults
+    Activity.create_defaults
+    Language.create(iso_639_1: 'zh-Hant', iso_639_2: 'zh-Hant', iso_639_3: 'zh-Hant', source_form: '繁體字', sort_order: 1,
+                    activated_on: Time.now.utc, language_group_id: 1)
+  end
+
+  def down
+    # Nothing to do.
+  end
+end

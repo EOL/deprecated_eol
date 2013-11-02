c8fa2a68282d80afeb652faa1f45d147589c99eb
JRice <jrice@eol.org>
2013-11-02 19:41:59 -0400

    Whoa. Much, much faster loading and ... everything. Damn. BUT ( ! ) ...it requires ( ! ) redis to be running, where it stores all translations.  You load the translations into redis using 'rake i18n:to_redis', and BAM.

--- config/initializers/i18n.rb
+++ config/initializers/i18n.rb
@@ -1,5 +1,5 @@
-require 'i18n' # without this, the gem will be loaded in the server but not in the console, for whatever reason
-I18n::Backend::Simple.send(:include, I18n::Backend::Fallbacks)
-
-# NOTE - JRice removed some code here.  Check 1fb9630c8ea16fedc61564cf33bc5ad9733ca472 to see it, if you want to
-# reformat errors (which should probably be a TODO ).
+# This allows some "intelligent" fallbacks for missing translations. See
+# https://github.com/svenfuchs/i18n/wiki/Fallbacks
+I18n::Backend::KeyValue.send(:include, I18n::Backend::Fallbacks)
+# And now we switch to using Redis:
+I18n.backend = I18n::Backend::KeyValue.new(Redis.new(db: 'eol_i18n'))

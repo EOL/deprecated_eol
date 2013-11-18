10c751f3e259c9e954de88a05f20b1f7e368e8ef
JRice <jrice@eol.org>
2013-11-18 09:12:28 -0500

    Fixed a failing spec because of a lack of CP licenses... which is weird, 'cause the defaults should include several, but apparently that doesn't work. Sigh.

--- app/models/curator_level.rb
+++ app/models/curator_level.rb
@@ -11,18 +11,6 @@ class CuratorLevel < ActiveRecord::Base
     alias :assistant :assistant_curator
   end
 
-  def self.create_defaults
-    if !self.master_curator
-      CuratorLevel.create(:label => 'Master Curator')
-    end
-    if !self.assistant_curator
-      CuratorLevel.create(:label => 'Assistant Curator')
-    end
-    if !self.full_curator
-      CuratorLevel.create(:label => 'Full Curator')
-    end
-  end
-
   def translated_label
     I18n.t("curator_level_#{label.gsub(' ', '_').downcase}")
   end

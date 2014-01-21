949c020c168640cd9019000898c53bed25e57e27
JRice <jrice@eol.org>
2014-01-21 13:43:38 -0500

    Removed I18nema, as it wasn't working on bocce.

--- config/initializers/i18n.rb
+++ config/initializers/i18n.rb
@@ -1,5 +1,4 @@
 require 'i18n' # without this, the gem will be loaded in the server but not in the console, for whatever reason
-I18n.backend = I18nema::Backend.new
 I18n::Backend::Simple.send(:include, I18n::Backend::Fallbacks)
 
 # NOTE - JRice removed some code here.  Check 1fb9630c8ea16fedc61564cf33bc5ad9733ca472 to see it, if you want to

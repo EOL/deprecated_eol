bcbe93b32f0681e68d20be83ac2e56b8ef22a1b9
JRice <jrice@eol.org>
2014-02-07 19:51:37 -0500

    Needed to remove one more occurrence of current_user_ratings

--- app/controllers/taxa/media_controller.rb
+++ app/controllers/taxa/media_controller.rb
@@ -11,8 +11,6 @@ class Taxa::MediaController < TaxaController
                                      type: params[:type],
                                      status: params[:status])
 
-    # TODO - current_user_ratings is unnecessary. We could handle this easily with duck-typing, but I don't want to do that right now:
-    @current_user_ratings = @taxon_media.applied_ratings
     @assistive_section_header = I18n.t(:assistive_media_header)
     set_canonical_urls(for: @taxon_page, paginated: @taxon_media.paginated, url_method: :taxon_media_url)
     current_user.log_activity(:viewed_taxon_concept_media, taxon_concept_id: @taxon_concept.id)

98a1a2e207b7e025b1d2810a7ce24836d69f23d8
JRice <jrice@eol.org>
2014-03-17 21:31:18 -0400

    Converted taxon_media_spec to use mocks. Much faster.

--- spec/controllers/taxa/media_controller_spec.rb
+++ spec/controllers/taxa/media_controller_spec.rb
@@ -2,70 +2,65 @@ require File.dirname(__FILE__) + '/../../spec_helper'
 
 describe Taxa::MediaController do
 
-  # TODO - get rid of this once you stub the stuff above
-  context 'stubbed' do
+  let(:taxon_concept) { TaxonConcept.gen }
+  let(:user) { FactoryGirl.build_stubbed(User) }
+  let(:taxon_page) { double(TaxonPage, scientific_name: 'Whatever somethingus', hierarchy_provider: "providedBy", preferred_scientific_name: "blahbalh") }
+  let(:data_object_1) { FactoryGirl.build_stubbed(DataObject) }
+  let(:data_object_2) { FactoryGirl.build_stubbed(DataObject) }
+  let(:data_object_3) { FactoryGirl.build_stubbed(DataObject) }
+  let(:taxon_media) { double(TaxonMedia, empty?: false, paginated: [data_object_1, data_object_2, data_object_3].paginate) }
 
-    let(:taxon_concept) { TaxonConcept.gen }
-    let(:user) { FactoryGirl.build_stubbed(User) }
-    let(:taxon_page) { double(TaxonPage, scientific_name: 'Whatever somethingus', hierarchy_provider: "providedBy", preferred_scientific_name: "blahbalh") }
-    let(:data_object_1) { FactoryGirl.build_stubbed(DataObject) }
-    let(:data_object_2) { FactoryGirl.build_stubbed(DataObject) }
-    let(:data_object_3) { FactoryGirl.build_stubbed(DataObject) }
-    let(:taxon_media) { double(TaxonMedia, empty?: false, paginated: [data_object_1, data_object_2, data_object_3].paginate) }
+  before do
+    allow(controller).to receive(:current_user) { user }
+    allow(TaxonPage).to receive(:new) { taxon_page }
+    allow(taxon_page).to receive(:media) { taxon_media }
+  end
+
+  context 'with no media' do
+
+    let(:taxon_media) { double(TaxonMedia, empty?: true, paginated: [].paginate) }
 
     before do
-      allow(controller).to receive(:current_user) { user }
-      allow(TaxonPage).to receive(:new) { taxon_page }
       allow(taxon_page).to receive(:media) { taxon_media }
     end
 
-    context 'with no media' do
-
-      let(:taxon_media) { double(TaxonMedia, empty?: true, paginated: [].paginate) }
- 
-      before do
-        allow(taxon_page).to receive(:media) { taxon_media }
-      end
-
-      it 'sets meta description as empty' do
-        get :index, id: taxon_concept.id
-        # NOTE - this is weird, but we're testing a private method using this syntax, since we need to check that this pseudo-helper (method) works:
-        # TODO - these meta_description methods really should be helpers.
-        expect(controller.meta_description).to match(/No multimedia is available/)
-      end
-
-    end
-
-    it 'indicates pictures, video, and audio in meta description' do
+    it 'sets meta description as empty' do
       get :index, id: taxon_concept.id
       # NOTE - this is weird, but we're testing a private method using this syntax, since we need to check that this pseudo-helper (method) works:
       # TODO - these meta_description methods really should be helpers.
-      expect(controller.meta_description).to match(/pictures/i)
-      expect(controller.meta_description).to match(/video/i)
-      expect(controller.meta_description).to match(/audio/i)
+      expect(controller.meta_description).to match(/No multimedia is available/)
     end
 
-    it 'should instantiate the taxon concept' do
-      get :index, id: taxon_concept.id
-      expect(assigns(:taxon_concept)).to eq(taxon_concept)
-    end
+  end
 
-    it 'should instantiate the taxon page' do
-      get :index, id: taxon_concept.id
-      expect(assigns(:taxon_page)).to eq(taxon_page)
-    end
+  it 'indicates pictures, video, and audio in meta description' do
+    get :index, id: taxon_concept.id
+    # NOTE - this is weird, but we're testing a private method using this syntax, since we need to check that this pseudo-helper (method) works:
+    # TODO - these meta_description methods really should be helpers.
+    expect(controller.meta_description).to match(/pictures/i)
+    expect(controller.meta_description).to match(/video/i)
+    expect(controller.meta_description).to match(/audio/i)
+  end
 
-    it 'should instantiate a TaxonMedia object' do
-      get :index, id: taxon_concept.id
-      expect(taxon_page).to have_received(:media) # TODO - this could / should also test the user and HE (if any) passed in.
-      expect(assigns(:taxon_media)).to eq(taxon_media)
-    end
+  it 'should instantiate the taxon concept' do
+    get :index, id: taxon_concept.id
+    expect(assigns(:taxon_concept)).to eq(taxon_concept)
+  end
 
-    it 'should instantiate an assistive header' do
-      get :index, id: taxon_concept.id
-      expect(assigns(:assistive_section_header)).to eq(I18n.t(:assistive_media_header))
-    end
+  it 'should instantiate the taxon page' do
+    get :index, id: taxon_concept.id
+    expect(assigns(:taxon_page)).to eq(taxon_page)
+  end
 
+  it 'should instantiate a TaxonMedia object' do
+    get :index, id: taxon_concept.id
+    expect(taxon_page).to have_received(:media) # TODO - this could / should also test the user and HE (if any) passed in.
+    expect(assigns(:taxon_media)).to eq(taxon_media)
+  end
+
+  it 'should instantiate an assistive header' do
+    get :index, id: taxon_concept.id
+    expect(assigns(:assistive_section_header)).to eq(I18n.t(:assistive_media_header))
   end
 
 end
--- spec/models/taxon_media_spec.rb
+++ spec/models/taxon_media_spec.rb
@@ -1,48 +1,54 @@
 require "spec_helper"
 
-# Surprisingly simple. ...That's because most of the work is (still) being done by TaxonConcept (which is really just calling Solr in
-# complex ways).  :\
+# Surprisingly simple. ...That's because most of the work is (still) being done by TaxonConcept (which is really
+# just calling Solr in complex ways).  :\
 describe TaxonMedia do
 
   before(:all) do
-    load_foundation_cache
-    @taxon_concept = TaxonConcept.gen # Doesn't need to be anything fancy, here.
-    @entry = HierarchyEntry.gen(taxon_concept: @taxon_concept)
-    @user = User.gen
+    DataType.create_enumerated
+    License.create_enumerated
+    Language.create_english
+    MimeType.create_enumerated
   end
 
-  def build_media
-    @media = TaxonMedia.new(@taxon_concept, @user)
+  let(:taxon_concept) { build_stubbed(TaxonConcept) }
+  let(:entry) { build_stubbed(HierarchyEntry, taxon_concept: taxon_concept) }
+  let(:user) { build_stubbed(User) }
+  let(:media) { TaxonMedia.new(taxon_concept, user) }
+
+  it 'has a paginated array of data objects' do
+    array = [DataObject.gen].paginate 
+    allow(taxon_concept).to receive(:data_objects_from_solr) { array }
+    expect(media.paginated).to eq(array)
+    expect(taxon_concept).to have_received(:data_objects_from_solr).at_least(1).times
   end
 
-  it 'should have an array of media to pass to will_paginate' do
-    a = [DataObject.gen].paginate
-    # NOTE - data_objects_from_solr is called in other places (for example, to get the exemplar image), but we don't care:
-    @taxon_concept.should_receive(:data_objects_from_solr).at_least(1).times.and_return a
-    build_media
-    @media.paginated.should == a
-  end
+  describe '#each_with_index' do
 
-  it 'should implement #each_with_index over the media' do
-    array = [DataObject.gen, DataObject.gen, DataObject.gen].paginate
-    # NOTE - data_objects_from_solr is called in other places (for example, to get the exemplar image), but we don't care:
-    @taxon_concept.should_receive(:data_objects_from_solr).at_least(1).times.and_return array
-    build_media
-    @media.each_with_index do |foo, bar|
-      foo.should == array[bar]
+    it 'iterates over data objects' do
+      array = build_list(DataObject, 3).paginate
+      allow(taxon_concept).to receive(:data_objects_from_solr) { array }
+      # Best image is loaded with .fetch:
+      allow(DataObject).to receive(:fetch) { array.first }
+      media.each_with_index do |object, index|
+        expect(object).to eq(array[index])
+      end
     end
+
+  end
+  
+  # NOTE - this is important mostly because a lot of the methods we'll end up calling on it in the controller/view
+  # are actually inherited from this class (which should be tested on its own, of course).
+  it 'inherits from TaxonUserClassificationFilter' do
+    expect(media).to be_a(TaxonUserClassificationFilter)
   end
 
-  it 'should be a TaxonUserClassificationFilter ... most of its methods are implemented (and speced) there' do
-    build_media
-    @media.should be_a(TaxonUserClassificationFilter)
-  end
-
-  # Ouch. But, really, we *do* want to ensure we get all the arguments right, here. Still, TODO, this would be really nice to... improve.
-  # Also, TODO, this is not exhaustive.  :\  We should pass in various arguments and see these change.
-  it 'should pass arguments to taxon concept for getting media' do
+  # Ouch. But, really, we *do* want to ensure we get all the arguments right, here. Still, TODO, this would be
+  # really nice to... improve.  Also, TODO, this is not exhaustive.  :\  We should pass in various arguments and see
+  # these change.
+  it 'passes an evil array of arguments to TaxonConcept#data_objects_from_solr' do
     array = [DataObject.gen].paginate
-    @taxon_concept.should_receive(:data_objects_from_solr).with(
+    allow(taxon_concept).to receive(:data_objects_from_solr).with(
       ignore_translations: true,
       return_hierarchically_aggregated_objects: true,
       page: 1,
@@ -52,23 +58,20 @@ describe TaxonMedia do
       vetted_types: ['trusted', 'unreviewed'],
       visibility_types: ['visible'],
       # NOTE - as noted in the class itself, I don't understand why we need this with skip_preload true.
-      preload_select: { data_objects: [ :id, :guid, :language_id, :data_type_id, :created_at, :mime_type_id, :object_title,
-                                                :object_cache_url, :object_url, :data_rating, :thumbnail_cache_url, :data_subtype_id,
-                                                :published ] }
-    ).ordered.and_return array
-    # This second call is made in order to get a count of all media. I'm not sure this is what was intended.  # TODO
-    @taxon_concept.stub(:data_objects_from_solr).with(
+      preload_select: { data_objects: [ :id, :guid, :language_id, :data_type_id, :created_at, :mime_type_id,
+        :object_title, :object_cache_url, :object_url, :data_rating, :thumbnail_cache_url, :data_subtype_id,
+        :published ] }
+    ) { array }
+    # This second call is made in order to get a count of all media.
+    allow(taxon_concept).to receive(:data_objects_from_solr).with(
       per_page: 1,
       sort_by: 'status',
       data_type_ids: DataType.image_type_ids,
       vetted_types: ['trusted', 'unreviewed'],
       visibility_types: ['visible'],
       published: true,
-      return_hierarchically_aggregated_objects: true).ordered.and_return array
-
-    # NOTE - this fails in the full test suite, occassionally.  Not sure why.  You might wanna check on that.
-    build_media
-    @media.paginated.should == array
+      return_hierarchically_aggregated_objects: true) { array }
+    media.paginated.should == array
   end
 
   # TODO - really, a lot of the above tests could be done with stubbing rather than reading the DB. Fix.
@@ -79,8 +82,8 @@ describe TaxonMedia do
       context 'with no media' do
 
         let(:taxon_media) do
-          allow(@taxon_concept).to receive(:data_objects_from_solr) { [].paginate }
-          TaxonMedia.new(@taxon_concept, @user)
+          allow(taxon_concept).to receive(:data_objects_from_solr) { [].paginate }
+          TaxonMedia.new(taxon_concept, user)
         end
 
         it 'knows it is empty' do
@@ -92,8 +95,8 @@ describe TaxonMedia do
       context 'with media' do
 
         let(:taxon_media) do
-          allow(@taxon_concept).to receive(:data_objects_from_solr) { [].paginate }
-          TaxonMedia.new(@taxon_concept, @user)
+          allow(taxon_concept).to receive(:data_objects_from_solr) { [].paginate }
+          TaxonMedia.new(taxon_concept, user)
         end
 
       end

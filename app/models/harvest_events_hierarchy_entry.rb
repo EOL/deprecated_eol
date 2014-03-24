50f0e1abb6006d336305740a5cc2ea1a49e0bd67
Patrick Leary <pleary@mbl.edu>
2014-03-24 00:55:32 -0400

    Fixes for WEB-4878, WEB-5096, WEB-5084, WEB-4913, WEB-5168

--- app/models/harvest_events_hierarchy_entry.rb
+++ app/models/harvest_events_hierarchy_entry.rb
@@ -1,3 +1,6 @@
+#
+# Some rows have been archived. See data_objects_harvest_events for more info.
+#
 class HarvestEventsHierarchyEntry < ActiveRecord::Base
   belongs_to :harvest_event
   belongs_to :hierarchy_entry

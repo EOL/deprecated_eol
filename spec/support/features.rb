1bd1ff8e6a0a0c0322a5385bf381b417285fbfdf
Dmitry Mozzherin <dmozzherin@gmail.com>
2014-04-04 11:26:05 -0400

    specs on linux

--- spec/support/features.rb
+++ spec/support/features.rb
@@ -1,3 +1,6 @@
+require_relative 'features/session_helpers'
+require_relative 'features/api_helpers'
+
 RSpec.configure do |config|
   config.include Features::SessionHelpers, type: :feature
   config.include Features::ApiHelpers, type: :feature

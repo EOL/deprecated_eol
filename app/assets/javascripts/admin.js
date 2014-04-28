443cbc1527d80a8bb14755172ececa06ba005871
JRice <jrice@eol.org>
2014-04-28 11:41:36 -0400

    WEB-5156 Added admin tool for collection item recount.
    I used this as an excuse to make the admin UI a *tiny* bit better (more
    general ajax usage; this has less code than before, but a little more of
    a framework in the view so it's not *quite* as ugly), and added to it
    the ability to re-count collection items.  Thus, both of the problems
    explained in the ticket can be solved from within the UI, now: the admin
    page's re-count, and the re-index on the collection page.

--- app/assets/javascripts/admin.js
+++ app/assets/javascripts/admin.js
@@ -1,7 +1,5 @@
-EOL.prep_flashes();
-
 $(function() {
   $('.data input').change(function() {
-    EOL.ajax_submit($(this), {url: $(this).data('href')+'&value='+$(this).is(':checked'), update: $('#flashes'), type: 'GET'});
+    $(this).closest('form').submit();
   });
 });
--- app/controllers/admins_controller.rb
+++ app/controllers/admins_controller.rb
@@ -5,8 +5,15 @@ class AdminsController < ApplicationController
   before_filter :check_authentication
   before_filter :restrict_to_admins
 
+  # TODO - this shouldn't be a show, it should be an index, right?
   def show
     @page_title = I18n.t(:admin_page_title)
+  end
+
+  def recount_collection_items
+    CollectionItem.counter_culture_fix_counts
+    flash[:notice] = I18n.t(:recount_collection_items_done)
+    redirect_to admin_path
   end
 
 end

d9bfb82d01775fc7b4d3a6725e31eaf2b0cb7653
Nathan Wilson <nathan@collectivesource.com>
2014-06-16 19:25:18 +0000

    Tweaks to the out-of-the-box rebuild process
    README.rdoc and local.rb.sample changes are based on PL's review
    of the eol_php_code process and deciding somethings should move
    here.
    
    The models.rb change is to get the test passing on Linux and parallel
    what is going on in features.rb.

--- spec/support/models.rb
+++ spec/support/models.rb
@@ -1,3 +1,6 @@
+require_relative 'models/cache_helpers'
+require_relative 'models/stdout_helpers'
+
 RSpec.configure do |config|
   config.include Models::CacheHelpers, type: :model
   config.include Models::StdoutHelpers, type: :model

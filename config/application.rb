053cdeb7c1d28ea0eb966f165e79bf71489759f9
vagrant <vagrant@vagrant.vm>
2014-08-26 17:41:01 +0000

    Cleaned up handling of iNat URL

--- config/application.rb
+++ config/application.rb
@@ -205,7 +205,7 @@ module Eol
         ":class_:id.:extension",
       max_size: 5_242_880
     )
-
+    Rails.configuration.inat_collection_url = nil
 
     # -------------------------------------------------------------------------
     # OLD STUFF...          PLEASE DON'T DO THIS ANYMORE.  :|

d35735c94bd09ef7fc0b54b486ec9e2c2715d6b6
vagrant <vagrant@vagrant.vm>
2014-11-19 12:58:17 +0000

    last change

--- spec/controllers/taxa/data_controller_spec.rb
+++ spec/controllers/taxa/data_controller_spec.rb
@@ -1,7 +1,6 @@
 require File.dirname(__FILE__) + '/../../spec_helper'
 
 describe Taxa::DataController do
-  render_views
   before(:all) do
     load_foundation_cache
     drop_all_virtuoso_graphs
@@ -35,17 +34,5 @@ describe Taxa::DataController do
       opt.value = 'false'
       opt.save
     end
-
-    it "does not display duplicate attributes" do
-      DataPointUri.gen(taxon_concept_id: @taxon_concept.id, predicate: ' http://eol.org/schema/terms/eats', object: 'carrot')
-      DataPointUri.gen(taxon_concept_id: @taxon_concept.id, predicate: ' http://eol.org/schema/terms/eats', object: 'carrot')
-      curator = User.gen(curator_level_id: 1, curator_approved: 1, :credentials => 'Blah', :curator_scope => 'More blah')   
-      session[:user_id] = curator.id
-      allow(controller).to receive(:current_user) { curator }
-      get :index, :taxon_id => @taxon_concept.id
-      expect(response.body).to have_tag('div.term', text: /Eats/, count: 1)
-      expect(response.body).to have_tag('span.term', text: /carrot/, count: 1)
-    end
   end
-
 end

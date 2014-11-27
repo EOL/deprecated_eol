aa1689021fad933b297f70aa1de0bd54c8ece743
vagrant <vagrant@vagrant.vm>
2014-11-27 12:44:34 +0000

    remove unnecssary rake file

--- db/migrate/20141112063325_remove_order_from_data_objects_refs.rb
+++
@@ -1,9 +0,0 @@
-class RemoveOrderFromDataObjectsRefs < ActiveRecord::Migration
-  def up
-    remove_column :data_objects_refs, :order
-  end
-
-  def down
-    add_column :data_objects_refs, :order, :int
-  end
-end

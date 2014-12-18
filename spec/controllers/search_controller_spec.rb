d3b6843d7f785bd38cf666ae6155b32e30299f1b
wala2.elsayed <walaa.elsayed@bibalex.org>
2014-12-18 12:43:07 +0200

    updating data search taxon autocomplete specs (delete load_foundation_cache)

--- spec/controllers/search_controller_spec.rb
+++ spec/controllers/search_controller_spec.rb
@@ -6,7 +6,7 @@ describe SearchController do
     
   describe 'index' do
     before(:all) do
-      truncate_all_tables
+      truncate_all_tables      
       Language.create_english
     end
   end
@@ -18,13 +18,18 @@ describe SearchController do
   end
   
   describe "taxon autocomplete" do
-    before(:all) do
-      truncate_all_tables
-      load_foundation_cache
-      @name = Name.first
-      @name.update_attributes(string: "cat")
-      @taxon = TaxonConcept.gen
-      @taxon_name = TaxonConceptName.gen(name: @name, taxon_concept: @taxon)
+    before(:all) do      
+      Vetted.create_enumerated
+      Visibility.create_enumerated
+      DataType.create_enumerated
+      SynonymRelation.gen_if_not_exists(label: "synonym")
+      SynonymRelation.gen_if_not_exists(label: "common name")
+      SynonymRelation.gen_if_not_exists(label: "genbank common name")
+      Language.gen_if_not_exists(label: 'Unknown', iso_639_1: '', source_form: 'Unknown')
+      Language.gen_if_not_exists(label: 'Scientific Name', iso_639_1: '', source_form: 'Scientific Name')
+      synonym = Synonym.gen
+      name = Name.find(synonym.name_id)
+      name.update_attributes(string: "cat")
       EOL::Solr::SiteSearchCoreRebuilder.begin_rebuild
     end
       

ea493f795d3b946be5f8297654c51e0858bdf968
YoustinaAtef <youstina.atef@bibalex.org>
2014-12-22 11:08:31 +0200

    fix views specs

--- spec/views/layouts/data.html.haml_spec.rb
+++ spec/views/layouts/data.html.haml_spec.rb
@@ -1,4 +1,4 @@
-describe 'data_objects/show' do
+describe 'layouts/data' do
 
   before(:all) do
     License.create_enumerated
@@ -7,12 +7,23 @@ describe 'data_objects/show' do
     Visibility.create_enumerated
     DataType.create_enumerated
     CuratorLevel.create_enumerated
-        @anonymous_user = User.gen
+    SpecialCollection.create_enumerated
+    c = Collection.gen
+    @anonymous_user = User.gen
     @curator = User.gen
     @curator.update_attributes(curator_approved: 1, curator_level_id: CuratorLevel.full.id)
+    @curator.build_watch_collection
     @master_curator = User.gen
     @master_curator.update_attributes(curator_approved: 1, curator_level_id: CuratorLevel.master.id)
+    @master_curator.build_watch_collection
   end
+  
+  before(:each) do
+    view.stub(:meta_data) { { title: 'The title' } }
+    view.stub(:current_url) { 'http://yes.we/really_have/this-helper.method' }
+    view.stub(:current_language) { Language.default }
+  end
+  
   context "published data_object" do
     before(:all) do
       d = DataObject.gen
@@ -21,27 +32,24 @@ describe 'data_objects/show' do
     end
     
     it "doesn't display delete button for anonymous user" do
-      view.stub(:current_url) { nil }
       view.stub(:current_user) { @anonymous_user }
       view.stub(:logged_in?) { false }
-      render :partial => "data_objects/show"
-      expect(rendered).not_to have_tag("a.button")
+      render 
+      expect(rendered).not_to have_tag("a.button", text: "delete")
     end
     
     it "doesn't display delete button for full curator or less" do
-      view.stub(:current_url) { nil }
       view.stub(:current_user) { @curator }
       view.stub(:logged_in?) { true }
-      render :partial => "data_objects/show"
-      expect(rendered).not_to have_tag("a.button")
+      render 
+      expect(rendered).not_to have_tag("a.button", text: "delete")
     end
     
     it "displays delete button for master curator" do
-      view.stub(:current_url) { nil }
       view.stub(:current_user) { @master_curator }
       view.stub(:logged_in?) { true }
-      render :partial => "data_objects/show"
-      expect(rendered).not_to have_tag("a.button")
+      render
+      expect(rendered).to have_tag("a.button", text: "#{I18n.t(:delete_data_object)}")
     end
   end
   
@@ -55,11 +63,10 @@ describe 'data_objects/show' do
     end
     
     it "doesn't display delete button" do
-      view.stub(:current_url) { nil }
       view.stub(:current_user) { @master_curator }
       view.stub(:logged_in?) { true }
-      render :partial => "data_objects/show"
-      expect(rendered).not_to have_tag("a.button")
+      render 
+      expect(rendered).not_to have_tag("a.button", text: "delete")
     end
   end
 end

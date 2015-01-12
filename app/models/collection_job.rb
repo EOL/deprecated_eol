98c2c9718f252855fd16b2591cf51ca395a9b3bf
wala2.elsayed <walaa.elsayed@bibalex.org>
2015-01-12 13:11:27 +0200

    WEB-5864: fix Missing item counts on collections

--- app/models/collection_job.rb
+++ app/models/collection_job.rb
@@ -147,20 +147,24 @@ private
 
   def reindex(affected)
     if remove?
+      # update collection items count
+      collection.update_attributes(collection_items_count: collection.collection_items.count)
       if all_items?
-        EOL::Solr::CollectionItemsCoreRebuilder.remove_collection(collection)
+        EOL::Solr::CollectionItemsCoreRebuilder.remove_collection(collection)        
       else
         EOL::Solr::CollectionItemsCoreRebuilder.remove_collection_items(collection_items)
       end
     else # move/copy
-      if ! all_items?
-        EOL::Solr::CollectionItemsCoreRebuilder.reindex_collection_items(affected)
-      else
-        EOL::Solr::CollectionItemsCoreRebuilder.reindex_collection(collection) unless copy?
-        if target_needed?
-          collections.each do |target_collection|
-            EOL::Solr::CollectionItemsCoreRebuilder.reindex_collection(target_collection)
-          end
+      unless copy?
+        EOL::Solr::CollectionItemsCoreRebuilder.reindex_collection(collection)
+        # update collection items count
+        collection.update_attributes(collection_items_count: collection.collection_items.count)
+      end
+      if target_needed?
+        collections.each do |target_collection|
+          EOL::Solr::CollectionItemsCoreRebuilder.reindex_collection(target_collection)
+          # update collection items count
+          target_collection.update_attributes(collection_items_count: target_collection.collection_items.count)
         end
       end
     end

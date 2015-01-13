2bf85293288098c980687095c54870d3343a164e
wala2.elsayed <walaa.elsayed@bibalex.org>
2015-01-13 13:12:35 +0200

    WEB-5866: migration for removing page_view_logs table

--- db/migrate/20150112120140_drop_page_view_logs.rb
+++ db/migrate/20150112120140_drop_page_view_logs.rb
@@ -1,5 +1,6 @@
-class DropPageViewLogs < ActiveRecord::Migration
-  def up
+class DropPageViewLogs < EOL::LoggingMigration
+  def self.up
+    connection.drop_table "page_view_logs"
   end
 
   def down

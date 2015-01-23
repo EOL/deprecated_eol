dc3a6cac773577aea7680a91ab19e506db4e46c1
Dmitry Mozzherin <dmozzherin@gmail.com>
2015-01-23 10:13:50 -0500

    unicorn env dependency

--- config/docker/unicorn.rb
+++ config/docker/unicorn.rb
@@ -7,6 +7,6 @@ pid "#{app_dir}/tmp/unicorn.pid"
 stderr_path "#{app_dir}/log/unicorn.stderr.log"
 stdout_path "#{app_dir}/log/unicorn.stdout.log"
 
-worker_processes 1
+worker_processes ENV["UNICORN_WORKER_PROCESSES"]
 listen "/tmp/unicorn.sock", :backlog => 64
 timeout 30

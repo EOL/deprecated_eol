d9ef8db7dc2b5f92aa90878e8b592f0e58ab553a
YoustinaAtef <youstina.atef@bibalex.org>
2015-02-08 13:12:30 +0200

    rake task to generate common_names.csv file

--- db/migrate/20150129134247_add_default_value_to_created_at.rb
+++
@@ -1,5 +0,0 @@
-class AddDefaultValueToCreatedAt < ActiveRecord::Migration
-  def change
-    change_column :image_sizes, :created_at, :datetime, :null => false, :default => CURRENT_TIMESTAMP
-  end
-end

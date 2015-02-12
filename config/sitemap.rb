6ea397394ba1578b4d373b6d1505b238a37e2730
AmrMMorad <amr.morad@bibalex.org>
2015-02-12 09:07:56 +0200

    Example added & little changes

--- config/sitemap.rb
+++ config/sitemap.rb
@@ -1,7 +1,5 @@
-SitemapGenerator::Interpreter.send :include, Rails.application.routes.url_helpers
 SitemapGenerator::Sitemap.create_index = :auto
 SitemapGenerator::Sitemap.default_host  = "http://localhost:3000"
-Rails.application.routes.default_url_options[:host] = 'http://localhost:3000'
 
 SitemapGenerator::Sitemap.create do
   #hard-coded links
@@ -18,8 +16,8 @@ SitemapGenerator::Sitemap.create do
   #cms_page_urls
   names_which_have_routes = [ 'help', 'about', 'news', 'explore_biodiversity', 
     'contact', 'terms_of_use', 'citing', 'privacy', 'curators' ]
-  content_pages = ContentPage.find(:all, :select => { :content_pages => [ :id, :page_name ] }, 
-    :conditions => "active = 1 AND page_name NOT IN ('#{names_which_have_routes.join("', '")}')")
+  content_pages = ContentPage.find(:all, select: { content_pages: [ :id, :page_name ] }, 
+    conditions: "active = 1 AND page_name NOT IN ('#{names_which_have_routes.join("', '")}')")
   content_pages.each do |content_page|
     page_url = cms_page_path(content_page.page_name)
     page_url.gsub!(/%20/, '_') # turn %20 into _
@@ -28,14 +26,14 @@ SitemapGenerator::Sitemap.create do
   end
   
   #community_page_urls
-  communities = Community.find(:all, :conditions => 'published = 1', :select => 'id, updated_at')
+  communities = Community.find(:all, conditions: 'published = 1', select: 'id, updated_at')
   communities.each do |community|
     add community_path(community.id), priority: 1, lastmod: community.updated_at
     add community_newsfeed_path(community.id), priority: 0.5    
   end
   
   #collection_page_urls
-  collections = Collection.find(:all, :conditions => 'published = 1', :select => 'id, updated_at')
+  collections = Collection.find(:all, conditions: 'published = 1', select: 'id, updated_at')
   collections.each do |collection|
     add collection_path(collection.id), priority: 1, lastmod: collection.updated_at if collection.updated_at
     add collection_newsfeed_path(collection.id), priority:0.5
@@ -44,11 +42,11 @@ SitemapGenerator::Sitemap.create do
   #content_partneer_urls
   content_partners = ContentPartner.where(is_public: 1).select([:id, :updated_at])
   content_partners.each do |content_partner|
-    add content_partner_path(content_partner.id), :changefreq => 'weekly', :priority => 1
+    add content_partner_path(content_partner.id), changefreq: 'weekly', priority: 1
   end
   
   #user_urls
-  users = User.find(:all, :conditions => 'active = 1 AND (hidden = 0 OR hidden IS NULL)', :select => 'id, updated_at')
+  users = User.find(:all, conditions: 'active = 1 AND (hidden = 0 OR hidden IS NULL)', select: 'id, updated_at')
   users.each do |user|
     add user_path(user.id),          priority: 1, lastmod: user.updated_at if user.updated_at
     add user_newsfeed_path(user.id), priority: 0.5

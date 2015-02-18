8804892f8e96f8313350199f9de01d148ee7141c
AmrMMorad <amr.morad@bibalex.org>
2015-02-18 08:10:41 +0200

    applying comments

--- config/sitemap.rb
+++ config/sitemap.rb
@@ -17,8 +17,7 @@ SitemapGenerator::Sitemap.create do
   #cms_page_urls
   names_which_have_routes = [ 'help', 'about', 'news', 'explore_biodiversity', 
     'contact', 'terms_of_use', 'citing', 'privacy', 'curators' ]
-  content_pages = ContentPage.find(:all, select: { content_pages: [ :id, :page_name ] }, 
-    conditions: "active = 1 AND page_name NOT IN ('#{names_which_have_routes.join("', '")}')")
+  content_pages =  ContentPage.select(:content_pages => [ :id, :page_name ]).where(["active = ? AND page_name NOT IN (?)", true, names_which_have_routes])
   content_pages.each do |content_page|
     page_url = cms_page_path(content_page.page_name)
     page_url.gsub!(/%20/, '_') # turn %20 into _
@@ -27,14 +26,14 @@ SitemapGenerator::Sitemap.create do
   end
   
   #community_page_urls
-  communities = Community.find(:all, conditions: 'published = 1', select: 'id, updated_at')
+  communities = Community.select([:id, :updated_at]).where(published: true)
   communities.each do |community|
     add community_path(community.id), priority: 1, lastmod: community.updated_at
     add community_newsfeed_path(community.id), priority: 0.5    
   end
   
   #collection_page_urls
-  collections = Collection.find(:all, conditions: 'published = 1', select: 'id, updated_at')
+  collections = Collection.select([:id, :updated_at]).where(published: true)
   collections.each do |collection|
     add collection_path(collection.id), priority: 1, lastmod: collection.updated_at if collection.updated_at
     add collection_newsfeed_path(collection.id), priority:0.5
@@ -47,7 +46,7 @@ SitemapGenerator::Sitemap.create do
   end
   
   #user_urls
-  users = User.find(:all, conditions: 'active = 1 AND (hidden = 0 OR hidden IS NULL)', select: 'id, updated_at')
+  users = User.select([:id, :updated_at]).where("active = ? AND (hidden = 0 OR hidden IS NULL)", true)
   users.each do |user|
     add user_path(user.id),          priority: 1, lastmod: user.updated_at if user.updated_at
     add user_newsfeed_path(user.id), priority: 0.5
@@ -55,20 +54,7 @@ SitemapGenerator::Sitemap.create do
   end
   
   #taxon_page_urls
-  base_conditions = "published = 1 AND supercedure_id = 0 AND vetted_id = #{Vetted.trusted.id}"
-  min_id, max_id = TaxonConcept.connection.execute("SELECT MIN(id), MAX(id) FROM taxon_concepts WHERE #{base_conditions}").first
-  min_id = min_id.to_i
-  max_id = max_id.to_i  
-  iteration_size = 200000
-  start_time = Time.now
-  start = min_id
-      
-  until start > max_id
-    concept_ids = TaxonConcept.connection.select_values("SELECT id FROM taxon_concepts WHERE #{base_conditions}
-    AND id BETWEEN #{start} AND #{start + iteration_size - 1}")
-    concept_ids.each do |tc_id|
-      add "/pages/#{tc_id}/overview", priority: 1, changefreq: 'weekly'
-    end
-    start += iteration_size
+  TaxonConcept.select(:id).where(published: true, supercedure_id: 0, vetted_id: Vetted.trusted.id).find_each do |tc|
+    add "/pages/#{tc.id}/overview", priority: 1, changefreq: 'weekly'
   end
 end

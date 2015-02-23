2616d9bcceeb26941ba85a617cbfa002cdb4c6bd
AmrMMorad <amr.morad@bibalex.org>
2015-02-23 14:44:20 +0200

    Remove comments

--- app/models/resource_validation.rb
+++ app/models/resource_validation.rb
@@ -4,10 +4,7 @@ class ResourceValidation
   def self.perform(current_user_id, content_partner_id, resource_id, port)
     partner = ContentPartner.find(content_partner_id, include: {resources: :resource_status })
     resource = partner.resources.find(resource_id)
-    # user = User.find(current_user_id)
     resource.upload_resource_to_content_master!(port)    
-    # Notifier.user_message(user.username, user.email, I18n.t(:content_partner_resource_validation_finish_notice, resource_status: resource.status_label)).deliver
-    debugger
     ResourceValidation.new.write_log_send_mail(current_user_id, resource_id)
   end
   

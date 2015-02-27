c24b76d797efb23e8d6d5fc7cde613483e15227f
JRice <jrice@eol.org>
2015-02-27 14:06:24 -0500

    Removing comments, updating script output

--- app/models/resource_destroyer.rb
+++ app/models/resource_destroyer.rb
@@ -6,20 +6,20 @@ class ResourceDestroyer
   # TODO: notifications. The user who submitted the request should get a comment
   # on their timeline when this is done, including any error messages.
   def self.perform(args)
-    Rails.logger.info "++ #{Time.now.strftime("%F %T")} "\
-      "ResourceDestroyer: #{args.values.join(', ')}"
+    Rails.logger.error "ResourceDestroyer: #{args.values.join(', ')}"
     Resource.with_master do
       if Resource.exists?(args["id"])
         begin
           Resource.find(args["id"]).destroy
         rescue => e
-          puts "   FAILED: #{e.message}"
+          Rails.logger.error "ResourceDestroyer (#{args["id"]}) FAILED: "\
+            " #{e.message}"
         end
       else
-        puts "   Resource #{args["id"]} doesn't exist, skippped."
+        Rails.logger.error "ResourceDestroyer: Resource #{args["id"]} doesn't exist, skippped."
       end
     end
-    puts "  #{Time.now.strftime("%F %T")} Done."
+    Rails.logger.error "ResourceDestroyer (#{args["id"]}) Done."
   end
 
   def self.enqueue(id)

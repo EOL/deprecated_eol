a9e3c62a4c8cb7c92670a02fbde30df7260bb932
JRice <jrice@eol.org>
2015-03-20 14:30:26 -0400

    update unicorn config

--- config/docker/unicorn.rb
+++ config/docker/unicorn.rb
@@ -7,6 +7,6 @@ pid "#{app_dir}/tmp/unicorn.pid"
 stderr_path "#{app_dir}/log/unicorn.stderr.log"
 stdout_path "#{app_dir}/log/unicorn.stdout.log"
 
-worker_processes ENV["UNICORN_WORKER_PROCESSES"]
+worker_processes ENV["EOL_UNICORN_WORKER_PROCESSES"].to_i
 listen "/tmp/unicorn.sock", :backlog => 64
 timeout 30

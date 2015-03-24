028c483cdd4c4414314c5bcdd4d4962c770cc694
JRice <jrice@eol.org>
2015-03-24 09:57:38 -0400

    Caching of overview data is better

--- app/controllers/taxa/overview_controller.rb
+++ app/controllers/taxa/overview_controller.rb
@@ -5,12 +5,11 @@ class Taxa::OverviewController < TaxaController
   before_filter :instantiate_taxon_page,
     :redirect_if_superceded,
     :instantiate_preferred_names
-    
+
   def show
     with_master_if_curator do
       @overview = @taxon_page.overview
       @data = @taxon_page.data
-      @overview_data = @data.get_data_for_overview
       @range_data = @data.ranges_for_overview
     end
     @assistive_section_header = I18n.t(:assistive_overview_header)

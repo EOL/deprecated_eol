a1745763a309b0de12a643ea53e6ebccfb98c8d6
JRice <jrice@eol.org>
2015-03-30 13:38:07 -0400

    One more migration change

--- db/migrate/20140822182026_content_partners_add_defaults.rb
+++ db/migrate/20140822182026_content_partners_add_defaults.rb
@@ -1,5 +1,6 @@
 class ContentPartnersAddDefaults < ActiveRecord::Migration
   def up
+    # TODO: This may not work; you may have to lower these 6000s to 5000s.
     change_column(:content_partners, :string, :notes, limit: 6000, default: "",
       null: true)
     change_column(:content_partners, :string, :description_of_data,
@@ -8,6 +9,8 @@ class ContentPartnersAddDefaults < ActiveRecord::Migration
       default: "", null: true)
     change_column(:content_partners, :string, :full_name, default: "",
       null: true)
+    change_column(:content_partners, :string, :admin_notes, default: "",
+      limit: 6000,null: true)
   end
 
   def down

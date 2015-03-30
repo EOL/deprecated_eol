fe8008ce45f0a0cd30b58f092500ff40cb8f7390
JRice <jrice@eol.org>
2015-03-30 09:38:39 -0400

    Finishing migration for trait.rb

--- app/models/trait.rb
+++ app/models/trait.rb
@@ -11,6 +11,8 @@ create_table :traits do |t|
   # A taxon_concept is always the subject.
   t.integer :taxon_concept_id, null: false
   t.integer :units_known_uri_id
+  # the "uri" here is actually the URI to the EOL TraitBank triplestore.
+  t.string :uri
   # predicate will map to a known_uri, usually.
   t.string :predicate, null: false
   # object will be a taxon concept URI, a known_uri, or a "raw" value. It may
@@ -20,3 +22,9 @@ create_table :traits do |t|
   t.string :life_stage
   t.string :statistical_method
   t.string :sex
+end
+add_index :traits, :data_point_uri_id
+add_index :traits, :uri # For searches, we don't have a taxon concept id.
+add_index :traits, [:uri, :taxon_concept_id], # For lookups from virtuoso
+  name: "taxon_and_uri", unique: true
+add_index :traits, :taxon_concept_id

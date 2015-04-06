f84d5e474664b272973e791d4421a12678ab1e98
HudaSoliman <Huda.Soliman@bibalex.org>
2015-04-06 13:07:45 +0200

    fixing a bug of adding a common_doesn't always redirect to the right tab

--- app/controllers/taxa/names_controller.rb
+++ app/controllers/taxa/names_controller.rb
@@ -64,8 +64,7 @@ class Taxa::NamesController < TaxaController
         expire_taxa([@taxon_concept.id])
       end
     end
-    store_location params[:return_to] unless params[:return_to].blank?
-    redirect_back_or_default common_names_taxon_names_path(@taxon_concept)
+    redirect_to common_names_taxon_names_path(@taxon_concept)
   end
 
   # PUT /pages/:taxon_id/names currently only used to update common_names

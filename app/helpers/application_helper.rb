004c08524a2412df8eafcfca8740d76f0fb509a8
HudaSoliman <Huda.Soliman@bibalex.org>
2015-04-09 13:53:51 +0200

    final changes

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -548,8 +548,14 @@ module ApplicationHelper
 
   def time_diff(start_time, end_time)
     seconds_diff = (start_time - end_time).to_i.abs
-    format_time(seconds_diff)
+    hours = seconds_diff / 3600
+    seconds_diff -= hours * 3600
+    minutes = seconds_diff / 60
+    seconds_diff -= minutes * 60
+    seconds = seconds_diff
+    "#{hours.to_s.rjust(2, '0')}:#{minutes.to_s.rjust(2, '0')}:#{seconds.to_s.rjust(2, '0')}"
   end
+    
 
   def format_time(given_seconds)
      Time.at(given_seconds).utc.strftime("%H:%M:%S")

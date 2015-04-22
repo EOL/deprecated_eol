efc096d75c0bcf8b31b3a06e310ada306fc4772e
JRice <jrice@eol.org>
2015-04-22 08:48:37 -0400

    Neglected to make one change to resource validation worker

--- app/models/resource_validation.rb
+++ app/models/resource_validation.rb
@@ -14,9 +14,9 @@ class ResourceValidation
     end
 
     def write_log_send_mail(user_id, resource_id)
-      log = CuratorActivityLog.create!(user_id: user_id, changeable_object_type_id: ChangeableObjectType.resource_validation.id,
+      activity = CuratorActivityLog.create!(user_id: user_id, changeable_object_type_id: ChangeableObjectType.resource_validation.id,
              target_id: resource_id, activity: Activity.resource_validation)
-      PendingNotification.create!(user_id: user_id, notification_frequency_id: NotificationFrequency.immediately.id, target: log, reason: 'auto_email_after_validation')
+      PendingNotification.create!(user_id: user_id, notification_frequency_id: NotificationFrequency.immediately.id, target: activity, reason: 'auto_email_after_validation')
       Resque.enqueue(PrepareAndSendNotifications)
     end
 

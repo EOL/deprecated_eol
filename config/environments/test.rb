0fad202e67cc3206fbc903a31db445ad1f646d2a
YoustinaAtef <youstina.atef@bibalex.org>
2015-05-12 14:01:12 +0300

    59 failures

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -8,7 +8,7 @@ Eol::Application.configure do
   # test suite. You never need to work with it otherwise. Remember that
   # your test database is "scratch space" for the test suite and is wiped
   # and recreated between test runs. Don't rely on the data there!
-  config.cache_store = :null_store
+  config.cache_store = :dalli_store
   config.cache_classes = false
 
   # Configure static asset server for tests with Cache-Control for performance

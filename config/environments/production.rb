723bbb96f25c6aaa86a8835c04c8d95437ce1b78
JRice <jrice@eol.org>
2015-06-05 10:41:34 -0400

    trying cfg change

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -22,7 +22,7 @@ Eol::Application.configure do
   config.assets.expire_after 2.months
 
   # Don't fallback to assets pipeline if a precompiled asset is missed
-  config.assets.compile = false
+  config.assets.compile = true
 
   # Generate digests for assets URLs
   config.assets.digest = true

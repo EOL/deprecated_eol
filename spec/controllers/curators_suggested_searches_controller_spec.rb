8e6cf2fa0741dc721b5c2a7a43314b8810ef67df
HudaSoliman <Huda.Soliman@bibalex.org>
2015-06-15 09:50:59 +0200

    pending

--- spec/controllers/curators_suggested_searches_controller_spec.rb
+++ spec/controllers/curators_suggested_searches_controller_spec.rb
@@ -1,5 +1,5 @@
 require 'spec_helper'
 
-describe CuratorsSuggestedSearchController do
+describe CuratorsSuggestedSearchesController do
 
 end
--- spec/models/curators_suggested_search_spec.rb
+++ spec/models/curators_suggested_search_spec.rb
@@ -1,11 +1,16 @@
 require 'spec_helper'
 
-render_views
 describe CuratorsSuggestedSearch do
+ include ApplicationHelper
   before(:all) do
-   @suggested_searches =  5.times{ |i| CuratorsSuggestedSearch.create(label: "label#{i}", uri: KnownUri.gen)}
+    @mass = KnownUri.gen_if_not_exists({ uri: Rails.configuration.uri_term_prefix + 'mass', name: 'Mass', uri_type_id: UriType.measurement.id })
+    @suggested_searches =  5.times{ |i| CuratorsSuggestedSearch.create(label: "label#{i}", uri: @mass)}
   end
-  it 'has 5 searches' do 
-    expect (CuratorsSuggestedSearch.count).to eq(5)
+  it 'has at least 5 searches' do 
+    expect(CuratorsSuggestedSearch.count).to eq(5)
+  end
+
+  describe '#suggested_searches' do
+    it 'gives'
   end
 end

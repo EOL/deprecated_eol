b4565fd38ce9b6062182fb68475d8088322b5cef
JRice <jrice@eol.org>
2015-07-01 11:00:54 -0400

    fixing bad data overview cache key, cleaning up comments

--- app/models/taxon_user_classification_filter.rb
+++ app/models/taxon_user_classification_filter.rb
@@ -1,25 +1,28 @@
-# This is an interface to the TaxonConcept that is aware of the user and whether the user wants to see data from a
-# single partner or not. This is the basis for may of the taxon_concept resources that are exposed on EOL.
+# This is an interface to the TaxonConcept that is aware of the user and whether
+# the user wants to see data from a single partner or not. This is the basis for
+# may of the taxon_concept resources that are exposed on EOL.
 #
-# NOTE - when you call #initialize, the TaxonConcept is *immediately* loaded, if it wasn't already... so if you're
-# counting on ARel to handle lazy loading, you will want to attach any selects and includes *before* calling
-# TaxonPage.
+# NOTE - when you call #initialize, the TaxonConcept is *immediately* loaded, if
+# it wasn't already... so if you're counting on ARel to handle lazy loading, you
+# will want to attach any selects and includes *before* calling TaxonPage.
 #
-# NOTE - as you write descendants of this class, please DO NOT rely on the #method_missing!  Please.  We want to
-# eventually remove that. Where needed, call your methods striaght from #taxon_concept.
+# NOTE - as you write descendants of this class, please DO NOT rely on the
+# #method_missing!  Please.  We want to eventually remove that. Where needed,
+# call your methods striaght from #taxon_concept.
 class TaxonUserClassificationFilter
 
   attr_reader :taxon_concept, :user
 
-  def initialize(taxon_concept, user = nil, hierarchy_entry = nil) 
+  def initialize(taxon_concept, user = nil, hierarchy_entry = nil)
     @taxon_concept = taxon_concept
     @user = user || EOL::AnonymousUser.new(Language.default)
     @_hierarchy_entry = hierarchy_entry
     after_initialize
   end
 
-  # You could say TaxonUserClassificationFilter has_a overview.  :)
-  # NOTE - what is a little odd is that an Overview now has an #overview (inherited).  ...Which is... weird... but hey.
+  # You could say TaxonUserClassificationFilter has_a overview.  :) NOTE - what
+  # is a little odd is that an Overview now has an #overview (inherited).
+  # ...Which is... weird... but hey.
   def overview
     TaxonOverview.new(taxon_concept, user, _hierarchy_entry)
   end
@@ -38,14 +41,16 @@ class TaxonUserClassificationFilter
     @media ||= TaxonMedia.new(taxon_concept, user, options)
   end
 
-  # NOTE - *THIS IS IMPORTANT* ... when you see "_hierarchy_entry", it means "the one specified by initialize." When
-  # you see "hierarchy_entry" (no leading underscore) it means "do the right thing".
+  # NOTE - *THIS IS IMPORTANT* ... when you see "_hierarchy_entry", it means
+  # "the one specified by initialize." When you see "hierarchy_entry" (no
+  # leading underscore) it means "do the right thing".
   def hierarchy_entry
     _hierarchy_entry || @taxon_concept.entry
   end
 
-  # This tells you whether the presenter is being viewed with a classification filter. ...Of course. Please don't
-  # trust the return value to be the actual HE; you should only be using this for t/f tests.
+  # This tells you whether the presenter is being viewed with a classification
+  # filter. ...Of course. Please don't trust the return value to be the actual
+  # HE; you should only be using this for t/f tests.
   def classification_filter?
     _hierarchy_entry
   end
@@ -55,10 +60,10 @@ class TaxonUserClassificationFilter
     hierarchy_entry_or_taxon_concept.title_canonical_italicized
   end
 
-  # helper.link_to "foo", app.taxon_overview_path(taxon_page) # Results depend on hierarchy_entry:
-  # => "<a href=\"/pages/910093/hierarchy_entries/16/overview\">foo</a>"
-  # OR
-  # => "<a href=\"/pages/910093/overview\">foo</a>"
+  # helper.link_to "foo", app.taxon_overview_path(taxon_page) # Results depend
+  # on hierarchy_entry: => "<a
+  # href=\"/pages/910093/hierarchy_entries/16/overview\">foo</a>" OR => "<a
+  # href=\"/pages/910093/overview\">foo</a>"
   def to_param
     classification_filter? ? "#{taxon_concept.to_param}/hierarchy_entries/#{_hierarchy_entry.to_param}" :
                             taxon_concept.to_param
@@ -68,7 +73,8 @@ class TaxonUserClassificationFilter
     map_taxon_concept.gbif_map_id
   end
 
-  # NOTE - this checks if the *filtered* page really has a map (as opposed to whether there is any map at all):
+  # NOTE - this checks if the *filtered* page really has a map (as opposed to
+  # whether there is any map at all):
   def map?
     map_taxon_concept.has_map? && map
   end
@@ -85,10 +91,12 @@ class TaxonUserClassificationFilter
     classification_filter? ? _hierarchy_entry.kingdom : hierarchy_entry.kingdom
   end
 
-  # We're not inheriting from Delegator here, because we want to be more surgical about what gets called on
-  # TaxonConcept... but this sets up *almost* everything to go to the TC (without maintaining a full list).
-  # TODO - we might want to stop doing this, so that TaxonPage (or the other classes that rely on it) has a
-  # nice, mangageable interface to all of the information we might want about a TaxonConcept.
+  # We're not inheriting from Delegator here, because we want to be more
+  # surgical about what gets called on TaxonConcept... but this sets up *almost*
+  # everything to go to the TC (without maintaining a full list). TODO - we
+  # might want to stop doing this, so that TaxonPage (or the other classes that
+  # rely on it) has a nice, mangageable interface to all of the information we
+  # might want about a TaxonConcept.
   def method_missing(method, *args, &block)
     super unless taxon_concept.respond_to?(method)
     class_eval { delegate method, to: :taxon_concept } # Won't use method_missing next time!
@@ -109,7 +117,8 @@ class TaxonUserClassificationFilter
     @hierarchy_entries
   end
 
-  # This is used by the TaxaController (and thus all its children) to help build information for ALL translations:
+  # This is used by the TaxaController (and thus all its children) to help build
+  # information for ALL translations:
   def hierarchy_provider
     classification_filter? ? _hierarchy_entry.hierarchy_provider : nil
   end
@@ -149,8 +158,8 @@ class TaxonUserClassificationFilter
   end
 
   # TODO - This belongs on TaxonNames or the like:
-  # TODO - rewrite EOL::CommonNameDisplay to make use of TaxonPage... and to not suck.
-  # options are just passed along to EOL::CommonNameDisplay.
+  # TODO - rewrite EOL::CommonNameDisplay to make use of TaxonPage... and to not
+  # suck. options are just passed along to EOL::CommonNameDisplay.
   def common_names(options = {})
     return @common_names if @common_names
     if _hierarchy_entry
@@ -189,7 +198,7 @@ class TaxonUserClassificationFilter
   # NOTE - this ONLY works on overview and media.
   # TODO - move this to a mixin, which we can then call on those two.
   def correct_bogus_exemplar_image
-    if image.nil? && ! @media.empty? && ! @media.first.map? 
+    if image.nil? && ! @media.empty? && ! @media.first.map?
       TaxonConceptCacheClearing.clear_exemplar_image(taxon_concept)
       @image = nil # Reload it the next time you need it.
     end
@@ -200,8 +209,9 @@ class TaxonUserClassificationFilter
   end
 
   # TODO - clearly this belongs in TaxonDetails...
-  # 10/2/13 - this has been turned into a class method and is used in multiple places including the API
-  # where we need a generic catch-all preload of info to present DataObject
+  # 10/2/13 - this has been turned into a class method and is used in multiple
+  # places including the API where we need a generic catch-all preload of info
+  # to present DataObject
   def self.preload_details(data_objects, user = nil)
     DataObject.replace_with_latest_versions!(data_objects, language_id: user ? user.language_id : nil, check_only_published: true)
     includes = [ {
@@ -227,12 +237,14 @@ class TaxonUserClassificationFilter
 
 private
 
-  # Using the Rails codebase's convention of putting an underscore before the method name, here, because this isn't a
-  # value you should ever be calling from outside this class... though we have a #hierarchy_entry method that you
-  # *can* call. The difference is that _this value can be nil (when no hierarchy_entry was passed into the
-  # constructor). There are places, such as #hierarchy_entry_or_taxon_concept, where we need to know this, but only
-  # privately. Outside of this class, you can test whether the entry was provided to the constructor using
-  # #classification_filter?
+  # Using the Rails codebase's convention of putting an underscore before the
+  # method name, here, because this isn't a value you should ever be calling
+  # from outside this class... though we have a #hierarchy_entry method that you
+  # *can* call. The difference is that _this value can be nil (when no
+  # hierarchy_entry was passed into the constructor). There are places, such as
+  # #hierarchy_entry_or_taxon_concept, where we need to know this, but only
+  # privately. Outside of this class, you can test whether the entry was
+  # provided to the constructor using #classification_filter?
   def _hierarchy_entry
     @_hierarchy_entry
   end
@@ -241,13 +253,17 @@ private
     _hierarchy_entry || taxon_concept
   end
 
-  # NOTE - the field aliases used in this query are required by #build_related_names_hash and are used in views.
+  # NOTE - the field aliases used in this query are required by
+  # #build_related_names_hash and are used in views.
   def get_related_names(which)
     from = which == :children ? 'he_child' : 'he_parent'
     other = which == :children ? 'he_parent' : 'he_child'
-    filter = classification_filter? ? "id=#{_hierarchy_entry.id}" : "taxon_concept_id=#{taxon_concept.id}"
-    # NOTE - if you chande this at all... even a space... the spec will fail. Perhaps you should re-write this? For
-    # example, could you make this a method on HierarchyEntry and create scopes using that method in a lambda?
+    filter = classification_filter? ?
+      "id=#{_hierarchy_entry.id}" :
+      "taxon_concept_id=#{taxon_concept.id}"
+    # NOTE - if you chande this at all... even a space... the spec will fail.
+    # Perhaps you should re-write this? For example, could you make this a
+    # method on HierarchyEntry and create scopes using that method in a lambda?
     HierarchyEntry.connection.execute("
       SELECT n.id name_id, n.string name_string, n.canonical_form_id, #{from}.taxon_concept_id,
         h.label hierarchy_label, #{from}.id hierarchy_entry_id
@@ -261,9 +277,10 @@ private
     ")
   end
 
-  # TODO - These are hashes that are used in the view(s), but we should document (or make obvious) how they are
-  # constructed and used:
-  # This is a method used to build and sort the hash we use in related names (synonyms) views (and sort the sources).
+  # TODO - These are hashes that are used in the view(s), but we should document
+  # (or make obvious) how they are constructed and used: This is a method used
+  # to build and sort the hash we use in related names (synonyms) views (and
+  # sort the sources).
   def build_related_names_hash(results)
     # NOTE - these field names come from the #get_related_names query.
     name_string_i = results.fields.index('name_string')
@@ -287,7 +304,9 @@ private
     grouped.sort {|a,b| a[0] <=> b[0]}
   end
 
-protected # You can only call these from the classes that inherit from TaxonUserClassificationFilter
+# You can only call these from the classes that inherit from
+# TaxonUserClassificationFilter
+protected
 
   def after_initialize
     # Do nothing. If you inherit from the class, you'll want to override this.

27c63153edcab48e681e00f6537ed05590e2b2e9
JRice <jrice@eol.org>
2015-10-15 08:26:39 -0400

    WIP, but almost ready with concept assignment

--- spec/models/hierarchy/concept_assignment.rb
+++ spec/models/hierarchy/concept_assignment.rb
@@ -9,6 +9,7 @@ class Hierarchy
       @hierarchy = hierarchy
       @compared = []
       @confirmed_exclusions = {}
+      @entries_matched = []
       @superceded = {}
     end
 
@@ -24,7 +25,7 @@ class Hierarchy
       get_confirmed_exclusions
       # TODO: this is slow; once we have harvesting ported, we can ensure that
       # it is corrected for each harvest, which is adequate. In that case,
-      # simple remove this fix_counts:
+      # simply remove this fix_counts:
       HierarchyEntry.counter_culture_fix_counts
       # TODO: DON'T hard-code this (this is GBIF Nub Taxonomy). Instead, add an
       # attribute to hierarchies called "never_merge_concepts" and check that.
@@ -32,7 +33,12 @@ class Hierarchy
       hierarchies = Hierarchy.where(["id NOT in (?)", 129]).
         order("hierarchy_entries_count DESC")
       # This is REALLY WEIRD (and lame), but it's what PHP did; This is, again,
-      # a GBIF Nub taxonomy:
+      # a GBIF Nub taxonomy. The only effect this has is to make it last in the
+      # list of hierarchies to compare. ...I haven't worked through this enough
+      # to decide whether that means it's less likely to match (if it's matched
+      # once, will it not match again?), so perhaps that's the intent. Perhaps
+      # it is not worth sorting by entry count; perhaps the curators should
+      # choose the order in which they are matched, if that's the case.
       hierarchies.find { |h| h.id == 800 }.hierarchy_entries_count = 1
       hierarchies.each do |other_hierarchy|
         # "Incomplete" hierarchies (e.g.: Flickr) actually can have multiple
@@ -53,25 +59,14 @@ class Hierarchy
         "#{hierarchy1.hierarchy_entries_count} entries) to #{hierarchy2.id} "\
         "(#{hierarchy2.label}; #{hierarchy2.hierarchy_entries_count} "\
         "entries)")
-      entries_matched = []
-      concepts_seen = []
-      visible_id = Visibility.get_visible.id
-      preview_id = Visibility.get_preview.id
+
       solr = SolrCore::HierarchyEntryRelationships.new
       # TODO: add (relationship:name OR confidence:[0.25 TO *]) [see below]
-      query = "hierarchy_id_1:#{hierarchy1.id} AND "\
-        "(visibility_id_1:#{visible_id} OR visibility_id_1:#{preview_id}) "\
-        "AND hierarchy_id_2:#{hierarchy2.id} AND "\
-        "(visibility_id_2:#{visible_id} OR visibility_id_2:#{preview_id}) "\
-        "AND same_concept:false"
-      options = { sort: "relationship asc, visibility_id_1 asc, "\
-        "visibility_id_2 asc, confidence desc, hierarchy_entry_id_1 asc, "\
-        "hierarchy_entry_id_2 asc"}
+
       entries = [] # Just to prevent weird infinite loops below. :\
       begin
         page ||= 0
         page += 1
-        # TODO: make method, don't use connection:
         # NOTE: This is a REALLY slow query. ...Which sucks. :\ Yes, even for Solr... it takes a VERY long time.
         response = solr.paginate(query,
           options.merge(page: page, per_page: 10000))
@@ -79,85 +74,102 @@ class Hierarchy
         # {"status"=>0, "QTime"=>14641, "params"=>{"sort"=>"relationship etc",
         # "wt"=>"ruby", "start"=>"0", "q"=>"blah blah", "rows"=>"10"}}
         response["response"]["docs"].each do |entry|
-          # Sample "entry": { "hierarchy_entry_id_1"=>47111837,
-          # "taxon_concept_id_1"=>71511, "hierarchy_id_1"=>949,
-          # "visibility_id_1"=>1, "hierarchy_entry_id_2"=>20466468,
-          # "taxon_concept_id_2"=>71511, "hierarchy_id_2"=>107,
-          # "visibility_id_2"=>0, "same_concept"=>true, "relationship"=>"name",
-          # "confidence"=>1.0 }
-          # TODO: move to query:
-          next if entry["relationship"] == 'syn' && entry["confidence"] < 0.25
-          # TODO: I hate this kind of reassignment. :\
-          id1 = entry["hierarchy_entry_id_1"]
-          visibility_id1 = entry["visibility_id_1"]
-          tc_id1 = entry["taxon_concept_id_1"]
-          id2 = entry["hierarchy_entry_id_2"]
-          visibility_id2 = entry["visibility_id_2"]
-          tc_id2 = entry["taxon_concept_id_2"]
-          score = entry["confidence"]
-          # this node in hierarchy 1 has already been matched
-          next if hierarchy1.complete? && entries_matched.include?(id2)
-          next if hierarchy2.complete? && entries_matched.include?(id1)
-          entries_matched += [id1, id2]
-          # PHP: "this comparison happens here instead of the query to ensure
-          # the sorting is always the same if this happened in the query and the
-          # entry was related to more than one taxa, and this function is run
-          # more than once then we'll start to get huge groups of concepts - all
-          # transitively related to one another"
-          next if tc_id1 == tc_id2
-          tc_id1 = follow_supercedure_cached(tc_id1)
-          tc_id2 = follow_supercedure_cached(tc_id2)
-          # NOTE: yes, PHP did this check again here. I wouldn't have, but I
-          # don't know if the problem explained above would continue to happen.
-          # I doubt it but it's only one quick check, so not worth the risk:
-          next if tc_id1 == tc_id2
-          # NOTE: the #find method follows supercedure
-          tc_id1 = follow_supercedure(tc_id1)
-          tc_id2 = follow_supercedure(tc_id2)
-          next if tc_id1 == tc_id2
-          EOL.log("Comparing entry #{id1} (hierarchy #{hierarchy1.id}) "\
-            "with #{id2} (hierarchy #{hierarchy2.id})", prefix: "?")
-          # PHP: "compare visible entries to other published entries"
-          if entry_published_in_hierarchy?(1, entry, hierarchy1)
-            EOL.log("SKIP: concept #{id2} published in hierarchy of #{id1}",
-              prefix: ".")
-            next
-          end
-          if entry_published_in_hierarchy?(2, entry, hierarchy2)
-            EOL.log("SKIP: concept #{tc_id1} published in hierarchy "\
-              "#{hierarchy2.id}", prefix: ".")
-            next
-          end
-          if entry_preview_in_hierarchy?(1, entry, hierarchy1)
-            EOL.log("SKIP: concept #{tc_id2} previewing in hierarchy "\
-              "#{hierarchy1.id}", prefix: ".")
-            next
-          end
-          if entry_preview_in_hierarchy?(2, entry, hierarchy2)
-            EOL.log("SKIP: concept #{tc_id1} previewing in hierarchy "\
-              "#{hierarchy2.id}", prefix: ".")
-            next
-          end
-          # TODO: method has too many args TODO: rename
-          if curators_denied_relationship?(entry)
-            EOL.log("SKIP: merge of entry #{id1} (concept #{tc_id1}) with "\
-              " #{id2} (concept #{tc_id2}) rejected by curator", prefix: ".")
-              next
-          end
-          # YOU WERE HERE
-          if affected = additional_hierarchy_affected_by_merge(tc_id1, tc_id2)
-            EOL.log("SKIP: A merge of #{id1} (concept #{tc_id1}) and #{id2} "\
-              "(concept #{tc_id2}) is not allowed by complete hierarchy "\
-              "#{affected.label} (#{affected.id})", prefix: ".")
-            next
-          end
-          EOL.log("MATCH: Concept #{tc_id1} = #{tc_id2}")
-          # TODO - log the supercedure somewhere to be cleaned up, e.g.: in
-          # CollectionItem.remove_superceded_taxa
-          tc = TaxonConcept.merge_ids(tc_id1, tc_id2)
-          @superceded[tc.id] = tc.supercedure_id
+          handle_entry(entry)
         end
       end while entries.count > 0
+    end
+
+    def compare_hierarchies_query
+      visible_id = Visibility.get_visible.id
+      preview_id = Visibility.get_preview.id
+      query = "hierarchy_id_1:#{hierarchy1.id} AND "\
+        "(visibility_id_1:#{visible_id} OR visibility_id_1:#{preview_id}) "\
+        "AND hierarchy_id_2:#{hierarchy2.id} AND "\
+        "(visibility_id_2:#{visible_id} OR visibility_id_2:#{preview_id}) "\
+        "AND same_concept:false"
+
+      query
+    end
+
+    def compare_hierarchies_options
+      # YOU WERE HERE
+      { sort: "relationship asc, visibility_id_1 asc, "\
+        "visibility_id_2 asc, confidence desc, hierarchy_entry_id_1 asc, "\
+        "hierarchy_entry_id_2 asc"}
+    end
+
+    def handle_entry(entry)
+      # Sample "entry": { "hierarchy_entry_id_1"=>47111837,
+      # "taxon_concept_id_1"=>71511, "hierarchy_id_1"=>949,
+      # "visibility_id_1"=>1, "hierarchy_entry_id_2"=>20466468,
+      # "taxon_concept_id_2"=>71511, "hierarchy_id_2"=>107,
+      # "visibility_id_2"=>0, "same_concept"=>true, "relationship"=>"name",
+      # "confidence"=>1.0 }
+      # TODO: move this criterion to the solr query (see above):
+      return(nil) if entry["relationship"] == 'syn' && entry["confidence"] < 0.25
+      id1 = entry["hierarchy_entry_id_1"]
+      tc_id1 = entry["taxon_concept_id_1"]
+      id2 = entry["hierarchy_entry_id_2"]
+      tc_id2 = entry["taxon_concept_id_2"]
+      # this node in hierarchy 1 has already been matched
+      return(nil) if hierarchy1.complete? && @entries_matched.include?(id2)
+      return(nil) if hierarchy2.complete? && @entries_matched.include?(id1)
+      @entries_matched += [id1, id2]
+      # PHP: "this comparison happens here instead of the query to ensure
+      # the sorting is always the same if this happened in the query and the
+      # entry was related to more than one taxa, and this function is run
+      # more than once then we'll start to get huge groups of concepts - all
+      # transitively related to one another"
+      return(nil) if tc_id1 == tc_id2
+      tc_id1 = follow_supercedure_cached(tc_id1)
+      tc_id2 = follow_supercedure_cached(tc_id2)
+      # NOTE: yes, PHP did this check again here. I wouldn't have, but I
+      # don't know if the problem explained above would continue to happen.
+      # I doubt it but it's only one quick check, so not worth the risk:
+      return(nil) if tc_id1 == tc_id2
+      # NOTE: the #find method follows supercedure
+      tc_id1 = follow_supercedure(tc_id1)
+      tc_id2 = follow_supercedure(tc_id2)
+      return(nil) if tc_id1 == tc_id2
+      EOL.log("Comparing entry #{id1} (hierarchy #{hierarchy1.id}) "\
+        "with #{id2} (hierarchy #{hierarchy2.id})", prefix: "?")
+      # PHP: "compare visible entries to other published entries"
+      if entry_published_in_hierarchy?(1, entry, hierarchy1)
+        EOL.log("SKIP: concept #{id2} published in hierarchy of #{id1}",
+          prefix: ".")
+        return(nil)
+      end
+      if entry_published_in_hierarchy?(2, entry, hierarchy2)
+        EOL.log("SKIP: concept #{tc_id1} published in hierarchy "\
+          "#{hierarchy2.id}", prefix: ".")
+        return(nil)
+      end
+      if entry_preview_in_hierarchy?(1, entry, hierarchy1)
+        EOL.log("SKIP: concept #{tc_id2} previewing in hierarchy "\
+          "#{hierarchy1.id}", prefix: ".")
+        return(nil)
+      end
+      if entry_preview_in_hierarchy?(2, entry, hierarchy2)
+        EOL.log("SKIP: concept #{tc_id1} previewing in hierarchy "\
+          "#{hierarchy2.id}", prefix: ".")
+        return(nil)
+      end
+      if curators_denied_relationship?(entry)
+        EOL.log("SKIP: merge of entry #{id1} (concept #{tc_id1}) with "\
+          " #{id2} (concept #{tc_id2}) rejected by curator", prefix: ".")
+          return(nil)
+      end
+      if affected = additional_hierarchy_affected_by_merge(tc_id1, tc_id2)
+        EOL.log("SKIP: A merge of #{id1} (concept #{tc_id1}) and #{id2} "\
+          "(concept #{tc_id2}) is not allowed by complete hierarchy "\
+          "#{affected.label} (#{affected.id})", prefix: ".")
+        return(nil)
+      end
+      EOL.log("MATCH: Concept #{tc_id1} = #{tc_id2}")
+      #  - log the supercedure somewhere to be cleaned up, e.g.: in
+      # CollectionItem.remove_superceded_taxa
+      tc = TaxonConcept.merge_ids(tc_id1, tc_id2)
+      @superceded[tc.id] = tc.supercedure_id
     end
 
     def lookup_preview_harvests
@@ -174,21 +186,19 @@ class Hierarchy
     end
 
     def get_confirmed_exclusions
-      # YOU WERE HERE
-      # $assertions = array();
-      # $result = $GLOBALS['mysqli_connection']->query("SELECT he1.id id1, he1.taxon_concept_id tc_id1, he2.id id2, he2.taxon_concept_id tc_id2, equivalent FROM curated_hierarchy_entry_relationships cher JOIN hierarchy_entries he1 ON (cher.hierarchy_entry_id_1=he1.id) JOIN hierarchy_entries he2 ON (cher.hierarchy_entry_id_2=he2.id) WHERE cher.equivalent=0");
-      # while($result && $row=$result->fetch_assoc())
-      # {
-      #     $id1 = $row['id1'];
-      #     $id2 = $row['id2'];
-      #     $tc_id1 = $row['tc_id1'];
-      #     $tc_id2 = $row['tc_id2'];
-      #     $equivalent = $row['equivalent'];
-      #     $assertions[$id1][$tc_id2] = $equivalent;
-      #     $assertions[$id2][$tc_id1] = $equivalent;
-      # }
-      # return $assertions;
-
+      CuratedHierarchyEntryRelationship.
+        includes(:from_hierarchy_entry, :to_hierarchy_entry).
+        where(equivalent: 0).
+        each do |cher|
+        from_entry = cher.from_hierarchy_entry.id
+        from_tc = cher.from_hierarchy_entry.taxon_concept_id
+        to_entry = cher.to_hierarchy_entry.id
+        to_tc = cher.to_hierarchy_entry.taxon_concept_id
+        @confirmed_exclusions[from_entry] ||= []
+        @confirmed_exclusions[from_entry] << to_tc
+        @confirmed_exclusions[to_entry] ||= []
+        @confirmed_exclusions[to_entry] << from_tc
+      end
     end
 
     # NOTE: we could query the DB to buld this full list. It takes about 30
@@ -261,8 +271,7 @@ class Hierarchy
     end
 
     def confirmed_exclusions_matches?(id, other_tc_id)
-      # TODO: what's equivalent ?
-      @confirmed_exclusions[id1].each do |tc_id, equivalent|
+      @confirmed_exclusions[id1].each do |tc_id|
         tc_id = follow_supercedure_cached(tc_id)
         return true if tc_id == other_tc_id
       end

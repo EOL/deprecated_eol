83b3db2661904a69830fc6dde7978fbdc6b7c8d6
JRice <jrice@eol.org>
2015-11-15 07:47:00 -0500

    Rename migration, sorry.

--- db/migrate/20151113165729_add_hide_from_gui_to_known_uris.rb
+++ db/migrate/20151113165729_add_hide_from_gui_to_known_uris.rb
@@ -1,4 +1,4 @@
-class AddHideFromGlossaryToKnownUris < ActiveRecord::Migration
+class AddHideFromGuiToKnownUris < ActiveRecord::Migration
   def change
     add_column :known_uris, :hide_from_gui, :boolean, :default => false
   end

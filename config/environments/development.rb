1a7335db735fff18819a39a5ddcfe07019a65e6d
JRice <jrice@eol.org>
2015-12-01 19:32:37 -0500

    I18n with a little more finesse...

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -55,5 +55,5 @@ end
 # TODO - Where do these go, now?  :S
 $UNSUBSCRIBE_NOTIFICATIONS_KEY = 'f0de2a0651aa88a090e5679e5e3a7d28'
 
-require File.expand_path('../../../lib/initializer_additions', __FILE__)
-InitializerAdditions.add("environments/#{Rails.env}_eol_org")
+# require File.expand_path('../../../lib/initializer_additions', __FILE__)
+# InitializerAdditions.add("environments/#{Rails.env}_eol_org")

cd7a43f2cb42ff978882a7d1672c077d0e5ec820
JRice <jrice@eol.org>
2015-12-10 11:52:05 -0500

    Removing translation logs; just complicate migrations.

--- db/migrate/20120509164521_add_unique_key_to_translation_logs.rb
+++
@@ -1,9 +0,0 @@
-class AddUniqueKeyToTranslationLogs < EOL::LoggingMigration
-  def self.up
-    TranslationLog.connection.execute("ALTER TABLE translation_logs ADD UNIQUE KEY `key` (`key`)")
-  end
-
-  def self.down
-    TranslationLog.connection.execute("ALTER TABLE translation_logs DROP KEY `key`")
-  end
-end

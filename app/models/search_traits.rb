ee54682a34ce58f77265649a4aff2133ec891d5a
JRice <jrice@eol.org>
2016-01-19 21:13:30 -0500

    Sundry fixes for TB

--- app/models/search_traits.rb
+++ app/models/search_traits.rb
@@ -1,6 +1,8 @@
 class SearchTraits < TraitSet
   attr_accessor :pages
 
+  # e.g.: s = SearchTraits.new(attribute: "http://purl.obolibrary.org/obo/OBA_0000056")
+
   # search_options = { querystring: @querystring, attribute: @attribute,
   #   min_value: @min_value, max_value: @max_value,
   #   unit: @unit, sort: @sort, language: current_language,
@@ -8,6 +10,7 @@ class SearchTraits < TraitSet
   #   required_equivalent_attributes: @required_equivalent_attributes,
   #   required_equivalent_values: @required_equivalent_values }
   def initialize(search_options)
+    raise "MUST specify a predicate" if search_options[:attribute].blank?
     # TODO: some of this could be generalized into TraitSet.
     @rdf = TraitBank::Scan.for(search_options)
     @pages = get_pages(@rdf.map { |trait| trait[:page].to_s })
@@ -15,11 +18,15 @@ class SearchTraits < TraitSet
     @points = DataPointUri.where(uri: trait_uris.to_a.map(&:to_s)).
       includes(:comments, :taxon_data_exemplars)
     uris = Set.new(@rdf.flat_map { |trait| trait.values.select { |v| v.uri? } })
+    uris << search_options[:attribute]
     # TODO: associations. We need the names of those taxa.
     @glossary = KnownUri.where(uri: uris.to_a.map(&:to_s)).
       includes(toc_items: :translated_toc_items)
     traits = @rdf.group_by { |trait| trait[:trait] }
-    @traits = traits.keys.map { |trait| Trait.new(traits[trait], self) }
+    @traits = traits.keys.map do |trait|
+      Trait.new(traits[trait], self, taxa: @pages,
+        predicate: search_options[:attribute])
+    end
     source_ids = Set.new(@traits.map { |trait| trait.source_id })
     source_ids.delete(nil) # Just in case.
     @sources = Resource.where(id: source_ids.to_a).includes(:content_partner)
@@ -34,6 +41,11 @@ class SearchTraits < TraitSet
       end
     end
     # TODO: various convenient joins and includes and the like, I'm sure:
+#     TaxonConceptPreferredEntry Load (1.6ms)  SELECT `taxon_concept_preferred_entries`.* FROM `taxon_concept_preferred_entries` WHERE `taxon_concept_preferred_entries`.`taxon_concept_id` = 485165 LIMIT 1
+# HierarchyEntry Load (2.1ms)  SELECT `hierarchy_entries`.* FROM `hierarchy_entries` WHERE `hierarchy_entries`.`id` = 53125426 LIMIT 1
+# Name Load (1.9ms)  SELECT `names`.* FROM `names` WHERE `names`.`id` = 6871071 LIMIT 1
+# CanonicalForm Load (1.7ms)  SELECT `canonical_forms`.* FROM `canonical_forms` WHERE `canonical_forms`.`id` = 321761 LIMIT 1
+  # TaxonConceptName Load (2.3ms)  SELECT `taxon_concept_names`.* FROM `taxon_concept_names` WHERE `taxon_concept_names`.`taxon_concept_id` =
     TaxonConcept.where(id: ids.to_a)
   end
 end

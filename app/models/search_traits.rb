6fc07ee719320d908d32124b13fddb50a25d0b69
JRice <jrice@eol.org>
2016-01-25 11:56:15 -0500

    Should fix value URIs, grrr.

--- app/models/search_traits.rb
+++ app/models/search_traits.rb
@@ -10,22 +10,48 @@ class SearchTraits < TraitSet
   #   required_equivalent_attributes: @required_equivalent_attributes,
   #   required_equivalent_values: @required_equivalent_values }
   def initialize(search_options)
-    raise "MUST specify a predicate" if search_options[:attribute].blank?
-    # TODO: some of this could be generalized into TraitSet.
-    @rdf = TraitBank::Scan.for(search_options)
-    @pages = get_pages(@rdf.map { |trait| trait[:page].to_s })
-    trait_uris = Set.new(@rdf.map { |trait| trait[:trait] })
-    @points = DataPointUri.where(uri: trait_uris.to_a.map(&:to_s)).
-      includes(:comments, :taxon_data_exemplars)
-    uris = Set.new(@rdf.flat_map { |trait| trait.values.select { |v| v.uri? } })
-    uris << search_options[:attribute]
-    # TODO: associations. We need the names of those taxa.
-    @glossary = KnownUri.where(uri: uris.to_a.map(&:to_s)).
-      includes(toc_items: :translated_toc_items)
-    traits = @rdf.group_by { |trait| trait[:trait] }
-    @traits = traits.keys.map do |trait|
-      Trait.new(traits[trait], self, taxa: @pages,
-        predicate: search_options[:attribute])
+    @attribute = search_options[:attribute]
+    @page = search_options[:page] || 1
+    @per_page = search_options[:per_page] || 100
+    if @attribute.blank?
+      @rdf = []
+      @pages = []
+      @points = []
+      @glossary = []
+      @sources = []
+      @traits = [].paginate
+    else
+      # TODO: some of this could be generalized into TraitSet.
+      @rdf = begin
+        TraitBank::Scan.for(search_options)
+      rescue EOL::Exceptions::SparqlDataEmpty => e
+        []
+      end
+      @pages = get_pages(@rdf.map { |trait| trait[:page].to_s })
+      trait_uris = Set.new(@rdf.map { |trait| trait[:trait] })
+      @points = DataPointUri.where(uri: trait_uris.to_a.map(&:to_s)).
+        includes(:comments, :taxon_data_exemplars)
+      uris = Set.new(@rdf.flat_map { |rdf|
+        rdf.values.select { |v| EOL::Sparql.is_uri?(v.to_s) } })
+      uris << @attribute
+      # TODO: associations. We need the names of those taxa.
+      @glossary = KnownUri.where(uri: uris.to_a.map(&:to_s)).
+        includes(toc_items: :translated_toc_items)
+      rdf_by_trait = @rdf.group_by { |trait| trait[:trait] }
+      traits = rdf_by_trait.keys.map do |trait|
+        Trait.new(rdf_by_trait[trait], self, taxa: @pages,
+          predicate: @attribute)
+      end
+      # TODO: a real count:
+      total = traits.count >= @per_page || @page > 1 ?
+        TraitBank::Scan.trait_count(search_options) :
+        traits.count
+      @traits = WillPaginate::Collection.create(@page, @per_page, total) do |pager|
+        pager.replace traits
+      end
+      source_ids = Set.new(@traits.map { |trait| trait.source_id })
+      source_ids.delete(nil) # Just in case.
+      @sources = Resource.where(id: source_ids.to_a).includes(:content_partner)
     end
     source_ids = Set.new(@traits.map { |trait| trait.source_id })
     source_ids.delete(nil) # Just in case.

b32fcfe90f50e82eca820140a162049cf7f6ef8a
JRice <jrice@eol.org>
2016-02-04 12:26:54 -0500

    DSF needs sort

--- app/models/trait_bank/scan.rb
+++ app/models/trait_bank/scan.rb
@@ -52,7 +52,7 @@ class TraitBank
         # TODO: This ORDER BY only really works if numeric! :S
         query += "?trait a eol:trait . "\
           "?trait dwc:measurementValue ?value . } } "
-        unless options[:count] || options[:sort] == :none
+        unless options[:count]
           # TODO: figure out how to sort properly, both numerically and alpha.
           orders = ["xsd:float(REPLACE(?value, \",\", \"\"))"] #, "?value"]
           orders.map! { |ord| "DESC(#{ord})" } if options[:sort] =~ /^desc$/i

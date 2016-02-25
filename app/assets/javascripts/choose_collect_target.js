c8fcc8298cbda712951af6f0fa7278072226efd4
hudasoliman <huda.soliman@bibalex.org>
2016-02-25 13:18:34 +0200

    modifications in collection-order-in-dialogue

--- app/assets/javascripts/choose_collect_target.js
+++ app/assets/javascripts/choose_collect_target.js
@@ -1,9 +1,9 @@
-$('#sort_collection_by').on('change', function() {
-  if ($('#sort_collection_by').val() == 'alpha') {
-    $('#collections_alpha').show();
-    $('#collections_recent').hide();
-  } else if ($('#sort_collection_by').val() == 'recent') {
-    $('#collections_alpha').hide();
-    $('#collections_recent').show();
-  }
-}); 
+  function changeList(){
+      if ($('#sort_collection_by').val() == 'alpha') {
+        $('#collections_alpha').show();
+        $('#collections_recent').hide();
+      } else if ($('#sort_collection_by').val() == 'recent') {
+        $('#collections_alpha').hide();
+        $('#collections_recent').show();
+      }
+    };

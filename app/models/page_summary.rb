695a6f873fc1350537638e18c88704eaf5387b86
JRice <jrice@eol.org>
2016-03-01 17:30:54 -0500

    Testing.

--- app/models/page_summary.rb
+++ app/models/page_summary.rb
@@ -1,14 +1,16 @@
 class PageSummary < ActiveRecord::Base
+
+  attr_accessible :id, :scientific_name, :image_key, :common_names
   # scientific_name, image_key, common_names. That's it.
   def self.from_concept(concept)
-    commons = "{"
+    commons = []
     concept.preferred_common_names.
             select { |pcn| pcn.language.activated_on }.
             each do |tcn|
-      commons += "\"#{tcn.language.iso_639_1}\": "\
+      commons << "\"#{tcn.language.iso_639_1}\": "\
         "\"#{tcn.name.string.gsub(/"/, '""')}\""
     end
-    commons += "}"
+    commons = "{#{commons.join(",")}}"
     image = concept.exemplar_or_best_image_from_solr.try(:object_cache_url)
     if PageSummary.exists?(concept.id)
       summary = PageSummary.find(concept.id)
@@ -31,10 +33,14 @@ class PageSummary < ActiveRecord::Base
     end
   end
 
+  def thumbnail
+    DataObject.image_cache_path(image_key, '130_130')
+  end
+
   def to_hash
     {
       id: id, scientific_name: scientific_name, image_key: image_key,
-      common_names: JSON.parse(common_names)
+      common_names: JSON.parse(common_names), thumbnail: thumbnail
     }
   end
 end

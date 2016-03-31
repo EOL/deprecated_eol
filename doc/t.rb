a0dcbcaf3ae85d057ffe624b983de10148774849
JRice <jrice@eol.org>
2016-03-31 16:28:57 -0400

    Safer merges. ...we hope.

--- doc/t.rb
+++ doc/t.rb
@@ -1,5 +1,52 @@
 # This is a temp file used for notes. Ignore it entirely!
 
+@solr = SolrCore::HierarchyEntryRelationships.new
+
+def get_page(query, page)
+  @solr.paginate(query, page: page, per_page: 1000)["response"]["docs"]
+end
+
+merges = {}
+ids.each do |id|
+  resource = Hierarchy.find(id).resource
+  next unless resource && resource.title
+  these_merges = Set.new
+  query = "confidence:0 AND relationship:name AND hierarchy_id_1:#{id}"
+  page = 0
+  docs = []
+  begin
+    page += 1
+    docs = get_page(query, page)
+    all_entry_ids = Set.new(docs.map { |d| d["hierarchy_entry_id_1"] })
+    all_entry_ids += docs.map { |d| d["hierarchy_entry_id_2"] }
+    entries = HierarchyEntry.includes(name: { canonical_form: :name }).
+      published.where(id: all_entry_ids.to_a)
+    docs.each do |doc|
+      id1 = doc["hierarchy_entry_id_1"]
+      id2 = doc["hierarchy_entry_id_2"]
+      entry1 = entries.find { |e| e.id == id1 }
+      entry2 = entries.find { |e| e.id == id2 }
+      if entry1 && entry2 && entry1.taxon_concept_id == entry2.taxon_concept_id
+        name1 = entry1.name.try(:canonical_form).try(:name).try(:string) || "NONAME!"
+        name2 = entry2.name.try(:canonical_form).try(:name).try(:string) || "NONAME!"
+        these_merges << "[#{entry1.taxon_concept_id}](http://eol.org/pages/#{entry1.taxon_concept_id}/overview) -> #{name1} (#{id1}), #{name2} (#{id2})"
+      end
+    end
+  end while docs.size > 0
+  merges["[#{resource.title}](http://eol.org/content_partners/#{resource.content_partner_id}/resources/#{resource.id})"] = these_merges
+end
+
+File.open("/app/log/resource_merges.md", "w") do |file|
+  merges.keys.each do |title|
+    file.write("#{title}:\n")
+    merges[title].each do |val|
+      file.write("  #{val}\n")
+    end
+  end
+end
+
+# --
+
 params = {page: 1, exact: true, id: "Cistanthe weberbaueri (Diels) Carolin ex M.A.Hershkovitz"}
 params[:q] = params[:id]
 

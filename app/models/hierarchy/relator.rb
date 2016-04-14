cfb947f3cde534fe9908dd1eeb85e9fd6832035b
JRice <jrice@eol.org>
2016-04-14 15:26:18 -0400

    Similarity...

--- app/models/hierarchy/relator.rb
+++ app/models/hierarchy/relator.rb
@@ -10,36 +10,6 @@ class Hierarchy
   # 0-100 scale, which is more stable. NOTE: this is called by
   # Hierarchy#reindex_and_merge_ids
   class Relator
-    # NOTE: PHP actually had a bug (!) where this was _only_ Kingdom, but the
-    # intent was clearly supposed to be this, so I'm going with it: TODO - this
-    # should be in the DB, anyway. :\
-    kingdom_friendly_ids = []
-    [:kingdom, :phylum, :class_rank, :order].each do |rank_name|
-      kingdom_friendly_ids += Rank.where(rank_group_id:
-        Rank.send(rank_name).rank_group_id).pluck(:id)
-    end
-    kingdom_friendly_index = {}
-    kingdom_friendly_ids.each { |id| kingdom_friendly_index[id] = true }
-    RANKS_ALLOWED_TO_MATCH_AT_KINGDOM_ONLY = kingdom_friendly_index
-    # NOTE: Genus is low because, presumedly, a binomial will always match at
-    # the Genus if it's matched the name. :\
-    RANK_WEIGHTS = { "genus" => 0.5, "family" => 0.9, "order" => 0.8,
-      "class" => 0.6, "phylum" => 0.4, "kingdom" => 0.2 }
-
-    # I am not going to freak out about the fact that TODO: this needs to be in
-    # the database. I've lost my energy to freak out about such things. :|
-    GOOD_SYNONYMY_HIERARCHY_IDS = [
-      123, # WORMS
-      143, # Fishbase
-      622, # IUCN
-      636, # Tropicos
-      759, # NCBI
-      787, # ReptileDB
-      860, # Avibase
-      903, # ITIS
-      949  # COL 2012
-    ]
-
     def self.relate(hierarchy, options = {})
       relator = self.new(hierarchy, options)
       relator.relate
@@ -64,8 +34,8 @@ class Hierarchy
       @per_page = 1000 unless @per_page > 0
       @solr = SolrCore::HierarchyEntries.new
       @scores = {}
-      @studied = {}
       @rank_conflicts = {}
+      @similarity = Hierarchy::Similarity.new
     end
 
     def relate
@@ -88,7 +58,8 @@ class Hierarchy
         entries = get_page_from_solr(page)
         entries.each do |entry|
           begin
-            compare_entry(study(entry)) if @new_entry_ids.has_key?(entry["id"])
+            compare_entry(@similarity.study(entry)) if
+              @new_entry_ids.has_key?(entry["id"])
           rescue => e
             EOL.log("Failed on entry ##{entry["id"]} (page #{page})")
             raise e
@@ -113,46 +84,6 @@ class Hierarchy
         EOL.log("Total of #{response["response"]["numFound"]} entries")
       end
       response["response"]["docs"]
-    end
-
-    def metaquote(string)
-      return "" if string.nil?
-      string.gsub(/\\/, "\\\\\\\\").gsub(/"/, "\\\"")
-    end
-
-    def study(entry)
-      if @studied.has_key?(entry["id"])
-        return @studied[entry["id"]]
-      end
-      # April 2016: we decided that missing a scientific name (which is due to
-      # non-ansi characters in the name) was insufficient cause to ignore it
-      # entirely; match instead on the canonical form, which is clean.
-      if entry["name"].blank?
-        entry["name"] = entry["canonical_form"]
-      end
-      entry["rank_id"] ||= 0
-      entry["kingdom_down"] = entry["kingdom"].try(:downcase)
-      entry["quoted_canonical"] = metaquote(entry["canonical_form"])
-      entry["quoted_name"] = metaquote(entry["name"])
-      entry["is_virus"] = virus?(entry)
-      entry["synonymy"] = synonymy_of(entry)
-      @studied[entry["id"]] = entry
-      entry
-    end
-
-    def virus?(entry)
-      return true if entry["kingdom_down"] &&
-        (entry["kingdom_down"] == "virus" || entry["kingdom_down"] == "viruses")
-      return entry["quoted_canonical"].size >= 5 &&
-        entry["quoted_canonical"][-5..-1] == "virus"
-    end
-
-    def synonymy_of(entry)
-      if GOOD_SYNONYMY_HIERARCHY_IDS[entry["hierarchy_id"]] && entry["synonym"]
-        entry["synonym"]
-      else
-        []
-      end
     end
 
     # TODO: cleanup. :|
@@ -187,15 +118,7 @@ class Hierarchy
       end
       matching_entries_from_solr = response["response"]["docs"]
       matching_entries_from_solr.each do |matching_entry|
-        compare_entries_from_solr(entry, study(matching_entry))
-      end
-    end
-
-    def compare_entries_from_solr(entry, matching_entry)
-      score = score_comparison(entry, matching_entry)
-      if score
-        store_score(entry["id"], matching_entry["id"], score)
-        store_score(matching_entry["id"], entry["id"], score)
+        compare_entries_from_solr(entry, matching_entry)
       end
     end
 
@@ -218,164 +141,25 @@ class Hierarchy
       end
     end
 
-    def score_comparison(from_entry, to_entry)
-      return nil if from_entry["id"] == to_entry["id"]
-      return nil if from_entry["name"].blank? || to_entry["name"].blank?
-      return nil if @hierarchy.complete? &&
-        from_entry["hierarchy_id"] == @hierarchy.id &&
-        to_entry["hierarchy_id"] == @hierarchy.id
-      return nil if
-        rank_ids_conflict?(from_entry["rank_id"], to_entry["rank_id"])
-      # PHP: "viruses are a pain and will not match properly right now"
-      return(nil) if from_entry["is_virus"] || to_entry["is_virus"]
-      is_synonym = false
-      # nil, 0.5 (canonical), or 1 (scientific)... TODO: Symbols.
-      name_score = compare_names(from_entry, to_entry)
-      # If it's not a complete match, check for synonyms:
-      if name_score.nil? || name_score == 0
-        # 0 (none), 0.5 (canon), or 1 (sci):
-        name_score = compare_synonyms(from_entry, to_entry)
-        is_synonym = name_score > 0
-      end
-      total_score = if name_score.nil? || name_score == 0
-        0
-      else
-        compare_ancestries(from_entry, to_entry)
-        if @ancestry_score.nil?
-          # One of the ancestries was totally empty:
-          name_score * 0.5
-        elsif @ancestry_score > 0
-          # Ancestry was reasonable match:
-          name_score * @ancestry_score
-        else
-          # Bad match:
-          0
-        end
-      end
-      # DO NOT SCORE ZEROES:
-      total_score <= 0 ? nil : { score: total_score, synonym: is_synonym }
-    end
-
-    def rank_ids_conflict?(rid1, rid2)
-      (low, high) = [rid1, rid2].sort
-      key = "#{low}v#{high}"
-      return @rank_conflicts[key] if @rank_conflicts.has_key?(key)
-      @rank_conflicts[key] = if @rank_groups.has_key?(rid1) &&
-                                @rank_groups.has_key?(rid2)
-        @rank_groups[rid1] != @rank_groups[rid2]
-      else
-        rid1 && rid1 != 0 && rid2 && rid2 != 0 && rid1 != rid2
+    def compare_entries_from_solr(entry, matching_entry)
+      score = @similarity.compare(entry, matching_entry,
+        from_studied: true, complete: @hierarchy.complete?)
+      if score.is_a?(Hash) && score[:score] && score[:score] > 0
+        store_score(score)
       end
     end
 
-    def compare_names(from_entry, to_entry)
-      return 1 if from_entry["name"] == to_entry["name"]
-      return 0.5 if from_entry["canonical_form"] && to_entry["canonical_form"] &&
-        from_entry["canonical_form"] == to_entry["canonical_form"]
-      return nil
-    end
-
-    def compare_synonyms(from_entry, to_entry)
-      if to_entry["synonymy"].include?(from_entry["name"]) ||
-        from_entry["synonymy"].include?(to_entry["name"])
-        1
-      elsif to_entry["synonymy"].include?(from_entry["canonical_form"]) ||
-        from_entry["synonymy"].include?(to_entry["canonical_form"])
-        0.5
-      else
-        0
-      end
-    end
-
-    # "check each rank in order of priority and return the respective weight on
-    # match"
-    def compare_ancestries(from_entry, to_entry)
-      @ancestry_score = nil
-      study_ancestry(from_entry, to_entry)
-      return nil if @ancestry_empty
-      # Never ever match bad kingdoms:
-      return 0 if @bad_kingdom
-      if @kingdoms_match
-        if @non_kingdoms_match
-          # We matched on Kingdom AND something else, which is great!
-          return 1
-        else
-          # We *only* had kingdoms to work with... If neither entry has other
-          # ranks at all, return the score based on kingdom only:
-          return RANK_WEIGHTS["kingdom"] unless
-            @both_ancestries_have_non_kingdoms
-          # So here we've got at least one entry with other ranks *available*,
-          # but they didn't match. This is only okay if both entries have a rank
-          # that allows this:
-          return 0 unless
-            allowed_to_match_at_kingdom_only?(from_entry, to_entry)
-          # If we haven't returned, then we're looking at a pair of higher-level
-          # entries that matched only at kingdom (which is fine)
-        end
-      end
-      @ancestry_score = @ancestry_score
-    end
-
-    def study_ancestry(from_entry, to_entry)
-      # NOTE: I'm using instance variables to stop MALLOC'ing every time we call
-      # this method, which is... a lot.
-      @kingdoms_match = false
-      @from_has_non_kingdom = false
-      @to_has_non_kingdom = false
-      @non_kingdoms_match = false
-      @ancestry_empty = true
-      @ancestry_score = 0
-      @bad_kingdom = false
-      @both_ancestries_have_non_kingdoms = false
-      RANK_WEIGHTS.sort_by { |k,v| - v }.each do |rank, weight|
-        if from_entry[rank] && to_entry[rank]
-          @ancestry_empty = false
-          unless rank == "kingdom"
-            @from_has_non_kingdom = true
-            @to_has_non_kingdom = true
-          end
-          if from_entry[rank] == to_entry[rank] # MATCH!
-            if rank == "kingdom"
-              @kingdoms_match = true
-            else
-              @non_kingdoms_match = true
-            end
-            @ancestry_score = weight if weight > @ancestry_score
-          else # CONTRADICTION!
-            if rank == "kingdom"
-              # If either of them is Animalia, absolutely DO NOT MATCH!
-              if from_entry[rank].downcase == "animalia" ||
-                 to_entry[rank].downcase == "animalia"
-                @bad_kingdom = true
-                return # Nothing more worth doing!
-              end
-            end
-          end
-        elsif from_entry[rank]
-          @ancestry_empty = false
-          @from_has_non_kingdom = true unless rank == "kingdom"
-        elsif to_entry[rank]
-          @ancestry_empty = false
-          @to_has_non_kingdom = true unless rank == "kingdom"
-        end
-      end
-      @both_ancestries_have_non_kingdoms =
-        @to_has_non_kingdom && @from_has_non_kingdom
-    end
-
-    def allowed_to_match_at_kingdom_only?(from_entry, to_entry)
-      RANKS_ALLOWED_TO_MATCH_AT_KINGDOM_ONLY[from_entry["rank_id"]] &&
-        RANKS_ALLOWED_TO_MATCH_AT_KINGDOM_ONLY[to_entry["rank_id"]]
-    end
-
-    def store_score(from, to, score)
+    def store_score(score)
       type = score[:synonym] ? 'syn' : 'name'
-      key = "#{from},#{to},'#{type}'"
-      unless @scores.has_key?(key) && @scores[key] < score[:score]
-        @scores[key] = score[:score]
+      keys = ["#{score[:from]},#{score[:to]},'#{type}'",
+              "#{score[:to]},#{score[:from]},'#{type}'"]
+      keys.each do |key|
+        unless @scores.has_key?(key) && @scores[key] < score[:score]
+          @scores[key] = score[:score]
+          size = @scores.size
+          EOL.log("#{size} relationships...", prefix: ".") if size % 1_000 == 0
+        end
       end
-      size = @scores.size
-      EOL.log("#{size} relationships...", prefix: ".") if size % 1_000 == 0
     end
 
     def add_curator_assertions

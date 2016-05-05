13d4ebd3c7d35e51b39318d70a1a9a22d92ca7ac
JRice <jrice@eol.org>
2016-05-05 10:07:10 -0400

    Better logs for data worker

--- app/models/data_file_maker.rb
+++ app/models/data_file_maker.rb
@@ -5,14 +5,15 @@ class DataFileMaker
   # TODO - logging here is bad; improve. I'm using #error just to ensure it shows up in the log, but that's probably subprime.
   def self.perform(args)
     EOL.log_call
-    EOL.log("#{args.values.join(', ')}", prefix: ".")
     # Once upon a time, a job was skipped because it didn't exist... but it should have.
     # I assume this was a case of slight slave lag and bad timing, so to ensure
     # that doesn't cause a problem:
     DataSearchFile.with_master do
       if DataSearchFile.exists?(args["data_file_id"])
         begin
-          DataSearchFile.find(args["data_file_id"]).build_file
+          dsf = DataSearchFile.find(args["data_file_id"])
+          EOL.log("DataSearchFile ##{dsf.id} for user #{dsf.user.full_name} (#{dsf.user_id})", prefix: ".")
+          dsf.build_file
         rescue => e
           EOL.log_error(e)
         end

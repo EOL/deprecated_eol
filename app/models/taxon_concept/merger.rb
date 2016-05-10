590cb31a2685637b3263d0af2920d326b6d606d7
JRice <jrice@eol.org>
2016-05-10 11:24:18 -0400

    Faster reindex of random taxa (march of life)

--- app/models/taxon_concept/merger.rb
+++ app/models/taxon_concept/merger.rb
@@ -8,6 +8,7 @@ class TaxonConcept
   # as the supercedure_id to see where it went. If you're calling this, you
   # probably care about both.
   class Merger
+    # TODO: RandomHierarchyImage should be scanned and updated, too!
     class << self
       def in_bulk(raw_merges)
         ids = Set.new

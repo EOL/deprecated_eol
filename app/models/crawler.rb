b3739fddc5bc9f709a1cb864877d0e379d03179f
JRice <jrice@eol.org>
2016-05-12 17:18:05 -0400

    Fix useless last commit

--- app/models/crawler.rb
+++ app/models/crawler.rb
@@ -38,7 +38,11 @@ class Crawler
         EOL.log("#{index}/#{count}: #{concept.id} (#{pj.ld.to_s.size})",
           prefix: ".") if index % 10 == 0
         if options["filename"]
-          next unless pj && pj.ld["item"] && pj.ld["item"]["traits"]
+          # NOTE: This means we're NOT adding taxa unless they have traits! This
+          # may not be desireable... we might want to know the names and the
+          # sameAs's for the page. ...TODO: choose?
+          next unless pj && pj.ld["item"] && pj.ld["item"]["traits"] &&
+            ! pj.ld["item"]["traits"].empty?
           # NOTE: it's inefficient to open and close the file for every taxon...
           # but A) this allows us to see partial results sooner, B) it allows us
           # to run this in the background when there is no file to write to, and

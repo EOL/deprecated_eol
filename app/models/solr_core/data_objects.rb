dde69e025733a165c217a98836d6d1de5dc40ef8
JRice <jrice@eol.org>
2016-05-27 16:16:47 -0400

    ,

--- app/models/solr_core/data_objects.rb
+++ app/models/solr_core/data_objects.rb
@@ -6,6 +6,20 @@ class SolrCore
       connect(CORE_NAME)
     end
 
+    def delete_by_ids(ids)
+      ids = Array(ids)
+      exist_ids = []
+      ids.in_groups_of(1000, false) do |id_group|
+        q = id_group.map { |id| "data_object_id:#{id}" }.join(" OR ")
+        response = @connection.paginate(1, 1000, "select", params: { q: q })
+        exist_ids += response["response"]["docs"].map { |d| d["data_object_id"] }
+      end
+      # NOTE: yes, this call is singular (but can take an array)
+      @connection.delete_by_query(exist_ids.map { |id| "data_object_id:#{id}" })
+      # TODO: error-checking
+      @connection.commit
+    end
+
     def best_image_for_page(id)
       paginate("taxon_concept_id:#{id} AND "\
         "data_type_id:#{DataType.image_type_ids.join(" OR data_type_id:")} AND "\

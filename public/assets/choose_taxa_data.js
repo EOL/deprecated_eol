389c9906c01b8dee408c6da63b8ea17f8b082ef4
JRice (resque) <jrice@eol.org>
2016-06-21 14:33:15 +0000

    New assets

--- public/assets/choose_taxa_data.js
+++ public/assets/choose_taxa_data.js
@@ -1,7 +1,1 @@
 $(document).ready(function(){$("#select_all").click(function(){this.checked?$(":checkbox").each(function(){this.checked=!0}):$(":checkbox").each(function(){this.checked=!1})})});
-
-$(function() {
-  $('table.taxon_collection tr .info_icon, table.taxon_collection.search tr .info_icon').each(function() {
-    EOL.create_collection_taxon_info_dialog(this);
-  });
-});

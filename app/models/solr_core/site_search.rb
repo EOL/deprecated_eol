f5b591677e8cdf737fce14e8fbd40dad5d896314
JRice <jrice@eol.org>
2016-07-13 11:57:48 -0400

    Site searh reindex is 1000 at a time, not 2500

--- app/models/solr_core/site_search.rb
+++ app/models/solr_core/site_search.rb
@@ -56,7 +56,7 @@ class SolrCore
       delete_batch(klass, ids)
       # Been getting "Broken pipe" errors on this next line, decrease from 6400
       # to 2500 to 1000 to attempt fix...
-      @objects.to_a.in_groups_of(2500, false) do |group|
+      @objects.to_a.in_groups_of(1000, false) do |group|
         EOL.log("Adding #{group.count} items...")
         connection.add(group)
       end

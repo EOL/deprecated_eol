cca7c1cc8daf626442f1f2427a970eaf6d2a6081
JRice <jrice@eol.org>
2016-07-18 14:32:14 -0400

    improve algorith to retry solr queries

--- app/models/solr_core/base.rb
+++ app/models/solr_core/base.rb
@@ -84,14 +84,23 @@
     end
 
     def paginate_with_timeout(page, per_page, params)
-      begin
-        connection.paginate(page, per_page, "select", params: params)
-      rescue Timeout::Error => e
-        EOL.log("SOLR TIMEOUT: page/per: #{page}/#{per_page} ; q: #{params[:q]}",
-          prefix: "!")
-        wait_for_recovery(0)
-        EOL.log("Solr appears to have recovered; retrying...")
-        connection.paginate(page, per_page, "select", params: params)
+      willing_to_try = 5
+      while willing_to_try > 0
+        begin
+          connection.paginate(page, per_page, "select", params: params)
+          willing_to_try = 0
+        rescue Timeout::Error => e
+          EOL.log("SOLR TIMEOUT: pg#{page}(#{per_page}) q: #{params[:q]}",
+            prefix: "!")
+          wait_for_recovery(0)
+          willing_to_try -= 1
+          if willing_to_try > 0
+            EOL.log("Solr recovered; retrying #{willing_to_try} times...")
+          else
+            EOL.log("I GIVE UP!")
+            raise e
+          end
+        end
       end
     end
 
@@ -131,6 +140,11 @@
     def wait_for_recovery(attempts)
       attempts ||= 0
       begin
+        # I want to see that it's STABLE and up...
+        try_recovery
+        sleep(1)
+        try_recovery
+        sleep(1)
         try_recovery
       rescue => e
         EOL.log("Solr still down (attempt #{attempts}), waiting...")
--- doc/t.rb
+++ doc/t.rb
@@ -1,1 +1,24 @@
 # This is a temp file used for notes. Ignore it entirely!
+
+foo = 0
+willing_to_try = 5
+begin
+  puts "here."
+  begin
+    foo += 1
+    puts "go # #{foo}"
+    raise Timeout::Error.new("Haha! #{foo}") unless foo == 4
+    willing_to_try = 0
+  rescue Timeout::Error => e
+    puts("Timeout")
+    # wait_for_recovery(0)
+    willing_to_try -= 1
+    if willing_to_try > 0
+      puts("Still willing to try #{willing_to_try} times")
+    else
+      puts("I GIVE UP!")
+      raise e
+    end
+  end
+  puts "I made it! (willing to try: #{willing_to_try})"
+end while willing_to_try > 0

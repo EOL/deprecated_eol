905793074cc76f63358bf38209c7b801e62fa66e
JRice <jrice@eol.org>
2016-07-20 13:08:16 -0400

    Debugging

--- doc/t.rb
+++ doc/t.rb
@@ -1,5 +1,4 @@
 # This is a temp file used for notes. Ignore it entirely!
-resource = Resource.find 267
-event = resource.harvest_events.last
-# resource.hierarchy.merge_matching_concepts
-event.sync_collection
+@resource = Resource.find(544)
+@event = @resource.harvest_events.last
+@event.publish

312b567ca5f65a5238091d1b15112dc7a567d392
JRice <jrice@eol.org>
2016-09-14 15:27:36 -0400

    Quick fix for data glossary cache

--- app/controllers/data_glossary_controller.rb
+++ app/controllers/data_glossary_controller.rb
@@ -7,7 +7,12 @@ class DataGlossaryController < ApplicationController
     @page_title = I18n.t(:data_glossary)
     respond_to do |format|
       format.html {}
-      format.js { render text: KnownUri.glossary_terms.select { |uri| ! uri.definition.blank? }.to_json }
+      format.js do
+        json = Rails.cache.fetch("data_glossary", expires_in: 1.week) do
+          KnownUri.glossary_terms.select { |uri| ! uri.definition.blank? }.to_json
+        end
+        render text: json
+      end
     end
   end
 

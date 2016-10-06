0390bfaccfe898ae8730192a436cc12b0e17a5eb
JRice <jrice@eol.org>
2016-10-06 16:10:09 -0400

    Fix bug when page has no traits

--- app/models/clade_serializer.rb
+++ app/models/clade_serializer.rb
@@ -1,27 +1,27 @@
 class CladeSerializer
   class << self
     def store_clade_starting_from(pid)
-      batch_size = 100 
+      batch_size = 100
       file_name = Rails.root.join("public", "store-#{pid}-clade.json").to_s
       File.open(file_name, "wb") do |file|
         file.write("[")
         index = 0
         clade_pages = []
-        
+
         TaxonConceptsFlattened.descendants_of(pid).find_each(batch_size: batch_size) do |descendant_page|
           index += 1
-          
+
           if index % batch_size == 0
             puts "  #{index}..."
             file.write(clade_pages.join("\n"))
             clade_pages = []
           end
-          
+
           clade_pages << (PageSerializer.get_page_data(descendant_page[:taxon_concept_id])).to_json + ","
           #remove last ,
           clade_pages[-1].chomp!(',') if index == TaxonConcept.find(pid).number_of_descendants
         end
-        
+
         if !clade_pages.blank?
           file.write(clade_pages.join("\n"))
         end

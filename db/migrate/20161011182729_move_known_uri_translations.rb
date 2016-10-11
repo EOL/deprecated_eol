b581e874b649818b4366fd7c87a5367a9f2c9b19
JRice <jrice@eol.org>
2016-10-11 15:27:19 -0400

    More migration changes.

--- db/migrate/20161011182729_move_known_uri_translations.rb
+++ db/migrate/20161011182729_move_known_uri_translations.rb
@@ -4,19 +4,23 @@ class MoveKnownUriTranslations < ActiveRecord::Migration
     # add_column :known_uris, :definition, :text
     # add_column :known_uris, :comment, :text
     # add_column :known_uris, :attribution, :text
-    TranslatedKnownUri.where(language_id: 152).find_each do |tku|
-      next unless tku.known_uri
-      tku.known_uri.update_attributes(
-        name: tku.name,
-        definition: tku.definition,
-        comment: tku.comment,
-        attribution: tku.attribution
-      )
-    end
-    change_column :known_uris, :name, :string, null: false, limit: 128
+    # # NOTE: this takes a long time to run. :S
+    # TranslatedKnownUri.where(language_id: 152).find_each do |tku|
+    #   next unless tku.known_uri
+    #   tku.known_uri.update_attributes(
+    #     name: tku.name,
+    #     definition: tku.definition,
+    #     comment: tku.comment,
+    #     attribution: tku.attribution
+    #   )
+    # end
+    # change_column :known_uris, :name, :string, null: false, limit: 128
+    # We do some lookups by this! Yeesh:
+    add_index(:known_uris, :name)
   end
 
   def down
+    remove_index(:known_uris, :name)
     remove_column :known_uris, :name
     remove_column :known_uris, :definition
     remove_column :known_uris, :comment

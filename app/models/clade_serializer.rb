3cc75f8795990e5921ec09089deb8b2c8b976b87
JRice <jrice@eol.org>
2016-11-10 09:27:42 -0500

    Clade Serializer updates

--- app/models/clade_serializer.rb
+++ app/models/clade_serializer.rb
@@ -1,9 +1,16 @@
 class CladeSerializer
   class << self
-    # e.g.: CladeSerializer.store_clade_starting_from(7662) => Carnivora
-    # e.g.: CladeSerializer.store_clade_starting_from(7665) => Procyonidae (smaller)
+    # e.g.:
+    # CladeSerializer.store_clade_starting_from(1) => Animalia (3_888_534) (don't hold your breath)
+    # CladeSerializer.store_clade_starting_from(694) => Chordata (718_192)
+    # CladeSerializer.store_clade_starting_from(2774383) => Vertebrata (213_935)
+    # CladeSerializer.store_clade_starting_from(12094272) => Gnathostomata (209_386)
+    # CladeSerializer.store_clade_starting_from(4712200) => Tetrapoda (135_778)
+    # CladeSerializer.store_clade_starting_from(1642) => Mamalia (88_165)
+    # CladeSerializer.store_clade_starting_from(7662) => Carnivora (10_369)
+    # CladeSerializer.store_clade_starting_from(7665) => Procyonidae (695)
 
-    def store_clade_starting_from(pid)
+    def store_clade_starting_from(pid, options = {})
       part = 0
       batch_size = 50
       stored = 0
@@ -13,9 +20,26 @@ class CladeSerializer
 
       clade_page = nil
       taxa.find_each(batch_size: batch_size) do |descendant_page|
-        batch << JSON.pretty_generate(PageSerializer.get_page_data(descendant_page[:taxon_concept_id]))
+        tid = descendant_page[:taxon_concept_id]
+        if options[:resume_from] && part <= options[:resume_from]
+          batch << :nothing
+        else
+          data = PageSerializer.get_page_data(tid)
+          batch << begin
+            JSON.pretty_generate(data)
+          rescue
+            EOL.log("CLS: There was an error writing page #{tid}, skipping...", prefix: "!")
+            EOL.log("#{data.inspect}", prefix: "=")
+            "{\"_comment\": \"There was an error processing page #{tid}.\"}"
+          end
+        end
         if batch.size >= batch_size
-          stored += flush(pid, batch, part += 1)
+          if options[:resume_from] && part <= options[:resume_from]
+            part += 1
+            EOL.log("Skipping part #{part}", prefix ".")
+          else
+            stored += flush(pid, batch, part += 1)
+          end
           batch = []
         end
       end
